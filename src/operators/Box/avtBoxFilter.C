f019d71e1d1024d52dd9935c46b73d609d5a3ea2
Hank Childs <hank@uoregon.edu>
2003-08-11 00:00:00 -0700

    Update from August 11, 2003
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@55 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/operators/Box/avtBoxFilter.C
+++ src/operators/Box/avtBoxFilter.C
@@ -88,6 +88,12 @@ vtkBoxFilter::~vtkBoxFilter()
   ;
 }
 
+// ****************************************************************************
+// Modifications:
+//   Akira Haddox, Fri Aug  8 13:48:22 PDT 2003
+//   Rewrote algorithm so that 'SomeOfCell' selection would be accurate.
+// ****************************************************************************
+
 void
 vtkBoxFilter::Execute(void)
 {
@@ -106,22 +112,91 @@ vtkBoxFilter::Execute(void)
     {
         pointMap[i] = -1;
     }
-    bool *isInBox = new bool[nPts];
-    for (i = 0 ; i < nPts ; i++)
-    {
-        float pt[3];
-        input->GetPoint(i, pt);
-        if (pt[0] >= MinX && pt[0] <= MaxX && 
-            pt[1] >= MinY && pt[1] <= MaxY &&
-            pt[2] >= MinZ && pt[2] <= MaxZ)
+    bool *isInBox;
+    char *relativeToBox[3];
+    float bxpts[8][3];
+    float bounds[6];
+
+    //
+    // Represents the connectivity between points that define the box.
+    //
+    const int boxLineMap[24] = {0, 1,   0, 2,   0, 4,
+                                1, 3,   1, 5,
+                                2, 3,   2, 6,
+                                3, 7,
+                                4, 5,   4, 6,
+                                5, 7,
+                                6, 7 };
+                                
+    
+    if (AllOfCell)
+    { 
+        isInBox= new bool[nPts];
+        for (i = 0 ; i < nPts ; i++)
         {
-            isInBox[i] = true;
-        }
-        else
-        {
-            isInBox[i] = false;
+            float pt[3];
+            input->GetPoint(i, pt);
+            if (pt[0] >= MinX && pt[0] <= MaxX && 
+                pt[1] >= MinY && pt[1] <= MaxY &&
+                pt[2] >= MinZ && pt[2] <= MaxZ)
+            {
+                isInBox[i] = true;
+            }
+            else
+            {
+                isInBox[i] = false;
+            }
         }
     }
+    else
+    {
+        relativeToBox[0] = new char[nPts];
+        relativeToBox[1] = new char[nPts];
+        relativeToBox[2] = new char[nPts];
+
+        bounds[0] = MinX;
+        bounds[1] = MaxX;
+        bounds[2] = MinY;
+        bounds[3] = MaxY;
+        bounds[4] = MinZ;
+        bounds[5] = MaxZ;
+
+        //
+        // Fill the bxpts structure so that it holds the points that
+        // define the box.
+        //
+#define SET_FOUR_POINTS(_a,_b,_c,_d, _x, _v)       \
+                bxpts[_a][_x] = bxpts[_b][_x] =    \
+                bxpts[_c][_x] = bxpts[_d][_x] = _v
+
+        SET_FOUR_POINTS(0, 1, 2, 3,  0, MinX);
+        SET_FOUR_POINTS(4, 5, 6, 7,  0, MaxX);
+        SET_FOUR_POINTS(0, 1, 4, 5,  1, MinY);
+        SET_FOUR_POINTS(2, 3, 6, 7,  1, MaxY);
+        SET_FOUR_POINTS(0, 2, 4, 6,  2, MinZ);
+        SET_FOUR_POINTS(1, 3, 5, 7,  2, MaxZ);
+        
+        //
+        // For each point, classify as being less than, greater than, or
+        // within the box range for each coordinate.
+        //
+        for (i = 0 ; i < nPts ; i++)
+        {
+            float pt[3];
+            input->GetPoint(i, pt);
+            int j;
+            for (j = 0; j < 3; ++j)
+            {
+                if (pt[j] < bounds[2 * j])
+                    relativeToBox[j][i] = -1;
+                else if (pt[j] > bounds[2 * j + 1])
+                    relativeToBox[j][i] = 1;
+                else
+                    relativeToBox[j][i] = 0;
+            }
+        } 
+    }
+
   
     //
     // Set up some the VTK vars that will (eventually) be copied over
@@ -146,6 +221,7 @@ vtkBoxFilter::Execute(void)
     int nextPointIndex = 0;
     vtkIdList *newCellPts = vtkIdList::New();
     newCellPts->Allocate(VTK_CELL_SIZE);
+
     for (i = 0 ; i < nCells ; i++)
     {
         //
@@ -155,28 +231,143 @@ vtkBoxFilter::Execute(void)
         vtkIdList *cellPts  = cell->GetPointIds();
         int        nCellPts = cell->GetNumberOfPoints();
 
-        //
-        // Determine which points are inside the box and which are outside
-        // the box.  Keep track of whether they are all inside and whether
-        // at least one is inside.
-        //
-        bool allInsideCell  = true;
-        bool oneInsideCell  = false;
-        for (j = 0 ; j < nCellPts ; j++)
+        bool meetsCriteria;
+        if (AllOfCell)
         {
-            int ptId = cellPts->GetId(j);
-            if (isInBox[ptId])
+            // Algorithm for 'AllOfCell' check:
+            //   Go through and break out early if a point is outside.
+            //   We meet the criteria if we didn't break out early.
+            for (j = 0 ; j < nCellPts ; ++j)
             {
-                oneInsideCell = true;
+                int ptId = cellPts->GetId(j);
+                if (!isInBox[ptId])
+                    break;
             }
+            meetsCriteria = (j == nCellPts);
+        }
+        else
+        {
+            // Algorithm for 'SomeOfCell' check:
+            //   Go through mapping where the cell seems to be relative
+            //   to the box.
+            //     If we find a point in the box, break out early.
+            //     Make notes of if the cell possibly spans the box.     
+            //   If we broke out early, we meet the critera and we're done.
+            //   If we didn't find any consistancies, we don't meet the
+            //    criteria, and we're done.
+            //   Otherwise, do more checks to confirm whether or not the
+            //    cell is inside or outside of the box.
+
+            bool possiblySpanning[3];
+            possiblySpanning[0] = false;
+            possiblySpanning[1] = false;
+            possiblySpanning[2] = false;
+
+            int fId = cellPts->GetId(0);
+            for (j = 0; j < nCellPts; ++j)
+            {
+                int ptId = cellPts->GetId(j);
+                if (!relativeToBox[0][ptId] && !relativeToBox[1][ptId] 
+                                            && !relativeToBox[2][ptId])
+                    break;
+                int k;
+                for (k = 0; k < 3; ++k)
+                {
+                    //
+                    // To be possibly spanning in a coordinate means
+                    // that either (a) the component is within the box,
+                    // or that it is inconsistant with another node
+                    // (in this case, the first node).
+                    //
+                    if (!relativeToBox[k][ptId])
+                        possiblySpanning[k] = true;
+                    else if (relativeToBox[k][ptId] != relativeToBox[k][fId])
+                        possiblySpanning[k] = true;
+                }
+            }
+
+            if (j != nCellPts)
+                meetsCriteria = true;
+            else if (!possiblySpanning[0] || !possiblySpanning[1]
+                                          || !possiblySpanning[2])
+                meetsCriteria = false;
             else
             {
-                allInsideCell = false;
-            }
+                meetsCriteria = false;
+                //
+                // It's possible that part of this cell is contained within
+                // the box, even though no vertices are within it.
+                //
+
+                //
+                // We need to look at lines between vertices. We search
+                // from the first vertice to all the other (since we
+                // used it as a comparison node), then we search from
+                // any nodes that have '0' for a relative box position.
+                //
+                  
+                for (j = 0; j < nCellPts && !meetsCriteria; ++j)
+                {
+                    int ptId = cellPts->GetId(j);
+                    if (j != 0 && relativeToBox[0][ptId] 
+                               && relativeToBox[1][ptId]
+                               && relativeToBox[2][ptId])
+                    {
+                        continue;
+                    }
+
+                    float pt1[3];
+                    input->GetPoint(ptId, pt1);
+                    
+                    int k;
+                    for (k = j + 1; k < nCellPts; ++k)
+                    {
+                        float pt2[3];
+                        input->GetPoint(cellPts->GetId(k), pt2);
+                        // Turn pt2 into a ray
+                        float ray[3];
+                        ray[0] = pt2[0] - pt1[0];
+                        ray[1] = pt2[1] - pt1[1];
+                        ray[2] = pt2[2] - pt1[2];
+                        
+                        float t;
+                        if (vtkCell::HitBBox(bounds, pt1, ray, pt2, t)
+                                && t < 1)
+                        {
+                            meetsCriteria = true;
+                            break;
+                        }
+                        
+                    }
+                }
+
+                if (!meetsCriteria)
+                {
+                    //
+                    // It's still possible that part of the cell is in the
+                    // box. In these cases, we check to see if the lines of
+                    // the box intersect the cell.
+                    //
+                    float xcoords[3];
+                    float t;
+                    float pcoords[3];
+                    int subid;
+                    for (j = 0; j < 24; j += 2)
+                    {
+                        float *pt1 = bxpts[boxLineMap[j]];
+                        float *pt2 = bxpts[boxLineMap[j + 1]];
+                        if (cell->IntersectWithLine(pt1, pt2, 1e-6, t,
+                                                    xcoords, pcoords, subid))
+                        {
+                            meetsCriteria = true;
+                            break;
+                        }
+                    }
+                }
+               
+           }
         }
 
-        bool meetsCriteria = (oneInsideCell && AllOfCell == 0) ||
-                             (allInsideCell && AllOfCell != 0);
         if (meetsCriteria)
         {
             //
@@ -213,7 +404,14 @@ vtkBoxFilter::Execute(void)
     // Prevent leaks and set up the output.
     //
     delete [] pointMap;
-    delete [] isInBox;
+    if (AllOfCell)
+        delete [] isInBox;
+    else
+    {
+        delete [] relativeToBox[0];
+        delete [] relativeToBox[1];
+        delete [] relativeToBox[2];
+    }
     newCellPts->Delete();
     output->SetPoints(pts);
     pts->Delete();
