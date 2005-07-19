5fbbc7bb7b48e0d9ed2d3dc0b21ed01f7d0757cd
Hank Childs <hank@uoregon.edu>
2005-07-19 00:00:00 -0700

    Update from July 19, 2005
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@504 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/plots/Scatter/avtScatterFilter.h
+++ src/plots/Scatter/avtScatterFilter.h
@@ -18,6 +18,9 @@
 //  Creation:   Tue Nov 2 22:31:23 PST 2004
 //
 //  Modifications:
+//    Brad Whitlock, Mon Jul 18 11:53:27 PDT 2005
+//    Added overrides of PreExecute, PerformRestriction, and added new 
+//    method PopulateDataInputs. Also added extents members.
 //
 // ****************************************************************************
 
@@ -46,11 +49,20 @@ protected:
 
     std::string                variableName;
     ScatterAttributes          atts;
+    double                     xExtents[2];
+    double                     yExtents[2];
+    double                     zExtents[2];
+    double                     colorExtents[2];
+    bool                       needXExtents;
+    bool                       needYExtents;
+    bool                       needZExtents;
+    bool                       needColorExtents;
 
+    virtual void               PreExecute(void);
     virtual vtkDataSet        *ExecuteData(vtkDataSet *, int, std::string);
     virtual void               RefashionDataObjectInfo(void);
-    //virtual avtPipelineSpecification_p     
-                               //PerformRestriction(avtPipelineSpecification_p spec);
+    virtual avtPipelineSpecification_p     
+                               PerformRestriction(avtPipelineSpecification_p spec);
     vtkDataArray              *GetDataArray(vtkDataSet *inDS,
                                             const std::string &name,
                                             avtCentering targetCentering,
@@ -59,9 +71,13 @@ protected:
                                         avtCentering cent) const;
     vtkDataSet                *PointMeshFromVariables(DataInput *d1,
                                                       DataInput *d2,
-                                                      DataInput *d3);
+                                                      DataInput *d3,
+                                                      DataInput *d4, bool &);
     int                        CountSpatialDimensions() const;
-
+    void                       PopulateDataInputs(DataInput *orderedArrays,
+                                                  vtkDataArray **arr) const;
+    void                       PopulateNames(const char **) const;
+    bool                       NeedSpatialExtents() const;
 };
 
 
