a567932f0125d3260a4d38e2e68d5efde513a967
Hank Childs <hank@uoregon.edu>
2006-06-07 00:00:00 -0700

    Update from June 7, 2006
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@680 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/databases/BOW/avtBOWFileFormat.C
+++ src/databases/BOW/avtBOWFileFormat.C
@@ -74,7 +74,11 @@
 
 using     std::string;
 
+#if defined(__GNUC__) && ((__GNUC__ < 3) || (__GNUC__ == 3 && __GNUC_MINOR__ < 2))
+#define DELOGIFY(V) ((V>=0.0)?1.0:-1.0) * pow(10.0, 16.0 * fabs(V) - 8.0)
+#else
 #define DELOGIFY(V) ((V>=0.0f)?1.0f:-1.0f) * powf(10.0f, 16.0f * fabsf(V) - 8.0f)
+#endif
 
 #define DEBUG_PRINT
 #ifdef DEBUG_PRINT
