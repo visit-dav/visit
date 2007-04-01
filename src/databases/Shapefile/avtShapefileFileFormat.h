6ded12df90ec37a6fd00e7739150c069cbb48f03
Hank Childs <hank@uoregon.edu>
2007-04-01 23:22:02 +0000

    Update from March 29, 2005
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@440 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ src/databases/Shapefile/avtShapefileFileFormat.h
@@ -0,0 +1,85 @@
+
+// ************************************************************************* //
+//                            avtShapefileFileFormat.h                       //
+// ************************************************************************* //
+
+#ifndef AVT_Shapefile_FILE_FORMAT_H
+#define AVT_Shapefile_FILE_FORMAT_H
+
+#include <avtSTSDFileFormat.h>
+#include <esriShapefile.h>
+#include <dbfFile.h>
+#include <vector>
+
+// ****************************************************************************
+//  Class: avtShapefileFileFormat
+//
+//  Purpose:
+//      Reads in ESRI Shapefile files as a plugin to VisIt.
+//
+//  Programmer: Brad Whitlock
+//  Creation:   Thu Mar 24 12:18:02 PDT 2005
+//
+//  Modifications:
+//
+// ****************************************************************************
+
+class avtShapefileFileFormat : public avtSTSDFileFormat
+{
+public:
+                       avtShapefileFileFormat(const char *filename);
+    virtual           ~avtShapefileFileFormat();
+
+    //
+    // This is used to return unconvention data -- ranging from material
+    // information to information about block connectivity.
+    //
+    // virtual void      &GetAuxiliaryData(const char *var, const char *type,
+    //                                  void *args, DestructorFunction &);
+    //
+
+    //
+    // These are used to declare what the current time and cycle are for the
+    // file.  These should only be defined if the file format knows what the
+    // time and/or cycle is.
+    //
+    // virtual bool      ReturnsValidCycle() const { return true; };
+    // virtual int       GetCycle(void);
+    // virtual bool      ReturnsValidTime() const { return true; };
+    // virtual double    GetTime(void);
+    //
+
+    virtual const char    *GetType(void)   { return "ESRI Shapefile"; };
+    virtual void           FreeUpResources(void); 
+
+    virtual void           ActivateTimestep(void);
+
+    virtual vtkDataSet    *GetMesh(const char *);
+    virtual vtkDataArray  *GetVar(const char *);
+    virtual vtkDataArray  *GetVectorVar(const char *);
+
+protected:
+    struct esriShape
+    {
+        esriShapeType_t  shapeType;
+        void            *shape;
+    };
+
+    typedef std::vector<esriShape> esriShapeVector;
+
+    void                   Initialize();
+
+    int                    CountMemberPoints(esriShapeType_t) const;
+    int                    CountShapes(esriShapeType_t) const;
+    int                    CountCellsForShape(esriShapeType_t) const;
+
+    bool                   initialized;
+    esriShapeVector        shapes;
+    int                    numShapeTypes;
+    dbfFile_t              *dbfFile;
+
+    virtual void           PopulateDatabaseMetaData(avtDatabaseMetaData *);
+};
+
+
+#endif
---
+++ src/databases/Shapefile/testdbf.c
@@ -0,0 +1,43 @@
+#include <dbfFile.h>
+    
+int
+main(int argc, char *argv[])
+{
+    dbfFile_t      *f = 0;
+    dbfFileError_t  fileError;
+    dbfReadError_t  rcError;
+    void           *data = 0;
+
+    dbfInitialize(1, 0, 0);
+
+#if 1
+#define DATABASE "m195mbsp.dbf"
+#define VARIABLE "EPA_NO"
+#else
+#define DATABASE "GebcoDepthContours.dbf"
+#define VARIABLE "DEPTH"
+#endif
+
+    f = dbfFileOpen(DATABASE, &fileError);
+
+    if(f != 0)
+    {
+        /* Try and read some data */
+        data = dbfFileReadField(f, VARIABLE, &rcError);
+        if(rcError == dbfReadErrorSuccess)
+        {
+            printf("We were able to read the %s field.\n", VARIABLE);
+            dbfFree(data);
+        }
+        else if(rcError == dbfReadErrorFailure)
+            printf("We were NOT able to read the %s field!\n", VARIABLE);
+        else
+            printf("%s is not a valid field name.\n", VARIABLE);
+    }
+
+    dbfFileClose(f);
+
+    dbfFinalize();
+
+    return 0;
+}
