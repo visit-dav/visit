ffc41a6194d264f00ed2d338f20104f7939d181b
David Bremer <bremer4@llnl.gov>
2007-06-15 03:28:18 +0000

    Added a regression test for the Nek3D reader.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@1203 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ test/tests/databases/nek3d.py
@@ -0,0 +1,50 @@
+# ----------------------------------------------------------------------------
+#  CLASSES: nightly
+#
+#  Test Case:  nek3d.py 
+#
+#  Tests:      nek3d dumps
+#
+#  Programmer: David Bremer
+#  Date:       June 12, 2007
+#
+# ----------------------------------------------------------------------------
+import os, string
+
+TurnOffAllAnnotations()
+OpenDatabase("../data/nek3d_test_data/7Pin.100blocks.nek3d")
+
+AddPlot("Mesh", "mesh")
+DrawPlots()
+Test("nek3d_reader1")
+
+
+v=GetView3D()
+v.viewNormal=(-0.58339, -0.40012, -0.706796)
+v.viewUp=(-0.664278, -0.265658, 0.698685)
+SetView3D(v)
+Test("nek3d_reader2")
+
+
+DeleteAllPlots()
+AddPlot("Vector", "velocity")
+v = VectorAttributes()
+v.nVectors = 10
+SetPlotOptions(v)
+DrawPlots()
+Test("nek3d_reader3")
+
+
+SetTimeSliderState(1)
+DrawPlots()
+Test("nek3d_reader4")
+
+
+SetTimeSliderState(2)
+DeleteAllPlots()
+AddPlot("Pseudocolor", "pressure")
+DrawPlots()
+Test("nek3d_reader5")
+
+
+
