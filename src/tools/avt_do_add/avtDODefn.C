178f7a61176188b10e21050bd5177217d3477ac5
Hank Childs <hank@uoregon.edu>
2007-10-01 04:06:18 +0000

    Add a utility that will codegen new derived types of avtDataObject.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@2118 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ src/tools/avt_do_add/avtDODefn.C
@@ -0,0 +1,186 @@
+/*****************************************************************************
+*
+* Copyright (c) 2000 - 2007, The Regents of the University of California
+* Produced at the Lawrence Livermore National Laboratory
+* All rights reserved.
+*
+* This file is part of VisIt. For details, see http://www.llnl.gov/visit/. The
+* full copyright notice is contained in the file COPYRIGHT located at the root
+* of the VisIt distribution or at http://www.llnl.gov/visit/copyright.html.
+*
+* Redistribution  and  use  in  source  and  binary  forms,  with  or  without
+* modification, are permitted provided that the following conditions are met:
+*
+*  - Redistributions of  source code must  retain the above  copyright notice,
+*    this list of conditions and the disclaimer below.
+*  - Redistributions in binary form must reproduce the above copyright notice,
+*    this  list of  conditions  and  the  disclaimer (as noted below)  in  the
+*    documentation and/or materials provided with the distribution.
+*  - Neither the name of the UC/LLNL nor  the names of its contributors may be
+*    used to  endorse or  promote products derived from  this software without
+*    specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT  HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR  IMPLIED WARRANTIES, INCLUDING,  BUT NOT  LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND  FITNESS FOR A PARTICULAR  PURPOSE
+* ARE  DISCLAIMED.  IN  NO  EVENT  SHALL  THE  REGENTS  OF  THE  UNIVERSITY OF
+* CALIFORNIA, THE U.S.  DEPARTMENT  OF  ENERGY OR CONTRIBUTORS BE  LIABLE  FOR
+* ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT  LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF  USE, DATA, OR PROFITS; OR  BUSINESS INTERRUPTION) HOWEVER
+* CAUSED  AND  ON  ANY  THEORY  OF  LIABILITY,  WHETHER  IN  CONTRACT,  STRICT
+* LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY  WAY
+* OUT OF THE  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+* DAMAGE.
+*
+*****************************************************************************/
+
+// ************************************************************************* //
+//                             avtDO_REPL_STRING.C                           //
+// ************************************************************************* //
+
+#include <avtDO_REPL_STRING.h>
+
+#include <avtDO_REPL_STRINGSource.h>
+
+
+// ****************************************************************************
+//  Method: avtDO_REPL_STRING constructor
+//
+//  Arguments:
+//      src     An data object source that is the upstream object.
+//
+//  Programmer: AUTHOR_REPL_STRING
+//  Creation:   DATE_REPL_STRING
+//
+// ****************************************************************************
+
+avtDO_REPL_STRING::avtDO_REPL_STRING(avtDataObjectSource *src)
+    : avtDataObject(src)
+{
+    ;
+}
+
+
+// ****************************************************************************
+//  Method: avtDO_REPL_STRING destructor
+//
+//  Purpose:
+//      Defines the destructor.  Note: this should not be inlined in the header
+//      because it causes problems for certain compilers.
+//
+//  Programmer: AUTHOR_REPL_STRING
+//  Creation:   DATE_REPL_STRING
+//
+// ****************************************************************************
+
+avtDO_REPL_STRING::~avtDO_REPL_STRING()
+{
+    ;
+}
+
+
+// ****************************************************************************
+//  Method: avtDO_REPL_STRING::InstantiateWriter
+//
+//  Purpose:
+//      Instantiates a writer that is appropriate for an avtDO_REPL_STRING.
+//
+//  Programmer: AUTHOR_REPL_STRING
+//  Creation:   DATE_REPL_STRING
+//
+// ****************************************************************************
+
+/*
+avtDataObjectWriter *
+avtDO_REPL_STRING::InstantiateWriter(void)
+{
+    return new avtDO_REPL_STRINGWriter;
+}
+ */
+
+
+// ****************************************************************************
+//  Method: avtDO_REPL_STRING::ReleaseData
+//
+//  Purpose:
+//      Free the data associated with this DO_REPL_STRING.
+//
+//  Programmer: AUTHOR_REPL_STRING
+//  Creation:   DATE_REPL_STRING
+//
+// ****************************************************************************
+
+void
+avtDO_REPL_STRING::ReleaseData(void)
+{
+    YOU MUST DEFINE WHAT IT MEANS TO RELEASE DATA FOR YOUR DATA OBJECT.
+}
+
+
+// ****************************************************************************
+//  Method: avtDO_REPL_STRING::Instance
+//
+//  Purpose:
+//      Creates an instance of an avtDO_REPL_STRING.
+//
+//  Programmer: AUTHOR_REPL_STRING
+//  Creation:   DATE_REPL_STRING
+//
+// ****************************************************************************
+
+avtDataObject *
+avtDO_REPL_STRING::Instance(void)
+{
+    avtDataObjectSource *src = NULL;
+    avtDO_REPL_STRING *new_do = new avtDO_REPL_STRING(src);
+    return new_do;
+}
+
+
+// ****************************************************************************
+//  Method: avtDO_REPL_STRING::DerivedCopy
+//
+//  Purpose:
+//      Copy over the image.
+//
+//  Programmer: AUTHOR_REPL_STRING
+//  Creation:   DATE_REPL_STRING
+//
+// ****************************************************************************
+
+void
+avtDO_REPL_STRING::DerivedCopy(avtDataObject *dob)
+{
+    avtDO_REPL_STRING *typed_dob = (avtDO_REPL_STRING *) dob;
+    YOU MUST IMPLEMENT HOW TO COPY THE DATA MEMBERS FROM typed_dob INTO
+    "this" OBJECT.
+    AS IN:
+    data_member1 = typed_dob->data_member1;
+    ...
+    data_memberN = typed_dob->data_memberN;
+}
+
+
+// ****************************************************************************
+//  Method: avtDO_REPL_STRING::GetNumberOfCells
+//
+//  Purpose:
+//      Estimates the number of cells needed to render this object.  This is
+//      used for determining if VisIt should go into its scalable rendering
+//      mode.
+//
+//  Programmer: AUTHOR_REPL_STRING
+//  Creation:   DATE_REPL_STRING
+//
+// ****************************************************************************
+
+int
+avtDO_REPL_STRING::GetNumberOfCells(bool onlyWantPolygonCount) const
+{
+   YOU MUST IMPLEMENT THIS.  IF YOU WILL NEVER BE RENDERING YOUR DATA OBJECT
+   (BECAUSE YOU WILL ALWAYS BE CONVERTING TO AN avtDataset), THEN JUST RETURN 0.
+}
+
+
+
---
+++ src/tools/avt_do_add/avtDOSink.C
@@ -0,0 +1,132 @@
+/*****************************************************************************
+*
+* Copyright (c) 2000 - 2007, The Regents of the University of California
+* Produced at the Lawrence Livermore National Laboratory
+* All rights reserved.
+*
+* This file is part of VisIt. For details, see http://www.llnl.gov/visit/. The
+* full copyright notice is contained in the file COPYRIGHT located at the root
+* of the VisIt distribution or at http://www.llnl.gov/visit/copyright.html.
+*
+* Redistribution  and  use  in  source  and  binary  forms,  with  or  without
+* modification, are permitted provided that the following conditions are met:
+*
+*  - Redistributions of  source code must  retain the above  copyright notice,
+*    this list of conditions and the disclaimer below.
+*  - Redistributions in binary form must reproduce the above copyright notice,
+*    this  list of  conditions  and  the  disclaimer (as noted below)  in  the
+*    documentation and/or materials provided with the distribution.
+*  - Neither the name of the UC/LLNL nor  the names of its contributors may be
+*    used to  endorse or  promote products derived from  this software without
+*    specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT  HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR  IMPLIED WARRANTIES, INCLUDING,  BUT NOT  LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND  FITNESS FOR A PARTICULAR  PURPOSE
+* ARE  DISCLAIMED.  IN  NO  EVENT  SHALL  THE  REGENTS  OF  THE  UNIVERSITY OF
+* CALIFORNIA, THE U.S.  DEPARTMENT  OF  ENERGY OR CONTRIBUTORS BE  LIABLE  FOR
+* ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT  LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF  USE, DATA, OR PROFITS; OR  BUSINESS INTERRUPTION) HOWEVER
+* CAUSED  AND  ON  ANY  THEORY  OF  LIABILITY,  WHETHER  IN  CONTRACT,  STRICT
+* LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY  WAY
+* OUT OF THE  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+* DAMAGE.
+*
+*****************************************************************************/
+
+// ************************************************************************* //
+//                           avtDO_REPL_STRINGSink.C                         //
+// ************************************************************************* //
+
+#include <avtDO_REPL_STRINGSink.h>
+
+#include <DebugStream.h>
+#include <ImproperUseException.h>
+
+
+// ****************************************************************************
+//  Method: avtDO_REPL_STRINGSink constructor
+//
+//  Programmer: AUTHOR_REPL_STRING
+//  Creation:   DATE_REPL_STRING
+//
+// ****************************************************************************
+
+avtDO_REPL_STRINGSink::avtDO_REPL_STRINGSink()
+{
+    input = NULL;
+}
+
+
+// ****************************************************************************
+//  Method: avtDO_REPL_STRINGSink destructor
+//
+//  Purpose:
+//      Defines the destructor.  Note: this should not be inlined in the header
+//      because it causes problems for certain compilers.
+//
+//  Programmer: AUTHOR_REPL_STRING
+//  Creation:   DATE_REPL_STRING
+//
+// ****************************************************************************
+
+avtDO_REPL_STRINGSink::~avtDO_REPL_STRINGSink()
+{
+    ;
+}
+
+
+// ****************************************************************************
+//  Method: avtDO_REPL_STRINGSink::SetTypedInput
+//
+//  Purpose:
+//      Sets the input of the sink and performs some type checking.
+//
+//  Arguments:
+//      in      The image as a data object.
+//
+//  Programmer: AUTHOR_REPL_STRING
+//  Creation:   DATE_REPL_STRING
+//
+// ****************************************************************************
+
+void
+avtDO_REPL_STRINGSink::SetTypedInput(avtDataObject_p in)
+{
+    if (*in != NULL && strcmp(in->GetType(), "avtDO_REPL_STRING") != 0)
+    {
+        //
+        // Should create a new exception here, but I'm under time constraints.
+        //
+        debug1 << "Looking for avtDO_REPL_STRING, but found type \""
+               << in->GetType() << "\"." << endl;
+        EXCEPTION0(ImproperUseException);
+    }
+
+    CopyTo(input, in);
+}
+
+
+// ****************************************************************************
+//  Method: avtDO_REPL_STRINGSink::GetInput
+//
+//  Purpose:
+//      Gets the input of the sink (properly typed as an avtDataObject).
+//
+//  Returns:    The input of the sink.
+//
+//  Programmer: AUTHOR_REPL_STRING
+//  Creation:   DATE_REPL_STRING
+//
+// ****************************************************************************
+
+avtDataObject_p
+avtDO_REPL_STRINGSink::GetInput(void)
+{
+    avtDataObject_p rv;
+    CopyTo(rv, input);
+    return rv;
+}
+
+
