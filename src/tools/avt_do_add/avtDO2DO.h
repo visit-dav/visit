fa2c2084ff6f96e0d7a1dcc7eddd5f0cbc0a55b0
Hank Childs <hank@uoregon.edu>
2007-10-01 04:06:18 +0000

    Add a utility that will codegen new derived types of avtDataObject.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@2118 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ src/tools/avt_do_add/avtDO2DO.h
@@ -0,0 +1,75 @@
+/*****************************************************************************
+*
+* Copyright (c) 2000 - 2007, The Regents of the University of California
+* Produced at the Lawrence Livermore National Laboratory
+* All rights reserved.
+*
+* This file is part of VisIt. For details, see http://www.llnl.gov/visit/. The
+* full copyright notice is contained in the file COPYRIGHT located at the root
+* of the VisIt distribution or at http://www.llnl.gov/visit/copyright.html.
+*
+* Redistribution  and  use  in  source  and  binary  forms,  with  or  without
+* modification, are permitted provided that the following conditions are met:
+*
+*  - Redistributions of  source code must  retain the above  copyright notice,
+*    this list of conditions and the disclaimer below.
+*  - Redistributions in binary form must reproduce the above copyright notice,
+*    this  list of  conditions  and  the  disclaimer (as noted below)  in  the
+*    documentation and/or materials provided with the distribution.
+*  - Neither the name of the UC/LLNL nor  the names of its contributors may be
+*    used to  endorse or  promote products derived from  this software without
+*    specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT  HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR  IMPLIED WARRANTIES, INCLUDING,  BUT NOT  LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND  FITNESS FOR A PARTICULAR  PURPOSE
+* ARE  DISCLAIMED.  IN  NO  EVENT  SHALL  THE  REGENTS  OF  THE  UNIVERSITY OF
+* CALIFORNIA, THE U.S.  DEPARTMENT  OF  ENERGY OR CONTRIBUTORS BE  LIABLE  FOR
+* ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT  LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF  USE, DATA, OR PROFITS; OR  BUSINESS INTERRUPTION) HOWEVER
+* CAUSED  AND  ON  ANY  THEORY  OF  LIABILITY,  WHETHER  IN  CONTRACT,  STRICT
+* LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY  WAY
+* OUT OF THE  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+* DAMAGE.
+*
+*****************************************************************************/
+
+// ************************************************************************* //
+//                 avtDO_REPL_STRINGToDO_REPL_STRINGFilter.h                 //
+// ************************************************************************* //
+
+#ifndef AVT_DO_REPL_STRING_TO_DO_REPL_STRING_FILTER_H
+#define AVT_DO_REPL_STRING_TO_DO_REPL_STRING_FILTER_H
+
+#include <pipeline_exports.h>
+
+#include <avtDO_REPL_STRINGToDataObjectFilter.h>
+#include <avtDataObjectToDO_REPL_STRINGFilter.h>
+
+
+// ****************************************************************************
+//  Class: avtDO_REPL_STRINGToDO_REPL_STRINGFilter
+//
+//  Purpose:
+//      A filter that takes in an DO_REPL_STRING as input and has an 
+//      DO_REPL_STRING as output.
+//
+//  Programmer: AUTHOR_REPL_STRING
+//  Creation:   DATE_REPL_STRING
+//
+// ****************************************************************************
+
+class PIPELINE_API avtDO_REPL_STRINGToDO_REPL_STRINGFilter
+    : virtual public avtDO_REPL_STRINGToDataObjectFilter,
+      virtual public avtDataObjectToDO_REPL_STRINGFilter
+{
+  public:
+                       avtDO_REPL_STRINGToDO_REPL_STRINGFilter();
+    virtual           ~avtDO_REPL_STRINGToDO_REPL_STRINGFilter();
+};
+
+
+#endif
+
+
