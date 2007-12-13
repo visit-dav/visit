e14dc7cd7fc8d8ebd4191463edc79c62b09b70da
Dave Pugmire <pugmire@ornl.gov>
2007-12-13 20:23:26 +0000

    Parallelize the GTC reader.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@2665 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/databases/GTC/avtGTCFileFormat.h
+++ src/databases/GTC/avtGTCFileFormat.h
@@ -45,6 +45,8 @@
 #include <avtSTMDFileFormat.h>
 #include <hdf5.h>
 
+using std::string;
+class parallelBuffer;
 
 // ****************************************************************************
 //  Class: avtGTCFileFormat
@@ -72,12 +74,63 @@ class avtGTCFileFormat : public avtSTMDFileFormat
 
   protected:
     // DATA MEMBERS
-    bool                   Initialize(bool, bool);
+    bool                   Initialize();
     virtual void           PopulateDatabaseMetaData(avtDatabaseMetaData *);
-    hid_t                  fileId;
-    int                    nVars;
+    void                   ReadVariable( int domain, int varIdx, int varDim, float **ptrVar );
+    
+    string                 IndexToVarName( int idx ) const;
+    int                    VarNameToIndex( const string &var ) const;
+    hid_t                  fileHandle, particleHandle;
+    int                    nVars, nTotalPoints, nPoints;
     bool                   initialized;
+    hsize_t                startOffset;                    
+
+#if PARALLEL
+    int                    nProcs, rank;
+
+    void                   ParallelReadVariable( int domain, int varDim, float *var, float *ids );
+    void                   ParallelReadParticles( int domain, float *pts, float *ids );
+    void                   BinData( int dim, parallelBuffer **array, float *vars, float *ids,
+				    float **myVars, float **myIds );
+    int *                  GetDataShareMatrix( parallelBuffer **array );
+    void                   CommunicateData( int dim, int *dataShare, parallelBuffer **array,
+					    float **myPts, float **myIds );
+#endif
+    
 };
+
+#ifdef PARALLEL
+
+// ****************************************************************************
+//  Class: parallelBuffer
+//
+//  Purpose:
+//      Dynamic array.
+//
+//  Programmer: Dave Pugmire
+//  Creation:   Tue Nov 20 14:08:56 PST 2007
+//
+// ****************************************************************************
+
+class parallelBuffer
+{
+public:
+    parallelBuffer( int elemSz );
+    ~parallelBuffer();
+
+    int ElemSize() const { return elemSize; }
+    int Size() const { return size; }
+    float* Get( int i ) { return &(pArray[i*elemSize]); }
+    void Add( float *data ) { AddElement( data ); }
+    
+protected:
+    void AddElement( float *data );
+    
+    float *pArray;
+    int size, buffSize, elemSize;
+};
+
+#endif
 
 
 #endif
