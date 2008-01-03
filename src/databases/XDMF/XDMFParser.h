88a721be924cbcd5a2bc8f6d6ea4d3e5d0cc77f8
Eric Brugger <brugger1@llnl.gov>
2008-01-03 18:32:22 +0000

    I added an initial version of an XDMF database reader. XDMF is a file format where the metadata is stored in an XML file with references to the raw data in HDF5 files. The file format was developed by ARL. The reader is functional but is not yet complete. Since it isn't ready for prime time I am not adding any information to the release notes for it.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@2806 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ src/databases/XDMF/XDMFParser.h
@@ -0,0 +1,103 @@
+/*****************************************************************************
+*
+* Copyright (c) 2000 - 2007, The Regents of the University of California
+* Produced at the Lawrence Livermore National Laboratory
+* All rights reserved.
+*
+* This file is part of VisIt. For details, see http://www.llnl.gov/visit/. The
+* full copyright notice is contained in the file COPYRIGHT located at the root
+* of the VisIt distribution or at http://www.llnl.gov/visit/copyright.html.
+*
+* Redistribution  and  use  in  source  and  binary  forms,  with  or  without
+* modification, are permitted provided that the following conditions are met:
+*
+*  - Redistributions of  source code must  retain the above  copyright notice,
+*    this list of conditions and the disclaimer below.
+*  - Redistributions in binary form must reproduce the above copyright notice,
+*    this  list of  conditions  and  the  disclaimer (as noted below)  in  the
+*    documentation and/or materials provided with the distribution.
+*  - Neither the name of the UC/LLNL nor  the names of its contributors may be
+*    used to  endorse or  promote products derived from  this software without
+*    specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT  HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR  IMPLIED WARRANTIES, INCLUDING,  BUT NOT  LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND  FITNESS FOR A PARTICULAR  PURPOSE
+* ARE  DISCLAIMED.  IN  NO  EVENT  SHALL  THE  REGENTS  OF  THE  UNIVERSITY OF
+* CALIFORNIA, THE U.S.  DEPARTMENT  OF  ENERGY OR CONTRIBUTORS BE  LIABLE  FOR
+* ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT  LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF  USE, DATA, OR PROFITS; OR  BUSINESS INTERRUPTION) HOWEVER
+* CAUSED  AND  ON  ANY  THEORY  OF  LIABILITY,  WHETHER  IN  CONTRACT,  STRICT
+* LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY  WAY
+* OUT OF THE  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+* DAMAGE.
+*
+*****************************************************************************/
+
+// ************************************************************************* //
+//                                XDMFParser.h                               //
+// ************************************************************************* //
+
+#ifndef XDMF_PARSER_H
+#define XDMF_PARSER_H
+
+#include <stdio.h>
+
+class XDMFParser
+{
+  public:
+                       XDMFParser();
+    virtual           ~XDMFParser();
+
+    enum               ElementType {TYPE_PI, TYPE_COMMENT, TYPE_SPECIAL,
+                                    TYPE_START_TAG, TYPE_END_TAG, TYPE_CDATA,
+                                    TYPE_EOF, TYPE_ERROR};
+
+    void               SetInputFileName(const char *);
+
+    ElementType        GetNextElement();
+    const char        *GetElementName() const;
+    void               SkipToEndTag();
+
+    bool               GetNextAttribute();
+    const char        *GetAttributeName() const;
+    const char        *GetAttributeValue() const;
+
+    int                GetCDataOffset() const;
+    int                GetCDataLength() const;
+    const char        *GetCDataValue() const;
+
+    void               PrintFile();
+
+  private:
+    void               UnGetChar();
+    void               UnGetChar(const char &);
+    char              *GetChar();
+
+    char              *fname;
+
+    FILE              *file;
+
+    char               nullChar[8];
+
+    int                offset;
+    int                ibuf;
+    int                lbuf;
+    bool               endOfFile;
+    char              *buffer;
+
+    // Return value for GetElementName.
+    char               elementName[1024];
+
+    // Return values for GetAttributeName and GetAttributeValue.
+    char               attributeName[1024];
+    char               attributeValue[1024];
+
+    // Return values for GetCDataOffset, GetCDataLength, and GetCDataValue.
+    int                cdataOffset;
+    int                cdataLength;
+    char               cdataValue[1024];
+};
+
+#endif
