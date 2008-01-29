bbba522801b2ac1f85ba2a4f1e8b2530eb215ca8
Mark Miller <miller86@llnl.gov>
2008-01-29 23:44:10 +0000

    improving lineout test data
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@3066 18c085ea-50e0-402c-830e-de6fd14e8384

--- data/lineout_test.C
+++ data/lineout_test.C
@@ -122,8 +122,8 @@ main(int argc, char **argv)
     DBPutUcdvar1(dbfile, "nodal_var", "mesh", nc_var, 12, NULL, 0, DB_FLOAT,
         DB_NODECENT, NULL);
 
-    float x1[] = {0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3};
-    float y1[] = {0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+    float x1[] = {-1.5, -.5, 0.5, 1.5, -1.5, -.5, 0.5, 1.5, -1.5, -.5, 0.5, 1.5, -1.5, -.5, 0.5, 1.5};
+    float y1[] = {-1.5, -1.5, -1.5, -1.5, -0.5, -0.5, -0.5, -0.5, 0.5, 0.5, 0.5, 0.5, 1.5, 1.5, 1.5, 1.5};
     int zonelist2[] = {0,1,5,4,   1,2,6,5,   2,3,7,6,
                        4,5,9,8,   5,6,10,9,  6,7,11,10,
 		       8,9,13,12, 9,10,14,13,10,11,15,14};
@@ -137,11 +137,18 @@ main(int argc, char **argv)
     DBPutUcdmesh(dbfile, "mesh2", 2, coordnames, coords, 16, 9,
                      "zl2d2", NULL, DB_FLOAT, NULL);
 
-    float zc_var2[] = {1, 0, -1, 0, 0, 0, -1, 0, 1};
+    float zc_var2[] = {1.5, 0, -1.5, 0, 0, 0, -1.5, 0, 1.5};
     DBPutUcdvar1(dbfile, "zonal_var2", "mesh2", zc_var2, 9, NULL, 0, DB_FLOAT,
         DB_ZONECENT, NULL);
 
-    float nc_var2[] = {2, 1, -1, -2, 1, 1, -1, -1, -1, -1, 1, 1, -2, -1, 1, 2};
+    // This is designed to yield a perfect quadratic along the diagonal
+    // y=x and y=-x lineouts such that value of nc_var2 is equal to distance
+    // from origin (0,0), squared. A resulting lineout *should* look like a
+    // perfect quadratic curve.
+    float a = sqrt(1.5*1.5 + 1.5*1.5);
+    float b = sqrt(0.5*0.5 + 1.5*1.5);
+    float c = sqrt(0.5*0.5 + 0.5*0.5);
+    float nc_var2[] = {-a, -b, b, a, -b, -c, c, b, b, c, -c, -b, a, b, -b, -a};
     DBPutUcdvar1(dbfile, "nodal_var2", "mesh2", nc_var2, 16, NULL, 0, DB_FLOAT,
         DB_NODECENT, NULL);
 
