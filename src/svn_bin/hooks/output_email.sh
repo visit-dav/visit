9c510334afa0b844fdb144d898ebcdd251f33dc9
Tom Fogal <tfogal@sci.utah.edu>
2008-02-03 18:54:48 +0000

    adding script to grab emails, and testing that hooks don't hate me.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@3115 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ src/svn_bin/hooks/output_email.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+# This is a subversion hook script intended to identify the email address of a
+# user who committed a change.  It relies on a shell script existing in your
+# repository which maps usernames to email addresses.  When run, this uses
+# svnlook to grab your script out of the repository and store it in a
+# temporary file.  Then it runs the script, giving the username of the
+# committer as the sole argument.  The output of the script should be the
+# committers email, and nothing else.
+# The script needs the repository path and revision identifier as arguments, in
+# that order.
+### For example:
+# We have /trunk/some_dir/emails.sh which looks something like this:
+#   #!/bin/sh
+#   case $1 in
+#      "zaphod")
+#         echo "zaphod@beeblebrox.com"
+#         ;;
+#      "paul")
+#         echo "paul@atreides.org"
+#         ;;
+#      "gently")
+#         echo "dirk@holisticdetectives.com"
+#         ;;
+#      *)
+#         echo "mailinglist@yourproject.org"
+#         ;;
+#   esac
+# So we set the below SCRIPTFILE variable to be 'trunk/some_dir/emails.sh'.
+#####
+#
+# Script file location: location of the script relative to the repository root.
+SCRIPTFILE="scripts/email.sh"
+# What should we use for the temporary file?
+TMPFILE="/tmp/email.sh"
+#### Program locations.
+SVNLOOK=/usr/bin/svnlook
+RM=/bin/rm
+BASH=/bin/bash
+
+###########################################################
+# You should not need to modify anything below this line. #
+###########################################################
+REPOS=$1
+REV=$2
+
+function error()
+{
+    echo $@ 1>&2
+    exit 1
+}
+
+if test "x${REPOS}" = "x"; then
+    error "Repository path not set in $0."
+fi
+if test "x${REV}" = "x"; then
+    error "Revision number not set in $0."
+fi
+
+author=`${SVNLOOK} author -r${REV} ${REPOS}`
+${SVNLOOK} cat ${REPOS} ${SCRIPTFILE} > ${TMPFILE}
+if test "x$?" = "x1"; then
+    error "email script does not exist in repository."
+fi
+address=`${BASH} ${TMPFILE} $author`
+/bin/rm -f ${TMPFILE}
+echo "${address}"
