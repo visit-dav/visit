4151fc4cf03ee0d1ded256796075008c041bde50
Jeremy Meredith <jsmeredith@gmail.com>
2008-02-08 18:53:15 +0000

    Improving test for parallel coordinates to test new features.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@3183 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/plots/parcoords.py
+++ test/tests/plots/parcoords.py
@@ -13,6 +13,10 @@
 #    Jeremy Meredith, Mon Feb  4 16:11:21 EST 2008
 #    Removed axis extents from plot attributes since they were unused.
 #
+#    Jeremy Meredith, Fri Feb  8 13:49:58 EST 2008
+#    Added support for array variable expressions, raw database array
+#    variables, clamping plot limits, and changing viewports.
+#
 # ----------------------------------------------------------------------------
 
 # Turn off all annotation
@@ -29,17 +33,15 @@ a.foregroundColor = (0, 0, 0, 255)
 a.backgroundColor = (255, 255, 255, 255)
 SetAnnotationAttributes(a)
 
-TestSection("Test plot options")
+#
+# Trying out collections of scalars as a source
+#
+TestSection("Test scalar collection sources")
 
 OpenDatabase("../data/noise.silo")
 
-#
-# Set up a simple 3 variable parallel coordinates plot.
-#
+# Set default plot variables
 p = ParallelCoordinatesAttributes()
-p.orderedAxisNames = ("hardyglobal", "shepardglobal", "chromeVf")
-p.extentMinima = (-1e+37, -1e+37, -1e+37)
-p.extentMaxima = (+1e+37, +1e+37, +1e+37)
 p.drawLines = 1
 p.linesColor = (128, 0, 0, 255)
 p.drawContext = 1
@@ -48,13 +50,17 @@ p.contextNumPartitions = 512
 p.contextColor = (0, 220, 0, 255)
 p.drawLinesOnlyIfExtentsOn = 1
 SetDefaultPlotOptions(p)
+
+# Set up a simple 3 variable parallel coordinates plot.
 AddPlot("ParallelCoordinates", "hardyglobal")
+p.orderedAxisNames = ("hardyglobal", "shepardglobal", "chromeVf")
+p.extentMinima = (-1e+37, -1e+37, -1e+37)
+p.extentMaxima = (+1e+37, +1e+37, +1e+37)
+SetPlotOptions(p)
 DrawPlots()
 Test("parcoords_01")
 
-#
 # Try setting some appearance-related attributes
-#
 p.contextGamma = 2.9
 p.contextNumPartitions = 128
 p.contextColor = (200, 0, 255, 255)
@@ -81,5 +87,72 @@ p.extentMinima = (5.7, -1e+37, -1e+37, -1e+37)
 p.extentMaxima = (5.8, +1e+37, +1e+37, +1e+37)
 SetPlotOptions(p)
 Test("parcoords_05")
+
+# Try unifying the extents across all axes
+p.unifyAxisExtents = 1
+SetPlotOptions(p)
+Test("parcoords_06")
+
+
+#
+# Trying out array variables as a source
+#
+TestSection("Test array expression sources")
+
+# Try an array variable
+DeleteAllPlots();
+
+OpenDatabase("../data/astronomy.okc")
+
+DefineArrayExpression("arrayvar1",
+                      "array_compose(d, b, b/2, b+l)")
+DefineArrayExpression("arrayvar2",
+                      "array_compose(d+200, l, l-2, b+l)")
+DefineArrayExpression("arrayvar2wb",
+                      "array_compose_with_bins(d+200, l, l-2, b+l, [0,1,4,15,20])")
+
+AddPlot("ParallelCoordinates", "arrayvar1")
+p = ParallelCoordinatesAttributes()
+p.contextColor = (100, 100, 255, 255)
+p.contextNumPartitions = 32
+SetPlotOptions(p)
+DrawPlots()
+Test("parcoords_07")
+
+# Try changing the array variable
+ChangeActivePlotsVar("arrayvar2")
+Test("parcoords_08")
+
+# Now change to one with bin-width defined axis x positions
+ChangeActivePlotsVar("arrayvar2wb")
+Test("parcoords_09")
+
+# Now clamp the axis array limits
+p.unifyAxisExtents = 1
+SetPlotOptions(p)
+Test("parcoords_10")
+
+#
+# Trying out raw database-generated array as a source
+#
+TestSection("Test raw database array sources")
+
+
+# Now change to a raw database-generated array variable
+ChangeActivePlotsVar("all_vars")
+p.unifyAxisExtents = 0;
+p.contextColor = (255,150,50,255)
+SetPlotOptions(p)
+
+# Try changing the viewport so it looks a little more normal
+v = GetViewAxisArray()
+v.viewportCoords = (0.15,0.9, 0.3,0.7)
+SetViewAxisArray(v)
+Test("parcoords_11")
+
+# Now clamp the axis array limits again
+p.unifyAxisExtents = 1
+SetPlotOptions(p)
+Test("parcoords_12")
 
 Exit()
