2214aa8eec4b6016884400708baebaf91e143645
Kathleen Bonnell <bonnell2@llnl.gov>
2008-04-03 18:20:36 +0000

    Add tests for situations where query might use the wrong varaible
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@3647 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/queries/queryMultiWindow.py
+++ test/tests/queries/queryMultiWindow.py
@@ -13,6 +13,10 @@
 #    Kathleen Bonnell, Mon Dec 20 15:54:04 PST 2004
 #    Changed 'Variable by Zone' to 'PickByZone' and 'Variable by Node' to
 #    'PickByNode'.
+#
+#    Kathleen Bonnell, Thu Apr  3 10:00:48 PDT 2008 
+#    Added tests from bug 8425. 
+#
 # ----------------------------------------------------------------------------
 
 def QueryMultiWindow():
@@ -74,12 +78,65 @@ def QueryMultiWindow():
     Query("NumZones", "original") 
     s = s + GetQueryOutputString() + "\n"
     
-    TestText("QueryMultiWindow", s)
     #Delete window 2
     DeleteWindow()
     #Delete plots in window 1
     DeleteAllPlots()
 
+    #bug 8425 (multiple windows, same db, same timestate)
+    OpenDatabase("../data/rect3d.silo")
+    DefineScalarExpression("X", "coord(quadmesh3d)[0]")
+    DefineScalarExpression("unnamed1", "X-1")
+    DefineScalarExpression("unnamed2", "X-2")
+    DefineScalarExpression("unnamed3", "X-3")
+    SetWindowLayout(4)
+    AddPlot("Pseudocolor", "unnamed1")
+    DrawPlots()
+    Query("Weighted Variable Sum", 0, 0, "default")
+    s = s + "\n" + GetQueryOutputString() + "\n"
+    SetActiveWindow(2)
+    AddPlot("Pseudocolor", "unnamed2")
+    DrawPlots()
+    Query("Weighted Variable Sum", 0, 0, "default")
+    s = s + GetQueryOutputString() + "\n"
+    SetActiveWindow(3)
+    AddPlot("Pseudocolor", "unnamed3")
+    DrawPlots()
+    Query("Weighted Variable Sum", 0, 0, "default")
+    s = s + GetQueryOutputString() + "\n"
+    SetActiveWindow(2)
+    Query("Weighted Variable Sum", 0, 0, "default")
+    s = s + GetQueryOutputString() + "\n"
+
+    SetActiveWindow(4)
+    DeleteWindow()
+    SetActiveWindow(3)
+    DeleteWindow()
+    SetActiveWindow(2)
+    DeleteWindow()
+    DeleteAllPlots()
+
+    #bug 8425 (multiple plots, same window, same db, same root var)
+    OpenDatabase("../data/multi_rect2d.silo")
+    DefineScalarExpression("p2", "p*p")
+    DefineScalarExpression("p3", "p*p*p")
+    AddPlot("Pseudocolor", "p")
+    AddPlot("Pseudocolor", "p2")
+    AddPlot("Pseudocolor", "p3")
+    DrawPlots()
+    SetActivePlots(0)
+    Query("Weighted Variable Sum")
+    s = s + "\n" + GetQueryOutputString() + "\n"
+    SetActivePlots(1)
+    Query("Weighted Variable Sum")
+    s = s + GetQueryOutputString() + "\n"
+    SetActivePlots(2)
+    Query("Weighted Variable Sum")
+    s = s + GetQueryOutputString() + "\n"
+
+    TestText("QueryMultiWindow", s)
+    
+    DeleteAllPlots() 
 
 # Call the main function
 QueryMultiWindow()
