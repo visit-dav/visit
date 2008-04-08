8e564df153e5c4e107781b54694c26bc4684d54c
Mark Miller <miller86@llnl.gov>
2008-04-08 17:36:49 +0000

    removing test hook
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@3750 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/svn_bin/hooks/fail.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# simple test to ensure messages get sent on failed post-commits
-echo "post-commit failed" 1>&2
-echo "post-commit failed" 1>&2
-echo "post-commit failed" 1>&2
-echo "here!" 
-exit 1
