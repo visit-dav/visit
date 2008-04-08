c4b7a6b3c3db87f8dfe9e295ac987863363c288a
Mark Miller <miller86@llnl.gov>
2008-04-08 03:32:03 +0000

    testing
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@3727 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/svn_bin/hooks/hooks_update.sh
+++ src/svn_bin/hooks/hooks_update.sh
@@ -24,6 +24,7 @@ if test -z "${REV}"; then
     error "Revision number not set in $0."
 fi
 
+hooksUpdateFile=""
 hookVarsFile=""
 preCommitFile=""
 postCommitFile=""
@@ -31,6 +32,9 @@ hookFiles=""
 files=`${SVNLOOK} changed -r ${REV} ${REPOS} | ${AWK} '{print $2}'`
 for f in ${files} ; do
     case ${f} in
+        *src/svn_bin/hooks/hooks_update.sh)
+	    hooksUpdateFile=$f
+            ;;
         *src/svn_bin/hooks/hook_vars.sh)
 	    hookVarsFile=$f
             ;;
@@ -52,8 +56,18 @@ done
 set +o noclobber
 
 #
+# Handle the hook variables file speciall. We can't install it
+# automatically as it is the current script that is installing
+# everything else.
+if test -n "$hooksUpdateFile"; then
+    echo "You are updating hooks_update.sh." 1>&2
+    echo "It cannot be installed automatically." 1>&2
+    echo "Remember to install hooks_update.sh manually." 1>&2
+fi
+
+#
 # Handle the hook variables file specially. It can effect everything
-# else.
+# else, so do it first.
 #
 if test -n "$hookVarsFile"; then
     ${SVNLOOK} cat -t $TXN $REPOS $hookVarsFile > $REPOS/hooks/hook_vars.sh
@@ -81,7 +95,6 @@ for f in $preCommitFile $postCommitFile ${hookFiles} ; do
     ${CHGRP} $VISIT_GROUP_NAME $REPOS/hooks/$bf
     ${CHMOD} 770 $REPOS/hooks/$bf
 done
-
 
 # all is well!
 exit 0
