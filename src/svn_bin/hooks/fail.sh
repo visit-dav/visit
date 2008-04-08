ec2716e1a4fbdd85d1cc65d9288cb9e1d0bf09a6
Mark Miller <miller86@llnl.gov>
2008-04-08 18:02:59 +0000

    updating post-commit to not exit on first failure
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@3758 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/svn_bin/hooks/fail.sh
+++ src/svn_bin/hooks/fail.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # simple test to ensure messages get sent on failed post-commits
 echo "Testing post-commit message. Please ignore" 1>&2
+echo "Testing post-commit message. Please ignore" 1>&2
 exit 1
