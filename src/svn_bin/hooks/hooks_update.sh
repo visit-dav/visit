062fba26af9b6366beebbd93ffc3fa982deb5114
Mark Miller <miller86@llnl.gov>
2008-04-08 03:07:40 +0000

    testing
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@3704 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/svn_bin/hooks/hooks_update.sh
+++ src/svn_bin/hooks/hooks_update.sh
@@ -30,6 +30,7 @@ postCommitFile=""
 hookFiles=""
 files=`${SVNLOOK} changed -r ${REV} ${REPOS} | ${AWK} '{print $2}'`
 for f in ${files} ; do
+echo $f >> foobar
     case ${f} in
         *src/svn_bin/hooks/hook_vars.sh)
 	    hookVarsFile=$f
@@ -66,6 +67,8 @@ fi
 #
 for f in $preCommitFile $postCommitFile ${hookFiles} ; do
     bf=`basename $f`
+    echo $f >> foobar
+    echo $bf >> foobar
 
     #
     # If we don't already have this hook installed, make sure
