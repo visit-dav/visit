f77e56f69daa0f165d86aa24f567b546d8d4400d
Mark Miller <miller86@llnl.gov>
2008-04-23 06:21:32 +0000

    updating itaps tests to test new subsetting functionality
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@4047 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/databases/itaps.py
+++ test/tests/databases/itaps.py
@@ -19,43 +19,31 @@
 #    Mark C. Miller, Tue Jul  3 15:26:41 PDT 2007
 #    Changed to use ITAPS_C plugin
 #
+#    Mark C. Miller, Tue Apr 22 23:20:43 PDT 2008
+#    Modified to test new subsetting functionality.
 # ----------------------------------------------------------------------------
 
-edgeSetId = -1
-faceSetId = -1
-regSetId = -1
-silr = 0
-
-def InitSetIds():
-    global edgeSetId
-    global faceSetId
-    global regSetId
-    global silr
-    edgeSetId = -1
-    faceSetId = -1
-    regSetId = -1
-    silr = SILRestriction()
-    for i in range(silr.NumSets()):
-        if silr.SetName(i) == "EDGE":
-            edgeSetId = i
-        if silr.SetName(i) == "FACE":
-            faceSetId = i
-        if silr.SetName(i) == "REGION":
-            regSetId = i
+def RestrictSetsInCategory(silr, className, setIds):
+    catName = className + "s";
+    if className == "Vertex":
+        catName = "Verticies"
+    i = 0
+    silr.TurnOnAll()
+    for set in silr.SetsInCategory(catName):
+        if i < len(setIds) and silr.SetName(set) == "%s_%03d"%(className, setIds[i]):
+            i = i + 1;
+            silr.TurnOnSet(set)
+        elif i < len(setIds) and silr.SetName(set) == "%s%d"%(className, setIds[i]):
+            print "Turning on set %s"%silr.SetName(set)
+            i = i + 1;
+            silr.TurnOnSet(set)
+        else:
+            print "Turning OFF set %s"%silr.SetName(set)
+            silr.TurnOffSet(set)
+        
 
 # Turn off all annotation
-a = AnnotationAttributes()
-a.axesFlag2D = 0
-a.axesFlag = 0
-a.triadFlag = 0
-a.bboxFlag = 0
-a.userInfoFlag = 0
-a.databaseInfoFlag = 0
-a.legendInfoFlag = 0
-a.backgroundMode = 0
-a.foregroundColor = (0, 0, 0, 255)
-a.backgroundColor = (255, 255, 255, 255)
-SetAnnotationAttributes(a)
+TurnOffAllAnnotations()
 
 # test the main mesh
 OpenDatabase("../data/iTaps_test_data/mbtest1", 0, "ITAPS_C_1.0")
@@ -66,61 +54,52 @@ v=GetView3D()
 v.viewNormal = (-0.761903, 0.456228, -0.45974)
 v.viewUp = (0.198721, 0.840238, 0.50449)
 SetView3D(v)
-InitSetIds()
-silr.TurnOffSet(edgeSetId)
-silr.TurnOffSet(faceSetId)
-SetPlotSILRestriction(silr)
 Test("itaps_01")
-silr.TurnOnSet(edgeSetId)
-silr.TurnOnSet(faceSetId)
-SetPlotSILRestriction(silr)
+DeleteAllPlots()
+CloseDatabase("../data/iTaps_test_data/mbtest1")
 
-SetActivePlots((0,1))
-
-# turn on/off various combinations of EDGEs, FACEs, and REGIONs
-silr.TurnOffSet(edgeSetId)
-silr.TurnOffSet(regSetId)
-SetPlotSILRestriction(silr)
+# open a database with some interesting sets
+OpenDatabase("../data/iTaps_test_data/bricks.cub")
+AddPlot("Mesh","mesh")
 DrawPlots()
+ResetView()
+v=GetView3D()
+v.viewNormal = (-0.883969, 0.342986, 0.31774)
+v.viewUp = (0.313293, 0.938981, -0.141992)
+SetView3D(v)
 Test("itaps_02")
 
-silr.TurnOffSet(faceSetId)
-silr.TurnOnSet(edgeSetId)
+SetActivePlots((0,1))
+silr=SILRestriction()
+
+# turn off some curves 
+RestrictSetsInCategory(silr, "Curve", (0,1,2,3,4,5,6,7,8,9,10,11))
 SetPlotSILRestriction(silr)
-DrawPlots()
 Test("itaps_03")
 
-silr.TurnOffSet(edgeSetId)
-silr.TurnOnSet(regSetId)
+# turn off some surfaces
+RestrictSetsInCategory(silr, "Surface", (1,2,7))
 SetPlotSILRestriction(silr)
-DrawPlots()
 Test("itaps_04")
 
-silr.TurnOnSet(edgeSetId)
-silr.TurnOnSet(faceSetId)
-silr.TurnOffSet(regSetId)
-SetPlotSILRestriction(silr)
+AddPlot("Pseudocolor","zonetype")
 DrawPlots()
 Test("itaps_05")
 
 DeleteAllPlots()
-CloseDatabase("../data/iTaps_test_data/mbtest1")
+CloseDatabase("../data/iTaps_test_data/bricks.cub")
 
-OpenDatabase("../data/iTaps_test_data/bricks.cub", 0, "ITAPS_C_1.0")
-AddPlot("Mesh","mesh")
-AddPlot("Pseudocolor","zonetype")
-DrawPlots()
-ResetView()
-InitSetIds()
-SetActivePlots((0,1))
-silr.TurnOffSet(faceSetId)
-silr.TurnOffSet(regSetId)
-SetPlotSILRestriction(silr)
-DrawPlots()
-Test("itaps_06")
+#OpenDatabase("../data/iTaps_test_data/ptest.cub")
+#AddPlot("Mesh","mesh")
+#DrawPlots()
+#ResetView()
+#silr=SILRestriction()
+#RestrictSetsInCategory(silr, "Volume", (0,2,4,6))
+#SetPlotSILRestriction(silr)
+#Test("itaps_06")
 
 DeleteAllPlots()
-CloseDatabase("../data/iTaps_test_data/bricks.cub")
+CloseDatabase("../data/iTaps_test_data/ptest.cub")
 
 # test another mesh with some different element types
 OpenDatabase("../data/iTaps_test_data/globe_mats", 0, "ITAPS_C_1.0")
@@ -133,22 +112,18 @@ ResetView()
 Test("itaps_07")
 
 DeleteAllPlots()
-CloseDatabase("../data/iTaps_test_data/bricks.cub")
+CloseDatabase("../data/iTaps_test_data/globe_mats")
 
 # test another mesh with some different element types
 OpenDatabase("../data/iTaps_test_data/mixed-hex-pyr-tet", 0, "ITAPS_C_1.0")
 AddPlot("Mesh","mesh")
 AddPlot("Pseudocolor","zonetype")
 DrawPlots()
-InitSetIds()
 ResetView()
 v=GetView3D()
 v.viewNormal = (-0.707107, 0, 0.707107)
 SetView3D(v)
 Test("itaps_08")
-
-setIdsTxt="entity type set ids = %d %d %d"%(edgeSetId, faceSetId, regSetId)
-TestText("itaps_09", setIdsTxt)
 
 DeleteAllPlots()
 CloseDatabase("../data/iTaps_test_data/mixed-hex-pyr-tet")
