6960c97b4cec2006cb8427d12975471c700d8b0d
Mark Miller <miller86@llnl.gov>
2008-05-16 03:46:54 +0000

    adding test for rect database
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@4239 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ test/tests/databases/rect.py
@@ -0,0 +1,43 @@
+# ----------------------------------------------------------------------------
+#  CLASSES: nightly
+#
+#  Test Case:  rect.py 
+#
+#  Tests:      rect files 
+#
+#  Programmer: Mark C. Miller 
+#  Date:       May 15, 2008 
+#
+# ----------------------------------------------------------------------------
+
+# Turn off all annotation
+TurnOffAllAnnotations()
+
+# test the main mesh
+OpenDatabase("../src/databases/Rect/data/datafile.rect")
+AddPlot("Mesh","Mesh")
+DrawPlots()
+v=GetView3D()
+v.RotateAxis(1, 30.0)
+SetView3D(v)
+Test("rect_01")
+
+# test a variable
+AddPlot("Pseudocolor","pressure")
+DrawPlots()
+Test("rect_02")
+
+# test going forward in time
+TimeSliderNextState()
+Test("rect_03")
+
+# test restriction
+silr = SILRestriction()
+for i in range(silr.NumSets()):
+    if silr.SetName(i) == "domain2":
+        silr.TurnOffSet(i)
+#SetPlotSILRestriction(silr) ghost zone comm. bug 8612 
+DrawPlots()
+Test("rect_04")
+
+Exit()
