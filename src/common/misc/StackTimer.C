913e4a5b1bcdaeeaae2e457d81bcc5336c2c49e7
Kathleen Bonnell <bonnell2@llnl.gov>
2008-06-20 17:20:40 +0000

    Added script to retrieve Version and use it in installation scripts.  Added script to retrieve and store as #define's in visit-config.h the VERSION and SVN_REVISION.  Updated project files.  Enabled creation of MSVC8 projects in the project-generation tools.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@4502 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/common/misc/StackTimer.C
+++ src/common/misc/StackTimer.C
@@ -36,70 +36,30 @@
 *****************************************************************************/
 
 // ************************************************************************* //
-//                              StackTimer.h                                 //
+//                              StackTimer.C                                 //
 // ************************************************************************* //
 
-#ifndef STACK_TIMER_H
-#define STACK_TIMER_H
-
-#include <TimingsManager.h>
+#include <StackTimer.h>
 
 // ****************************************************************************
-//  Class: StackTimer
+//  StackTimer Constructor
 //
 //  Purpose:
-//      Simplified mechanism for timing blocks of code.  Create a StackTimer on
-//      the stack and it will record timing information when it goes out of
-//      scope.  For example:
-//
-//         if(doLongComplicatedTask) {
-//             StackTimer task_identifier("my task");
-//             this->Function();
-//         }
-//
-//      For short blocks, it may be easier to use the macro:
-//
-//         TimedCodeBlock("my task", this->Function());
 //
 //  Programmer: Tom Fogal
 //  Creation:   July 2, 2007
 //
 //  Modifications:
-//    Kathleen Bonnell, Fri Jun 20 08:15:27 PDT 2008
-//    Moved constructor and destructor to 'C' file, in order for this class'
-//    methods to be available on windows platform.
 //
 // ****************************************************************************
 
-class MISC_API StackTimer
+StackTimer::StackTimer(const char *msg) 
 {
-  public:
-    StackTimer(const char *msg); 
-    ~StackTimer() ;
+    this->message = std::string(msg);
+    this->timer_index = visitTimer->StartTimer();
+}
 
-  private:
-
-    // Unimplemented.  It does not make sense to have a timer without a
-    // message: there will be no way to identify it.
-    StackTimer();
-    // Unimplemented.  You should not be trying to pass timers around.
-    StackTimer(const StackTimer &);
-    const StackTimer & operator =(const StackTimer &);
-
-    // Unimplemented.  Allowing pointers would prevent automatic destruction
-    // and defeat the whole purpose.
-    static void *operator new(size_t);
-
-  private:
-
-    std::string                message;
-    int                        timer_index;
-};
-
-#define TimedCodeBlock(msg, block)            \
-    do {                                      \
-        StackTimer _generic_stack_timer(msg); \
-        block;                                \
-    } while(0)
-
-#endif /* STACK_TIMER_H */
+StackTimer::~StackTimer() 
+{
+    visitTimer->StopTimer(this->timer_index, this->message);
+}
