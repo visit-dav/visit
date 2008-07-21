021bf22c6851ae050c5c9327e4cdf986d04bdf9a
Tom Fogal <tfogal@sci.utah.edu>
2008-07-21 18:58:55 +0000

    * Clean up the python script, and do the looping over modes in the   shell.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@4640 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/sr-benchmark/scaling-pixels.sh
+++ test/sr-benchmark/scaling-pixels.sh
@@ -20,10 +20,32 @@ function doRun
     ${VISIT} -host ${host} ${TIMING} $@ ${SCRIPT}
 }
 
-doRun "-np 4 -geometry 1024x768+0+0"
-#doRun "-geometry 1100x825+0+0"
-#doRun "-geometry 1280x1024+0+0"
-#doRun "-geometry 1300x975+0+0"
-#doRun "-geometry 1400x1050+0+0"
-#doRun "-geometry 1500x1125+0+0"
-#doRun "-geometry 1600x1200+0+0"
+extract_data()
+{
+    if ! test -f ${DB} ; then
+        echo "'${DB}' does not exist, creating ..."
+        sqlite3 ${DB} < create.sql
+    fi
+    awk --assign pat=NM::Render -f timing.awk "$@" | sqlite3 ${DB}
+}
+
+geometry=("1024x768+0+0"
+          "1280x1024+0+0"
+          "1300x975+0+0"
+          "1400x1050+0+0"
+          "1500x1125+0+0"
+          "1600x1200+0+0")
+
+rm -f ./*\.timings
+
+for geom in ${geometry[@]} ; do
+    for procs in $(seq 8 8 32) ; do
+        for mode in "-icet" "" ; do
+            echo "mode is: ${mode}"
+            args="-np ${procs} -geometry ${geom} ${mode}"
+            doRun "${args}"
+            extract_data engine_par.000.timings
+            rm -f engine_par*timings
+        done
+    done
+done
