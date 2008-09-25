ae5e79c02279481d3e0d3c0e43f9b7419af6e2f3
Kathleen Bonnell <bonnell2@llnl.gov>
2008-09-25 21:19:14 +0000

    Fix NumNodes query to work correctly on mat-selected operator-that-removes-zones-applied data. Update release notes
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@5230 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/Queries/Queries/avtActualDataNumNodesQuery.C
+++ src/avt/Queries/Queries/avtActualDataNumNodesQuery.C
@@ -41,6 +41,9 @@
 // ************************************************************************* //
 
 #include <avtActualDataNumNodesQuery.h>
+#include <avtCondenseDatasetFilter.h>
+#include <avtOriginatingSource.h>
+#include <avtSourceFromAVTDataset.h>
 
 
 // ****************************************************************************
@@ -50,11 +53,16 @@
 //  Creation:   February 18, 2004 
 //
 //  Modifications:
+//    Kathleen Bonnell, Thu Sep 25 13:37:48 PDT 2008
+//    Initialize condense filter.
 //
 // ****************************************************************************
 
 avtActualDataNumNodesQuery::avtActualDataNumNodesQuery() : avtNumNodesQuery() 
 {
+    condense = new avtCondenseDatasetFilter;
+    condense->KeepAVTandVTK(true);
+    condense->BypassHeuristic(true);
 }
 
 
@@ -65,8 +73,45 @@ avtActualDataNumNodesQuery::avtActualDataNumNodesQuery() : avtNumNodesQuery()
 //  Creation:   February 18, 2004 
 //
 //  Modifications:
+//    Kathleen Bonnell, Thu Sep 25 13:37:48 PDT 2008
+//    Delete condense filter.
 //
 // ****************************************************************************
 avtActualDataNumNodesQuery::~avtActualDataNumNodesQuery() 
 {
+    if (condense != NULL)
+    {
+        delete condense;
+        condense = NULL;
+    }
+}
+
+
+// ****************************************************************************
+//  Method: avtActualDataNumNodesQuery::ApplyFilters
+//
+//  Purpose:
+//    Applies the condense filter to input and returns the filter's output. 
+//
+//  Programmer: Kathleen Bonnell
+//  Creation:   September 23, 2008 
+//
+//  Modifications:
+//
+// ****************************************************************************
+
+avtDataObject_p
+avtActualDataNumNodesQuery::ApplyFilters(avtDataObject_p inData)
+{
+    avtContract_p contract = 
+        inData->GetOriginatingSource()->GetGeneralContract();
+
+    avtDataset_p ds;
+    CopyTo(ds, inData);
+    avtSourceFromAVTDataset termsrc(ds);
+    avtDataObject_p obj = termsrc.GetOutput();
+    condense->SetInput(obj);
+    avtDataObject_p retObj = condense->GetOutput();
+    retObj->Update(contract);
+    return retObj;
 }
