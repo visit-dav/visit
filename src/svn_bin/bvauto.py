91a7134fbf6ecb2b013fe21e688c3ecd63477a6a
Cyrus Harrison <cyrush@llnl.gov>
2008-12-10 22:05:19 +0000

    completed first usable version of bvauto.py w/ remote posting
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@5816 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/svn_bin/bvauto.py
+++ src/svn_bin/bvauto.py
@@ -16,47 +16,120 @@
 import sys,os,time,datetime,glob,socket
 
 def sexe(cmd):
+    """
+    Runs a shell command.
+    """
     print "[exe: %s]" % cmd
     os.system(cmd)
+    
+def parse_args():
+    """ 
+    Handles argument parsing for bvauto.
+    """
+    res = {}
+    nargs = len(sys.argv)
+    if nargs < 2:
+        print "usage:"
+        print " Test build_visit on current machine:"
+        print "  bvauto.py [bvauto.input]"
+        print " Test build_visit on current machine and post results"
+        print "  bvauto.py [bvauto.input] <--post> [result root] [result group]"
+        print "  example: bvauto.py bvauto.input --post cyrush@davinci.nersc.gov:/path/to/results/ visitdev"
+        print " Reindex Result Root (local machine only):"
+        print "  bvauto.py <--reindex> [result root] [result group]"
+        print "  example: bvauto.py --reindex /path/to/results/ visitdev"
+        sys.exit(-1)
+    if sys.argv[1] == "--reindex":
+        if nargs <4:
+            print "usage:"    
+            print "  bvauto.py <--reindex> [result root] [result group]"
+            sys.exit(-1)
+        res["op"] = "reindex"
+        res["result_root"] = os.path.abspath(sys.argv[2])
+        res["result_group"] = sys.argv[3]
+    else:
+        res["op"] = "test"
+        res["input_file"] = sys.argv[1]
+        res["mode"] = "svn" # support wget later ? 
+        res["post"] = False
+        res["cleanup"] = False
+        if nargs > 2:
+            if sys.argv[2] != "--post" or nargs < 5:
+                print "usage:"
+                print "  bvauto.py [bvauto.input] <--post> [result root] [result group]"
+                sys.exit(-1)
+            res["post"] = True
+            res["cleanup"] = True
+            res["result_root"] = sys.argv[3]
+            res["result_group"] = sys.argv[4]
+    if "result_root" in res.keys():
+        if res["result_root"][-1]== "/":
+            res["result_root"]=res["result_root"][:-1]
+    return res
 
+def get_current_time():
+    """
+    Returns human readable string of current time.
+    """
+    t = datetime.datetime.now()
+    return "%d-%02d-%02d.%02d-%02d-%02d" % (t.year,t.month,t.day,t.hour,t.minute,t.second)
+    
 def get_svn_root():
+    """
+    Constructs a svn root for the VisIt repo using env var SVN_NERSC_NAME.
+    """
     if not "SVN_NERSC_NAME" in os.environ.keys():
-        sys.stderr.write("<Error: Could not obtain nersc svn user name from enviroment. Make sure SVN_NERSC_NAME is set!>\n")
+        sys.stderr.write("<Error: Could not obtain nersc svn user name from environment.")
+        sys.stderr.write("Make sure SVN_NERSC_NAME is set!>\n")
         sys.exit(-1)
     return "svn+ssh://%s@svn.nersc.gov/svn/visit/trunk/src" % os.environ["SVN_NERSC_NAME"]
     
 def create_run_dir(mode):
-    t = datetime.datetime.now()
+    """
+    Creates a temporary directory for running a set of build_visit tests.
+    """
     host = socket.gethostname()
-    res = "bv.auto.%s.%d-%02d-%02d.%02d-%02d-%02d.%s" % (mode,t.year,t.month,t.day,t.hour,t.minute,t.second,host)
+    tstr = get_current_time()
+    res = "bv.auto.%s.%s.%s" % (mode,tstr,host)
     if not os.path.exists(res):
         sexe("mkdir %s" % res)
     return res
 
-def parse_tests():
+def parse_tests(input_file):
+    """
+    Parses test commands from an input file.
+    Extracts one command per line.
+    """
     res = []
-    for l in open("bvauto.input").readlines():
+    for l in open(input_file).readlines():
         res.append(l.strip())
     return res
 
-def check_success(rfile):
+def analyze_result(rfile):
+    """
+    Checks a result file for host, architecture, and success.
+    """
     data = open(rfile).read()
+    # get host, arch and result color
+    host = data[data.find('host="')+len('host="'):]
+    host = host[:host.find('"')]
+    arch = data[data.find('arch="')+len('arch="'):]
+    arch = arch[:arch.find('"')]
     if data.find("failure") >=0:
-        return "red"
-    return "green"
-
-def rsort(a,b):
-    r = cmp(a,b)
-    if r == 0:
-        return r
-    elif a == -1:
-        return 1
+        result = "red"
     else:
-        return -1
-    
-def update_index(result_root):
+        result = "green"
+    return host,arch,result
+
+def reindex_results(result_root,result_group):
+    """
+    Rebuilds the index.html file at a given result_root and sets 
+    group read permisions.
+    """
+    print "[bvauto: reindex %s %s]" % (result_root,result_group)
     rdirs = glob.glob("%s/bv.auto.*/*.xml" % result_root)
-    rdirs.sort(rsort)
+    rdirs.sort()
+    rdirs.reverse()
     res = "<html>\n"
     res += "<head>\n"
     res += "<title>\n"
@@ -65,27 +138,40 @@ def update_index(result_root):
     res += '<link rel="stylesheet" type="text/css" href="bvtest.css" title="Style">\n'
     res += "</head>\n"
     res += '<table class="wikitable">\n'
-    res +='<tr class="rh"><td>Updated: %s</td></tr>\n' % time.asctime(time.gmtime())
+    res +='<tr><td colspan=3><b>bvauto results: updated %s</b></td></tr>\n' % get_current_time()
     if len(rdirs) == 0:
         res += '<tr class="red"><td>No results?</td></tr>\n'
+    cdate = ""
     for r in rdirs:
-        val   = check_success(r)
+        host,arch,rval = analyze_result(r)
+        # date is currently between 3th & 3th "."
         rfile = r[r.rfind("/")+1:]
-        res += '<tr class="%s"><td><a href="%s">%s</a></td></tr>\n' %(val,r,rfile)
+        rdate = rfile.split(".")[3]
+        if cdate != rdate:
+            cdate = rdate
+            res += '<tr class="rh"><td colspan=3>%s</td></tr>\n' % cdate
+        res += '<tr class="%s">' % rval 
+        res += '<td>%s</td><td>%s</td>' % (host,arch)
+        res += '<td><a href="%s">%s</a></td></tr>\n' %(r,rfile)
     res +="</table>\n"
     res+="</html>\n"
     f = open("%s/index.html" % result_root,"w")
     f.write(res)
     f.close()
-
-def run(result_root,result_group,mode):
+    sexe("chgrp -R %s %s " % (result_group,result_root))
+    sexe("chmod -R +rx %s " % result_root)
+   
+def run_tests(input_file,mode):
+    """
+    Rebuilds the index.html file at a given result_root and sets 
+    group read permisions.
+    """
+    print "[bvauto: test %s %s]" % (input_file,mode)
     svn_root = get_svn_root()
     run_dir = create_run_dir(mode)
-    libcmds = parse_tests()
-    print "[Result Root: %s]" % result_root
+    libcmds = parse_tests(input_file)
     print "[Svn Root:    %s]" % svn_root
     print "[Run Dir:     %s]" % run_dir
-    result_dir="%s/%s" % (result_root,run_dir)
     result_file="%s.xml" % run_dir
     cwd = os.getcwd()
     os.chdir(run_dir);
@@ -100,31 +186,69 @@ def run(result_root,result_group,mode):
     bvt = bvtest.BuildTest( "_bv_output",libcmds,"./build_visit",mode)
     bvt.execute()
     open(res_file,"w").write(bvt.result_xml())
-    
-    # copy results to public dir (xml,xsl,css,bv_log_*txt)
-    if not os.path.exists(result_dir):
-        sexe("mkdir %s/" % result_dir)
-    sexe("cp *xml *xsl _bv_output/bv_log*.txt *css %s/" % result_dir)
-    sexe("cp *css %s/" % result_root)
     os.chdir(cwd);
-    sexe("rm -rf %s/" % run_dir)
-    update_index(result_root)
-    sexe("chgrp -R %s %s " % (result_group,result_root))
-    sexe("chmod -R +rx %s " % result_root)
-    
-if __name__ == "__main__":
-    nargs = len(sys.argv)
-    if nargs < 2:
-        print "usage: bvauto.py [result path]"
-        sys.exit(-1)
-    result_dir = os.path.abspath(sys.argv[1])
-    result_group = sys.argv[2]
-    if result_dir[-1] == "/":
-        result_dir[:-1]
-    mode = "svn"
-    if nargs ==4:
-        mode = sys.argv[3]
-    run(result_dir,result_group,mode)
+    return run_dir
 
+def post(pattern,result_dir,result_group):
+    """
+    Posts a set of files from the current directory to a result directory.
+    Handles posting to local file system (via cp) or remote system (via ssh/scp).
+    """
+    if result_dir.find("@") > 0 and result_dir.find(":") > 0:
+        print "[Posting to remote path]"
+        remote_host, remote_path = result_dir.split(":")
+        sexe("ssh %s mkdir %s" % (remote_host,remote_path))
+        sexe("scp %s %s" % (pattern,result_dir))
+        sexe("ssh %s chgrp -R %s %s" % (remote_host,result_group,remote_path))
+        sexe("ssh %s chmod -R g+rx %s" % (remote_host,remote_path))
+    else:
+        print "[Posting to local path]"
+        # copy results to public dir (xml,xsl,css,bv_log_*txt)
+        if not os.path.exists(result_dir):
+            sexe("mkdir %s/" % result_dir)
+        sexe("cp %s %s" % (pattern,result_dir))
+        sexe("chgrp -R %s %s"  % (result_group,result_dir))
+        sexe("chmod -R g+rx %s"  % result_dir)
+
+def post_results(run_dir,result_root,result_group):
+    """
+    Posts the result of a test run.
+    """
+    result_dir="%s/%s" % (result_root,run_dir)
+    cwd = os.getcwd()
+    os.chdir(run_dir);
+    post("*xml *xsl bv_log*.txt *css",result_dir,result_group) 
+    os.chdir(cwd);
+    
+def cleanup_run_dir(run_dir):
+    """
+    Removes temporary run directory.
+    """
+    sexe("rm -rf %s/" % run_dir)
+    
+def main():
+    """
+    Main driver routine.
+    """
+    args = parse_args()
+    if args["op"] == "reindex":
+        result_root  = args["result_root"]
+        result_group = args["result_group"]
+        reindex_results(result_root,result_group)
+    elif args["op"] == "test":
+        input_file = args["input_file"]
+        mode = args["mode"]
+        run_dir = run_tests(input_file,mode)
+        if args["post"] == True:
+            result_root  = args["result_root"]
+            result_group = args["result_group"]
+            post_results(run_dir,result_root,result_group)
+        if args["cleanup"]:
+            cleanup_run_dir(run_dir)
+    else:
+        print args
+
+if __name__ == "__main__":
+    main()
 
 
