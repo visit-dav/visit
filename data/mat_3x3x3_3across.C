a5f53d96de8c6da2360afc1abde6fa819a113fb0
Jeremy Meredith <jsmeredith@gmail.com>
2009-02-13 16:30:39 +0000

    Adding 3D (3x3x3) 3-material test case. Making 2D 3x3 3-material test case material numbers more complex.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@6282 18c085ea-50e0-402c-830e-de6fd14e8384

--- data/mat_3x3x3_3across.C
+++ data/mat_3x3x3_3across.C
@@ -13,6 +13,8 @@
  *
  *
  * Modifications:
+ *    Jeremy Meredith, Fri Feb 13 10:23:34 EST 2009
+ *    Copied from old silo vob, updated, and renamed into visit source.
  *
  *-----------------------------------------------------------------------*/
 #include <stdlib.h>
@@ -21,27 +23,49 @@
 
 #define zx 3
 #define zy 3
+#define zz 3
 
 #define nx 4
 #define ny 4
+#define nz 4
 
-float x[nx*ny]={-1, 0, 1, 2,  -1, 0, 1, 2,  -1, 0, 1, 2,  -1, 0, 1, 2};
-float y[nx*ny]={-1,-1,-1,-1,   0, 0, 0, 0,   1, 1, 1, 1,   2, 2, 2, 2};
+float x[nx*ny*nz]={-1, 0, 1, 2,  -1, 0, 1, 2,  -1, 0, 1, 2,  -1, 0, 1, 2,
+                   -1, 0, 1, 2,  -1, 0, 1, 2,  -1, 0, 1, 2,  -1, 0, 1, 2,
+                   -1, 0, 1, 2,  -1, 0, 1, 2,  -1, 0, 1, 2,  -1, 0, 1, 2,
+                   -1, 0, 1, 2,  -1, 0, 1, 2,  -1, 0, 1, 2,  -1, 0, 1, 2};
+float y[nx*ny*nz]={-1,-1,-1,-1,   0, 0, 0, 0,   1, 1, 1, 1,   2, 2, 2, 2,
+                   -1,-1,-1,-1,   0, 0, 0, 0,   1, 1, 1, 1,   2, 2, 2, 2,
+                   -1,-1,-1,-1,   0, 0, 0, 0,   1, 1, 1, 1,   2, 2, 2, 2,
+                   -1,-1,-1,-1,   0, 0, 0, 0,   1, 1, 1, 1,   2, 2, 2, 2};
+float z[nx*ny*nz]={-1,-1,-1,-1,  -1,-1,-1,-1,  -1,-1,-1,-1,  -1,-1,-1,-1, 
+                    0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
+                    1, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 1,
+                    2, 2, 2, 2,   2, 2, 2, 2,   2, 2, 2, 2,   2, 2, 2, 2};
 
-int nmat=4;
-int matnos[] = {7,1,3,2};
+int nmat=7;
+int matnos[] = {15,7,1,9,3,6,2};
 
-int     matlist[nx*ny]    = { 1,-1,3,  1,-4,3,  1,-7,3 };
-int     mix_mat [100]     = { 1,2,3,   1,2,3,  1,2,3  };
-int     mix_next[100]     = { 2,3,0,   5,6,0,  8,9,0 };
+int     matlist[nx*ny*nz] = { 1,-1,3,  1,-4,3,  1,-7,3,
+                              1,-10,3, 1,-13,3, 1,-16,3,
+                              1,-19,3, 1,-22,3, 1,-25,3 };
+int     mix_mat [100]     = { 1,2,3,   1,2,3,  1,2,3, 
+                              1,2,3,   1,2,3,  1,2,3,
+                              1,2,3,   1,2,3,  1,2,3  };
+int     mix_next[100]     = { 2,3,0,   5,6,0,  8,9,0,
+                              11,12,0, 14,15,0, 17,18,0,
+                              20,21,0, 23,24,0, 26,27,0 };
 #define M1 .2
 #define M2 .6
 #define M3 .2
 
-float   mix_vf  [100]     = { M1,M2,M3, M1,M2,M3, M1,M2,M3 };
+float   mix_vf  [100]     = { M1,M2,M3, M1,M2,M3, M1,M2,M3,
+                              M1,M2,M3, M1,M2,M3, M1,M2,M3,
+                              M1,M2,M3, M1,M2,M3, M1,M2,M3 };
 
-int     mix_zone[100]     = {  2, 2, 2,  5, 5, 5,  8, 8, 8 };
-int     mixlen = 9;
+int     mix_zone[100]     = {  2, 2, 2,  5, 5, 5,  8, 8, 8,
+                              11,11,11, 14,14,14, 17,17,17,
+                              20,20,20, 23,23,23, 26,26,26 };
+int     mixlen = 27;
 
 int     dims[3];
 
@@ -53,7 +77,7 @@ int     dims[3];
 int main(int argc, char **argv) {
     DBfile *db;
     int            i, driver = DB_HDF5;
-    char          *filename = "mat_3x3_3across.silo";
+    char          *filename = "mat_3x3x3_3across.silo";
     char  *coordnames[3];
     float *coord[3];
 
@@ -88,23 +112,23 @@ int main(int argc, char **argv) {
 
   coord[0] = x;
   coord[1] = y;
-  coord[2] = NULL;
+  coord[2] = z;
 
   dims[0]=nx;
   dims[1]=ny;
-  dims[2]=1;
+  dims[2]=nz;
 
   db=DBCreate(filename, DB_CLOBBER, DB_LOCAL,
               "Mixed zone 2d test", driver);
 
-  DBPutQuadmesh(db, "mesh", coordnames, coord, dims, 2, 
+  DBPutQuadmesh(db, "mesh", coordnames, coord, dims, 3, 
                 DB_FLOAT, DB_NONCOLLINEAR, NULL);
 
   dims[0]=zx;
   dims[1]=zy;
-  dims[2]=1;
+  dims[2]=zz;
 
-  DBPutMaterial(db, "material", "mesh", nmat, matnos, matlist, dims, 2, 
+  DBPutMaterial(db, "material", "mesh", nmat, matnos, matlist, dims, 3, 
                 mix_next, mix_mat, mix_zone, mix_vf, mixlen, DB_FLOAT, NULL);
 
 
