7fde0d9b09934c5b434a7dcec2a45e6181efbe18
Jeremy Meredith <jsmeredith@gmail.com>
2009-02-23 23:13:41 +0000

    Adding simplistic Edge operator (disabled by default).
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@6356 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ src/operators/Edge/PyEdgeAttributes.h
@@ -0,0 +1,60 @@
+/*****************************************************************************
+*
+* Copyright (c) 2000 - 2008, Lawrence Livermore National Security, LLC
+* Produced at the Lawrence Livermore National Laboratory
+* LLNL-CODE-400142
+* All rights reserved.
+*
+* This file is  part of VisIt. For  details, see https://visit.llnl.gov/.  The
+* full copyright notice is contained in the file COPYRIGHT located at the root
+* of the VisIt distribution or at http://www.llnl.gov/visit/copyright.html.
+*
+* Redistribution  and  use  in  source  and  binary  forms,  with  or  without
+* modification, are permitted provided that the following conditions are met:
+*
+*  - Redistributions of  source code must  retain the above  copyright notice,
+*    this list of conditions and the disclaimer below.
+*  - Redistributions in binary form must reproduce the above copyright notice,
+*    this  list of  conditions  and  the  disclaimer (as noted below)  in  the
+*    documentation and/or other materials provided with the distribution.
+*  - Neither the name of  the LLNS/LLNL nor the names of  its contributors may
+*    be used to endorse or promote products derived from this software without
+*    specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT  HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR  IMPLIED WARRANTIES, INCLUDING,  BUT NOT  LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND  FITNESS FOR A PARTICULAR  PURPOSE
+* ARE  DISCLAIMED. IN  NO EVENT  SHALL LAWRENCE  LIVERMORE NATIONAL  SECURITY,
+* LLC, THE  U.S.  DEPARTMENT OF  ENERGY  OR  CONTRIBUTORS BE  LIABLE  FOR  ANY
+* DIRECT,  INDIRECT,   INCIDENTAL,   SPECIAL,   EXEMPLARY,  OR   CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT  LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF  USE, DATA, OR PROFITS; OR  BUSINESS INTERRUPTION) HOWEVER
+* CAUSED  AND  ON  ANY  THEORY  OF  LIABILITY,  WHETHER  IN  CONTRACT,  STRICT
+* LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY  WAY
+* OUT OF THE  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+* DAMAGE.
+*
+*****************************************************************************/
+
+#ifndef PY_EDGEATTRIBUTES_H
+#define PY_EDGEATTRIBUTES_H
+#include <Python.h>
+#include <EdgeAttributes.h>
+
+//
+// Functions exposed to the VisIt module.
+//
+void           PyEdgeAttributes_StartUp(EdgeAttributes *subj, void *data);
+void           PyEdgeAttributes_CloseDown();
+PyMethodDef *   PyEdgeAttributes_GetMethodTable(int *nMethods);
+bool           PyEdgeAttributes_Check(PyObject *obj);
+EdgeAttributes *  PyEdgeAttributes_FromPyObject(PyObject *obj);
+PyObject *      PyEdgeAttributes_New();
+PyObject *      PyEdgeAttributes_Wrap(const EdgeAttributes *attr);
+void           PyEdgeAttributes_SetParent(PyObject *obj, PyObject *parent);
+void           PyEdgeAttributes_SetDefaults(const EdgeAttributes *atts);
+std::string    PyEdgeAttributes_GetLogString();
+std::string    PyEdgeAttributes_ToString(const EdgeAttributes *, const char *);
+
+#endif
+
