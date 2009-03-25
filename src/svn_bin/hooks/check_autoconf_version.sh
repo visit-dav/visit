4952e04460f32dd61e292deee2977cec664040cb
Mark Miller <miller86@llnl.gov>
2009-03-25 05:07:13 +0000

    fixing autoconf version hook where dependent files are concerned
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@6690 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/svn_bin/hooks/check_autoconf_version.sh
+++ src/svn_bin/hooks/check_autoconf_version.sh
@@ -28,14 +28,18 @@
 #   Adjusted file loop to deal with fact that FLIST file now contains
 #   both status chars and filename. Replaced ${<VARNAME>} command refs
 #   with just the commands themselves.
+#
+#   Mark C. Miller, Tue Mar 24 22:02:44 PDT 2009
+#   Changed logic to simply record the configure file in one variable
+#   and all other files that serve as input to configure in another
+#   variable.
 ##############################################################################
 REPOS="$1"
 TXN="$2"
 FLIST="$3"
 
 theConfigureFile=""
-theConfigureDotInFile=""
-m4_file=""
+configureInputFiles=""
 while read fline; do
 
     #
@@ -48,30 +52,32 @@ while read fline; do
     # We're looking for only these specific files
     # for this hook.
     case $fname in
-        *src/configure.in)
-            theConfigureDotInFile=$fname
-            ;;
-        *src/*m4)
-            m4_file=$fname
-            ;;
         *src/configure)
             theConfigureFile=$fname
+            ;;
+        *src/configure.in)
+        *src/*m4)
+        *src/ac/*)
+            configureInputFiles="$configureInputFiles $fname"
             ;;
     esac
 
 done < $FLIST
 
-# Check that both configure.in and configure are getting updated
-if test -n "$theConfigureDotInFile" || test -n "$m4_file"; then
-    if test -z "$theConfigureFile"; then
-        log "Attempt to update \"configure.in\" without also updating \"configure\""
-        exit 1
-    fi
-elif test -n "$theConfigureFile"; then
-    if test -z "$theConfigureDotInFile" && test -z "$m4_file" ; then
-        log "Attempt to update \"configure\" without also updating \"configure.in\""
-        exit 1
-    fi
+# Check that if any files that serve as input to configure are getting
+# updated, then configure itself is also getting updated. 
+if test -n "$configureInputFiles" && test -z "$theConfigureFile"; then
+    log "Attempt to commit changes to $configureInputFiles"
+    log "without also committing changes to \"configure\""
+    exit 1
+fi
+
+# Check that if configure itself is getting updated, then at least
+# one of the files it depends on is also getting updated.
+if test -z "$configureInputFiles" && test -n "$theConfigureFile"; then
+    log "Attempt to commit changes to $theConfigureFile"
+    log "without also committing changes to any of its input files."
+    exit 1
 fi
 
 # get and check autoconf version number
