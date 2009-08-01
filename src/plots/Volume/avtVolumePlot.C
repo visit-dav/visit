2eb1e63bca137eed7ecdef871c93057deecd53e3
Tom Fogal <tfogal@sci.utah.edu>
2009-08-01 17:14:10 +0000

    Do distributed resampling in the volume plot.
    The issue before was that we couldn't use shaders via the software
    path.  With the updated Mesa, now we can.
    
    I'm hoping that this is the start along a series of commits which
    will allow me to remove the terrible ugly + hacky
    "OnlyRootNodeHasData" in the NetworkManager.
    
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@8021 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/plots/Volume/avtVolumePlot.C
+++ src/plots/Volume/avtVolumePlot.C
@@ -518,6 +518,9 @@ avtVolumePlot::ApplyOperators(avtDataObject_p input)
 //    Hank Childs, Wed Dec 31 13:47:37 PST 2008
 //    Renamed ResampleAtts to InternalResampleAtts.
 //
+//    Tom Fogal, Sun Jul 26 15:21:25 MDT 2009
+//    Enable distributed resampling.
+//
 // ****************************************************************************
 
 avtDataObject_p
@@ -554,6 +557,7 @@ avtVolumePlot::ApplyRenderingTransformation(avtDataObject_p input)
     else
     {
         InternalResampleAttributes resampleAtts;
+        resampleAtts.SetDistributedResample(true);
         resampleAtts.SetTargetVal(atts.GetResampleTarget());
         resampleAtts.SetUseTargetVal(true);
         resampleAtts.SetPrefersPowersOfTwo(
