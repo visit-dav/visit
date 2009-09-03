3880975869629abc662ed74039af6dd26bdd480f
Gunther Weber <ghweber@lbl.gov>
2009-09-03 17:31:45 +0000

    Fixes for building/distributing on Mac.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@8306 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ src/svn_bin/changeQtInstallName.sh
@@ -0,0 +1,20 @@
+QtBaseDir=$1
+
+QtFrameworks="QtCore QtGui QtNetwork QtOpenGL QtScript QtSql QtSvg QtXml"
+for QtFW in $QtFrameworks; do
+    echo $QtFW;
+    filename="$QtBaseDir/lib/${QtFW}.framework/${QtFW}"
+    debugFilename="${filename}_debug"
+    oldSOName=$(otool -D $filename | tail -n -1)
+    oldDebugSOName=$(otool -D $debugFilename | tail -n -1)
+    internalFWPath=$(echo $oldSOName | sed -e 's/.*\.framework\///')
+    internalDebugFWPath=$(echo $oldDebugSOName | sed -e 's/.*\.framework\///')
+    newSOName="@executable_path/../lib/${QtFW}.framework/$internalFWPath"
+    newDebugSOName="@executable_path/../lib/${QtFW}.framework/$internalDebugFWPath"
+    install_name_tool -id $newSOName $filename
+    install_name_tool -id $newDebugSOName $debugFilename
+    for otherQtFW in $QtFrameworks; do
+        install_name_tool -change $oldSOName $newSOName $QtBaseDir/lib/${otherQtFW}.framework/${otherQtFW}
+        install_name_tool -change $oldSOName $newSOName $QtBaseDir/lib/${otherQtFW}.framework/${otherQtFW}_debug
+    done
+done
