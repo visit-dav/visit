6e7e4302ab9e999334d086fdbbed9b9b8b8ca3ab
Gunther Weber <ghweber@lbl.gov>
2010-01-05 00:05:21 +0000

    Updates for fry.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@9313 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/fry.lbl.gov.cmake
+++ src/config-site/fry.lbl.gov.cmake
@@ -1,4 +1,4 @@
-#/Users/ghweber/devel/visit_3rdparty/cmake/2.6.4/i386-apple-darwin10_gcc-4.2/bin/cmake
+#/opt/local/bin/cmake
 
 ##
 ## ../visit_svn/trunk/src/svn_bin/build_visit generated host.conf
@@ -21,22 +21,12 @@ VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.5/${VISITARCH})
 ##
 ## Specify the location of the vtk include files and libraries.
 ##
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0c/${VISITARCH}/lib/vtk-5.0)
-
-##
-## Specify the location of cmake.
-##
-
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0d/${VISITARCH}/lib/vtk-5.0)
 
 ##
 ## Specify the location of the qt include files and libraries.
 ##
 VISIT_OPTION_DEFAULT(VISIT_QT_BIN /opt/local/libexec/qt4-mac/bin)
-#VISIT_QT_BIN=$VISITHOME/qt/4.6.0-beta1/$VISITARCH/bin
-#not done -- QT_INCLUDE = /opt/local/libexec/qt4-mac/include
-#QT_INCLUDE=$VISITHOME/qt/4.6.0-beta1/$VISITARCH/include
-#not done -- QT_LIB = /opt/local/libexec/qt4-mac/lib
-#QT_LIB=$VISITHOME/qt/4.6.0-beta1/$VISITARCH/lib
 
 ##
 ## Specify the location of the python include and libraries.
@@ -52,10 +42,11 @@ SET(VISIT_CXX_COMPILER g++)
 SET(VISIT_CXX_FLAGS "-O2 -fno-common -fexceptions -D__USE_ISOC99")
 
 ##
-##  Parallel Args
+##  Parallel flags
 ##
-SET(VISIT_C_FLAGS "-D_REENTRANT")
-SET(VISIT_MPI_C_FLAGS "-DOMPI_SKIP_MPICXX")
+SET(VISIT_MPI_C_FLAGS "-D_REENTRANT -DOMPI_SKIP_MPICXX")
+SET(VISIT_MPI_CXX_FLAGS "-D_REENTRANT -DOMPI_SKIP_MPICXX")
+SET(VISIT_MPI_LIBS "-Wl,-u,_munmap -Wl,-multiply_defined,suppress -lmpi -lopen-rte -lopen-pal -lutil")
 
 ##
 ## Database reader plugin support libraries
---
+++ src/config-site/fry.local.cmake
@@ -0,0 +1,1 @@
+fry.lbl.gov.cmake
