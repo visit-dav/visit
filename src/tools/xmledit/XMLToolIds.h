837fe242114e1b4ad9c784e168232f22c037c741
David Camp <dcamp@lbl.gov>
2010-01-05 02:28:26 +0000

    Change the code use xml2cmake on both Windows and Unix/Linux systems. Renamed Makefile tab to CMake.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@9316 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/tools/xmledit/XMLToolIds.h
+++ src/tools/xmledit/XMLToolIds.h
@@ -7,7 +7,7 @@ enum
     ID_XML2JAVA = 1,
     ID_XML2PYTHON = 2,
     ID_XML2WINDOW = 3,
-    ID_XML2MAKEFILE = 4,
+    ID_XML2CMAKE = 4,
     ID_XML2INFO = 5,
     ID_XML2AVT = 6,
     ID_XML_MAX = 7,
