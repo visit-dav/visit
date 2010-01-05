dea85e72bc7e84223df7d03167f28ec3754f6807
Gunther Weber <ghweber@lbl.gov>
2010-01-05 18:36:19 +0000

    Updated config after rebuilding libraries.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@9322 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/fry.lbl.gov.cmake
+++ src/config-site/fry.lbl.gov.cmake
@@ -1,9 +1,8 @@
 #/opt/local/bin/cmake
-
 ##
-## ../visit_svn/trunk/src/svn_bin/build_visit generated host.conf
-## created: Tue Oct 20 15:05:35 PDT 2009
-## system: Darwin parallax.lbl.gov 10.0.0 Darwin Kernel Version 10.0.0: Fri Jul 31 22:47:34 PDT 2009; root:xnu-1456.1.25~1/RELEASE_I386 i386
+## ../visit_svn/trunk/src/svn_bin/build_visit generated host.cmake
+## created: Mon Jan  4 19:13:25 PST 2010
+## system: Darwin fry.local 10.2.0 Darwin Kernel Version 10.2.0: Tue Nov  3 10:37:10 PST 2009; root:xnu-1486.2.11~1/RELEASE_I386 i386
 ## by: ghweber
 
 ##
@@ -14,22 +13,23 @@ SET(VISITARCH i386-apple-darwin10_gcc-4.2)
 SET(VISIT_VERBOSE_MAKEFILE TRUE)
 
 ##
-## Specify the location of the mesa include files and libraries.
+## Specify the location of Mesa.
 ##
 VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.5/${VISITARCH})
 
 ##
-## Specify the location of the vtk include files and libraries.
+## Specify the location of VTK.
 ##
 VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0d/${VISITARCH}/lib/vtk-5.0)
 
 ##
-## Specify the location of the qt include files and libraries.
+## Specify the Qt4 binary dir. 
+## (qmake is used to locate & setup Qt4 dependencies)
 ##
 VISIT_OPTION_DEFAULT(VISIT_QT_BIN /opt/local/libexec/qt4-mac/bin)
 
 ##
-## Specify the location of the python include and libraries.
+## Specify the location of Python.
 ##
 VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR /usr)
 
@@ -46,6 +46,7 @@ SET(VISIT_CXX_FLAGS "-O2 -fno-common -fexceptions -D__USE_ISOC99")
 ##
 SET(VISIT_MPI_C_FLAGS "-D_REENTRANT -DOMPI_SKIP_MPICXX")
 SET(VISIT_MPI_CXX_FLAGS "-D_REENTRANT -DOMPI_SKIP_MPICXX")
+SET(VISIT_PARALLEL_LINKER_FLAGS "-Wl,-undefined,dynamic_lookup")
 SET(VISIT_MPI_LIBS "-Wl,-u,_munmap -Wl,-multiply_defined,suppress -lmpi -lopen-rte -lopen-pal -lutil")
 
 ##
@@ -54,66 +55,21 @@ SET(VISIT_MPI_LIBS "-Wl,-u,_munmap -Wl,-multiply_defined,suppress -lmpi -lopen-r
 ##############################################################
 
 ##
-## AdvIO
-##
-
-##
 ## Boxlib
 ##
 VISIT_OPTION_DEFAULT(VISIT_BOXLIB2D_DIR ${VISITHOME}/boxlib/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_BOXLIB3D_DIR ${VISITHOME}/boxlib/${VISITARCH})
 
 ##
-## CFITSIO
-##
-
-##
-## CGNS
-##
-
-##
-## Exodus
-##
-
-##
-## GDAL
-##
-
-##
-## H5Part
-##
-
-##
-## FastBit
-##
-
-##
-## HDF4
-##
-
-##
 ## HDF5
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.3/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.2/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz)
-
-##
-## Mili
-##
-
-##
-## NetCDF
-##
 
 ##
 ## SZIP
 ##
 SET(SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
-
-##
-## CCMIO
-##
-##
 
 ##
 ## Silo
