4b2d83a6f21608465394a4330d889cf66612bef1
Gunther Weber <ghweber@lbl.gov>
2010-01-06 03:05:38 +0000

    Updated franklin config file.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@9333 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/franklin.nersc.gov.cmake
+++ src/config-site/franklin.nersc.gov.cmake
@@ -1,6 +1,5 @@
-#/usr/common/graphics/installs/visit_3rdparty/cmake/2.6.4/linux-x86_64_gcc-4.2.0/bin/cmake
+#/usr/common/graphics/installs/visit_3rdparty/cmake/2.6.4/linux-x86_64_gcc-4.4/bin/cmake
 
-# 1. YOU MUST UPGRADE TO cmake 2.6.4 !!!! This script assumes 2.6.4 but it is unknown whether it exists on your system.
 ##
 ## build_vist generated host.conf
 ## created: Mon Dec  8 17:15:07 PST 2008
@@ -11,7 +10,7 @@
 ## Set the VISITHOME environment variable.
 ##
 SET(VISITHOME /usr/common/graphics/installs/visit_3rdparty)
-SET(VISITARCH linux-x86_64_gcc-4.2.0)
+SET(VISITARCH linux-x86_64_gcc-4.4)
 SET(VISIT_VERBOSE_MAKEFILE TRUE)
 
 ##
@@ -22,17 +21,12 @@ VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.5/${VISITARCH})
 ##
 ## Specify the location of the vtk include files and libraries.
 ##
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0c/${VISITARCH}/lib/vtk-5.0)
-
-##
-## Specify the location of cmake.
-##
-
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0d/${VISITARCH}/lib/vtk-5.0)
 
 ##
 ## Specify the location of the qt include files and libraries.
 ##
-VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.5.1/${VISITARCH}/bin)
+VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.4.3/${VISITARCH}/bin)
 
 ##
 ## Specify the location of the python include and libraries.
@@ -42,13 +36,13 @@ VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.5/${VISITARCH})
 ## Compiler flags.
 ##
 SET(VISIT_C_COMPILER gcc)
-SET(VISIT_C_FLAGS "-march=barcelona -fPIC")
+SET(VISIT_C_FLAGS "-march=barcelona -fPIC -DVIZSCHEMA_DECOMPOSE_DOMAINS")
 SET(VISIT_MPI_C_FLAGS "-DMPICH_IGNORE_CXX_SEEK -I/opt/mpt/default/xt/mpich2-gnu/include")
 SET(VISIT_CXX_COMPILER g++)
-SET(VISIT_CXX_FLAGS "-march=barcelona -fPIC")
+SET(VISIT_CXX_FLAGS "-march=barcelona -fPIC -DVIZSCHEMA_DECOMPOSE_DOMAINS")
 SET(VISIT_MPI_CXX_FLAGS "-DMPICH_IGNORE_CXX_SEEK -I/opt/mpt/default/xt/mpich2-gnu/include")
 # Get these via CC -v
-SET(VISIT_MPI_LIBS /opt/mpt/default/xt/mpich2-gnu/lib/libmpich.a /opt/mpt/default/xt/pmi/lib/libpmi.a /opt/mpt/default/xt/util/lib/libalpslli.a /opt/mpt/default/xt/util/lib/libalpsutil.a /opt/xt-service/default/lib/snos64/libportals.a pthread)
+SET(VISIT_MPI_LIBS /opt/mpt/default/xt/mpich2-gnu/lib/libmpich.a /opt/mpt/default/xt/pmi/lib/libpmi.a /opt/mpt/default/xt/util/lib/libalpslli.a /opt/mpt/default/xt/util/lib/libalpsutil.a /opt/xt-service/default/lib/snos64/libportals.a pthread rt)
 
 ##
 ## Database reader plugin support libraries
@@ -79,7 +73,7 @@ VISIT_OPTION_DEFAULT(VISIT_EXODUSII_DIR ${VISITHOME}/exodus/4.46/${VISITARCH})
 ##
 ## GDAL
 ##
-VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR ${VISITHOME}/gdal/1.3.2/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR ${VISITHOME}/gdal/1.6.2/${VISITARCH})
 
 ##
 ## H5Part
@@ -100,13 +94,13 @@ VISIT_OPTION_DEFAULT(VISIT_HDF4_LIBDEP /usr/lib jpeg)
 ##
 ## HDF5
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.2/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.3/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz)
 
 ##
 ## Silo
 ##
-VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.6.2/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
@@ -117,7 +111,7 @@ VISIT_OPTION_DEFAULT(VISIT_MILI_DIR ${VISITHOME}/mili/1.10.0/${VISITARCH})
 ##
 ## NetCDF
 ##
-VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/3.6.0/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/3.6.3/${VISITARCH})
 
 ##
 ## SZIP
