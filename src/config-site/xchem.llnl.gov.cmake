6db3228feb4dbd884b35f8c738de6525d6e42fb4
Eric Brugger <brugger1@llnl.gov>
2010-03-03 18:41:12 +0000

    I updated the config-site file for xchem.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@10095 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/xchem.llnl.gov.cmake
+++ src/config-site/xchem.llnl.gov.cmake
@@ -1,94 +1,114 @@
-#/what/is/the/path/to/bin/cmake
+#/usr/gapps/visit/cmake/2.6.4/linux-x86_64-gcc-4.0.2/bin/cmake
+##
+## ./build_visit generated host.cmake
+## created: Fri Feb 26 13:16:05 PST 2010
+## system: Linux xchem.llnl.gov 2.6.17-1.2142_FC4smp #1 SMP Tue Jul 11 22:59:20 EDT 2006 x86_64 x86_64 x86_64 GNU/Linux
+## by: brugger
 
 ##
-## Set the VISITHOME environment variable.
+## Setup VISITHOME & VISITARCH variables.
 ##
-SET(VISITHOME /opt/llnl/visit)
+SET(VISITHOME /usr/gapps/visit)
+SET(VISITARCH linux-x86_64-gcc-4.0.2)
+
 SET(VISIT_VERBOSE_MAKEFILE TRUE)
 
 ##
-## If MESA is not set, use VisIt's mesa.
+## Specify the location of the mesa.
 ##
-#if test -z "$MESA"; then
-VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/5.0/linux_fedora4_gcc_4.0.2)
-#fi
+VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.5/${VISITARCH})
 
 ##
-## If VTK is not set, use VisIt's vtk.
+## Specify the location of the vtk.
 ##
-#if test -z "$VTK"; then
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0c/linux_fedora4_gcc_4.0.2/lib/vtk-5.0)
-#fi
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0d/${VISITARCH}/lib/vtk-5.0/)
 
 ##
-## If QT is not set, use VisIt's Qt.
+## Specify the Qt4 binary dir. 
+## (qmake us used to locate & setup Qt4 dependencies)
 ##
-#if test -z "$VISIT_QT_BIN"; then
-VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/3.3.2/linux_fedora4_gcc_4.0.2/bin)
-#fi
-#if test -z "$QT_INCLUDE"; then
-#fi
-#if test -z "$QT_LIB"; then
-#fi
+VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.4.3/${VISITARCH}/bin)
 
 ##
-## Use VisIt's Python.
+## Specify the location of the python.
 ##
-VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.5/linux_fedora4_gcc_4.0.2)
+VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.5/${VISITARCH})
+
+##
+## Compiler flags.
+##
+VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc)
+VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++)
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS " -m64 -fPIC")
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS "-Wno-deprecated -m64 -fPIC")
+VISIT_OPTION_DEFAULT(VISIT_EXE_LINKER_FLAGS "-Wl,-rpath=/usr/lib64/tls")
 
 ##
 ## Add parallel arguments.
 ##
-SET(VISIT_C_FLAGS "-fPIC")
-SET(VISIT_MPI_CXX_FLAGS "-DMPICH_IGNORE_CXX_SEEK")
+VISIT_OPTION_DEFAULT(VISIT_PARALLEL ON)
+VISIT_OPTION_DEFAULT(VISIT_MPI_CXX_FLAGS -DMPICH_IGNORE_CXX_SEEK -I/opt/llnl/mpich2_shared/ch3-ssm/include)
+VISIT_OPTION_DEFAULT(VISIT_MPI_C_FLAGS   -DMPICH_IGNORE_CXX_SEEK -I/opt/llnl/mpich2_shared/ch3-ssm/include)
+VISIT_OPTION_DEFAULT(VISIT_MPI_LD_FLAGS  "-L/opt/llnl/mpich2_shared/ch3-ssm/lib -Wl,-rpath=/opt/llnl/mpich2_shared/ch3-ssm/lib -Wl,-rpath=/usr/lib64/tls")
+VISIT_OPTION_DEFAULT(VISIT_MPI_LIBS     mpich rt)
 
-##
-## Turn off warnings for deprecated features on g++
-##
-SET(VISIT_CXX_FLAGS "-Wno-deprecated")
-
+##############################################################
 ##
 ## Database reader plugin support libraries
 ##
-###############################################################################
- 
+##############################################################
+
 ##
 ## Boxlib
 ##
-##DEFAULT_BOXLIB2D_INCLUDE=$VISITHOME/boxlib/linux_fedora4_gcc_4.0.2/include/2D
-##DEFAULT_BOXLIB2D_LIBS=$VISITHOME/boxlib/linux_fedora4_gcc_4.0.2/lib
-##DEFAULT_BOXLIB3D_INCLUDE=$VISITHOME/boxlib/linux_fedora4_gcc_4.0.2/include/3D
-##DEFAULT_BOXLIB3D_LIBS=$VISITHOME/boxlib/linux_fedora4_gcc_4.0.2/lib
+VISIT_OPTION_DEFAULT(VISIT_BOXLIB2D_DIR ${VISITHOME}/boxlib/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_BOXLIB3D_DIR ${VISITHOME}/boxlib/${VISITARCH})
+
+##
+## CCMIO
+##
+VISIT_OPTION_DEFAULT(VISIT_CCMIO_DIR ${VISITHOME}/ccmio/2.6.1/${VISITARCH})
 
 ##
 ## CFITSIO
 ##
-VISIT_OPTION_DEFAULT(VISIT_CFITSIO_DIR ${VISITHOME}/cfitsio/3006/linux_fedora4_gcc_4.0.2)
+VISIT_OPTION_DEFAULT(VISIT_CFITSIO_DIR ${VISITHOME}/cfitsio/3006/${VISITARCH})
 
 ##
 ## CGNS
 ##
-VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR ${VISITHOME}/cgns/2.4/linux_fedora4_gcc_4.0.2)
+VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR ${VISITHOME}/cgns/2.4/${VISITARCH})
 
 ##
 ## HDF4
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF4_DIR ${VISITHOME}/hdf4/2.4.1/linux_fedora4_gcc_4.0.2)
-VISIT_OPTION_DEFAULT(VISIT_HDF4_LIBDEP /usr/lib jpeg)
+VISIT_OPTION_DEFAULT(VISIT_HDF4_DIR ${VISITHOME}/hdf4/4.2.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF4_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz /usr/lib jpeg)
 
 ##
 ## HDF5
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.1/linux_fedora4_gcc_4.0.2)
+VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.4/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz)
 
 ##
-## netCDF
+## Mili
 ##
-VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/3.6.0/linux_fedora4_gcc_4.0.2)
+VISIT_OPTION_DEFAULT(VISIT_MILI_DIR ${VISITHOME}/mili/1.10.0/${VISITARCH})
+
+##
+## NetCDF
+##
+VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/3.6.3/${VISITARCH})
+
+##
+## SZIP
+##
+VISIT_OPTION_DEFAULT(VISIT_SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
 
 ##
 ## Silo
 ##
-VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.6.2/linux_fedora4_gcc_4.0.2)
+VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7.2/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
+
