bc1e67243ff73e600abaa0a42afc42b5def21ffc
Eric Brugger <brugger1@llnl.gov>
2010-03-26 23:49:12 +0000

    I corrected an error in the IceT network manager that was causing some of the scalable rendering tests to fail.  In particular an internal state variable was not being reset after being used.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@10584 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/engine/main/IceTNetworkManager.C
+++ src/engine/main/IceTNetworkManager.C
@@ -286,6 +286,11 @@ IceTNetworkManager::TileLayout(size_t width, size_t height) const
 //    Add case detecting plots down their own transparency.  For example
 //    splatting volume rendering, which doesn't work with Ice-T.
 //
+//    Eric Brugger, Fri Mar 26 16:37:34 PDT 2010
+//    I added a fix to Hank's change that was causing scalable rendering
+//    to fail in some instances.  In particular workingNet needed to be set
+//    back to NULL after it was used.
+//
 // ****************************************************************************
 
 avtDataObject_p
@@ -316,6 +321,7 @@ IceTNetworkManager::Render(bool, intVector networkIds, bool getZBuffer,
                 plotDoingTransparencyOutsideTransparencyActor = true;
             }
         }
+        workingNet = NULL;
 
         // We can't easily figure out a compositing order, which IceT requires
         // in order to properly composite transparent geometry.  Thus if there
