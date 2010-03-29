537acc25639fb848f83d151e3df86b5c2856d7d2
Eric Brugger <brugger1@llnl.gov>
2010-03-29 20:59:15 +0000

    1) I added some config site files.
    2) I updated the closed visit build script to use cmake.
    
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.0RC@10612 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/crier.llnl.gov.cmake
+++ src/config-site/crier.llnl.gov.cmake
@@ -1,100 +1,118 @@
-#/what/is/the/path/to/bin/cmake
+#/usr/gapps/visit/cmake/2.8.0/linux-i686_gcc-3.2.3/bin/cmake
+##
+## ./build_visit generated host.cmake
+## created: Mon Mar 29 11:44:15 PDT 2010
+## system: Linux crier.llnl.gov 2.4.29 #1 SMP Sun Jan 30 20:07:32 PST 2005 i686 i686 i386 GNU/Linux
+## by: brugger1
 
 ##
-## Set the VISITHOME environment variable.
+## Setup VISITHOME & VISITARCH variables.
 ##
 SET(VISITHOME /usr/gapps/visit)
-SET(VISIT_VERBOSE_MAKEFILE TRUE)
+SET(VISITARCH linux-i686_gcc-3.2.3)
 
 ##
-## If MESA is not set, use VisIt's mesa.
+## Specify the location of the mesa.
 ##
-#if test -z "$MESA"; then
-VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/5.0/linux_rhel3_gcc_3.2.3)
-#fi
+VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.5/${VISITARCH})
 
 ##
-## If VTK is not set, use VisIt's vtk.
+## Specify the location of the vtk.
 ##
-#if test -z "$VTK"; then
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0c/linux_rhel3_gcc_3.2.3/lib/vtk-5.0)
-#fi
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0d/${VISITARCH}/lib/vtk-5.0/)
 
 ##
-## If QT is not set, use VisIt's Qt.
+## Specify the Qt4 binary dir. 
+## (qmake us used to locate & setup Qt4 dependencies)
 ##
-VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.4.3/linux_rhel3_gcc_3.4.6/bin)
+VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.6.1/${VISITARCH}/bin)
 
 ##
-## Use VisIt's Python.
+## Specify the location of the python.
 ##
-VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.5/linux_rhel3_gcc_3.2.3)
+VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.6.4/${VISITARCH})
 
 ##
-## Turn off warnings for deprecated features.
+## Compiler flags.
 ##
-SET(VISIT_CXX_FLAGS "-Wno-deprecated")
+VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc)
+VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++)
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS "-O2")
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS "-O2")
 
+##############################################################
 ##
 ## Database reader plugin support libraries
 ##
-###############################################################################
+##############################################################
 
 ##
 ## Boxlib
 ##
-VISIT_OPTION_DEFAULT(VISIT_BOXLIB2D_DIR /usr/gapps/visit/boxlib/linux_rhel3_gcc_3.2.3)
-VISIT_OPTION_DEFAULT(VISIT_BOXLIB3D_DIR /usr/gapps/visit/boxlib/linux_rhel3_gcc_3.2.3)
+VISIT_OPTION_DEFAULT(VISIT_BOXLIB2D_DIR ${VISITHOME}/boxlib/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_BOXLIB3D_DIR ${VISITHOME}/boxlib/${VISITARCH})
+
+##
+## CCMIO
+##
+VISIT_OPTION_DEFAULT(VISIT_CCMIO_DIR ${VISITHOME}/ccmio/2.6.1/${VISITARCH})
 
 ##
 ## CFITSIO
 ##
-VISIT_OPTION_DEFAULT(VISIT_CFITSIO_DIR ${VISITHOME}/cfitsio/3006/linux_rhel3_gcc_3.2.3)
+VISIT_OPTION_DEFAULT(VISIT_CFITSIO_DIR ${VISITHOME}/cfitsio/3006/${VISITARCH})
 
 ##
 ## CGNS
 ##
-VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR /usr/gapps/visit/cgns/2.4/linux_rhel3_gcc_3.2.3)
+VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR ${VISITHOME}/cgns/2.4/${VISITARCH})
 
 ##
 ## Exodus
 ##
-VISIT_OPTION_DEFAULT(VISIT_EXODUSII_DIR ${VISITHOME}/exodus/4.46/linux_rhel3_gcc_3.2.3)
+VISIT_OPTION_DEFAULT(VISIT_EXODUSII_DIR ${VISITHOME}/exodus/4.46/${VISITARCH})
 
 ##
 ## GDAL
 ##
-VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR /usr/gapps/visit/gdal/1.3.0/linux_rhel3_gcc_3.2.3)
+VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR ${VISITHOME}/gdal/1.7.1/${VISITARCH})
 
 ##
 ## HDF4
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF4_DIR /usr/gapps/visit/hdf4/2.0/linux_rhel3_gcc_3.2.3)
-VISIT_OPTION_DEFAULT(VISIT_HDF4_LIBDEP /usr/lib jpeg)
+VISIT_OPTION_DEFAULT(VISIT_HDF4_DIR ${VISITHOME}/hdf4/4.2.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF4_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz /usr/lib jpeg)
 
 ##
 ## HDF5
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR /usr/gapps/silo/hdf5/1.8.1/i686_Linux_ELsmp)
-VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz)
+VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.4/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz /usr/lib z)
+
+##
+## H5Part
+##
+VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR ${VISITHOME}/h5part/1.6.0/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_H5PART_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_IBDEP})
 
 ##
 ## Mili
 ##
-VISIT_OPTION_DEFAULT(VISIT_MILI_DIR /usr/gapps/visit/mili/current/linux_rhel3_gcc_3.2.3)
+VISIT_OPTION_DEFAULT(VISIT_MILI_DIR ${VISITHOME}/mili/1.10.0/${VISITARCH})
 
 ##
-## netCDF
+## NetCDF
 ##
-VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR /usr/gapps/visit/netcdf/3.6.0/linux_rhel3_gcc_3.2.3)
+VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/3.6.3/${VISITARCH})
+
+##
+## SZIP
+##
+VISIT_OPTION_DEFAULT(VISIT_SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
 
 ##
 ## Silo
 ##
-VISIT_OPTION_DEFAULT(VISIT_SILO_DIR /usr/gapps/silo/4.7/i686_Linux_ELsmp)
+VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7.2/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
-##
-## ViSUS 
-##
-VISIT_OPTION_DEFAULT(VISIT_VISUS_DIR /usr/gapps/visit/visus/linux_rhel3_gcc_3.2.3_new)
--- src/config-site/hopi1.cmake
+++ src/config-site/hopi1.cmake
@@ -1,96 +1,125 @@
-#/what/is/the/path/to/bin/cmake
+#/usr/gapps/visit/cmake/2.8.0/linux-x86_64__gcc-4.1.2/bin/cmake
+##
+## ./build_visit generated host.cmake
+## created: Wed Mar 24 13:48:54 PDT 2010
+## system: Linux hopi4 2.6.18-76chaos #1 SMP Fri Aug 14 13:22:06 EDT 2009 x86_64 x86_64 x86_64 GNU/Linux
+## by: brugger
 
 ##
-## Set the VISITHOME environment variable.
+## Setup VISITHOME & VISITARCH variables.
 ##
 SET(VISITHOME /usr/gapps/visit)
-SET(VISIT_VERBOSE_MAKEFILE TRUE)
+SET(VISITARCH linux-x86_64_gcc-4.1.2)
 
 ##
-## If MESA is not set, use VisIt's mesa.
+## Specify the location of the mesa.
 ##
-#if test -z "$MESA"; then
-VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/5.0/chaos_3_x86_64_ib_gcc_3.4.4)
-#fi
+VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.5/${VISITARCH})
 
 ##
-## If VTK is not set, use VisIt's vtk.
+## Specify the location of the vtk.
 ##
-#if test -z "$VTK"; then
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0c/chaos_3_x86_64_ib_gcc_3.4.4/lib/vtk-5.0)
-#fi
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0d/${VISITARCH}/lib/vtk-5.0/)
 
 ##
-## If QT is not set, use VisIt's Qt.
+## Specify the Qt4 binary dir. 
+## (qmake us used to locate & setup Qt4 dependencies)
 ##
-VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.4.3/chaos_4_x86_64_ib_gcc_4.1.2/bin)
+VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.6.1/${VISITARCH}/bin)
 
 ##
-## Use VisIt's Python.
+## Specify the location of the python.
 ##
-VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.5/chaos_3_x86_64_ib_gcc_3.4.4)
+VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.6.4/${VISITARCH})
 
 ##
 ## Add parallel arguments.
 ##
-SET(VISIT_C_FLAGS "-I/usr/local/tools/mvapich-gnu/include -fPIC")
-SET(VISIT_CXX_FLAGS "-I/usr/local/tools/mvapich-gnu/include -fPIC")
+VISIT_OPTION_DEFAULT(VISIT_PARALLEL ON)
+VISIT_OPTION_DEFAULT(VISIT_MPI_CXX_FLAGS -I/usr/local/tools/mvapich-gnu/include)
+VISIT_OPTION_DEFAULT(VISIT_MPI_C_FLAGS   -I/usr/local/tools/mvapich-gnu/include)
+VISIT_OPTION_DEFAULT(VISIT_MPI_LD_FLAGS  "-L/usr/local/tools/mvapich-gnu/lib/shared -L/usr/local/tools/mvapich-gnu/lib -Wl,-rpath=/usr/local/tools/mvapich-gnu/lib/shared")
+VISIT_OPTION_DEFAULT(VISIT_MPI_LIBS     mpich)
 
 ##
 ## Turn off warnings for deprecated features on g++
 ##
-SET(VISIT_CXX_FLAGS "-Wno-deprecated")
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS "-Wno-deprecated -fPIC")
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS "-Wno-deprecated -fPIC")
 
+##############################################################
 ##
 ## Database reader plugin support libraries
 ##
-###############################################################################
- 
+##############################################################
+
 ##
 ## Boxlib
 ##
-VISIT_OPTION_DEFAULT(VISIT_BOXLIB2D_DIR ${VISITHOME}/boxlib/chaos_3_x86_64_ib_gcc_3.4.4)
-VISIT_OPTION_DEFAULT(VISIT_BOXLIB3D_DIR ${VISITHOME}/boxlib/chaos_3_x86_64_ib_gcc_3.4.4)
+VISIT_OPTION_DEFAULT(VISIT_BOXLIB2D_DIR ${VISITHOME}/boxlib/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_BOXLIB3D_DIR ${VISITHOME}/boxlib/${VISITARCH})
+
+##
+## CCMIO
+##
+VISIT_OPTION_DEFAULT(VISIT_CCMIO_DIR ${VISITHOME}/ccmio/2.6.1/${VISITARCH})
+
+##
+## CFITSIO
+##
+VISIT_OPTION_DEFAULT(VISIT_CFITSIO_DIR ${VISITHOME}/cfitsio/3006/${VISITARCH})
 
 ##
 ## CGNS
 ##
-VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR /usr/gapps/visit/cgns/2.4/chaos_3_x86_64_ib_gcc_3.4.4)
+VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR ${VISITHOME}/cgns/2.4/${VISITARCH})
+
+##
+## Exodus
+##
+VISIT_OPTION_DEFAULT(VISIT_EXODUSII_DIR ${VISITHOME}/exodus/4.46/${VISITARCH})
 
 ##
 ## GDAL
 ##
-VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR /usr/gapps/visit/gdal/1.3.0/chaos_3_x86_64_ib_gcc_3.4.4)
+VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR ${VISITHOME}/gdal/1.7.1/${VISITARCH})
 
 ##
 ## HDF4
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF4_DIR /usr/gapps/visit/hdf4/2.4.1/chaos_3_x86_64_ib_gcc_3.4.4)
-VISIT_OPTION_DEFAULT(VISIT_HDF4_LIBDEP /usr/lib jpeg)
+VISIT_OPTION_DEFAULT(VISIT_HDF4_DIR ${VISITHOME}/hdf4/4.2.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF4_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz /usr/lib jpeg)
 
 ##
 ## HDF5
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR /usr/gapps/silo/hdf5/1.8.1/chaos_4_x86_64_ib)
-VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz)
+VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.4/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz /usr/lib z)
+
+##
+## H5Part
+##
+VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR ${VISITHOME}/h5part/1.6.0/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_H5PART_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_IBDEP})
 
 ##
 ## Mili
 ##
-VISIT_OPTION_DEFAULT(VISIT_MILI_DIR /usr/gapps/visit/mili/current/chaos_3_x86_64_ib_gcc_3.4.4)
+VISIT_OPTION_DEFAULT(VISIT_MILI_DIR ${VISITHOME}/mili/1.10.0/${VISITARCH})
 
 ##
-## netCDF
+## NetCDF
 ##
-VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR /usr/gapps/visit/netcdf/3.6.0/chaos_3_x86_64_ib_gcc_3.4.4)
+VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/3.6.3/${VISITARCH})
+
+##
+## SZIP
+##
+VISIT_OPTION_DEFAULT(VISIT_SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
 
 ##
 ## Silo
 ##
-VISIT_OPTION_DEFAULT(VISIT_SILO_DIR /usr/gapps/silo/4.7/chaos_4_x86_64_ib)
+VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7.2/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
-##
-## ViSUS
-##
-VISIT_OPTION_DEFAULT(VISIT_VISUS_DIR /usr/gapps/visus/1.0a5/chaos_3_x86_64_ib_gcc3.4.4)
