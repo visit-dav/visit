731cf1e9aef8b1a732c174760598e712c254a63a
Mark Miller <miller86@llnl.gov>
2010-04-26 04:31:01 +0000

    adding missing slice operator
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@11015 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/databases/imgvol.py
+++ test/tests/databases/imgvol.py
@@ -7,6 +7,10 @@
 #
 #  Mark C. Miller, Fri Apr 23 23:01:06 PDT 2010
 #
+#  Modifications:
+# 
+#    Mark C. Miller, Sun Apr 25 21:30:29 PDT 2010
+#    Added missing Slice operator for contour plot.
 # ----------------------------------------------------------------------------
 
 OpenDatabase("../data/imgvol_test_data/tcup.imgvol")
@@ -29,9 +33,12 @@ Test("imgvol_03")
 RemoveLastOperator()
 
 AddPlot("Contour","intensity")
+AddOperator("Slice")
+SetOperatorOptions(sa)
 ca=ContourAttributes()
 ca.contourValue = (65,)
 ca.contourMethod = ca.Value
+ca.lineWidth = 2
 SetPlotOptions(ca)
 DrawPlots()
 Test("imgvol_04")
