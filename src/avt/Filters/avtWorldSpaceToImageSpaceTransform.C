bac79b38860e1064618c438a7d28d818681b752b
Hank Childs <hank@uoregon.edu>
2010-05-08 14:32:53 +0000

    Fix bug where half the data is being clipped away when ray-casted volume rendering point meshes.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.0RC@11308 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/Filters/avtWorldSpaceToImageSpaceTransform.C
+++ src/avt/Filters/avtWorldSpaceToImageSpaceTransform.C
@@ -732,6 +732,10 @@ avtWorldSpaceToImageSpaceTransform::UpdateDataObjectInfo(void)
 //    Hank Childs, Wed Dec 24 09:48:59 PST 2008
 //    Add support for sampling in the wbuffer.
 //
+//    Hank Childs, Sat May  8 09:19:44 CDT 2010
+//    If we are looking at polydata (point meshes), then do the same 
+//    perspective trick as we do for curvilinear and unstructured meshes.
+//
 // ****************************************************************************
 
 vtkDataSet *
@@ -780,7 +784,8 @@ avtWorldSpaceToImageSpaceTransform::ExecuteData(vtkDataSet *in_ds, int domain,
     // mass voxel extractor.  Otherwise, we need to change the z-buffer
     // to a w-buffer, so we can space out the sample points better.
     if (mid_ds->GetDataObjectType() == VTK_STRUCTURED_GRID ||
-        mid_ds->GetDataObjectType() == VTK_UNSTRUCTURED_GRID)
+        mid_ds->GetDataObjectType() == VTK_UNSTRUCTURED_GRID ||
+        mid_ds->GetDataObjectType() == VTK_POLY_DATA)
     {
         vtkPointSet *ps = (vtkPointSet *) mid_ds;
         vtkPoints *pts = ps->GetPoints();
