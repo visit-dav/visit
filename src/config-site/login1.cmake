0a54f8777c76cb82f22fdd6d87b83d58f50f148d
Hank Childs <hank@uoregon.edu>
2010-05-12 17:47:30 +0000

    Update ANL Eureka's config file.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.0RC@11360 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/login1.cmake
+++ src/config-site/login1.cmake
@@ -1,14 +1,14 @@
-#./visit/cmake/2.6.4/linux-sles10-x86_64_gcc-4.1/bin/cmake
+#/soft/apps/visit/cmake/2.8.0/linux-sles10-x86_64_gcc-4.1/bin/cmake
 ##
 ## ./build_visit generated host.cmake
-## created: Sun Jan 31 23:45:54 CST 2010
+## created: Fri May  7 08:37:44 CDT 2010
 ## system: Linux login1 2.6.16.60-0.42.8-smp #1 SMP Tue Dec 15 17:28:00 UTC 2009 x86_64 x86_64 x86_64 GNU/Linux
 ## by: childs
 
 ##
 ## Setup VISITHOME & VISITARCH variables.
 ##
-SET(VISITHOME /home/childs/trunk/src/svn_bin/visit)
+SET(VISITHOME /soft/apps/visit)
 SET(VISITARCH linux-sles10-x86_64_gcc-4.1)
 
 ##
@@ -19,26 +19,27 @@ VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.5/${VISITARCH})
 ##
 ## Specify the location of the vtk.
 ##
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0d/${VISITARCH}/lib/vtk-5.0/)
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0e/${VISITARCH}/lib/vtk-5.0/)
 
 ##
 ## Specify the Qt4 binary dir. 
 ## (qmake us used to locate & setup Qt4 dependencies)
 ##
-VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.4.3/${VISITARCH}/bin)
+VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.6.1/${VISITARCH}/bin)
 
 ##
 ## Specify the location of the python.
 ##
-VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.5/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.6.4/${VISITARCH})
 
 ##
 ## Compiler flags.
 ##
 VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc)
 VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++)
-VISIT_OPTION_DEFAULT(VISIT_C_FLAGS " -m64 -fPIC")
-VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS " -m64 -fPIC")
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS " -m64 -fPIC -fvisibility=hidden")
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS " -m64 -fPIC -fvisibility=hidden")
+SET(VISIT_VERBOSE_MAKEFILE TRUE)
 SET(VISIT_MPI_C_FLAGS "-I/soft/apps/mpich-mx-1.2.7..7-1/include")
 SET(VISIT_MPI_CXX_FLAGS "-I/soft/apps/mpich-mx-1.2.7..7-1/include")
 SET(VISIT_MPI_LD_FLAGS "-L/soft/apps/mpich-mx-1.2.7..7-1/lib -L/opt/mx/lib")
@@ -54,13 +55,13 @@ VISIT_OPTION_DEFAULT(VISIT_PARALLEL ON)
 ##############################################################
 
 ##
-## Silo
-##
-VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
-
-##
 ## HDF5
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.2/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.4/${VISITARCH})
+
+##
+## Silo
+##
+VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7.2/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
