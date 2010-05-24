3bc15660c8ae6036396f5335ceb7fe7edbb074ee
Brad Whitlock <bjw@ilight.com>
2010-05-24 22:44:11 +0000

    Mac 10.6 fixes.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@11472 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/dantooine.llnl.gov.cmake
+++ src/config-site/dantooine.llnl.gov.cmake
@@ -1,15 +1,15 @@
-#/Users/whitlock2/Development/visit/cmake/2.8.0/i386-apple-darwin8_gcc-4.0/bin/cmake
+#/Users/whitlock2/Development/visit/cmake/2.8.0/i386-apple-darwin10_gcc-4.2/bin/cmake
 ##
 ## ./build_visit generated host.cmake
-## created: Tue Jan  5 14:00:25 PST 2010
-## system: Darwin dantooine.llnl.gov 8.11.1 Darwin Kernel Version 8.11.1: Wed Oct 10 18:23:28 PDT 2007; root:xnu-792.25.20~1/RELEASE_I386 i386 i386
+## created: Thu May 20 10:50:55 PDT 2010
+## system: Darwin dantooine.llnl.gov 10.3.0 Darwin Kernel Version 10.3.0: Fri Feb 26 11:58:09 PST 2010; root:xnu-1504.3.12~1/RELEASE_I386 i386
 ## by: whitlock2
 
 ##
 ## Setup VISITHOME & VISITARCH variables.
 ##
 SET(VISITHOME /Users/whitlock2/Development/visit)
-SET(VISITARCH i386-apple-darwin8_gcc-4.0)
+SET(VISITARCH i386-apple-darwin10_gcc-4.2)
 
 ##
 ## Specify the location of the mesa.
@@ -19,13 +19,13 @@ VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.5/${VISITARCH})
 ##
 ## Specify the location of the vtk.
 ##
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0d/${VISITARCH}/lib/vtk-5.0/)
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0e/${VISITARCH}/lib/vtk-5.0/)
 
 ##
 ## Specify the Qt4 binary dir. 
 ## (qmake us used to locate & setup Qt4 dependencies)
 ##
-VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.4.3/${VISITARCH}/bin)
+VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.6.1/${VISITARCH}/bin)
 
 ##
 ## Specify the location of the python.
@@ -37,21 +37,20 @@ VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.6.4/${VISITARCH})
 ##
 VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc)
 VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++)
-VISIT_OPTION_DEFAULT(VISIT_C_FLAGS "-fvisibility=hidden -fno-common -fexceptions")
-VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS "-fvisibility=hidden -fno-common -fexceptions")
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS "-O2 -fno-common -fexceptions -fvisibility=hidden")
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS "-O2 -fno-common -fexceptions -fvisibility=hidden")
 
 ##
 ## Parallel Build Setup.
 ##
 VISIT_OPTION_DEFAULT(VISIT_PARALLEL ON)
 ## (configured w/ mpi compiler wrapper)
-VISIT_OPTION_DEFAULT(VISIT_MPI_COMPILER /Users/whitlock2/i386-apple-darwin8.8.1/bin/mpic++)
-VISIT_OPTION_DEFAULT(VISIT_MPI_FORTRAN_COMPILER /Users/whitlock2/i386-apple-darwin8.8.1/bin/mpif77)
+VISIT_OPTION_DEFAULT(VISIT_MPI_COMPILER /usr/bin/mpic++)
 
 ##
 ## Enable fortran for example programs.
 ##
-VISIT_OPTION_DEFAULT(VISIT_FORTRAN ON)
+#VISIT_OPTION_DEFAULT(VISIT_FORTRAN ON)
 
 ##############################################################
 ##
@@ -65,6 +64,12 @@ VISIT_OPTION_DEFAULT(VISIT_FORTRAN ON)
 VISIT_OPTION_DEFAULT(VISIT_ADVIO_DIR ${VISITHOME}/AdvIO/1.2/${VISITARCH}/)
 
 ##
+## Boxlib
+##
+VISIT_OPTION_DEFAULT(VISIT_BOXLIB2D_DIR ${VISITHOME}/boxlib/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_BOXLIB3D_DIR ${VISITHOME}/boxlib/${VISITARCH})
+
+##
 ## CFITSIO
 ##
 VISIT_OPTION_DEFAULT(VISIT_CFITSIO_DIR ${VISITHOME}/cfitsio/3006/${VISITARCH})
@@ -75,19 +80,14 @@ VISIT_OPTION_DEFAULT(VISIT_CFITSIO_DIR ${VISITHOME}/cfitsio/3006/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR ${VISITHOME}/cgns/2.4/${VISITARCH})
 
 ##
-## ExodusII
+## Exodus
 ##
 VISIT_OPTION_DEFAULT(VISIT_EXODUSII_DIR ${VISITHOME}/exodus/4.46/${VISITARCH})
 
 ##
 ## GDAL
 ##
-VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR ${VISITHOME}/gdal/1.3.2/${VISITARCH})
-
-##
-## H5Part
-##
-#VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR ${VISITHOME}/h5part/1.6.0/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR ${VISITHOME}/gdal/1.7.1/${VISITARCH})
 
 ##
 ## HDF5
@@ -96,9 +96,15 @@ VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.4/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz)
 
 ##
+## H5Part
+##
+VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR ${VISITHOME}/h5part/1.6.0/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_H5PART_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
+
+##
 ## Mili
 ##
-VISIT_OPTION_DEFAULT(VISIT_MILI_DIR ${VISITHOME}/mili/1.10.0/${VISITARCH})
+#VISIT_OPTION_DEFAULT(VISIT_MILI_DIR ${VISITHOME}/mili/1.10.0/${VISITARCH})
 
 ##
 ## NetCDF
@@ -109,11 +115,6 @@ VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/3.6.3/${VISITARCH})
 ## SZIP
 ##
 VISIT_OPTION_DEFAULT(VISIT_SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
-
-##
-## CCMIO
-##
-VISIT_OPTION_DEFAULT(VISIT_CCMIO_DIR ${VISITHOME}/ccmio/2.6.1/${VISITARCH})
 
 ##
 ## Silo
@@ -127,3 +128,7 @@ VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP
 VISIT_OPTION_DEFAULT(VISIT_XDMF_DIR ${VISITHOME}/Xdmf/2.1/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_XDMF_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
+##
+## Ice-T
+##
+#VISIT_OPTION_DEFAULT(VISIT_ICET_DIR ${VISITHOME}/icet/0.5.4/${VISITARCH})
