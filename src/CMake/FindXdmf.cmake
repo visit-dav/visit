adbde453b7403a39ce92c2c2050c77992273ad3d
Kathleen Bonnell <bonnell2@llnl.gov>
2010-05-28 00:53:05 +0000

    Add windows libs for Xdmf. Fix small compile error with Xdmf reader.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@11520 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/CMake/FindXdmf.cmake
+++ src/CMake/FindXdmf.cmake
@@ -34,6 +34,10 @@
 # OUT OF THE  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 # DAMAGE.
 #
+# Modifications:
+#   Kathleen Bonnell, Thu May 27 17:01:22 MST 2010
+#   Windows builds can be handled the same as non-windows for XDMF.
+#
 #****************************************************************************/
 
 # Use the XDMF_DIR hint from the config-site .cmake file 
@@ -41,11 +45,7 @@
 
 INCLUDE(${VISIT_SOURCE_DIR}/CMake/SetUpThirdParty.cmake)
 
-IF (WIN32)
-  SET_UP_THIRD_PARTY(XDMF lib/${VISIT_MSVC_VERSION} include Xdmf vtklibxml2)
-ELSE (WIN32)
-  SET_UP_THIRD_PARTY(XDMF lib include Xdmf vtklibxml2)
-ENDIF (WIN32)
+SET_UP_THIRD_PARTY(XDMF lib include Xdmf vtklibxml2)
 
 SET(XDMF_DIR ${XDMF_LIBRARY_DIR}/XdmfCMake)
 FIND_PACKAGE(XDMF QUIET)
