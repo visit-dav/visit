d22566b7519df0210d85f19b5d9bccddde72e8c6
Brad Whitlock <bjw@ilight.com>
2010-06-04 16:44:36 +0000

    I changed some example code for plugin development.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@11567 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/tools/DataManualExamples/DatabasePlugin/invaliddbtype.C
+++ src/tools/DataManualExamples/DatabasePlugin/invaliddbtype.C
@@ -44,17 +44,38 @@
 avtXXXXFileFormat::avtXXXXFileFormat(const char *filename)
     : avtSTSDFileFormat(filename)
 {
-    bool fileOpened = false;
+    initialized = false;
+}
 
-    // Open the file specified by the filename argument here using
-    // your file format API and set fileOpened accordingly.
-    YOU MUST IMPLEMENT THIS
+// Override this method in your reader
+void
+avtXXXXFileFormat::ActivateTimestep()
+{
+    Initialize();
+}
 
-    // If your file format API could not open the file then throw
-    // an exception.
-    if (!fileOpened)
+// Provide this method in your reader
+void
+avtXXXXFileFormat::Initialize())
+{
+    if(!initialized)
     {
-        EXCEPTION1(InvalidDBTypeException,
-           "The file could not be opened");
+        bool okay = false;
+
+        // Open the file specified by the filename argument here using
+        // your file format API. See if the file has the right things in it.
+        // If so, set okay to true.
+        YOU MUST IMPLEMENT THIS
+
+        // If your file format API could not open the file then throw
+        // an exception.
+        if (!okay)
+        {
+            EXCEPTION1(InvalidDBTypeException,
+                "The file could not be opened");
+        }
+
+        initialized = true;
     }
 }
+
