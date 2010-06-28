66806141b16e080b3f067cf2e35e6b619f98a819
Mark Miller <miller86@llnl.gov>
2010-06-28 22:19:38 +0000

    adding config-site file for itaps.mcs.anl.gov
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@11786 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ src/config-site/itaps.cmake
@@ -0,0 +1,100 @@
+#/usr/local/itaps/visit/build/visit/cmake/2.8.0/linux-x86_64_gcc-4.4/bin/cmake
+##
+## ./build_visit generated host.cmake
+## created: Wed Jun  2 14:41:07 CDT 2010
+## system: Linux itaps 2.6.32-10-generic #14-Ubuntu SMP Thu Jan 7 17:38:08 UTC 2010 x86_64 GNU/Linux
+## by: mcmiller
+
+##
+## Setup VISITHOME & VISITARCH variables.
+##
+SET(VISITHOME /usr/local/itaps/visit/build/visit)
+SET(VISITARCH linux-x86_64_gcc-4.4)
+
+##
+## Specify the location of the mesa.
+##
+VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.5/${VISITARCH})
+
+##
+## Specify the location of the vtk.
+##
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0e/${VISITARCH}/lib/vtk-5.0/)
+
+##
+## Specify the Qt4 binary dir. 
+## (qmake us used to locate & setup Qt4 dependencies)
+##
+VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.6.1/${VISITARCH}/bin)
+
+##
+## Specify the location of the python.
+##
+VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.6.4/${VISITARCH})
+
+##
+## Compiler flags.
+##
+VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc)
+VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++)
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS " -m64 -fPIC -fvisibility=hidden")
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS " -m64 -fPIC -fvisibility=hidden")
+
+##
+## Parallel Build Setup.
+##
+VISIT_OPTION_DEFAULT(VISIT_PARALLEL ON)
+## (configured w/ mpi compiler wrapper)
+VISIT_OPTION_DEFAULT(VISIT_MPI_COMPILER /usr/bin/mpic++)
+
+##############################################################
+##
+## Database reader plugin support libraries
+##
+##############################################################
+
+##
+## CGNS
+##
+VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR ${VISITHOME}/cgns/2.4/${VISITARCH})
+
+##
+## Exodus
+##
+VISIT_OPTION_DEFAULT(VISIT_EXODUSII_DIR ${VISITHOME}/exodus/4.46/${VISITARCH})
+
+##
+## HDF5
+##
+VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.4/${VISITARCH})
+
+##
+## NetCDF
+##
+VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/3.6.3/${VISITARCH})
+
+##
+## Silo
+##
+VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7.2/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
+
+##
+## ITAPS
+##
+## MOAB implementation
+ITAPS_INCLUDE_DIRECTORIES(MOAB ${VISITHOME}/itaps/MOAB/3.99-10Jun10/${VISITARCH}/include)
+ITAPS_FILE_PATTERNS(MOAB *.cub)
+ITAPS_LINK_LIBRARIES(MOAB iMesh MOAB hdf5 z netcdf_c++ netcdf vtkGraphics)
+ITAPS_LINK_DIRECTORIES(MOAB  ${VISITHOME}/itaps/MOAB/3.99-10Jun10/${VISITARCH}/lib  ${VISITHOME}/hdf5/1.8.4/${VISITARCH}/lib ${VISITHOME}/netcdf/3.6.3/${VISITARCH}/lib)
+## FMDB implementation
+ITAPS_INCLUDE_DIRECTORIES(FMDB ${VISITHOME}/itaps/FMDB/1.1-03Jun10/${VISITARCH}/include/FMDB)
+ITAPS_FILE_PATTERNS(FMDB *.sms)
+ITAPS_LINK_LIBRARIES(FMDB FMDB SCORECModel SCORECUtil vtkGraphics)
+ITAPS_LINK_DIRECTORIES(FMDB ${VISITHOME}/itaps/FMDB/1.1-03Jun10/${VISITARCH}/lib)
+## GRUMMP implementation
+ITAPS_INCLUDE_DIRECTORIES(GRUMMP ${VISITHOME}/itaps/GRUMMP/0.5.0-03Jun10/${VISITARCH}/include)
+ITAPS_FILE_PATTERNS(GRUMMP *.bdry *.smesh *.vmesh)
+ITAPS_LINK_LIBRARIES(GRUMMP iMesh_GRUMMP GR_3D GR_surf GR_2D GR_base SUMAAlog_lite OptMS vtkGraphics)
+ITAPS_LINK_DIRECTORIES(GRUMMP ${VISITHOME}/itaps/GRUMMP/0.5.0-03Jun10/${VISITARCH}/lib)
+
