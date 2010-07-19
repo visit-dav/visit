977b718651fb09af22a7813d4f0b08031e34a317
Dave Pugmire <pugmire@ornl.gov>
2010-07-19 14:11:02 +0000

    Ellipsoid glyphing.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@11925 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/visit_vtk/full/vtkVectorGlyph.C
+++ src/visit_vtk/full/vtkVectorGlyph.C
@@ -48,6 +48,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <vtkObjectFactory.h>
 #include <vtkPolyData.h>
 
+#include <vtkSphereSource.h>
+#include <vtkTransformFilter.h>
+#include <vtkTransform.h>
+
 // ****************************************************************************
 //  Modifications:
 //    Kathleen Bonnell, Wed Mar  6 17:10:03 PST 2002 
@@ -66,10 +70,15 @@ vtkStandardNewMacro(vtkVectorGlyph);
 //    Added settings to draw the stem as a cylinder (vs a line), and its
 //    width, to increase the number of polygons, and to cap the ends of
 //    the cone/cylinder.
+//
+//    Dave Pugmire, Mon Jul 19 09:38:17 EDT 2010
+//    Add ellipsoid glyphing.
+//    
 // ****************************************************************************
 
 vtkVectorGlyph::vtkVectorGlyph()
 {
+  Arrow = 1;
   LineStem = 1;
   HighQuality = 0;
   CapEnds = 0;
@@ -99,6 +108,9 @@ vtkVectorGlyph::vtkVectorGlyph()
 //    Hank Childs, Sun Jan 24 10:26:07 PST 2010
 //    Fix problem where glyph heads don't show up in 2D.
 //
+//    Dave Pugmire, Mon Jul 19 09:38:17 EDT 2010
+//    Add ellipsoid glyphing.    
+//
 // ****************************************************************************
 
 void vtkVectorGlyph::Execute(void)
@@ -117,150 +129,193 @@ void vtkVectorGlyph::Execute(void)
     output->SetPolys(polys);
     polys->Delete();
 
-    //
-    // The vector will be with the bounding box x,y,z=-0.5,0.5.
-    // The head will start at (0.5, 0., 0.) and
-    // the tail will end at (-0.5,0.,0.)
-    // 
-    float realHeadSize = MakeHead ? this->HeadSize : 0;
-    float endOfHead = 0.5 - realHeadSize;
-    float proportion = realHeadSize;
-    float cylThickness = this->StemWidth;
-
-    vtkIdType pt[4];
-
-    // Determine the number of points around the glyph cone/cylinder
-    // (For a 2D glyph, 2 steps makes a triangle head & rectangle stem.)
-    int nSteps = (!ConeHead) ? 2 : (HighQuality ? 16 : 6);
-
-    // Determine the total number of points
-    int nPts = 3 + nSteps + (LineStem ? 0 : nSteps*2);
-    pts->SetNumberOfPoints(nPts);
-
-    // Add the points along the central axis
-    pts->SetPoint(0, OriginOffset + 0.5, 0., 0.);
-    pts->SetPoint(1, OriginOffset - 0.5, 0., 0.);
-    pts->SetPoint(2, OriginOffset + endOfHead, 0., 0.);
-
-    // Add the points around the base of the cone
-    for (int i=0; i<nSteps; i++)
+    if (Arrow)
     {
-        float theta = 2*M_PI * float(i)/float(nSteps);
-        float x = OriginOffset + endOfHead;
-        float y = proportion * 0.5 * cos(theta);
-        float z;
-        if (ConeHead)
-            z = proportion * 0.5 * sin(theta); 
-        else
-            z = 0;
-        pts->SetPoint(3 + i, x, y, z);
-    }
-
-    // Draw the stem as a line or cylinder(3d)/rectangle(2d)
-    if (LineStem)
-    {
-        lines->InsertNextCell(2);
-        lines->InsertCellPoint(0);
-        lines->InsertCellPoint(1);
-    }
-    else
-    {
-        float startx = OriginOffset - 0.5;
-        float endx;
-        // If we're in 3D and not capping the cone's base, then extend the
-        // cylinder all the way to where it intersects the cone's facets.
-        // Otherwise, just stop at the base of the cone.
-        if (ConeHead && !CapEnds)
-        {
-            float len = (realHeadSize<cylThickness)?realHeadSize:cylThickness;
-            endx = OriginOffset + .5 - len;
-        }
-        else
-        {
-            endx = OriginOffset + endOfHead;
-        }
-
+        //
+        // The vector will be with the bounding box x,y,z=-0.5,0.5.
+        // The head will start at (0.5, 0., 0.) and
+        // the tail will end at (-0.5,0.,0.)
+        // 
+        float realHeadSize = MakeHead ? this->HeadSize : 0;
+        float endOfHead = 0.5 - realHeadSize;
+        float proportion = realHeadSize;
+        float cylThickness = this->StemWidth;
+        
+        vtkIdType pt[4];
+        
+        // Determine the number of points around the glyph cone/cylinder
+        // (For a 2D glyph, 2 steps makes a triangle head & rectangle stem.)
+        int nSteps = (!ConeHead) ? 2 : (HighQuality ? 16 : 6);
+        
+        // Determine the total number of points
+        int nPts = 3 + nSteps + (LineStem ? 0 : nSteps*2);
+        pts->SetNumberOfPoints(nPts);
+        
+        // Add the points along the central axis
+        pts->SetPoint(0, OriginOffset + 0.5, 0., 0.);
+        pts->SetPoint(1, OriginOffset - 0.5, 0., 0.);
+        pts->SetPoint(2, OriginOffset + endOfHead, 0., 0.);
+        
+        // Add the points around the base of the cone
         for (int i=0; i<nSteps; i++)
         {
             float theta = 2*M_PI * float(i)/float(nSteps);
-            float y = cylThickness * 0.5 * cos(theta);
-            float z = cylThickness * 0.5 * sin(theta); 
-            pts->SetPoint(3 + 1*nSteps + i, startx, y, z);
-            pts->SetPoint(3 + 2*nSteps + i, endx,   y, z);
+            float x = OriginOffset + endOfHead;
+            float y = proportion * 0.5 * cos(theta);
+            float z;
+            if (ConeHead)
+                z = proportion * 0.5 * sin(theta); 
+            else
+                z = 0;
+            pts->SetPoint(3 + i, x, y, z);
         }
-        for (int i=0; i<nSteps; i++)
+        
+        // Draw the stem as a line or cylinder(3d)/rectangle(2d)
+        if (LineStem)
         {
-            pt[0] = 3 + 1*nSteps + i;
-            pt[1] = 3 + 1*nSteps + (i+1)%nSteps;
-            pt[2] = 3 + 2*nSteps + (i+1)%nSteps;
-            pt[3] = 3 + 2*nSteps + i;
-            polys->InsertNextCell(4, pt);
-
-            // If we're in 2D, no point in capping the end of a rectangle
-            // even if "CapEnds" is true.
-            if (CapEnds && ConeHead)
-            {
-                pt[0] = 1;
-                pt[1] = 3 + 1*nSteps + i;
-                pt[2] = 3 + 1*nSteps + (i+1)%nSteps;
-                polys->InsertNextCell(3, pt);
-                if (realHeadSize < cylThickness)
-                {
-                    pt[0] = 2;
-                    pt[1] = 3 + 2*nSteps + (i+1)%nSteps;
-                    pt[2] = 3 + 2*nSteps + i;
-                    polys->InsertNextCell(3, pt);
-                }
-            }
-        }
-    }
-
-    // If asked, draw a head as either a cone or a triangle
-    if (MakeHead)
-    {
-        if (ConeHead)
-        {
-            for (int i=0; i<nSteps; i++)
-            {
-                pt[0] = 0;
-                pt[1] = 3 + i;
-                pt[2] = 3 + (i+1)%nSteps;
-                polys->InsertNextCell(3, pt);
-                if (CapEnds)
-                {
-                    pt[0] = 2;
-                    pt[1] = 3 + (i+1)%nSteps;
-                    pt[2] = 3 + i;
-                    polys->InsertNextCell(3, pt);
-                }
-            }
+            lines->InsertNextCell(2);
+            lines->InsertCellPoint(0);
+            lines->InsertCellPoint(1);
         }
         else
         {
-            pt[0] = 0;
-            pt[1] = 3;
-            pt[2] = 4;
-            polys->InsertNextCell(3, pt);
+            float startx = OriginOffset - 0.5;
+            float endx;
+            // If we're in 3D and not capping the cone's base, then extend the
+            // cylinder all the way to where it intersects the cone's facets.
+            // Otherwise, just stop at the base of the cone.
+            if (ConeHead && !CapEnds)
+            {
+                float len = (realHeadSize<cylThickness)?realHeadSize:cylThickness;
+                endx = OriginOffset + .5 - len;
+            }
+            else
+            {
+                endx = OriginOffset + endOfHead;
+            }
+            
+            for (int i=0; i<nSteps; i++)
+            {
+                float theta = 2*M_PI * float(i)/float(nSteps);
+                float y = cylThickness * 0.5 * cos(theta);
+                float z = cylThickness * 0.5 * sin(theta); 
+                pts->SetPoint(3 + 1*nSteps + i, startx, y, z);
+                pts->SetPoint(3 + 2*nSteps + i, endx,   y, z);
+            }
+            for (int i=0; i<nSteps; i++)
+            {
+                pt[0] = 3 + 1*nSteps + i;
+                pt[1] = 3 + 1*nSteps + (i+1)%nSteps;
+                pt[2] = 3 + 2*nSteps + (i+1)%nSteps;
+                pt[3] = 3 + 2*nSteps + i;
+                polys->InsertNextCell(4, pt);
+                
+                // If we're in 2D, no point in capping the end of a rectangle
+                // even if "CapEnds" is true.
+                if (CapEnds && ConeHead)
+                {
+                    pt[0] = 1;
+                    pt[1] = 3 + 1*nSteps + i;
+                    pt[2] = 3 + 1*nSteps + (i+1)%nSteps;
+                    polys->InsertNextCell(3, pt);
+                    if (realHeadSize < cylThickness)
+                    {
+                        pt[0] = 2;
+                        pt[1] = 3 + 2*nSteps + (i+1)%nSteps;
+                        pt[2] = 3 + 2*nSteps + i;
+                        polys->InsertNextCell(3, pt);
+                    }
+                }
+            }
         }
+        
+        // If asked, draw a head as either a cone or a triangle
+        if (MakeHead)
+        {
+            if (ConeHead)
+            {
+                for (int i=0; i<nSteps; i++)
+                {
+                    pt[0] = 0;
+                    pt[1] = 3 + i;
+                    pt[2] = 3 + (i+1)%nSteps;
+                    polys->InsertNextCell(3, pt);
+                    if (CapEnds)
+                    {
+                        pt[0] = 2;
+                        pt[1] = 3 + (i+1)%nSteps;
+                        pt[2] = 3 + i;
+                        polys->InsertNextCell(3, pt);
+                    }
+                }
+            }
+            else
+            {
+                pt[0] = 0;
+                pt[1] = 3;
+                pt[2] = 4;
+                polys->InsertNextCell(3, pt);
+            }
+        }
+    }
+    else
+    {
+        vtkSphereSource *sphere = vtkSphereSource::New();
+        int res = (HighQuality ? 40 : 20);
+        sphere->SetThetaResolution(res);
+        sphere->SetPhiResolution(res);
+        sphere->SetCenter(OriginOffset, OriginOffset, OriginOffset);
+        sphere->SetRadius(1.0);
+        sphere->Update();
+        
+        vtkTransformFilter *xform = vtkTransformFilter::New();
+        vtkTransform *trn = vtkTransform::New();
+        trn->Scale(1.0, 0.3333, 0.3333);
+        xform->SetTransform(trn);
+        
+        xform->SetInput(sphere->GetOutput());
+        xform->Update();
+        vtkPolyData *spoly = (vtkPolyData *)xform->GetOutput();
+        int np = spoly->GetPoints()->GetNumberOfPoints();
+        pts->SetNumberOfPoints(np);
+        
+        //set points.
+        for (int i = 0; i < np; i++)
+            pts->SetPoint(i, spoly->GetPoints()->GetPoint(i));
+        
+        //set polys.
+        np = spoly->GetPolys()->GetNumberOfCells();
+        for (int i = 0; i < np; i++)
+        {
+            vtkIdType n, *p;
+            spoly->GetPolys()->GetNextCell(n, p);
+            polys->InsertNextCell(n, p);
+        }
+        sphere->Delete();
+        xform->Delete();
+        trn->Delete();
     }
 }
 
-  
 // ****************************************************************************
 //  Modifications:
 //    Jeremy Meredith, Mon Mar 19 14:33:15 EDT 2007
 //    Added settings to draw the stem as a cylinder (vs a line), and its
 //    width, to increase the number of polygons, and to cap the ends of
 //    the cone/cylinder.
+//    
+//    Dave Pugmire, Mon Jul 19 09:38:17 EDT 2010
+//    Add ellipsoid glyphing.        
 // ****************************************************************************
 void vtkVectorGlyph::PrintSelf(ostream &os, vtkIndent indent)
 {
    this->Superclass::PrintSelf(os, indent);
+   os << indent << "Arrow:    " << this->Arrow << "\n";
    os << indent << "MakeHead: " << this->MakeHead << "\n";
    os << indent << "ConeHead: " << this->ConeHead << "\n";
    os << indent << "LineStem: " << this->LineStem << "\n";
    os << indent << "StemWidth: " << this->StemWidth << "\n";
-   os << indent << "CapEnds: " << this->CapEnds << "\n";
+   os << indent << "CapEnds:  " << this->CapEnds << "\n";
    os << indent << "HighQuality: " << this->HighQuality << "\n";
    os << indent << "Relative Size of Heads: " << this->HeadSize << "\n";
 }
