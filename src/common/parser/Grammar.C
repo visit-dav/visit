644a6208aef364f7c915a1f5f6436f0037f37001
Eric Brugger <brugger1@llnl.gov>
2010-07-20 16:01:44 +0000

    I updated the LLNL review and release number in the copyright notice in all the "*.C", "*.h", "*.java", "*.cmake", "*.txt" and "*.in" files in the src directory.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@11946 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/common/parser/Grammar.C
+++ src/common/parser/Grammar.C
@@ -2,7 +2,7 @@
 *
 * Copyright (c) 2000 - 2010, Lawrence Livermore National Security, LLC
 * Produced at the Lawrence Livermore National Laboratory
-* LLNL-CODE-400124
+* LLNL-CODE-442911
 * All rights reserved.
 *
 * This file is  part of VisIt. For  details, see https://visit.llnl.gov/.  The
@@ -478,7 +478,7 @@ Grammar::WriteStateInitialization(const string &name, ostream &o)
     o << "*" << endl;
     o << "* Copyright (c) 2000 - 2010, Lawrence Livermore National Security, LLC" << endl;
     o << "* Produced at the Lawrence Livermore National Laboratory" << endl;
-    o << "* LLNL-CODE-400124" << endl;
+    o << "* LLNL-CODE-442911" << endl;
     o << "* All rights reserved." << endl;
     o << "*" << endl;
     o << "* This file is  part of VisIt. For  details, see https://visit.llnl.gov/.  The" << endl;
