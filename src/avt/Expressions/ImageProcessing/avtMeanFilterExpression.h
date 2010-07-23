2177026c54d5f1072a228762c1f5dd44ba831a60
Gunther Weber <ghweber@lbl.gov>
2010-07-23 02:07:40 +0000

    Added 'secret' parameter to mean filter expression to specify filter width. Added Larry Pezzaglia's modification for running on 'nautilus' to internallauncher. Updated configuration for fry.lbl.gov.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.0RC@12018 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/Expressions/ImageProcessing/avtMeanFilterExpression.h
+++ src/avt/Expressions/ImageProcessing/avtMeanFilterExpression.h
@@ -58,6 +58,12 @@ class     vtkDataArray;
 //  Programmer: Hank Childs
 //  Creation:   August 14, 2005
 //
+//  Modifications:
+//    Gunther H. Weber, Thu Mar  5 13:13:49 PST 2009
+//    Added ability to specify filter width as argument (adding private
+//    variable width[3] and the method ProcessArguments())
+//    
+//
 // ****************************************************************************
 
 class EXPRESSION_API avtMeanFilterExpression : public avtUnaryMathExpression
@@ -70,6 +76,7 @@ class EXPRESSION_API avtMeanFilterExpression : public avtUnaryMathExpression
                                  { return "avtMeanFilterExpression"; };
     virtual const char       *GetDescription(void)
                                  { return "Taking mean filter"; };
+    virtual void              ProcessArguments(ArgsExpr*, ExprPipelineState *);
 
   protected:
     bool             haveIssuedWarning;
@@ -77,6 +84,9 @@ class EXPRESSION_API avtMeanFilterExpression : public avtUnaryMathExpression
     virtual void     PreExecute(void);
     virtual void     DoOperation(vtkDataArray *in1, vtkDataArray *out, 
                                  int ncomps, int ntuples);
+
+  private:
+    int              width[3];
 };
 
 
