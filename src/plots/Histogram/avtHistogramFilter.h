ed7da8e34e232739e6b5ccbf8daa0e091e1b2b77
Eric Brugger <brugger1@llnl.gov>
2010-08-02 20:35:07 +0000

    I corrected a problem with the histogram plot that got exposed when the Silo plugin was modified to no longer convert raw data to single precision. The problem was that the histogram plot stored the extents of the data as double but the data was processed as float. This could lead to the situation where the float data was outside of the range stored as double causing zones to be erroneously skipped. The solution was to store the extents as float. This corrects the QueryHistogram test in the queries/database.py test in the test suite.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.0RC@12079 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/plots/Histogram/avtHistogramFilter.h
+++ src/plots/Histogram/avtHistogramFilter.h
@@ -75,6 +75,11 @@
 //    Hank Childs, Tue Dec 11 20:25:22 PST 2007
 //    Added support for weighting by a variable.
 //
+//    Eric Brugger, Mon Aug  2 13:00:36 PDT 2010
+//    I changed the data type of workingRange from double to float to avoid
+//    a problem where the raw data to be processed may be outside of the
+//    working range.
+//
 // ****************************************************************************
 
 class avtHistogramFilter : public avtDataTreeIterator
@@ -94,7 +99,8 @@ class avtHistogramFilter : public avtDataTreeIterator
     float                    *bins;
     float                     binStep, logBinStep, sqrtBinStep;
     double                    dataValueRange[2];
-    double                    workingRange[2], logWorkingRange[2], sqrtWorkingRange[2];
+    float                     workingRange[2];
+    double                    logWorkingRange[2], sqrtWorkingRange[2];
     int                       workingNumBins;
 
     virtual vtkDataSet       *ExecuteData(vtkDataSet *, int, std::string);
