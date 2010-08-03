3e0b600209009650f4cec5d8fc8457539e19875c
Eric Brugger <brugger1@llnl.gov>
2010-08-03 18:11:12 +0000

    1) I changed the date on the splash screen to be August, 2010.
    2) I changed the open install script to no longer install on xchem or
       ellipse by default.
    
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.0RC@12099 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/gui/SplashScreen.C
+++ src/gui/SplashScreen.C
@@ -276,7 +276,7 @@ SplashScreen::SplashScreen(bool cyclePictures)
            << tr("October")
            << tr("November")
            << tr("December");
-    int currentMonth = 7;
+    int currentMonth = 8;
     lLayout->addWidget(new QLabel(versionText, this));
     lLayout->addWidget(new QLabel(months[currentMonth-1] + " 2010", this));
 
