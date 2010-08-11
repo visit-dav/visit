4851704eb75639c8d4c3e3e8a8092b2cc1b2b685
Eric Brugger <brugger1@llnl.gov>
2010-08-11 22:03:09 +0000

    I added more test cases for the XRay Image query. They consisted of tests for unstructured grids with tetrahedra, pyramids, wedges and hexahedra.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@12206 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/queries/xrayimage.py
+++ test/tests/queries/xrayimage.py
@@ -74,4 +74,96 @@ Test("xrayimage05", 0, 1)
 s = GetQueryOutputString()
 TestText("xrayimage06", s)
 
+Query("XRay Image", "png", 1, 0., 0., 0., 90, 0, 10., 10., 300, 300, ("d", "p"))
+
+os.rename("output00.png", "current/queries/xrayimage/xrayimage07.png")
+Test("xrayimage07", 0, 1)
+
+s = GetQueryOutputString()
+TestText("xrayimage08", s)
+
+#
+# Test an unstructured grid with scalars.
+#
+DefineScalarExpression("u1", 'recenter(((u+10.)*0.01), "zonal")')
+DefineScalarExpression("v1", 'recenter(((v+10.)*0.01*matvf(mat1,1)), "zonal")')
+DefineScalarExpression("v2", 'recenter(((v+10.)*0.01*matvf(mat1,2)), "zonal")')
+DefineScalarExpression("v3", 'recenter(((v+10.)*0.01*matvf(mat1,3)), "zonal")')
+DefineScalarExpression("v4", 'recenter(((v+10.)*0.01*matvf(mat1,4)), "zonal")')
+DefineScalarExpression("w1", 'recenter(((w+10.)*0.01), "zonal")')
+
+DeleteAllPlots()
+
+OpenDatabase("../data/silo_%s_test_data/globe.silo"%SILO_MODE)
+AddPlot("Pseudocolor", "u")
+DrawPlots()
+
+# Do tets.
+Query("XRay Image", "png", 1, 0., 0., 0., 0, 0, 1., 1., 300, 300, ("w1", "v1"))
+
+os.rename("output00.png", "current/queries/xrayimage/xrayimage09.png")
+Test("xrayimage09", 0, 1)
+
+s = GetQueryOutputString()
+TestText("xrayimage10", s)
+
+Query("XRay Image", "png", 1, 0., 0., 0., 90, 0, 4., 4., 300, 300, ("w1", "v1"))
+
+os.rename("output00.png", "current/queries/xrayimage/xrayimage11.png")
+Test("xrayimage11", 0, 1)
+
+s = GetQueryOutputString()
+TestText("xrayimage12", s)
+
+# Do pyramids.
+Query("XRay Image", "png", 1, 0., 0., 0., 0, 0, 4., 4., 300, 300, ("w1", "v2"))
+
+os.rename("output00.png", "current/queries/xrayimage/xrayimage13.png")
+Test("xrayimage13", 0, 1)
+
+s = GetQueryOutputString()
+TestText("xrayimage14", s)
+
+Query("XRay Image", "png", 1, 0., 0., 0., 90, 0, 4., 4., 300, 300, ("w1", "v2"))
+
+os.rename("output00.png", "current/queries/xrayimage/xrayimage15.png")
+Test("xrayimage15", 0, 1)
+
+s = GetQueryOutputString()
+TestText("xrayimage16", s)
+
+# Do wedges.
+Query("XRay Image", "png", 1, 0., 0., 0., 0, 0, 8., 8., 300, 300, ("w1", "v3"))
+
+os.rename("output00.png", "current/queries/xrayimage/xrayimage17.png")
+Test("xrayimage17", 0, 1)
+
+s = GetQueryOutputString()
+TestText("xrayimage18", s)
+
+Query("XRay Image", "png", 1, 0., 0., 0., 90, 0, 20., 20., 300, 300, ("w1", "v3"))
+
+os.rename("output00.png", "current/queries/xrayimage/xrayimage19.png")
+Test("xrayimage19", 0, 1)
+
+s = GetQueryOutputString()
+TestText("xrayimage20", s)
+
+# Do hexes.
+Query("XRay Image", "png", 1, 0., 0., 0., 0, 0, 20., 20., 300, 300, ("w1", "v4"))
+
+os.rename("output00.png", "current/queries/xrayimage/xrayimage21.png")
+Test("xrayimage21", 0, 1)
+
+s = GetQueryOutputString()
+TestText("xrayimage22", s)
+
+Query("XRay Image", "png", 1, 0., 0., 0., 90, 0, 20., 20., 300, 300, ("w1", "v4"))
+
+os.rename("output00.png", "current/queries/xrayimage/xrayimage23.png")
+Test("xrayimage23", 0, 1)
+
+s = GetQueryOutputString()
+TestText("xrayimage24", s)
+
 Exit()
