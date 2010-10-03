82e5e858010b2b81c9da02826469589ae0740173
Hank Childs <hank@uoregon.edu>
2010-10-03 04:36:17 +0000

    Add new streamline regression tests.    Also update streamlines.py for changes in plot attributes.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@12665 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ test/tests/plots/sl_integration.py
@@ -0,0 +1,102 @@
+# ----------------------------------------------------------------------------
+#  CLASSES: nightly
+#
+#  Test Case:  sl_integration.py
+#
+#  Defect ID:  -
+#
+#  Programmer: Hank Childs
+#  Date:       October 2, 2010
+#
+# ----------------------------------------------------------------------------
+
+OpenDatabase("../data/silo_%s_test_data/noise.silo"%SILO_MODE)
+
+AddPlot("Streamline", "grad", 1, 0)
+
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.270729, 0.624198, 0.732859)
+View3DAtts.focus = (0.496062, 0.99603, 0.496062)
+View3DAtts.viewUp = (-0.0922782, 0.774611, -0.62567)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 12.1829
+View3DAtts.nearPlane = -24.3658
+View3DAtts.farPlane = 24.3658
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+SetView3D(View3DAtts)
+
+StreamlineAtts = StreamlineAttributes()
+StreamlineAtts.sourceType = StreamlineAtts.SpecifiedPlane  # SpecifiedPoint, SpecifiedPointList, SpecifiedLine, SpecifiedCircle, SpecifiedPlane, SpecifiedSphere, SpecifiedBox
+StreamlineAtts.limitMaximumTimestep = 1
+StreamlineAtts.maxTimeStep = 0.02
+StreamlineAtts.termination = 200
+StreamlineAtts.planeOrigin = (0.5, 1, 0.5)
+StreamlineAtts.planeNormal = (0, 1, 0)
+StreamlineAtts.planeUpAxis = (1, 0, 0)
+StreamlineAtts.sampleDensity0 = 5
+StreamlineAtts.sampleDensity1 = 5
+StreamlineAtts.sampleDistance0 = 18
+StreamlineAtts.sampleDistance1 = 18
+StreamlineAtts.displayMethod = StreamlineAtts.Tubes  # Lines, Tubes, Ribbons
+StreamlineAtts.streamlineDirection = StreamlineAtts.Both  # Forward, Backward, Both
+StreamlineAtts.relTol = 1e-06
+StreamlineAtts.absTolSizeType = StreamlineAtts.Absolute
+StreamlineAtts.absTolAbsolute = 1e-07
+StreamlineAtts.terminationType = StreamlineAtts.Time  # Distance, Time, Step
+StreamlineAtts.integrationType = StreamlineAtts.DormandPrince  # DormandPrince, AdamsBashforth, M3DC1Integrator
+StreamlineAtts.maxStreamlineProcessCount = 10
+StreamlineAtts.maxDomainCacheSize = 3
+StreamlineAtts.workGroupSize = 32
+StreamlineAtts.coloringMethod = StreamlineAtts.ColorByTime
+
+# test defaults
+SetPlotOptions(StreamlineAtts)
+DrawPlots()
+Test( "sl_integration_01" )
+
+StreamlineAtts.maxTimeStep = 0.05
+SetPlotOptions(StreamlineAtts)
+Test( "sl_integration_02" )
+
+StreamlineAtts.limitMaximumTimestep = 0
+SetPlotOptions(StreamlineAtts)
+Test( "sl_integration_03" )
+
+StreamlineAtts.absTolAbsolute = 1e-03
+SetPlotOptions(StreamlineAtts)
+Test( "sl_integration_04" )
+
+StreamlineAtts.absTolSizeType = StreamlineAtts.FractionOfBBox
+SetPlotOptions(StreamlineAtts)
+Test( "sl_integration_05" )
+
+StreamlineAtts.absTolBBox = 1e-09
+SetPlotOptions(StreamlineAtts)
+Test( "sl_integration_06" )
+
+StreamlineAtts.absTolBBox = 1e-04
+SetPlotOptions(StreamlineAtts)
+Test( "sl_integration_07" )
+
+StreamlineAtts.integrationType = StreamlineAtts.AdamsBashforth
+SetPlotOptions(StreamlineAtts)
+Test( "sl_integration_08" )
+
+StreamlineAtts.maxStepLength = 0.2
+SetPlotOptions(StreamlineAtts)
+Test( "sl_integration_09" )
+
+StreamlineAtts.maxStepLength = 1.0
+SetPlotOptions(StreamlineAtts)
+Test( "sl_integration_10" )
+
+StreamlineAtts.maxStepLength = 0.01
+SetPlotOptions(StreamlineAtts)
+Test( "sl_integration_11" )
+
+Exit()
---
+++ test/tests/plots/streamline_geom.py
@@ -0,0 +1,119 @@
+# ----------------------------------------------------------------------------
+#  CLASSES: nightly
+#
+#  Test Case:  streamline_geom.py
+#
+#  Defect ID:  -
+#
+#  Programmer: Hank Childs
+#  Date:       October 2, 2010
+#
+# ----------------------------------------------------------------------------
+
+OpenDatabase("../data/silo_%s_test_data/noise.silo"%SILO_MODE)
+
+AddPlot("Streamline", "grad", 1, 0)
+
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.270729, 0.624198, 0.732859)
+View3DAtts.focus = (0.496062, 0.99603, 0.496062)
+View3DAtts.viewUp = (-0.0922782, 0.774611, -0.62567)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 12.1829
+View3DAtts.nearPlane = -24.3658
+View3DAtts.farPlane = 24.3658
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+SetView3D(View3DAtts)
+
+StreamlineAtts = StreamlineAttributes()
+StreamlineAtts.sourceType = StreamlineAtts.SpecifiedPlane  # SpecifiedPoint, SpecifiedPointList, SpecifiedLine, SpecifiedCircle, SpecifiedPlane, SpecifiedSphere, SpecifiedBox
+StreamlineAtts.limitMaximumTimestep = 1
+StreamlineAtts.maxTimeStep = 0.02
+StreamlineAtts.termination = 200
+StreamlineAtts.planeOrigin = (0.5, 1, 0.5)
+StreamlineAtts.planeNormal = (0, 1, 0)
+StreamlineAtts.planeUpAxis = (1, 0, 0)
+StreamlineAtts.sampleDensity0 = 5
+StreamlineAtts.sampleDensity1 = 5
+StreamlineAtts.sampleDistance0 = 18
+StreamlineAtts.sampleDistance1 = 18
+StreamlineAtts.displayMethod = StreamlineAtts.Tubes  # Lines, Tubes, Ribbons
+StreamlineAtts.streamlineDirection = StreamlineAtts.Both  # Forward, Backward, Both
+StreamlineAtts.relTol = 1e-06
+StreamlineAtts.absTolSizeType = StreamlineAtts.Absolute
+StreamlineAtts.absTolAbsolute = 1e-07
+StreamlineAtts.terminationType = StreamlineAtts.Time  # Distance, Time, Step
+StreamlineAtts.integrationType = StreamlineAtts.DormandPrince  # DormandPrince, AdamsBashforth, M3DC1Integrator
+StreamlineAtts.maxStreamlineProcessCount = 10
+StreamlineAtts.maxDomainCacheSize = 3
+StreamlineAtts.workGroupSize = 32
+StreamlineAtts.coloringMethod = StreamlineAtts.ColorByTime
+
+# test defaults
+SetPlotOptions(StreamlineAtts)
+DrawPlots()
+Test( "streamline_geom_01" )
+
+StreamlineAtts.seedRadiusBBox = 0.03
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_02" )
+
+StreamlineAtts.seedRadiusAbsolute = 0.6
+StreamlineAtts.seedRadiusSizeType = StreamlineAtts.Absolute
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_03" )
+
+StreamlineAtts.displayMethod = StreamlineAtts.Tubes
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_04" )
+
+StreamlineAtts.tubeRadiusBBox = 0.01
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_05" )
+
+StreamlineAtts.tubeRadiusAbsolute = 0.2
+StreamlineAtts.tubeSizeType = StreamlineAtts.Absolute
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_06" )
+
+StreamlineAtts.displayMethod = StreamlineAtts.Ribbons
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_07" )
+
+StreamlineAtts.ribbonWidthBBox = 0.02
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_08" )
+
+StreamlineAtts.ribbonWidthAbsolute = 0.4
+StreamlineAtts.ribbonWidthSizeType = StreamlineAtts.Absolute
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_09" )
+
+StreamlineAtts.displayMethod = StreamlineAtts.Lines
+StreamlineAtts.showHeads = 1
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_10" )
+
+StreamlineAtts.headRadiusBBox = 0.04
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_11" )
+
+StreamlineAtts.headRadiusAbsolute = 0.8
+StreamlineAtts.headRadiusSizeType = StreamlineAtts.Absolute
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_12" )
+
+StreamlineAtts.headDisplayType = StreamlineAtts.Cone
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_13" )
+
+StreamlineAtts.headHeightRatio = 3
+SetPlotOptions(StreamlineAtts)
+Test( "streamline_geom_14" )
+
+Exit()
