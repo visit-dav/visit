478a9bb801d319429fc820abf43dbc941dfd6e2f
Hank Childs <hank@uoregon.edu>
2010-10-09 17:57:52 +0000

    Update regression tests for changes in streamline attributes for termination. Add regression tests for termination criteria of streamlines.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@12717 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/plots/sl_termination.py
+++ test/tests/plots/sl_termination.py
@@ -1,12 +1,12 @@
 # ----------------------------------------------------------------------------
 #  CLASSES: nightly
 #
-#  Test Case:  streamline_geom.py
+#  Test Case:  sl_termination.py
 #
 #  Defect ID:  -
 #
 #  Programmer: Hank Childs
-#  Date:       October 2, 2010
+#  Date:       October 9, 2010
 #
 # ----------------------------------------------------------------------------
 
@@ -32,11 +32,8 @@ SetView3D(View3DAtts)
 
 StreamlineAtts = StreamlineAttributes()
 StreamlineAtts.sourceType = StreamlineAtts.SpecifiedPlane  # SpecifiedPoint, SpecifiedPointList, SpecifiedLine, SpecifiedCircle, SpecifiedPlane, SpecifiedSphere, SpecifiedBox
-StreamlineAtts.limitMaximumTimestep = 1
-StreamlineAtts.maxTimeStep = 0.02
 StreamlineAtts.terminateByTime = 1
 StreamlineAtts.termTime = 200
-StreamlineAtts.maxSteps = 100000
 StreamlineAtts.planeOrigin = (0.5, 1, 0.5)
 StreamlineAtts.planeNormal = (0, 1, 0)
 StreamlineAtts.planeUpAxis = (1, 0, 0)
@@ -58,63 +55,36 @@ StreamlineAtts.coloringMethod = StreamlineAtts.ColorByTime
 # test defaults
 SetPlotOptions(StreamlineAtts)
 DrawPlots()
-Test( "streamline_geom_01" )
+Test( "sl_termination_01" )
 
-StreamlineAtts.seedRadiusBBox = 0.03
+StreamlineAtts.termTime = 50
 SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_02" )
+Test( "sl_termination_02" )
 
-StreamlineAtts.seedRadiusAbsolute = 0.6
-StreamlineAtts.seedRadiusSizeType = StreamlineAtts.Absolute
+StreamlineAtts.terminateByDistance = 1
+StreamlineAtts.termDistance = 4
 SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_03" )
+Test( "sl_termination_03" )
 
-StreamlineAtts.displayMethod = StreamlineAtts.Tubes
+StreamlineAtts.terminateByDistance = 1
+StreamlineAtts.termDistance = 12
 SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_04" )
+Test( "sl_termination_04" )
 
-StreamlineAtts.tubeRadiusBBox = 0.01
+StreamlineAtts.terminateByTime = 0
 SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_05" )
+Test( "sl_termination_05" )
 
-StreamlineAtts.tubeRadiusAbsolute = 0.2
-StreamlineAtts.tubeSizeType = StreamlineAtts.Absolute
+StreamlineAtts.maxSteps = 40
 SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_06" )
+Test( "sl_termination_06" )
 
-StreamlineAtts.displayMethod = StreamlineAtts.Ribbons
+StreamlineAtts.termDistance = 25
 SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_07" )
+Test( "sl_termination_07" )
 
-StreamlineAtts.ribbonWidthBBox = 0.02
+StreamlineAtts.maxSteps = 10000
 SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_08" )
-
-StreamlineAtts.ribbonWidthAbsolute = 0.4
-StreamlineAtts.ribbonWidthSizeType = StreamlineAtts.Absolute
-SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_09" )
-
-StreamlineAtts.displayMethod = StreamlineAtts.Lines
-StreamlineAtts.showHeads = 1
-SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_10" )
-
-StreamlineAtts.headRadiusBBox = 0.04
-SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_11" )
-
-StreamlineAtts.headRadiusAbsolute = 0.8
-StreamlineAtts.headRadiusSizeType = StreamlineAtts.Absolute
-SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_12" )
-
-StreamlineAtts.headDisplayType = StreamlineAtts.Cone
-SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_13" )
-
-StreamlineAtts.headHeightRatio = 3
-SetPlotOptions(StreamlineAtts)
-Test( "streamline_geom_14" )
+Test( "sl_termination_08" )
 
 Exit()
