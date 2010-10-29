a46103f42db521639f27d36a87bb307341ca92f8
Mark Miller <miller86@llnl.gov>
2010-10-29 20:56:32 +0000

    adding test for DBYieldedNoDataException
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@12899 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ test/tests/databases/empty_db.py
@@ -0,0 +1,20 @@
+# ----------------------------------------------------------------------------
+#  CLASSES: nightly
+#
+#  Test Case:  empty_db.py 
+#
+#  Programmer: Mark C. Miller 
+#  Date:       28Oct10
+#
+# ----------------------------------------------------------------------------
+import re
+
+OpenDatabase("../data/empty.silo")
+errStr = GetLastError()
+tmpType = re.search(",\nno data was found in the file for VisIt to work with.", errStr)
+msg = "UnknownException\n"
+if tmpType != None:
+    msg = "DBYieldedNoDataException\n"
+TestText("empty_01", msg)
+
+Exit()
