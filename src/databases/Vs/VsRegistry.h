7c5a7fb10c7d1d82f1e260c3a142f3e5e24fecf0
Marc Durant <mdurant@txcorp.com>
2010-11-02 20:41:23 +0000

    Improving error handling in Vs plugin - now throws an error if no properly built Vizschema objects are found in the data file
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.1RC@12940 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/databases/Vs/VsRegistry.h
+++ src/databases/Vs/VsRegistry.h
@@ -109,6 +109,7 @@ public:
   void addExpression(std::string name, std::string value);
   void writeAllExpressions();
   std::map<std::string, std::string>* getAllExpressions();
+  int numExpressions();
 
   //Variable components
   void getComponentInfo(std::string componentName, NamePair* namePair);
@@ -125,7 +126,6 @@ public:
   double getStep() { return step; }
     
 private:
-
   /**
    * A boolean flag for when objects are being deleted.
    */
