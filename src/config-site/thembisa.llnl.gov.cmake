859af3feda4d34f93ece61d1e73d20f527263565
Kathleen Bonnell <bonnell2@llnl.gov>
2010-11-03 17:20:39 +0000

    update config-site file for my machine
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@12957 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/thembisa.llnl.gov.cmake
+++ src/config-site/thembisa.llnl.gov.cmake
@@ -1,67 +1,69 @@
+#/usr/gapps/visit/thirdparty_shared/cmake/2.8.0/linux-i686_gcc-4.1/bin/cmake
 ##
-## Set the VISITHOME environment variable.
-##
-SET(VISITHOME /usr/gapps/visit)
-SET(VISITARCH linux_rhel4_gcc_3.4.6)
-
-SET(VISIT_CXX_FLAGS -Wno-deprecated)
-
-VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/5.0/${VISITARCH})
-
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0c/test/${VISITARCH}/lib/vtk-5.0)
-
-VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.4.3/${VISITARCH}/bin)
-
-SET(GLEW_DIR ${VISITHOME}/glew/1.3.4/${VISITARCH})
-
-VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR ${VISITHOME}/cgns/2.4/${VISITARCH})
-
-VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.1/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz)
-
-VISIT_OPTION_DEFAULT(VISIT_HDF4_DIR ${VISITHOME}/hdf4/4.2.1/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_HDF4_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz)
-
-VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.6.2/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
-
-VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR ${VISITHOME}/gdal/1.3.2/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_CCMIO_DIR ${VISITHOME}/ccmio/2.6.1/${VISITARCH})
+## build_visit generated host.cmake
+## created: Mon Nov  1 17:34:37 PDT 2010
+## system: Linux thembisa.llnl.gov 2.6.18-194.11.4.el5PAE #1 SMP Fri Sep 17 05:03:23 EDT 2010 i686 i686 i386 GNU/Linux
+## by: bonnell2
 
 ##
-## Use VisIt's Python.
+## Setup VISITHOME & VISITARCH variables.
 ##
-#PYDIR=$VISITHOME/python/2.5/$VISITARCH
-#PYVERSION=python2.5
+SET(VISITHOME /usr/gapps/visit/thirdparty_shared)
+SET(VISITARCH linux-i686_gcc-4.1)
 
 ##
-## Add parallel arguments.
+## Specify the location of the mesa.
 ##
-SET(VISIT_MPI_LIBRARY_DIR /misc/gapps/mpich/1.2.4/Linux/serial/64/debug/lib)
-SET(VISIT_MPI_CXX_FLAGS "-I/misc/gapps/mpich/1.2.4/Linux/serial/64/debug/include")
-SET(VISIT_MPI_LD_FLAGS "-L${VISIT_MPI_LIBRARY_DIR}")
-SET(VISIT_MPI_LIBS mpich)
-
+VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.8.2/${VISITARCH})
 
 ##
-## If enable_viewer_mesa_stub in not set, then disable it.
+## Specify the location of the vtk.
 ##
-#if test -z "$enable_viewer_mesa_stub"; then
-#   enable_viewer_mesa_stub=no
-#fi
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0g/${VISITARCH}/lib/vtk-5.0/)
 
+##
+## Specify the Qt4 binary dir. 
+## (qmake us used to locate & setup Qt4 dependencies)
+##
+VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.6.1/${VISITARCH}/bin)
+
+##
+## Specify the location of the python.
+##
+VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.6.4/${VISITARCH})
+
+##
+## Compiler flags.
+##
+VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc)
+VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++)
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS "-O2 -fvisibility=hidden")
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS "-O2 -fvisibility=hidden")
+
+##############################################################
 ##
 ## Database reader plugin support libraries
 ##
-###############################################################################
+## For libraries with LIBDEP settings, order matters.
+## Libraries with LIBDEP settings that depend on other
+## Library's LIBDEP settings must come after them.
+##############################################################
+
+##
+## AdvIO
+##
+VISIT_OPTION_DEFAULT(VISIT_ADVIO_DIR ${VISITHOME}/AdvIO/1.2/${VISITARCH}/)
 
 ##
 ## Boxlib
 ##
-#DEFAULT_BOXLIB2D_INCLUDE=$VISITHOME/boxlib/$VISITARCH/include/2D
-#DEFAULT_BOXLIB2D_LIBS=$VISITHOME/boxlib/$VISITARCH/lib
-#DEFAULT_BOXLIB3D_INCLUDE=$VISITHOME/boxlib/$VISITARCH/include/3D
-#DEFAULT_BOXLIB3D_LIBS=$VISITHOME/boxlib/$VISITARCH/lib
+VISIT_OPTION_DEFAULT(VISIT_BOXLIB2D_DIR ${VISITHOME}/boxlib/2010.10.01/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_BOXLIB3D_DIR ${VISITHOME}/boxlib/2010.10.01/${VISITARCH})
+
+##
+## CCMIO
+##
+VISIT_OPTION_DEFAULT(VISIT_CCMIO_DIR ${VISITHOME}/ccmio/2.6.1/${VISITARCH})
 
 ##
 ## CFITSIO
@@ -69,34 +71,65 @@ SET(VISIT_MPI_LIBS mpich)
 VISIT_OPTION_DEFAULT(VISIT_CFITSIO_DIR ${VISITHOME}/cfitsio/3006/${VISITARCH})
 
 ##
-## Exodus
+## CGNS
 ##
-#DEFAULT_EXODUS_INCLUDES=$VISITHOME/exodus/4.46/$VISITARCH/inc
-#DEFAULT_EXODUS_LIBRARY=$VISITHOME/exodus/4.46/$VISITARCH/lib
-
+VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR ${VISITHOME}/cgns/3.0.8/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_CGNS_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
-## H5PART
+## GDAL
 ##
-VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR ${VISITHOME}/h5part/1.4.0/${VISITARCH})
+#VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR ${VISITHOME}/gdal/1.7.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR /scratch/kbonnell/visit/gdal/1.7.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_GDAL_LIBDEP /usr/lib curl)
 
+##
+## HDF5
+##
+VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.4/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz /usr/lib z)
+
+##
+## H5Part
+## Must come AFTER HDF5 to ensure LIBDEP for H5Part gets hdf5 LIBDEPS
+##
+VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR ${VISITHOME}/h5part/1.6.0/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_H5PART_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
 ## Mili
 ##
-#VISIT_MILI_DIR=$VISITHOME/mili/1.10.0/$VISITARCH
+VISIT_OPTION_DEFAULT(VISIT_MILI_DIR ${VISITHOME}/mili/1.10.0/${VISITARCH})
 
 ##
-## netCDF
+## NetCDF
+## Must come AFTER HDF5 to ensure LIBDEP for NetCDF gets hdf5 LIBDEPS
 ##
-VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/3.6.0/${VISITARCH})
-
+VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/4.1.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_NETCDF_LIBDEP HDF5_LIBRARY_DIR hdf5 HDF5_LIBRARY_DIR hdf5_hl ${VISIT_HDF5_LIBDEP} /usr/lib curl)
 
 ##
-## ViSUS 
+## Exodus
+## Must come AFTER netcdf to ensure LIBDEP for Exodus gets netcdf LIBDEPS
 ##
-#DEFAULT_VISUS_INCLUDE=/usr/gapps/visit/visus/$VISITARCH/include
-#DEFAULT_VISUS_LIB=/usr/gapps/visit/visus/$VISITARCH/lib
+VISIT_OPTION_DEFAULT(VISIT_EXODUSII_DIR ${VISITHOME}/exodus/4.46/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_EXODUSII_LIBDEP NETCDF_LIBRARY_DIR netcdf ${VISIT_NETCDF_LIBDEP})
 
+##
+## SZIP
+##
+VISIT_OPTION_DEFAULT(VISIT_SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
 
-#DEFAULT_TCMALLOC_DIR=$VISITHOME/google-perftools/0.97/linux-i686_gcc-3.2.3/lib
+##
+## Silo
+## Must come AFTER HDF5 to ensure LIBDEP for Silo gets hdf5 LIBDEPS
+##
+VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.8/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
+
+##
+## Xdmf
+##
+VISIT_OPTION_DEFAULT(VISIT_XDMF_DIR ${VISITHOME}/Xdmf/2.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_XDMF_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
+
