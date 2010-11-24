f2b8cf159bb23bfe517fcc0dbbc594f8c5792e24
Kathleen Bonnell <bonnell2@llnl.gov>
2010-11-24 19:39:16 +0000

    Update to cgns and boxlib libs on Windows.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@13109 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/deluxe.cmake
+++ src/config-site/deluxe.cmake
@@ -1,2 +1,6 @@
 include(${VISIT_SOURCE_DIR}/config-site/windows.cmake)
+
 # can provide overrides for windows.cmake settings below
+
+VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR     ${VISITHOME}/cgns-3.0.8)
+
