c5f2251e3f5a77282c036459d0340ef6daf1ed29
Dave Pugmire <pugmire@ornl.gov>
2010-12-02 20:04:38 +0000

    more tweaks...
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@13159 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/tools/synthetic_diagnostic/SyntheticDiagnosticApp.C
+++ src/tools/synthetic_diagnostic/SyntheticDiagnosticApp.C
@@ -1,6 +1,7 @@
 #include <SyntheticDiagnosticApp.h>
 #include <VisItViewer.h>
 #include <ViewerMethods.h>
+#include <GlobalAttributes.h>
 
 #include <QButtonGroup>
 #include <QDir>
@@ -16,43 +17,47 @@
 
 // State objects that we use.
 #include <avtDatabaseMetaData.h>
-#include <SaveWindowAttributes.h>
+#include <PlotList.h>
 
 // Include this last since something about it on X11 systems seems to
 // interfere with some of our other enums. X11 headers must have some
 // naughty #define directives.
 #include <vtkQtRenderWindow.h>
 
-#include "CommandParser.h"
+QWidget *
+create_application_main_window(VisItViewer *v)
+{
+    return new SyntheticDiagnosticApp(v);
+}
 
+void
+show_application_main_window(QWidget *w)
+{
+    // Do it like this since virtual show isn't working...
+    SyntheticDiagnosticApp *app = (SyntheticDiagnosticApp *)w;
+    app->show();
+    app->raise();
+}
+
+// Window creation callback. Returns the vis window pointers to VisIt.
 vtkQtRenderWindow *
 SyntheticDiagnosticApp::ReturnVisWin(void *data)
 {
     SyntheticDiagnosticApp *This = (SyntheticDiagnosticApp *)data;
-    return This->viswin;
+    // Return vis window 1, 2, ...
+    int idx = ++This->currentWindow;
+    return (idx <= 3) ? This->viswindows[idx] : NULL;
 }
-
-// ****************************************************************************
-// Method: SyntheticDiagnosticApp::SyntheticDiagnosticApp
-//
-// Purpose: 
-//   Constructor.
-//
-// Arguments:
-//   v : The VisItViewer object that we'll use to control the viewer.
-//
-//   Programmer: Dave Pugmire
-//   Creation:   Wed Nov 24 08:07:28 EST 2010
-//
-// Modifications:
-//
-// ****************************************************************************
 
 SyntheticDiagnosticApp::SyntheticDiagnosticApp(VisItViewer *v) : QMainWindow()
 {
     viewer = v;
-    setWindowTitle(tr("Synthetic Diagnostic Tool"));
-    plotType = 0;
+    windowsAdded = false;
+    currentWindow = 0;
+    for(int i = 0; i < NWINDOWS + 1; ++i)
+        viswindows[i] = 0;
+
+    setWindowTitle(tr("Multiwindow visualization"));
 
     // Create the window.
     QWidget *central = new QWidget(this);
@@ -65,59 +70,28 @@ SyntheticDiagnosticApp::SyntheticDiagnosticApp(VisItViewer *v) : QMainWindow()
     leftLayout->setSpacing(10);
     hLayout->addLayout(leftLayout);
 
-    scalarLabel = new QLabel(tr("Scalar variables"), central);
+    QLabel *scalarLabel = new QLabel(tr("Scalar variables"), central);
     leftLayout->addWidget(scalarLabel);
 
     variables = new QListWidget(central);
     leftLayout->addWidget(variables);
     connect(variables, SIGNAL(currentTextChanged(const QString &)),
-            this, SLOT(changeVariable(const QString &)));
+            this, SLOT(onSelectVariable(const QString &)));
 
-    plotTypeWidget = new QWidget(central);
-    leftLayout->addWidget(plotTypeWidget);
-    QHBoxLayout *ptLayout = new QHBoxLayout(plotTypeWidget);
-    ptLayout->setSpacing(10);
-    ptLayout->addWidget(new QLabel(tr("Plot type"), plotTypeWidget));
-    plotType = new QButtonGroup(plotTypeWidget);
-    QRadioButton *rb = new QRadioButton(tr("Pseudocolor"), plotTypeWidget);
-    plotType->addButton(rb, 0);
-    ptLayout->addWidget(rb);
-    rb = new QRadioButton(tr("Contour"), plotTypeWidget);
-    plotType->addButton(rb, 1);
-    ptLayout->addWidget(rb);
-    connect(plotType, SIGNAL(buttonClicked(int)),
-            this, SLOT(changePlotType(int)));
+    // Create the visualization windows. Do an easy layout in this example.
+    QSplitter *splitter = new QSplitter(central);
+    splitter->setOrientation(Qt::Horizontal);
+    hLayout->addWidget(splitter, 100);
 
-    contourWidget = new QWidget(central);
-    leftLayout->addWidget(contourWidget);
-    QHBoxLayout *cLayout = new QHBoxLayout(contourWidget);
-    cLayout->setSpacing(10);
-    nContours = new QSpinBox(contourWidget);
-    nContours->setRange(1,40);
-    nContours->setValue(10);
-    connect(nContours, SIGNAL(valueChanged(int)),
-            this, SLOT(setNContours(int)));
-    cLayout->addWidget(new QLabel(tr("Number of contours"), contourWidget));
-    cLayout->addWidget(nContours);
+    viswindows[WINDOW_3D] = new vtkQtRenderWindow(splitter);
+    viswindows[WINDOW_3D]->setMinimumSize(QSize(300,400));
 
-    // Create the vis window directly.
-    viswin = new vtkQtRenderWindow(central);
-    viswin->setMinimumSize(QSize(500,500));
-    hLayout->addWidget(viswin, 100);
-
-
-    // Create menus
-    QMenu *fileMenu = menuBar()->addMenu(tr("File"));
-    fileMenu->addAction(tr("Open . . ."), this, SLOT(selectFile()));
-    fileMenu->addAction(tr("Open commands. . ."), this, SLOT(execFile()));
-    fileMenu->addSeparator();
-    fileMenu->addAction(tr("Save window"), this, SLOT(saveWindow()));
-    fileMenu->addSeparator();
-    fileMenu->addAction(tr("Quit"), qApp, SLOT(quit()));
+    viswindows[WINDOW_2D] = new vtkQtRenderWindow(splitter);
+    viswindows[WINDOW_2D]->setMinimumSize(QSize(300,400));
     
-    QMenu *controlsMenu = menuBar()->addMenu(tr("Controls"));
-    controlsMenu->addAction(tr("Open GUI"), this, SLOT(openGUI()));
-
+    viswindows[WINDOW_1D] = new vtkQtRenderWindow(splitter);
+    viswindows[WINDOW_1D]->setMinimumSize(QSize(300,400));
+ 
     //
     // Register a window creation function (before Setup) that will
     // return the vtkQtRenderWindow objects that we've already
@@ -125,62 +99,44 @@ SyntheticDiagnosticApp::SyntheticDiagnosticApp(VisItViewer *v) : QMainWindow()
     //
     viewer->SetWindowCreationCallback(ReturnVisWin, (void *)this);
 
-    // Set the initial widget sensitivity.
-    resetWindow();
+    // Create menus
+    QMenu *fileMenu = menuBar()->addMenu(tr("File"));
+    fileMenu->addAction(tr("Open . . ."), this, SLOT(selectFile()));
+    fileMenu->addSeparator();
+    fileMenu->addAction(tr("Quit"), qApp, SLOT(quit()));
 
-    cmd = 0;
+    QMenu *controlsMenu = menuBar()->addMenu(tr("Controls"));
+    controlsMenu->addAction(tr("Open GUI"), this, SLOT(openGUI()));
+
+
 }
 
 SyntheticDiagnosticApp::~SyntheticDiagnosticApp()
 {
-    delete cmd;
 }
-
-void
-SyntheticDiagnosticApp::resetWindow()
-{
-    variables->blockSignals(true);
-    variables->clear();
-    variables->blockSignals(false);
-
-    plotType->blockSignals(true);
-    plotType->button(0)->setChecked(true);
-    plotType->blockSignals(false);
-
-    scalarLabel->setEnabled(false);
-    variables->setEnabled(false);
-    plotTypeWidget->setEnabled(false);
-    contourWidget->setEnabled(false);
-
-    viewer->Methods()->DeleteActivePlots();
-}
-
-//
-// Qt slots
-//
 
 void
 SyntheticDiagnosticApp::show()
 {
     // Tell the viewer to hide toolbars in all windows.
-    viewer->Methods()->HideToolbars(true);
-    // Tell the viewer to show all windows. This does not show our windows
-    // since our windows are embedded but it does do some extra setup for
-    // the windows and thus needs to be called.
-    viewer->Methods()->ShowAllWindows();
+    viewer->DelayedMethods()->HideToolbars(true);
 
+    if(!windowsAdded)
+    {
+        windowsAdded = true;
+        // Call AddWindow() N-1 times to make sure that VisIt calls the window
+        // creation callback the appropriate number of times.
+        viewer->DelayedMethods()->ToggleLockViewMode();
+        for(int i = 0; i < NWINDOWS-1; ++i)
+        {
+            viewer->DelayedMethods()->AddWindow();
+            viewer->DelayedMethods()->ToggleLockViewMode();
+        }
+    }
+    viewer->DelayedMethods()->ShowAllWindows();
+ 
     QMainWindow::show();
 }
-
-// ****************************************************************************
-// Method: SyntheticDiagnosticApp::selectFile
-//
-//   Programmer: Dave Pugmire
-//   Creation:   Wed Nov 24 08:07:28 EST 2010
-//
-// Modifications:
-//
-// ****************************************************************************
 
 void
 SyntheticDiagnosticApp::selectFile()
@@ -201,8 +157,8 @@ SyntheticDiagnosticApp::openFile(const QString &filename)
 {
     if(!filename.isEmpty())
     {
-        // Open the file.
-        viewer->Methods()->OpenDatabase(filename.toStdString());
+        activeFile = filename;
+        resetWindow();
 
         // Get the file's metadata and populate the variable list.
         const avtDatabaseMetaData *md = viewer->GetMetaData(filename.toStdString());
@@ -211,97 +167,68 @@ SyntheticDiagnosticApp::openFile(const QString &filename)
             variables->blockSignals(true);
             for(int i = 0; i < md->GetNumScalars(); ++i)
                 variables->addItem(md->GetScalar(i)->name.c_str());
-            variables->setCurrentRow(0);
             variables->blockSignals(false);
-            if(md->GetNumScalars() > 0)
-            {
-                variables->setEnabled(true);
-                plotTypeWidget->setEnabled(true);
-                contourWidget->setEnabled(plotType->checkedId() == 1);
-                // Add a plot of the first variable
-                changePlotType(plotType->checkedId());
-            }
-            scalarLabel->setEnabled(true);
-            variables->setEnabled(true);
+            variables->setEnabled(md->GetNumScalars() > 0);
         }
-        else
-            resetWindow();
     }
-    else
-        resetWindow();
 }
 
 void
-SyntheticDiagnosticApp::changeVariable(const QString &var)
+SyntheticDiagnosticApp::resetWindow()
 {
-    viewer->Methods()->ChangeActivePlotsVar(var.toStdString());
-}
-
-void
-SyntheticDiagnosticApp::changeVariableAndUpdate(const QString &var)
-{
-    changeVariable(var);
-
     variables->blockSignals(true);
-    for(int i = 0; i < variables->count(); ++i)
-    {
-        if(variables->item(i)->text() == var)
-        {
-            variables->setCurrentRow(i);
-            break;
-        }
-    }
+    variables->clear();
     variables->blockSignals(false);
+
+    viewer->Methods()->SetActiveWindow(WINDOW_1D);
+    viewer->Methods()->DeleteActivePlots();
+    viewer->Methods()->SetActiveWindow(WINDOW_2D);
+    viewer->Methods()->DeleteActivePlots();
+    viewer->Methods()->SetActiveWindow(WINDOW_3D);
+    viewer->Methods()->DeleteActivePlots();
 }
 
 void
-SyntheticDiagnosticApp::changePlotType(int val)
+SyntheticDiagnosticApp::onSelectVariable(const QString &var)
 {
-    if(variables->currentRow() == -1)
-        return;
+    viewer->State()->GetGlobalAttributes()->SetApplyOperator(false);
 
-    // Determine the variable.
-    std::string var(variables->currentItem()->text().toStdString());
-
-    // Delete the active plots.
-    viewer->Methods()->DeleteActivePlots();
-    if(val == 0)
+    if(viewer->State()->GetPlotList()->GetNumPlots() == 1)
     {
-        int Pseudocolor = viewer->GetPlotIndex("Pseudocolor");
-        viewer->Methods()->AddPlot(Pseudocolor, var);
+        viewer->DelayedMethods()->SetActiveWindow(WINDOW_1D);
+        viewer->DelayedMethods()->ChangeActivePlotsVar(var.toStdString());
+
+        viewer->DelayedMethods()->SetActiveWindow(WINDOW_2D);
+        viewer->DelayedMethods()->ChangeActivePlotsVar(var.toStdString());
+
+        viewer->DelayedMethods()->SetActiveWindow(WINDOW_3D);
+        viewer->DelayedMethods()->ChangeActivePlotsVar(var.toStdString());
     }
     else
     {
-        int Contour = viewer->GetPlotIndex("Contour");
-        viewer->Methods()->AddPlot(Contour, var);
+        viewer->DelayedMethods()->SetActiveWindow(WINDOW_3D);
+        viewer->DelayedMethods()->OpenDatabase(activeFile.toStdString());
+        viewer->DelayedMethods()->DeleteActivePlots();
+        viewer->DelayedMethods()->AddPlot(viewer->GetPlotIndex("Contour"),
+                                          var.toStdString());
+        viewer->DelayedMethods()->DrawPlots();
+        
+
+        viewer->DelayedMethods()->SetActiveWindow(WINDOW_2D);
+        viewer->DelayedMethods()->OpenDatabase(activeFile.toStdString());
+        viewer->DelayedMethods()->DeleteActivePlots();
+        viewer->DelayedMethods()->AddPlot(viewer->GetPlotIndex("Pseudocolor"),
+                                          var.toStdString());
+        viewer->DelayedMethods()->AddOperator(viewer->GetOperatorIndex("Slice"));
+        viewer->DelayedMethods()->DrawPlots();
+
+        viewer->DelayedMethods()->SetActiveWindow(WINDOW_1D);
+        viewer->DelayedMethods()->OpenDatabase(activeFile.toStdString());
+        viewer->DelayedMethods()->DeleteActivePlots();
+        viewer->DelayedMethods()->AddPlot(viewer->GetPlotIndex("Pseudocolor"),
+                                          var.toStdString());
+        viewer->DelayedMethods()->DrawPlots();
     }
-    contourWidget->setEnabled(val == 1);
-    viewer->Methods()->DrawPlots();
-}
-
-void
-SyntheticDiagnosticApp::setNContours(int nc)
-{
-    int Contour = viewer->GetPlotIndex("Contour");
-    AttributeSubject *contourAtts = viewer->DelayedState()->GetPlotAttributes(Contour);
-    if(contourAtts != 0)
-    {
-        contourAtts->SetValue("contourNLevels", nc);
-        contourAtts->Notify();
-        viewer->DelayedMethods()->SetPlotOptions(Contour);
-    }
-}
-
-void
-SyntheticDiagnosticApp::saveWindow()
-{
-    // Set the output format to JPEG
-    SaveWindowAttributes *swa = viewer->State()->GetSaveWindowAttributes();
-    swa->SetFormat(SaveWindowAttributes::JPEG);
-    swa->SetScreenCapture(true);
-
-    // Save the window
-    viewer->Methods()->SaveWindow();
 }
 
 void
@@ -312,32 +239,8 @@ SyntheticDiagnosticApp::openGUI()
 }
 
 void
-SyntheticDiagnosticApp::execFile()
+SyntheticDiagnosticApp::showTheWindow()
 {
-    // Get a filename from the file dialog.
-    QString filename = QFileDialog::getOpenFileName(this,
-               tr("Open File"),
-               QDir::current().path(),
-               tr("command files (*.txt)"));
-        
-    if(!filename.isEmpty())
-    {
-        if(cmd == NULL)
-        {
-            // Create the command parser.
-            cmd = new CommandParser(viewer);
-            connect(cmd, SIGNAL(openFile(const QString &)),
-                    this, SLOT(openFile(const QString &)));
-            connect(cmd, SIGNAL(changeVariable(const QString &)),
-                    this, SLOT(changeVariableAndUpdate(const QString &)));
-            connect(cmd, SIGNAL(changePlotType(int)),
-                    this, SLOT(changePlotType(int)));
-            connect(cmd, SIGNAL(setNContours(int)),
-                    this, SLOT(setNContours(int)));
-            connect(cmd, SIGNAL(saveWindow()),
-                    this, SLOT(saveWindow()));
-        }
-
-        cmd->ProcessCommands(filename);
-    }
+    show();
+    raise();
 }
--- src/tools/synthetic_diagnostic/SyntheticDiagnosticApp.h
+++ src/tools/synthetic_diagnostic/SyntheticDiagnosticApp.h
@@ -1,63 +1,43 @@
-#ifndef SIMPLE_VIS_APP_H
-#define SIMPLE_VIS_APP_H
+#ifndef MULTI_WINDOW_VIS_APP_H
+#define MULTI_WINDOW_VIS_APP_H
 #include <QMainWindow>
-
 class VisItViewer;
-class CommandParser;
-
-class QLabel;
 class QListWidget;
-class QButtonGroup;
-class QSpinBox;
-class QWidget;
 class vtkQtRenderWindow;
 
-// ****************************************************************************
-// Class: SyntheticDiagnosticApp
-//
-// Purpose:
-//
-// Notes:      
-//
-//   Dave Pugmire, Wed Nov 24 08:06:26 EST 2010
-//
-// Modifications:
-//   
-// ****************************************************************************
+#define NWINDOWS           3
+ 
+#define WINDOW_3D 1
+#define WINDOW_2D 2
+#define WINDOW_1D 3
 
 class SyntheticDiagnosticApp : public QMainWindow
 {
     Q_OBJECT
 public:
-    SyntheticDiagnosticApp(VisItViewer *);
+    SyntheticDiagnosticApp(VisItViewer *v);
     virtual ~SyntheticDiagnosticApp();
+
+    // Window creation callback. Returns the vis window pointers to VisIt.
+    static vtkQtRenderWindow *ReturnVisWin(void *data);
 
 public slots:
     virtual void show();
+
 private slots:
     void selectFile();
     void openFile(const QString &);
-    void changeVariable(const QString &);
-    void changeVariableAndUpdate(const QString &);
-    void changePlotType(int);
-    void setNContours(int);
-    void saveWindow();
-    void openGUI();
-    void execFile();
-private:
-    static vtkQtRenderWindow *ReturnVisWin(void *);
+    void onSelectVariable(const QString &var);
     void resetWindow();
-
-    QLabel            *scalarLabel;
-    QListWidget       *variables;
-    QWidget           *plotTypeWidget;
-    QButtonGroup      *plotType;
-    QWidget           *contourWidget;
-    QSpinBox          *nContours;
-    vtkQtRenderWindow *viswin;
+    void openGUI();
+    void showTheWindow();
+private:
+    bool               windowsAdded;
+    QString            activeFile;
     VisItViewer       *viewer;
-
-    CommandParser     *cmd;
+    QListWidget       *variables;
+    int                currentWindow;
+    vtkQtRenderWindow *viswindows[NWINDOWS+1];
 };
 
 #endif
