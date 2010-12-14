38889b2a78bf7386335fdf3d40260b15d3986b06
Eric Brugger <brugger1@llnl.gov>
2010-12-14 00:08:20 +0000

    1) I re-built all the third party libraries on ellipse and updated its    config-site file.
    2) I made some minor changes to the open build and install scripts for
       ellipse and removed references to the up machine from the open install
       script since it no longer exists.
    
    
    
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@13267 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/ellipse.uchicago.edu.cmake
+++ src/config-site/ellipse.uchicago.edu.cmake
@@ -1,25 +1,25 @@
-#/autonfs/home/brugger/visit/cmake/2.8.0/linux-i686-gcc-3.4.6/bin/cmake
+#/home/brugger/visit/cmake/2.8.0/linux-rhel4-ia32_gcc-3.4/bin/cmake
 ##
-## ./build_visit generated host.cmake
-## created: Tue Mar 23 12:31:50 CDT 2010
-## system: Linux ellipse.uchicago.edu 2.6.9-89.0.7.ELsmp #1 SMP Wed Aug 5 14:12:18 EDT 2009 i686 i686 i386 GNU/Linux
+## ./build_visit2_1_2 generated host.cmake
+## created: Mon Dec 13 16:12:34 CST 2010
+## system: Linux ellipse.uchicago.edu 2.6.9-89.0.16.ELsmp #1 SMP Tue Oct 27 03:49:28 EDT 2009 i686 i686 i386 GNU/Linux
 ## by: brugger
 
 ##
 ## Setup VISITHOME & VISITARCH variables.
 ##
-SET(VISITHOME /autonfs/home/brugger/visit)
-SET(VISITARCH linux-i686-gcc-3.4.6)
+SET(VISITHOME /home/brugger/visit)
+SET(VISITARCH linux-rhel4-ia32_gcc-3.4)
 
 ##
 ## Specify the location of the mesa.
 ##
-VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.5/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.8.2/${VISITARCH})
 
 ##
 ## Specify the location of the vtk.
 ##
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0e/${VISITARCH}/lib/vtk-5.0/)
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0g/${VISITARCH}/lib/vtk-5.0/)
 
 ##
 ## Specify the Qt4 binary dir. 
@@ -37,17 +37,9 @@ VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.6.4/${VISITARCH})
 ##
 VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc)
 VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++)
-VISIT_OPTION_DEFAULT(VISIT_C_FLAGS "-O2")
-VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS "-O2")
-
-##
-## Add parallel arguments.
-##
-VISIT_OPTION_DEFAULT(VISIT_PARALLEL ON)
-VISIT_OPTION_DEFAULT(VISIT_MPI_CXX_FLAGS -I/soft/mpich-gm-1.2.7p1.2-intel-r1/include)
-VISIT_OPTION_DEFAULT(VISIT_MPI_C_FLAGS   -I/soft/mpich-gm-1.2.7p1.2-intel-r1/include)
-VISIT_OPTION_DEFAULT(VISIT_MPI_LD_FLAGS  "-L/soft/mpich-gm-1.2.7p1.2-intel-r1/lib -Wl,-rpath,/soft/gm-2.1.29_Linux-r1/lib -L/soft/gm-2.1.29_Linux-r1/lib -L/soft/pvfs-2.6.2-r1/lib -L/soft/pvfs-1.6.3-r1/lib -L/soft/intel-c-9.1.042-f-9.1.036-r1/lib")
-VISIT_OPTION_DEFAULT(VISIT_MPI_LIBS     mpich gm pvfs2 pvfs pthread rt irc)
+VISIT_OPTION_DEFAULT(CMAKE_BUILD_TYPE Release)
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS "")
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS "")
 
 ##############################################################
 ##
@@ -77,6 +69,16 @@ VISIT_OPTION_DEFAULT(VISIT_CFITSIO_DIR ${VISITHOME}/cfitsio/3006/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR ${VISITHOME}/cgns/2.4/${VISITARCH})
 
 ##
+## Exodus
+##
+VISIT_OPTION_DEFAULT(VISIT_EXODUSII_DIR ${VISITHOME}/exodus/4.46/${VISITARCH})
+
+##
+## GDAL
+##
+VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR ${VISITHOME}/gdal/1.7.1/${VISITARCH})
+
+##
 ## HDF4
 ##
 VISIT_OPTION_DEFAULT(VISIT_HDF4_DIR ${VISITHOME}/hdf4/4.2.1/${VISITARCH})
@@ -86,12 +88,13 @@ VISIT_OPTION_DEFAULT(VISIT_HDF4_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz
 ## HDF5
 ##
 VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.4/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz)
+VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz /usr/lib z)
 
 ##
 ## H5Part
 ##
 VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR ${VISITHOME}/h5part/1.6.0/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_H5PART_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
 ## Mili
@@ -113,4 +116,10 @@ VISIT_OPTION_DEFAULT(VISIT_SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
 ##
 VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7.2/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
+
+##
+## Xdmf
+##
+VISIT_OPTION_DEFAULT(VISIT_XDMF_DIR ${VISITHOME}/Xdmf/2.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_XDMF_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
