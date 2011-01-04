203b03f331a1a0b59870f78aa81aa42884037753
Mark Miller <miller86@llnl.gov>
2011-01-04 17:28:38 +0000

    updating for newest libs and optimized builds
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@13413 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/sidious.llnl.gov.cmake
+++ src/config-site/sidious.llnl.gov.cmake
@@ -1,8 +1,8 @@
-#/usr/gapps/visit/thirdparty_shared/cmake/2.8.0/linux-x86_64_gcc-4.1/bin/cmake
+#/usr/gapps/visit/thirdparty_shared/cmake/2.8.3/linux-x86_64_gcc-4.1/bin/cmake
 ##
 ## ./build_visit generated host.cmake
-## created: Mon Sep 27 16:38:06 PDT 2010
-## system: Linux sidious.llnl.gov 2.6.18-194.8.1.el5 #1 SMP Wed Jun 23 10:52:51 EDT 2010 x86_64 x86_64 x86_64 GNU/Linux
+## created: Mon Jan  3 21:11:17 PST 2011
+## system: Linux sidious.llnl.gov 2.6.18-194.17.1.el5 #1 SMP Mon Sep 20 07:12:06 EDT 2010 x86_64 x86_64 x86_64 GNU/Linux
 ## by: miller86
 
 ##
@@ -37,25 +37,24 @@ VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.6.4/${VISITARCH})
 ##
 VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc)
 VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++)
-VISIT_OPTION_DEFAULT(VISIT_C_FLAGS " -m64 -fPIC -g -fvisibility=hidden")
-VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS " -m64 -fPIC -g -Wno-deprecated -fvisibility=hidden")
+VISIT_OPTION_DEFAULT(CMAKE_BUILD_TYPE Release)
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS " -m64 -fPIC -fvisibility=hidden")
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS " -m64 -fPIC -fvisibility=hidden")
 
 ##
 ## Parallel Build Setup.
 ##
 VISIT_OPTION_DEFAULT(VISIT_PARALLEL ON)
-VISIT_OPTION_DEFAULT(VISIT_MPI_CXX_FLAGS "-DHAVE_STDLIB_H=1 -DHAVE_STRING_H=1 -DHAVE_UNISTD_H=1 -DHAVE_STDARG_H=1 -DUSE_STDARG=1 -DMALLOC_RET_VOID=1 -I/usr/gapps/visit/thirdparty_shared/mpich/1.2.7p1/linux-x86_64_gcc-4.1/include")
-VISIT_OPTION_DEFAULT(VISIT_MPI_C_FLAGS   "-DHAVE_STDLIB_H=1 -DHAVE_STRING_H=1 -DHAVE_UNISTD_H=1 -DHAVE_STDARG_H=1 -DUSE_STDARG=1 -DMALLOC_RET_VOID=1 -I/usr/gapps/visit/thirdparty_shared/mpich/1.2.7p1/linux-x86_64_gcc-4.1/include")
-VISIT_OPTION_DEFAULT(VISIT_MPI_LD_FLAGS  "-L/usr/gapps/visit/thirdparty_shared/mpich/1.2.7p1/linux-x86_64_gcc-4.1/lib")
-VISIT_OPTION_DEFAULT(VISIT_MPI_LIBS   mpich)
-
 ## (configured w/ mpi compiler wrapper)
-#VISIT_OPTION_DEFAULT(VISIT_MPI_COMPILER /usr/gapps/visit/thirdparty_shared/mpich/1.2.7p1/linux-x86_64_gcc-4.1/bin/mpicxx)
+VISIT_OPTION_DEFAULT(VISIT_MPI_COMPILER /usr/gapps/visit/thirdparty_shared/mpich/1.2.7p1/linux-x86_64_gcc-4.1/bin/mpicxx)
 
 ##############################################################
 ##
 ## Database reader plugin support libraries
 ##
+## For libraries with LIBDEP settings, order matters.
+## Libraries with LIBDEP settings that depend on other
+## Library's LIBDEP settings must come after them.
 ##############################################################
 
 ##
@@ -83,6 +82,7 @@ VISIT_OPTION_DEFAULT(VISIT_CFITSIO_DIR ${VISITHOME}/cfitsio/3006/${VISITARCH})
 ## CGNS
 ##
 VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR ${VISITHOME}/cgns/3.0.8/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_CGNS_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
 ## HDF4
@@ -98,22 +98,29 @@ VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz
 
 ##
 ## H5Part
+## Must come AFTER HDF5 to ensure LIBDEP for H5Part gets hdf5 LIBDEPS
 ##
 VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR ${VISITHOME}/h5part/1.6.0/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_H5PART_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
+## Mili
+##
+VISIT_OPTION_DEFAULT(VISIT_MILI_DIR ${VISITHOME}/mili/1.10.0/${VISITARCH})
+
+##
 ## NetCDF
+## Must come AFTER HDF5 to ensure LIBDEP for NetCDF gets hdf5 LIBDEPS
 ##
 VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/4.1.1/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_NETCDF_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
 ## Exodus
+## Must come AFTER netcdf to ensure LIBDEP for Exodus gets netcdf LIBDEPS
 ##
 VISIT_OPTION_DEFAULT(VISIT_EXODUSII_DIR ${VISITHOME}/exodus/4.46/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_EXODUSII_LIBDEP NETCDF_LIBRARY_DIR netcdf ${VISIT_NETCDF_LIBDEP})
-
 
 ##
 ## SZIP
@@ -122,8 +129,9 @@ VISIT_OPTION_DEFAULT(VISIT_SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
 
 ##
 ## Silo
+## Must come AFTER HDF5 to ensure LIBDEP for Silo gets hdf5 LIBDEPS
 ##
-VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7.2/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.8/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
@@ -144,7 +152,7 @@ VISIT_OPTION_DEFAULT(VISIT_ICET_DIR ${VISITHOME}/icet/0.5.4/${VISITARCH})
 ITAPS_INCLUDE_DIRECTORIES(MOAB ${VISITHOME}/itaps/MOAB/3.99-10Jun10/${VISITARCH}/include)
 ITAPS_FILE_PATTERNS(MOAB *.cub)
 ITAPS_LINK_LIBRARIES(MOAB iMesh MOAB hdf5 sz z netcdf_c++ netcdf vtkGraphics)
-ITAPS_LINK_DIRECTORIES(MOAB  ${VISITHOME}/itaps/MOAB/3.99-10Jun10/${VISITARCH}/lib  ${VISITHOME}/hdf5/1.8.4/${VISITARCH}/lib  ${VISITHOME}/szip/2.1/${VISITARCH}/lib  ${VISITHOME}/netcdf/3.6.3/${VISITARCH}/lib)
+ITAPS_LINK_DIRECTORIES(MOAB  ${VISITHOME}/itaps/MOAB/3.99-10Jun10/${VISITARCH}/lib  ${VISITHOME}/hdf5/1.8.4/${VISITARCH}/lib  ${VISITHOME}/szip/2.1/${VISITARCH}/lib  ${VISITHOME}/netcdf/4.1.1/${VISITARCH}/lib)
 ## FMDB implementation
 ITAPS_INCLUDE_DIRECTORIES(FMDB ${VISITHOME}/itaps/FMDB/1.1-30Aug10/${VISITARCH}/include/FMDB)
 ITAPS_FILE_PATTERNS(FMDB *.sms)
