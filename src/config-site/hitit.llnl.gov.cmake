5b1154bbbb7a9e2cf27703f0c73ac4e9621b2998
Eric Brugger <brugger1@llnl.gov>
2011-01-04 00:34:00 +0000

    I rebuilt the third party libraries for one of the LLNL machines and updated the config site for it. I also added links to it for a couple of other machines of the same architecture.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.1RC@13392 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/hitit.llnl.gov.cmake
+++ src/config-site/hitit.llnl.gov.cmake
@@ -1,20 +1,25 @@
-#/misc/gapps/visit/cmake/2.8.0/linux-i686_gcc-4.1.2/bin/cmake
+#/misc/gapps/visit/cmake/2.8.0/linux-i686_gcc-4.1/bin/cmake
+##
+## ./build_visit2_1_2 generated host.cmake
+## created: Mon Jan  3 12:27:37 PST 2011
+## system: Linux terminator.isrd.llnl.gov 2.6.18-194.11.4.el5 #1 SMP Fri Sep 17 04:55:26 EDT 2010 i686 i686 i386 GNU/Linux
+## by: brugger1
 
 ##
 ## Setup VISITHOME & VISITARCH variables.
 ##
 SET(VISITHOME /misc/gapps/visit)
-SET(VISITARCH linux-i686_gcc-4.1.2)
+SET(VISITARCH linux-i686_gcc-4.1)
 
 ##
 ## Specify the location of the mesa.
 ##
-VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.5/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.8.2/${VISITARCH})
 
 ##
 ## Specify the location of the vtk.
 ##
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0e/${VISITARCH}/lib/vtk-5.0/)
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0g/${VISITARCH}/lib/vtk-5.0/)
 
 ##
 ## Specify the Qt4 binary dir. 
@@ -32,8 +37,9 @@ VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.6.4/${VISITARCH})
 ##
 VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc)
 VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++)
-VISIT_OPTION_DEFAULT(VISIT_C_FLAGS "-O2")
-VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS "-O2")
+VISIT_OPTION_DEFAULT(CMAKE_BUILD_TYPE Release)
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS " -fvisibility=hidden")
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS " -fvisibility=hidden")
 
 ##############################################################
 ##
@@ -88,6 +94,7 @@ VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz
 ## H5Part
 ##
 VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR ${VISITHOME}/h5part/1.6.0/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_H5PART_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
 ## Mili
@@ -109,3 +116,10 @@ VISIT_OPTION_DEFAULT(VISIT_SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
 ##
 VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7.2/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
+
+##
+## Xdmf
+##
+VISIT_OPTION_DEFAULT(VISIT_XDMF_DIR ${VISITHOME}/Xdmf/2.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_XDMF_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
+
