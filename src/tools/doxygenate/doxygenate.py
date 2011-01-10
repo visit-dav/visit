c51ef4068eca6ee8491a46fc139ef531578a6b4b
Cyrus Harrison <cyrush@llnl.gov>
2011-01-10 22:26:58 +0000

    update doxygenate w/ excludes for new cmake setup, fix some errors with path construction
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@13470 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/tools/doxygenate/doxygenate.py
+++ src/tools/doxygenate/doxygenate.py
@@ -7,22 +7,26 @@ Created: February 24, 2008
 Purpose: Creates a doxygented copy of input source tree and an input Doxyfile.
 
 Usage:   doxygenate [src_dir] [des_dir]
+
+Modifications:
+    Cyrus Harrison,Mon Jan 10 14:25:14 PST 2011
+    Update exludes to include necessary CMake paths, fix some errors with
+    path construction.
 """
 
-import sys
-import os
-import glob
-import shutil
-import subprocess
-import re
-
+import sys,os, glob, shutil, subprocess, re
+from os.path import join as pjoin
 from DocBlock import *
+
+def script_path():
+    return os.path.split(os.path.abspath(__file__))[0]
 
 def check_if_excluded(path):
     """
     Check if path is one we know we dont care about.
     """
-    exclusions= [ "src/ac",
+    exclusions= [ "src/CMake",
+                  "src/_CPack_Packages",
                   "src/bin",
                   "src/archives",
                   "src/config-site",
@@ -38,9 +42,10 @@ def check_if_excluded(path):
                   "src/java",
                   "src/svn_bin",
                   "src/visitpy",
-                  ".svn"]
+                  ".svn",
+                  "CMakeFiles"]
     for e in exclusions:
-        if path.find("/"+e+"/") >=0:
+        if path.find("/"+ e ) >=0:
             return True
     return False
 
@@ -95,9 +100,7 @@ def doxygenate_file(fin,fout):
     """
     Extracts all doc blocks from input and creates a doxygenated output.
     """
-    input  = open(fin)
-    input_lines = input.readlines()
-    input.close()
+    lines = open(fin).readlines()
     # get all doc blocks for the file
     blocks = extract_doc_blocks(fin)
     bidx = 0
@@ -105,22 +108,21 @@ def doxygenate_file(fin,fout):
     lidx = 0
     # create "doxygenated" output
     output = open(fout,"w")
-    for l in input_lines:
+    for l in lines:
         if more_blocks and lidx == blocks[bidx].lclose + 1:
             output.write(blocks[bidx].doxygenate())
             bidx +=1
             more_blocks = bidx < len(blocks)
         output.write(l)
-        lidx +=1       
+        lidx +=1
 
 def doxygenate_source_tree(src,des):
     """
     Creates a doxygenated copy of src in des.
     """
-    if src[-1] != "/": src +="/"
     # create dir in des with same base name as src in des
-    src_base = os.path.split(src[:-1])[1] + "/"
-    des_sub  = des + src_base
+    src_base = os.path.split(src)[1]
+    des_sub  = pjoin(des,src_base)
     # if the des sub dir exists we want to remove it
     if os.path.exists(des_sub):
         shutil.rmtree(des_sub)
@@ -128,15 +130,15 @@ def doxygenate_source_tree(src,des):
     print "[Creating Destination Sub Directory: %s]" % des_sub
     os.mkdir(des_sub)
     # extract *{h,C} files from src, and doxygenate into des
-    files = glob.glob(src + "*.h")
-    files.extend(glob.glob(src + "*.C"))
+    files = glob.glob(pjoin(src,"*.h"))
+    files.extend(glob.glob(pjoin(src,"*.C")))
     for fsrc in files:
         dn,fn = os.path.split(fsrc)
-        fdes = des_sub + fn; 
+        fdes = pjoin(des_sub,fn);
         print "[Doxifing: %s to %s]" % (fsrc,fdes)
         doxygenate_file(fsrc,fdes)
     # Decend to subdirs
-    dirs = [ d +"/" for d in glob.glob(src + "*") if os.path.isdir(d)]
+    dirs = [ d for d in glob.glob(pjoin(src,"*")) if os.path.isdir(d)]
     dirs.sort()
     for d in dirs:
         if check_if_excluded(d): 
@@ -146,6 +148,9 @@ def doxygenate_source_tree(src,des):
             doxygenate_source_tree(d,des_sub)
 
 def prepare_directories(src,des):
+    # get abs path & make sure we have trailing "/" on both src + des.
+    src = os.path.abspath(src) + "/"
+    des = os.path.abspath(des) + "/"
     # make src directory exists
     if not os.path.exists(src):
         print "<Error>: Source Directory: %s does not exist!" % src
@@ -154,36 +159,33 @@ def prepare_directories(src,des):
     if not os.path.isdir(src):
         print "<Error>: Source: %s is not a directory!" % src
         sys.exit(-1)
-    # if the des directory does not exist create it 
+    # if the des directory does not exist create it
     if not os.path.exists(des):
         print "[Creating Destination Directory: %s]" % des
         os.mkdir(des)
     elif not os.path.isdir(des): # check if it exists but is not a directory
         print "<Error>: Destination: %s exists, but is not a directory!" % des
         sys.exit(-1)
-    # make sure we have trailing "/" on both paths
-    if src[-1] != "/": src +="/"
-    if des[-1] != "/": des +="/"
     # at this point we are sure both exist and are directories, make sure
-    # they are not the same directory 
-    if os.path.abspath(src) == os.path.abspath(des):
+    # they are not the same directory
+    if src == des:
         print "<Error>: Source Directory == Destination Directory, ",
         print "Refusing to destroy source tree!"
         sys.exit(-1)
     return src, des
 
-   
+
 def get_visit_version(src):
     """
     Attempts to get the VisIt Version number from the source tree.
     """
     version = "<Unknown>"
-    if os.path.isfile(src +"VERSION"):
-        f = open(src +"VERSION")
+    if os.path.isfile(pjoin(src,"VERSION")):
+        f = open(pjoin(src,"VERSION"))
         version = f.readline().strip()
-        f.close()    
+        f.close()
     return version
-    
+
 def get_visit_svn_rev(src):
     """
     Attempts to get the VisIt SVN Reversion number from the source tree.
@@ -195,30 +197,30 @@ def get_visit_svn_rev(src):
     if not res is None:
         svnrev = result[res.start():res.end()].split()[1].strip()
     return svnrev
-            
+
 def prepare_doxyfile(src,des):
     """
     Creates a Doxyfile from skeleton Doxyfile.in
     Note: This currently assumes doxygenate.py is run in 
-    the src/tools/doxygenate dir 
+    the src/tools/doxygenate dir
     (so it can obtain Doxyfile.in and Index.doxy)
     """
-    f = open("Doxyfile.in")
+    doxyfile = pjoin(script_path(),"Doxyfile.in")
+    f = open(doxyfile)
     data = f.read()
     f.close()
     rpairs =[ ["$$VERSION$$",get_visit_version(src)],
               ["$$SVN_REV$$",get_visit_svn_rev(src)],
               ["$$DOXY_DIR$$",des ],
-              ["$$OUTPUT_DIR$$",des + "doxyout/"]]
+              ["$$OUTPUT_DIR$$",pjoin(des,"doxyout/")] ]
     for p in rpairs:
         data = data.replace(p[0],p[1])
-    f = open("Doxyfile","w")
+    f = open(pjoin(des,"Doxyfile"),"w")
     f.write(data)
     f.close()
-    shutil.copyfile("Index.doxy",des + "Index.doxy")
-            
+    shutil.copyfile(pjoin(script_path(),"Index.doxy"),pjoin(des,"Index.doxy"))
+
 if __name__ == "__main__":
-    # if des dir already exists, prompt user to delete it
     if len(sys.argv) < 3:
         print "usage: doxygenate [src_dir] [des_dir]"
         sys.exit(-1)
