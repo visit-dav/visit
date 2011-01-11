7ca8259cbe2662c941b7e7dbdff0720d17b6192d
Kathleen Bonnell <bonnell2@llnl.gov>
2011-01-11 22:08:46 +0000

    Added tarball for h5part-1.6.2. Updated h5part libs for windows to version 1.6.2
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@13480 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/deluxe.cmake
+++ src/config-site/deluxe.cmake
@@ -2,5 +2,3 @@ include(${VISIT_SOURCE_DIR}/config-site/windows.cmake)
 
 # can provide overrides for windows.cmake settings below
 
-VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR     ${VISITHOME}/cgns-3.0.8)
-
