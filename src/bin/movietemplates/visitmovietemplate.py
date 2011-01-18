a1ef44b1bfb856f7a27b98c9c41d34a469b564e0
Eric Brugger <brugger1@llnl.gov>
2011-01-18 18:18:42 +0000

    1) I updated the copyright dates in the copyright notice in a few files    I missed in my last pass.
    2) I updated the script that updates the copyright notices.
    
    
    
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@13581 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/bin/movietemplates/visitmovietemplate.py
+++ src/bin/movietemplates/visitmovietemplate.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2000 - 2010, The Regents of the University of California
+# Copyright (c) 2000 - 2011, Lawrence Livermore National Security, LLC
 # Produced at the Lawrence Livermore National Laboratory
 # All rights reserved.
 #
