f1963055178f43c45f3893bfb413af01e3cef987
Dave Pugmire <pugmire@ornl.gov>
2011-01-19 18:42:25 +0000

    update.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@13600 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/lens0.cmake
+++ src/config-site/lens0.cmake
@@ -1,63 +1,79 @@
-#/sw/sources/visit/thirdparty/analysis-x64/visit/cmake/2.6.4/linux-x86_64_gcc-4.1.1/bin/cmake
-
-# 1. YOU MUST UPGRADE TO cmake 2.6.4 !!!! This script assumes 2.6.4 but it is unknown whether it exists on your system.
+#/autofs/na1_sw/sources/visit/analysis-x64/thirdparty/visit/cmake/2.8.0/linux-x86_64_gcc-4.2/bin/cmake
 ##
-## build_visit generated host.conf
-## created: Wed Apr 29 08:34:35 EDT 2009
-## system: Linux lens0 2.6.18-92.1.6.el5 #1 SMP Wed Jun 25 12:38:37 EDT 2008 x86_64 x86_64 x86_64 GNU/Linux
+## ./build_visit generated host.cmake
+## created: Wed Sep 22 09:52:31 EDT 2010
+## system: Linux lens-login2 2.6.18-92.1.6.el5 #1 SMP Wed Jun 25 12:38:37 EDT 2008 x86_64 x86_64 x86_64 GNU/Linux
 ## by: pugmire
 
 ##
-## Set the VISITHOME environment variable.
+## Setup VISITHOME & VISITARCH variables.
 ##
-SET(VISITHOME /sw/sources/visit/thirdparty/analysis-x64/visit)
-SET(VISITARCH linux-x86_64_gcc-4.1.1)
+SET(VISITHOME /autofs/na1_sw/sources/visit/analysis-x64/thirdparty/visit)
+SET(VISITARCH linux-x86_64_gcc-4.2)
 SET(VISIT_VERBOSE_MAKEFILE TRUE)
+VISIT_OPTION_DEFAULT(VISIT_PARALLEL ON)
+VISIT_OPTION_DEFAULT(CMAKE_INSTALL_PREFIX /sw/analysis-x64/visit)
+VISIT_OPTION_DEFAULT(CMAKE_BUILD_TYPE Release)
 
-##
-## Specify the location of the mesa include files and libraries.
-##
-VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/5.0/${VISITARCH})
 
-##
-## Specify the location of the vtk include files and libraries.
-##
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0c/${VISITARCH}/lib/vtk-5.0)
-
-##
-## Specify the location of cmake.
-##
 
 
 ##
-## Specify the location of the qt include files and libraries.
+## Specify the location of the mesa.
 ##
-VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/3.3.8/${VISITARCH}/bin)
+VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.8.2/${VISITARCH})
 
 ##
-## Specify the location of the python include and libraries.
+## Specify the location of the vtk.
 ##
-VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.5/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0g/${VISITARCH}/lib/vtk-5.0/)
 
+##
+## Specify the Qt4 binary dir. 
+## (qmake us used to locate & setup Qt4 dependencies)
+##
+VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.6.1/${VISITARCH}/bin)
 
+##
+## Specify the location of the python.
+##
+VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.6.4/${VISITARCH})
+
+##
 ## Compiler flags.
 ##
-SET(VISIT_C_COMPILER gcc)
-SET(VISIT_C_FLAGS "-m64 -fPIC")
-SET(VISIT_CXX_COMPILER g++)
-SET(VISIT_CXX_FLAGS "-m64 -fPIC -pthread")
+VISIT_OPTION_DEFAULT(VISIT_C_COMPILER mpicc)
+VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER mpiCC)
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS " -m64 -fPIC -O2 -fvisibility=hidden")
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS " -m64 -fPIC -O2 -fvisibility=hidden")
 SET(VISIT_MPI_CXX_FLAGS "-I/sw/analysis-x64/ompi/1.2.6/sl5_gcc4.2.0/include")
 
+##Parallel options
+VISIT_OPTION_DEFAULT(VISIT_MPI_CXX_FLAGS "-I/sw/analysis-x64/ompi/1.2.6/sl5_gcc4.2.0/include -pthread")
+VISIT_OPTION_DEFAULT(VISIT_MPI_LD_FLAGS  "-pthread -L/sw/analysis-x64/ompi/1.2.6/sl5_gcc4.2.0/lib -Wl,--export-dynamic")
+VISIT_OPTION_DEFAULT(VISIT_MPI_LIBS mpi_cxx mpi open-rte open-pal dl nsl util m dl)
+
+##############################################################
 ##
 ## Database reader plugin support libraries
 ##
 ##############################################################
 
 ##
+## AdvIO
+##
+#VISIT_OPTION_DEFAULT(VISIT_ADVIO_DIR ${VISITHOME}/AdvIO/1.2/${VISITARCH}/)
+
+##
 ## Boxlib
 ##
 VISIT_OPTION_DEFAULT(VISIT_BOXLIB2D_DIR ${VISITHOME}/boxlib/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_BOXLIB3D_DIR ${VISITHOME}/boxlib/${VISITARCH})
+
+##
+## CCMIO
+##
+VISIT_OPTION_DEFAULT(VISIT_CCMIO_DIR ${VISITHOME}/ccmio/2.6.1/${VISITARCH})
 
 ##
 ## CFITSIO
@@ -73,55 +89,78 @@ VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR ${VISITHOME}/cgns/2.4/${VISITARCH})
 ## Exodus
 ##
 VISIT_OPTION_DEFAULT(VISIT_EXODUSII_DIR ${VISITHOME}/exodus/4.46/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_EXODUSII_LIBDEP NETCDF_LIBRARY_DIR netcdf)
-
-##
-## GDAL
-##
-VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR ${VISITHOME}/gdal/1.3.2/${VISITARCH})
-
-##
-## H5Part
-##
-VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR ${VISITHOME}/h5part/1.4.0/${VISITARCH})
 
 ##
 ## FastBit
 ##
+VISIT_OPTION_DEFAULT(VISIT_FASTBIT_DIR ${VISITHOME}/fastbit/1.0.9/${VISITARCH})
 
 ##
 ## HDF4
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF4_DIR ${VISITHOME}/hdf4/2.4.1/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_HDF4_LIBDEP /usr/lib jpeg)
+VISIT_OPTION_DEFAULT(VISIT_HDF4_DIR ${VISITHOME}/hdf4/4.2.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF4_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz /usr/lib jpeg)
 
 ##
 ## HDF5
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.1/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz)
+VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.4/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz /usr/lib z)
+
+##
+## H5Part
+##
+#VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR ${VISITHOME}/h5part/1.6.0/${VISITARCH})
+#VISIT_OPTION_DEFAULT(VISIT_H5PART_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
 ## Mili
 ##
+#VISIT_OPTION_DEFAULT(VISIT_MILI_DIR ${VISITHOME}/mili/1.10.0/${VISITARCH})
 
 ##
 ## NetCDF
 ##
-VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/3.6.0/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/3.6.3/${VISITARCH})
 
 ##
 ## SZIP
 ##
-##
-## CCMIO
-##
-VISIT_OPTION_DEFAULT(VISIT_CCMIO_DIR ${VISITHOME}/ccmio/2.6.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
 
 ##
 ## Silo
 ##
-VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7.2/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
+## Xdmf
+##
+VISIT_OPTION_DEFAULT(VISIT_XDMF_DIR ${VISITHOME}/Xdmf/2.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_XDMF_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
+
+##
+## Ice-T
+##
+VISIT_OPTION_DEFAULT(VISIT_ICET_DIR ${VISITHOME}/icet/0.5.4/${VISITARCH})
+
+##
+## ITAPS
+##
+## MOAB implementation
+#ITAPS_INCLUDE_DIRECTORIES(MOAB ${VISITHOME}/itaps/MOAB/3.99-10Jun10/${VISITARCH}/include)
+#ITAPS_FILE_PATTERNS(MOAB *.cub)
+#ITAPS_LINK_LIBRARIES(MOAB iMesh MOAB hdf5 sz z netcdf_c++ netcdf vtkGraphics)
+#ITAPS_LINK_DIRECTORIES(MOAB  ${VISITHOME}/itaps/MOAB/3.99-10Jun10/${VISITARCH}/lib  ${VISITHOME}/hdf5/1.8.4/${VISITARCH}/lib  ${VISITHOME}/szip/2.1/${VISITARCH}/lib  ${VISITHOME}/netcdf/3.6.3/${VISITARCH}/lib)
+## FMDB implementation
+#ITAPS_INCLUDE_DIRECTORIES(FMDB ${VISITHOME}/itaps/FMDB/1.1-30Aug10/${VISITARCH}/include/FMDB)
+#ITAPS_FILE_PATTERNS(FMDB *.sms)
+#ITAPS_LINK_LIBRARIES(FMDB FMDB SCORECModel SCORECUtil vtkGraphics)
+#ITAPS_LINK_DIRECTORIES(FMDB ${VISITHOME}/itaps/FMDB/1.1-30Aug10/${VISITARCH}/lib)
+## GRUMMP implementation
+#ITAPS_INCLUDE_DIRECTORIES(GRUMMP ${VISITHOME}/itaps/GRUMMP/0.5.0-03Jun10/${VISITARCH}/include)
+#ITAPS_FILE_PATTERNS(GRUMMP *.bdry *.smesh *.vmesh)
+#ITAPS_LINK_LIBRARIES(GRUMMP iMesh_GRUMMP GR_3D GR_surf GR_2D GR_base SUMAAlog_lite OptMS vtkGraphics)
+#ITAPS_LINK_DIRECTORIES(GRUMMP ${VISITHOME}/itaps/GRUMMP/0.5.0-03Jun10/${VISITARCH}/lib)
+
