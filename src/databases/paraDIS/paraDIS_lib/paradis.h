ce4f234f4c54be2a3c89b1899f31678fe1c05cfb
Rich Cook <rcook@llnl.gov>
2011-01-21 21:41:57 +0000

    remove boost refs from paraDIS_lib
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@13639 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/databases/paraDIS/paraDIS_lib/paradis.h
+++ src/databases/paraDIS/paraDIS_lib/paradis.h
@@ -81,12 +81,7 @@
 #define DEBUG 1
 
 /* now for the API */  
-#include <boost/cstdint.hpp>
-using boost::int8_t;
-using boost::int16_t;
-using boost::int32_t;
-using boost::uint32_t;
-
+#include <stdint.h>
 #include <stdio.h>
 #include <vector>
 #include <set>
