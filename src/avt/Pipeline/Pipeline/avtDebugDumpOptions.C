b826eb9955d4953ad6e47c0daef4d53324218c19
Kathleen Bonnell <bonnell2@llnl.gov>
2011-01-25 15:59:01 +0000

    For windows, change location for -dump results to user's VisIt directory. Fix problem with specifying a -dump location from command-line on windows.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@13697 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/Pipeline/Pipeline/avtDebugDumpOptions.C
+++ src/avt/Pipeline/Pipeline/avtDebugDumpOptions.C
@@ -43,14 +43,26 @@
 #include <avtDebugDumpOptions.h>
 #include <visit-config.h>
 #include <FileFunctions.h>
+#include <InstallationFunctions.h>
 #include <DebugStream.h>
+#ifdef _WIN32
+  #ifndef _WIN32_LEAN_AND_MEAN
+    #define _WIN32_LEAN_AND_MEAN
+  #endif
+  #include <windows.h>
+#endif
 using std::string;
 
 // init static members
 
 bool   avtDebugDumpOptions::doDump = false;
 bool   avtDebugDumpOptions::doDatasetDump = false;
+
+#ifdef _WIN32
+string avtDebugDumpOptions::outputDir = GetUserVisItDirectory();
+#else
 string avtDebugDumpOptions::outputDir = "";
+#endif
 
 
 // ****************************************************************************
@@ -172,28 +184,50 @@ avtDebugDumpOptions::DisableDatasetDump()
 //    Cyrus Harrison, Tue Feb 19 08:29:10 PST 2008
 //    Support for Windows directories. 
 //
+//    Kathleen Bonnell, Mon Jan 24 17:38:51 MST 2011 
+//    Change how existence of directory is tested on Windows. If invalid
+//    directory, set to user's VisIt directory, as "" as the 'launch' directory
+//    may not be valid or writeable.
+//
 // ****************************************************************************
 
 void
 avtDebugDumpOptions::SetDumpDirectory(const string &odir)
 {
     string res_dir = odir;
+    bool invalidDir = false;
     if(res_dir != "")
     {
         // make sure it has a "/" or "\" (windows) suffix
         string slash = string(VISIT_SLASH_STRING);
         if (res_dir[res_dir.length()-1] != slash[0] )
             res_dir += slash;
+
         // make sure the dir exists!
+#ifdef WIN32
+        DWORD resAttrs = GetFileAttributes(res_dir.c_str());
+        if (resAttrs == INVALID_FILE_ATTRIBUTES)
+            invalidDir = true;
+        else if (!(resAttrs & FILE_ATTRIBUTE_DIRECTORY))
+            invalidDir = true;
+#else
         VisItStat_t s;
         VisItStat(res_dir.c_str(), &s);
         mode_t mode = s.st_mode;
-        if(!S_ISDIR(mode))
+        invalidDir = (!S_ISDIR(mode));
+#endif
+        if(invalidDir)
         {
             debug1 << "Debug Dump:: Warning \"" << odir  << "\""
                    << " is not a valid directory." << endl;
+#ifdef _WIN32
+            res_dir = GetUserVisItDirectory();
+            debug1 << "Debug Dump:: Output directory set to user's VisIt"
+                   << " directory."<<endl;
+#else
             // revert to cwd
             res_dir = "";
+#endif
         }
     }
     
