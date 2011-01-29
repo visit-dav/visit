1edef5c0101e5f21aafd32730d1eb2ef206a77ec
Brad Whitlock <bjw@ilight.com>
2011-01-29 00:32:22 +0000

    Initial 2.3.0 splashscreen update.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@13795 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/gui/SplashScreen.C
+++ src/gui/SplashScreen.C
@@ -279,7 +279,7 @@ SplashScreen::SplashScreen(bool cyclePictures)
            << tr("October")
            << tr("November")
            << tr("December");
-    int currentMonth = 1;
+    int currentMonth = 4;
     lLayout->addWidget(new QLabel(versionText, this));
     lLayout->addWidget(new QLabel(months[currentMonth-1] + " 2011", this));
 
