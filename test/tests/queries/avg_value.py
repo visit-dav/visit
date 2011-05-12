df6db80dfee40eba02fcb96f749c5ad9f24a4fcf
Hank Childs <hank@uoregon.edu>
2011-05-12 23:31:31 +0000

    Add baselines for new average value query.  Fix slight change in output in curvature query.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@14799 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ test/tests/queries/avg_value.py
@@ -0,0 +1,46 @@
+# ----------------------------------------------------------------------------
+#  CLASSES: nightly
+#
+#  Test Case:  avg_value.py
+#  Tests:      queries     - average value 
+#
+#  Programmer: Hank Childs
+#  Date:       May 11, 2011
+#
+# ----------------------------------------------------------------------------
+
+TurnOffAllAnnotations()
+
+OpenDatabase("../data/silo_%s_test_data/globe.silo"%SILO_MODE)
+AddPlot("Pseudocolor", "t")
+DrawPlots()
+Query("Average Value")
+t = GetQueryOutputString()
+TestText("avg_value_01", t)
+
+DeleteAllPlots()
+AddPlot("Vector", "vel")
+DrawPlots()
+Query("Average Value")
+t = GetQueryOutputString()
+TestText("avg_value_02", t)
+
+DefineVectorExpression("vel2", "{1, t, 0}")
+ChangeActivePlotsVar("vel2")
+Query("Average Value")
+t = GetQueryOutputString()
+TestText("avg_value_03", t)
+
+OpenDatabase("../data/silo_%s_test_data/wave.visit"%SILO_MODE)
+AddPlot("Pseudocolor", "pressure")
+DrawPlots()
+Query("Average Value")
+t = GetQueryOutputString()
+TestText("avg_value_04", t)
+
+QueryOverTime("Average Value")
+SetActiveWindow(2)
+TurnOffAllAnnotations()
+Test("avg_value_05")
+
+Exit()
