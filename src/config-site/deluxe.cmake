98ff759f3d83daca9c0dc86165b18234bea25a6b
Kathleen Bonnell <bonnell2@llnl.gov>
2011-05-26 18:33:21 +0000

    Fix issues with windows installer: command-line options, silent mode and Silex being preferred over VisIt to open Silo files.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@14891 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/deluxe.cmake
+++ src/config-site/deluxe.cmake
@@ -2,3 +2,11 @@ include(${VISIT_SOURCE_DIR}/config-site/windows.cmake)
 
 # can provide overrides for windows.cmake settings below
 
+# disable some warnings
+# 4244 conversion double to float etc
+# 4305 truncation from 'double' to 'float', etc
+# 4800 'int' forcing value to bool (performance warning)
+ADD_DEFINITIONS(/wd4244 /wd4305 /wd4800)
+
+OPTION(VISIT_INSTALL_THIRD_PARTY "Install VisIt's 3rd party I/O libs and includes to permit plugin development" ON)
+
