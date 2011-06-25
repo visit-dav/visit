e5103d042884f0909d09b4d922979b890ea5c0f9
Allen Sanderson <allen@sci.utah.edu>
2011-06-25 00:16:14 +0000

    added support for parallel with index select and minor bug fixes
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@15210 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/databases/Vs/VsH5Reader.h
+++ src/databases/Vs/VsH5Reader.h
@@ -5,14 +5,16 @@
  *
  * @brief Interface for retrieving structure and data of an hdf5 file.
  * 
- * Top-level class in the VizSchema codebase.  Provides high-level accessors
- * to get data from an hdf5 file in the form of meshes, variables, and attributes.
+ * Top-level class in the VizSchema codebase.  Provides high-level
+ * accessors to get data from an hdf5 file in the form of meshes,
+ * variables, and attributes.
  *
  * Copyright &copy; 2008 by Tech-X Corporation
  */
 
 #include <hdf5.h>
 #include <visit-hdf5.h>
+
 #if HDF5_VERSION_GE(1, 8, 1)
 
 #ifndef VS_H5_READER
@@ -36,12 +38,12 @@ class VsRectilinearMesh;
 
 
 /**
- * VsH5Reader is a class for getting the vizschema metadata of
- * objects in and HDF5 file and also being able to return such objects.
+ * VsH5Reader is a class for getting the vizschema metadata of objects
+ * in and HDF5 file and also being able to return such objects.
  */
 class VsH5Reader {
 
-  public:
+public:
   /**
    * Constructor.
    * @param filename the name of the HDF5 file
@@ -77,52 +79,7 @@ class VsH5Reader {
                      int* destCounts = 0,
                      int* destStrides = 0 ) const;    
 
-  /**
-   * Get the values of the whole variable (dataset).
-   * Caller owns returned array and must allocate memory and delete it.
-   * @param varname the variable name
-   * @param indx index of the component
-   * @param partnumber number of mesh part (along axis with leargest extent to load) for partial loading
-   * @param numparts into how many parts should mesh be split along axis of longest extent
-   * @param splitDims optional... unknown purpose
-   */
-  void* getVariableComponent(const std::string& varname, size_t indx,
-                             size_t partnumber=0, size_t numparts=1,
-                             size_t* splitDims=0);
-
-
-  /**
-   * Get attribute component of a mesh.
-   * @param cname The name of the mesh component to retrieve
-   * @param meta The metadata of the mesh
-   * @param data The return value containing the requested attribute - ownership passes
-   */
-  //herr_t getAttMeshComponent (const std::string& cname, VsMesh& meta,
-  //    void* data) const;
-
-  /**
-   * Get mesh of a var with mesh.
-   * @param meta metadata for the var with mesh
-   * @param data the array to be filled with mesh points
-   */
-  herr_t getVarWithMeshMesh(VsVariableWithMesh& meta, void* data,
-                            size_t partStart, size_t partCount) const;
-
-  /**
-   * Get variable component of a var with mesh
-   * Caller owns data and must allocate and delete it.
-   * @param varname the variable name
-   * @param idx index of component to read
-   * @param data the returned filled array
-   * @return 0 on success
-   */
-  herr_t getVarWithMeshComponent(const std::string& varname, size_t idx,
-      void* data, size_t partStart, size_t partCount) const;
-
-  private:
-  // File id -opened once
- // hid_t fileId;
-  
+private:
   VsH5File* fileData;
   
   static int numInstances;
@@ -131,4 +88,5 @@ class VsH5Reader {
 };
 
 #endif
+
 #endif
