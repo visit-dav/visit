11692ee34c2c6d2dd6c3097a1dda0f026c11fc80
Kathleen Bonnell <bonnell2@llnl.gov>
2011-08-09 16:55:43 +0000

    consistent use of cmake var type for VISIT_OPTION_DEFAULT
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@15712 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/SAGAURO.cmake
+++ src/config-site/SAGAURO.cmake
@@ -1,4 +1,4 @@
 INCLUDE(${VISIT_SOURCE_DIR}/config-site/windows.cmake)
 ADD_DEFINITIONS(/wd4244 /wd4305 /wd4800)
-OPTION(VISIT_INSTALL_THIRD_PARTY "Install VisIt's 3rd party I/O libs and includes to permit plugin development" ON)
+VISIT_OPTION_DEFAULT(VISIT_INSTALL_THIRD_PARTY ON TYPE BOOL)
 
