ea7db46239d15616480b0b3d78e3f42656f3a0c5
Gunther Weber <ghweber@lbl.gov>
2011-08-19 01:17:30 +0000

    Merged 2.3RC into trunk: Added support for reading NumPy files to BOV reader.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@15852 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/databases/BOV/BOVPluginInfo.C
+++ src/databases/BOV/BOVPluginInfo.C
@@ -167,6 +167,7 @@ BOVGeneralPluginInfo::GetDefaultFilePatterns() const
 {
     std::vector<std::string> defaultPatterns;
     defaultPatterns.push_back("*.bov");
+    defaultPatterns.push_back("*.npy");
 
     return defaultPatterns;
 }
