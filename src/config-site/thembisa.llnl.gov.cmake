e63292afaaeeb4c9b1f7149b606aa358626d287d
Kathleen Bonnell <bonnell2@llnl.gov>
2011-10-12 21:02:38 +0000

    udpate my machine's config-site file for vtk 5.8
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@16406 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/thembisa.llnl.gov.cmake
+++ src/config-site/thembisa.llnl.gov.cmake
@@ -19,8 +19,7 @@ VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.10.2/${VISITARCH})
 ##
 ## Specify the location of the vtk.
 ##
-#VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0i/${VISITARCH}/lib/vtk-5.0/)
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR /scratch/kbonnell/VTK59/INSTALL_Debug)
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.8.0/${VISITARCH})
 
 ##
 ## Specify the Qt4 binary dir. 
