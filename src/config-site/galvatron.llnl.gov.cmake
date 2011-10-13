65bca5e43fbaf0b8e49d66ddeb3b45fc96844ab5
Brad Whitlock <bjw@ilight.com>
2011-10-13 18:21:51 +0000

    Fix the CGNS reader so it works with file grouping.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@16423 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/galvatron.llnl.gov.cmake
+++ src/config-site/galvatron.llnl.gov.cmake
@@ -25,12 +25,17 @@ VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.8.0/${VISITARCH})
 ## Specify the Qt4 binary dir. 
 ## (qmake us used to locate & setup Qt4 dependencies)
 ##
-VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.6.1/${VISITARCH}/bin)
+VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.7.4/${VISITARCH}/bin)
 
 ##
 ## Specify the location of the python.
 ##
 VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.6.4/${VISITARCH})
+
+##
+## Specify the PySide dir. 
+##
+VISIT_OPTION_DEFAULT(VISIT_PYSIDE_DIR ${VISITHOME}/pyside/1.0.7/${VISITARCH}/)
 
 ##
 ## Compiler flags.
