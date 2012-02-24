4613ed8bf917ab97df2b13eec6b03ea7af579929
Brad Whitlock <bjw@ilight.com>
2012-02-24 19:39:14 +0000

    I added vector variables to a visit_writer example for the data manual.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@17430 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/tools/DataManualExamples/CreatingCompatible/vwrect3d.c
+++ src/tools/DataManualExamples/CreatingCompatible/vwrect3d.c
@@ -52,26 +52,39 @@ main(int argc, char *argv[])
     int ndims = 3;
     /* Zonal and Nodal variable data. */
     float zonal[NZ-1][NY-1][NX-1], nodal[NZ][NY][NX];
+    float zonalvec[NZ-1][NY-1][NX-1][3], nodalvec[NZ][NY][NX][3];
     /* Info about the variables to pass to visit_writer. */
-    int nvars = 2;
-    int vardims[] = {1, 1};
-    int centering[] = {0, 1};
-    const char *varnames[] = {"zonal", "nodal"};
-    float *vars[] = {(float*)zonal, (float*)nodal};
+    int nvars = 4;
+    int vardims[] = {1, 1, 3, 3};
+    int centering[] = {0, 1, 0, 1};
+    const char *varnames[] = {"zonal", "nodal", "zonalvec", "nodalvec"};
+    float *vars[] = {(float*)zonal, (float*)nodal, (float*)zonalvec, (float*)nodalvec};
 
-    /* Create a zonal variable. */
+    /* Create 2 zonal variables; 1 scalar, 1 vector. */
     int i,j,k,index = 0;
     for(k = 0; k < NZ-1; ++k)
         for(j = 0; j < NY-1; ++j)
             for(i = 0; i < NX-1; ++i, ++index)
+            {
                 zonal[k][j][i] = (float)index;
 
-    /* Create a nodal variable. */
+                zonalvec[k][j][i][0] = 1.f;
+                zonalvec[k][j][i][1] = 0.f;
+                zonalvec[k][j][i][2] = 0.f;
+            }
+
+    /* Create 2 nodal variables; 1 scalar, 1 vector. */
     index = 0;
     for(k = 0; k < NZ; ++k)
         for(j = 0; j < NY; ++j)
             for(i = 0; i < NX; ++i, ++index)
+            {
                 nodal[k][j][i] = (float)index;
+
+                nodalvec[k][j][i][0] = 0.f;
+                nodalvec[k][j][i][1] = 1.f;
+                nodalvec[k][j][i][2] = 0.f;
+            }
 
     /* Pass the data to visit_writer to write a VTK file.*/
     write_rectilinear_mesh("vwrect3d.vtk", 0, dims, x, y, z, nvars,
