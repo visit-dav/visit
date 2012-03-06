f80bd87109227a1c030d65fe40122a5e7596a79f
Eric Brugger <brugger1@llnl.gov>
2012-03-06 17:40:22 +0000

    I made some more copyright notice fixes in the src directory.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@17594 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/tools/DataManualExamples/CreatingCompatible/time.c
+++ src/tools/DataManualExamples/CreatingCompatible/time.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
 *
-* Copyright (c) 2000 - 2008, Lawrence Livermore National Security, LLC
+* Copyright (c) 2000 - 2012, Lawrence Livermore National Security, LLC
 * Produced at the Lawrence Livermore National Laboratory
 * LLNL-CODE-442911
 * All rights reserved.
