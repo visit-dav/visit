4626c647589a4e550568010850f965437db58c03
Eric Brugger <brugger1@llnl.gov>
2012-03-06 00:51:07 +0000

    I updated the year in the copyright notice from "2000 - 2011" to "2000 - 2012" in all the ".c" and ".C" files in the src directory.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@17584 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/common/parser/Grammar.C
+++ src/common/parser/Grammar.C
@@ -1,6 +1,6 @@
 /*****************************************************************************
 *
-* Copyright (c) 2000 - 2011, Lawrence Livermore National Security, LLC
+* Copyright (c) 2000 - 2012, Lawrence Livermore National Security, LLC
 * Produced at the Lawrence Livermore National Laboratory
 * LLNL-CODE-442911
 * All rights reserved.
@@ -476,7 +476,7 @@ Grammar::WriteStateInitialization(const string &name, ostream &o)
 {
     o << "/*****************************************************************************" << endl;
     o << "*" << endl;
-    o << "* Copyright (c) 2000 - 2011, Lawrence Livermore National Security, LLC" << endl;
+    o << "* Copyright (c) 2000 - 2012, Lawrence Livermore National Security, LLC" << endl;
     o << "* Produced at the Lawrence Livermore National Laboratory" << endl;
     o << "* LLNL-CODE-442911" << endl;
     o << "* All rights reserved." << endl;
--- src/tools/xml/main8.C
+++ src/tools/xml/main8.C
@@ -1,6 +1,6 @@
 /*****************************************************************************
 *
-* Copyright (c) 2000 - 2011, Lawrence Livermore National Security, LLC
+* Copyright (c) 2000 - 2012, Lawrence Livermore National Security, LLC
 * Produced at the Lawrence Livermore National Laboratory
 * LLNL-CODE-442911
 * All rights reserved.
@@ -39,7 +39,7 @@
 const char *java_copyright_str = 
 "// ***************************************************************************\n"
 "//\n"
-"// Copyright (c) 2000 - 2011, Lawrence Livermore National Security, LLC\n"
+"// Copyright (c) 2000 - 2012, Lawrence Livermore National Security, LLC\n"
 "// Produced at the Lawrence Livermore National Laboratory\n"
 "// LLNL-CODE-442911\n"
 "// All rights reserved.\n"
