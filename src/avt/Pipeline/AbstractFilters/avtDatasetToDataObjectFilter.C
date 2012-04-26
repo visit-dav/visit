dac38b2687130ea00932a5a5e90d47728dd53a42
Kathleen Bonnell <bonnell2@llnl.gov>
2012-04-26 21:02:45 +0000

    add debug log statements to aid in debugging new filters that inadvertently trigger behind-the-scenes mesh transformations.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@17989 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/Pipeline/AbstractFilters/avtDatasetToDataObjectFilter.C
+++ src/avt/Pipeline/AbstractFilters/avtDatasetToDataObjectFilter.C
@@ -185,6 +185,11 @@ avtDatasetToDataObjectFilter::SearchDataForSpatialExtents(double *extents)
 //  Programmer:  Jeremy Meredith
 //  Creation:    February 15, 2007
 //
+//  Modifications:
+//    Kathleen Biagas, Thu Apr 26 13:56:47 PDT 2012
+//    Added debug statement to aid in debugging new filters that inadvertently
+//    trigger the transformation.
+//
 // ****************************************************************************
 
 void
@@ -201,6 +206,8 @@ avtDatasetToDataObjectFilter::PreExecute(void)
     if (inatts.GetRectilinearGridHasTransform() &&
         !FilterUnderstandsTransformedRectMesh())
     {
+        debug3 << "avtDatasetToDataObjectFilter transforming rectilinear grid"
+               << " to curvilinear grid." << endl;
         avtDataTree_p tree = GetInputDataTree();
         bool dummy;
         tree->Traverse(CApplyTransformToRectGrid,
