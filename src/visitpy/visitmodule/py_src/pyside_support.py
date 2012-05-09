809d60c183e7205d52a93d59474dbe4f6a5d964c
Cyrus Harrison <cyrush@llnl.gov>
2012-05-09 15:07:22 +0000

    few small changes to pyside_support.py
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.5RC@18161 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/visitpy/visitmodule/py_src/pyside_support.py
+++ src/visitpy/visitmodule/py_src/pyside_support.py
@@ -1,6 +1,51 @@
+###############################################################################
+#
+# Copyright (c) 2000 - 2012, Lawrence Livermore National Security, LLC
+# Produced at the Lawrence Livermore National Laboratory
+# LLNL-CODE-442911
+# All rights reserved.
+#
+# This file is  part of VisIt. For  details, see https://visit.llnl.gov/.  The
+# full copyright notice is contained in the file COPYRIGHT located at the root
+# of the VisIt distribution or at http://www.llnl.gov/visit/copyright.html.
+#
+# Redistribution  and  use  in  source  and  binary  forms,  with  or  without
+# modification, are permitted provided that the following conditions are met:
+#
+#  - Redistributions of  source code must  retain the above  copyright notice,
+#    this list of conditions and the disclaimer below.
+#  - Redistributions in binary form must reproduce the above copyright notice,
+#    this  list of  conditions  and  the  disclaimer (as noted below)  in  the
+#    documentation and/or other materials provided with the distribution.
+#  - Neither the name of  the LLNS/LLNL nor the names of  its contributors may
+#    be used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT  HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR  IMPLIED WARRANTIES, INCLUDING,  BUT NOT  LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND  FITNESS FOR A PARTICULAR  PURPOSE
+# ARE  DISCLAIMED. IN  NO EVENT  SHALL LAWRENCE  LIVERMORE NATIONAL  SECURITY,
+# LLC, THE  U.S.  DEPARTMENT OF  ENERGY  OR  CONTRIBUTORS BE  LIABLE  FOR  ANY
+# DIRECT,  INDIRECT,   INCIDENTAL,   SPECIAL,   EXEMPLARY,  OR   CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT  LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF  USE, DATA, OR PROFITS; OR  BUSINESS INTERRUPTION) HOWEVER
+# CAUSED  AND  ON  ANY  THEORY  OF  LIABILITY,  WHETHER  IN  CONTRACT,  STRICT
+# LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY  WAY
+# OUT OF THE  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+# DAMAGE.
+#
+###############################################################################
+# file: pyside_support.py
+# Purpose: Shapes pyside support for the visit module api.
+#
+# Programmer: Cyrus Harrison & Harinarayan Krishnan
+# Creation: Tue May  8 08:45:23 PDT 2012
 #
 #
+# Modifications:
 #
+#
+###############################################################################
 
 import sys
 import os
@@ -45,21 +90,22 @@ class ProcessCLIInput(Thread):
                     if msvcrt.kbhit():
                         self.qtapp.exit(0)
 
-def GetPySideInstance():
+def GetPySideViewerInstance():
     global __pyside_viewer_inst__
     if __pyside_viewer_inst__ is None:
         __pyside_viewer_inst__ = pyside_viewer.PySideViewer.instance()
     return __pyside_viewer_inst__
 
 def SetupTimer():
-    if ProcessCLIInput.instance is None:
-        ProcessCLIInput.instance = ProcessCLIInput(0.001)
-        ProcessCLIInput.instance.start()
+    if using_pyside:
+        if ProcessCLIInput.instance is None:
+            ProcessCLIInput.instance = ProcessCLIInput(0.001)
+            ProcessCLIInput.instance.start()
 
 def IsPySideViewerEnabled():
     res = False
     if using_pyside:
-        inst = GetPySideInstance() 
+        inst = GetPySideViewerInstance()
         if not inst is None:
             res = True
     return res
@@ -67,7 +113,7 @@ def IsPySideViewerEnabled():
 def GetRenderWindow(i):
     if using_pyside:
         # this will return None, unless properly inited
-        inst = GetPySideInstance()
+        inst = GetPySideViewerInstance()
         if not inst is None:
             return inst.GetRenderWindow(i)
     else:
