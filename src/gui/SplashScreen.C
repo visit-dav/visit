dbb88341744a4089d755c2b98018a376a9cd8084
Brad Whitlock <bjw@ilight.com>
2012-05-11 23:09:29 +0000

    Check in BATL reader.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.5RC@18217 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/gui/SplashScreen.C
+++ src/gui/SplashScreen.C
@@ -295,7 +295,7 @@ SplashScreen::SplashScreen(bool cyclePictures)
            << tr("October")
            << tr("November")
            << tr("December");
-    int currentMonth = 4;
+    int currentMonth = 6;
     lLayout->addWidget(new QLabel(versionText, this));
     lLayout->addWidget(new QLabel(months[currentMonth-1] + " 2012", this));
 
