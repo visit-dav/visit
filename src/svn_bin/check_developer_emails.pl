871453373e8fcd8e4b5be28f4c6da05c35dee163
Jeremy Meredith <jsmeredith@gmail.com>
2012-08-15 16:56:58 +0000

    changing chec_developer_emails wget commands to ignore expired ssl cert
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@19003 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/svn_bin/check_developer_emails.pl
+++ src/svn_bin/check_developer_emails.pl
@@ -119,9 +119,9 @@ if ($verbose)
 if ($passwd ne "")
 {
     # authenticate as admin and save the cookie
-    @auth = `wget -q -O- --keep-session-cookies --save-cookies=tmpcookies "https://elist.ornl.gov/mailman/admin/visit-developers/members?adminpw=$passwd"`;
+    @auth = `wget --no-check-certificate -q -O- --keep-session-cookies --save-cookies=tmpcookies "https://elist.ornl.gov/mailman/admin/visit-developers/members?adminpw=$passwd"`;
     # get the list using the authentication session cookie
-    @html = `wget -q -O- --load-cookies=tmpcookies "https://elist.ornl.gov/mailman/roster/visit-developers"`;
+    @html = `wget --no-check-certificate -q -O- --load-cookies=tmpcookies "https://elist.ornl.gov/mailman/roster/visit-developers"`;
     # get just the members
     @members = grep(m|href="../options/visit-developers/|, @html);
     if ((scalar @members) < 1)
