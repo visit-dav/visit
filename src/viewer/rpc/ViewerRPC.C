1ef54282da9a20ed489b709450b428a07dc86111
Kathleen Bonnell <bonnell2@llnl.gov>
2012-10-19 20:57:22 +0000

    Added DDT rpcs to ViewerRPC.xml and ran xml2atts to fix inconsistencies
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@19395 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/viewer/rpc/ViewerRPC.C
+++ src/viewer/rpc/ViewerRPC.C
@@ -110,20 +110,21 @@ static const char *ViewerRPCType_strings[] = {
 "InitializeNamedSelectionVariablesRPC", "MenuQuitRPC", "SetPlotDescriptionRPC", 
 "MovePlotOrderTowardFirstRPC", "MovePlotOrderTowardLastRPC", "SetPlotOrderToFirstRPC", 
 "SetPlotOrderToLastRPC", "RenamePickLabelRPC", "GetQueryParametersRPC", 
+"DDTConnectRPC", "DDTFocusRPC", "ReleaseToDDTRPC", 
 "MaxRPC"};
 
 std::string
 ViewerRPC::ViewerRPCType_ToString(ViewerRPC::ViewerRPCType t)
 {
     int index = int(t);
-    if(index < 0 || index >= 196) index = 0;
+    if(index < 0 || index >= 199) index = 0;
     return ViewerRPCType_strings[index];
 }
 
 std::string
 ViewerRPC::ViewerRPCType_ToString(int t)
 {
-    int index = (t < 0 || t >= 196) ? 0 : t;
+    int index = (t < 0 || t >= 199) ? 0 : t;
     return ViewerRPCType_strings[index];
 }
 
@@ -131,7 +132,7 @@ bool
 ViewerRPC::ViewerRPCType_FromString(const std::string &s, ViewerRPC::ViewerRPCType &val)
 {
     val = ViewerRPC::CloseRPC;
-    for(int i = 0; i < 196; ++i)
+    for(int i = 0; i < 199; ++i)
     {
         if(s == ViewerRPCType_strings[i])
         {
