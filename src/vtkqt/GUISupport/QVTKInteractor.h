712c4e809e3517b92d5239e8e0c12622d6940b89
Brad Whitlock <bjw@ilight.com>
2013-01-10 23:37:16 +0000

    Fix Windows build.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@19995 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/vtkqt/GUISupport/QVTKInteractor.h
+++ src/vtkqt/GUISupport/QVTKInteractor.h
@@ -60,7 +60,7 @@ class QVTKInteractorInternal;
 // .SECTION Description
 // QVTKInteractor is an interactor for a QVTKWiget.
 
-class QVTK_EXPORT QVTKInteractor : public vtkRenderWindowInteractor
+class QVTKInteractor : public vtkRenderWindowInteractor
 {
 public:
   static QVTKInteractor* New();
