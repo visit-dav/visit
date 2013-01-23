54781e3254d7ef3ff66f88566ab26f0e0d89e974
Eric Brugger <brugger1@llnl.gov>
2013-01-23 17:11:14 +0000

    1) I updated the INSTALL_NOTES for 2.6.1.
    2) I updated the date in the splash screen to January 2013.
    
    3) I updated the version number in build_visit to 2.6.1.
    
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.6RC@20105 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/gui/SplashScreen.C
+++ src/gui/SplashScreen.C
@@ -174,6 +174,9 @@
 //    Eric Brugger, Wed Nov 14 14:06:00 PST 2012
 //    Changed the date on the splash screen to November 2012.
 //
+//    Eric Brugger, Wed Jan 23 09:06:55 PST 2013
+//    Changed the date on the splash screen to January 2013.
+//
 // ****************************************************************************
 
 SplashScreen::SplashScreen(bool cyclePictures) 
@@ -298,9 +301,9 @@ SplashScreen::SplashScreen(bool cyclePictures)
            << tr("October")
            << tr("November")
            << tr("December");
-    int currentMonth = 11;
+    int currentMonth = 1;
     lLayout->addWidget(new QLabel(versionText, this));
-    lLayout->addWidget(new QLabel(months[currentMonth-1] + " 2012", this));
+    lLayout->addWidget(new QLabel(months[currentMonth-1] + " 2013", this));
 
     copyrightButton = 0;
     contributorButton = 0;
