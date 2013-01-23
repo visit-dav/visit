fad2e9f9bbe22d969e0687ae60a5a1374807cae0
Mark Miller <miller86@llnl.gov>
2013-01-23 01:44:34 +0000

    updating to silo 4.9-pre2
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@20099 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/svn_bin/bv_support/bv_silo.sh
+++ src/svn_bin/bv_support/bv_silo.sh
@@ -33,9 +33,9 @@ function bv_silo_depends_on
 
 function bv_silo_info
 {
-export SILO_VERSION=${SILO_VERSION:-"4.9-pre1"}
+export SILO_VERSION=${SILO_VERSION:-"4.9-pre2"}
 export SILO_FILE=${SILO_FILE:-"silo-${SILO_VERSION}.tar.gz"}
-export SILO_COMPATIBILITY_VERSION=${SILO_COMPATIBILITY_VERSION:-"4.9-pre1"}
+export SILO_COMPATIBILITY_VERSION=${SILO_COMPATIBILITY_VERSION:-"4.9-pre2"}
 export SILO_BUILD_DIR=${SILO_BUILD_DIR:-"silo-${SILO_VERSION}"}
 export SILO_URL=${SILO_URL:-https://wci.llnl.gov/codes/silo/silo-${SILO_VERSION}}
 export SILO_MD5_CHECKSUM="9831cf1dfa8d0689a06c2c54c5c65aaf"
