a4d3d71c700ae2fd502fe91277c1c4ee27728a67
Brad Whitlock <bjw@ilight.com>
2013-01-30 02:04:17 +0000

    Add ability to build/install MPICH into a VisIt binary distribution.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@20171 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/CMake/FindPySide.cmake
+++ src/CMake/FindPySide.cmake
@@ -98,7 +98,7 @@ IF(PySide_FOUND)
             GET_FILENAME_COMPONENT(libname ${pysidelib} NAME)
             INSTALL(CODE
                     "EXECUTE_PROCESS(WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}
-                    COMMAND /bin/sh ${VISIT_SOURCE_DIR}/CMake/osxfixup -lib ${VISIT_OSX_USE_RPATH}
+                    COMMAND /bin/sh ${VISIT_SOURCE_DIR}/CMake/osxfixup -lib ${VISIT_MPICH_INSTALL} ${VISIT_OSX_USE_RPATH}
                     \"\$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/${PYSIDE_MODULE_INSTALLED_DIR}/${libname}\"
                     OUTPUT_VARIABLE OSXOUT)
                     MESSAGE(STATUS \"\${OSXOUT}\")
