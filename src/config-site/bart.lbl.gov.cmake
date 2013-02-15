95b6abdabe2db7225395104b85b91890d7df77a3
Gunther Weber <ghweber@lbl.gov>
2013-02-15 21:23:54 +0000

    Update configuration for bart.lbl.gov to new machine.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.6RC@20309 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/bart.lbl.gov.cmake
+++ src/config-site/bart.lbl.gov.cmake
@@ -1,132 +1,112 @@
-#/scratch/build/visit_3rdparty/cmake/2.6.4/linux-x86_64_gcc-4.3/bin/cmake
-
-# 1. YOU MUST UPGRADE TO cmake 2.6.4 !!!! This script assumes 2.6.4 but it is unknown whether it exists on your system.
+#/opt/local/bin/cmake
 ##
-## ./visit_svn/trunk/src/svn_bin/build_visit generated host.conf
-## created: Mon Aug  3 18:33:41 PDT 2009
-## system: Linux bart 2.6.28-14-generic #47-Ubuntu SMP Sat Jul 25 01:19:55 UTC 2009 x86_64 GNU/Linux
+## ../visit_svn/2.6RC_trunk/src/svn_bin/build_visit generated host.cmake
+## created: Fri Dec  7 17:18:25 PST 2012
+## system: Darwin bart.dhcp.lbl.gov 12.2.0 Darwin Kernel Version 12.2.0: Sat Aug 25 00:48:52 PDT 2012; root:xnu-2050.18.24~1/RELEASE_X86_64 x86_64
 ## by: ghweber
 
 ##
-## Set the VISITHOME environment variable.
+## Setup VISITHOME & VISITARCH variables.
 ##
-SET(VISITHOME /scratch/build/visit_3rdparty)
-SET(VISITARCH linux-x86_64_gcc-4.3)
-SET(VISIT_VERBOSE_MAKEFILE TRUE)
+SET(VISITHOME /Users/ghweber/devel/visit_3rdparty)
+SET(VISITARCH i386-apple-darwin12_gcc-4.2)
+VISIT_OPTION_DEFAULT(CMAKE_INSTALL_PREFIX /usr/local/visit)
+SET(CMAKE_BUILD_TYPE Release CACHE STRING "")
+VISIT_OPTION_DEFAULT(VISIT_PYTHON_FILTERS ON)
+VISIT_OPTION_DEFAULT(VISIT_CREATE_APPBUNDLE_PACKAGE ON)
 
 ##
-## Specify the location of the mesa include files and libraries.
-##
-VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.5/${VISITARCH})
-
-##
-## Specify the location of the vtk include files and libraries.
-##
-VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.0.0c/${VISITARCH}/lib/vtk-5.0)
-
-##
-## Specify the location of cmake.
-##
-
-
-##
-## Specify the location of the qt include files and libraries.
-##
-VISIT_OPTION_DEFAULT(VISIT_QT_BIN /usr/bin)
-
-##
-## Specify the location of the python include and libraries.
-##
-VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR /usr)
-
-
 ## Compiler flags.
 ##
-SET(VISIT_C_COMPILER gcc)
-SET(VISIT_C_FLAGS "-g -m64 -DUSE_STDARG -DHAVE_STDLIB_H=1 -DHAVE_STRING_H=1 -DHAVE_UNISTD_H=1 -DHAVE_STDARG_H=1 -DUSE_STDARG=1 -DMALLOC_RET_VOID=1")
-SET(VISIT_MPI_C_FLAGS "-I/usr/lib/mpich-shmem/include")
-SET(VISIT_CXX_COMPILER g++)
-SET(VISIT_CXX_FLAGS "-g -m64 -DUSE_STDARG -DHAVE_STDLIB_H=1 -DHAVE_STRING_H=1 -DHAVE_UNISTD_H=1 -DHAVE_STDARG_H=1 -DUSE_STDARG=1 -DMALLOC_RET_VOID=1")
-SET(VISIT_MPI_CXX_FLAGS "-I/usr/lib/mpich-shmem/include")
+VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc TYPE FILEPATH)
+VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++ TYPE FILEPATH)
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS "-fno-common -fexceptions -fvisibility=hidden" TYPE STRING)
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS "-fno-common -fexceptions -fvisibility=hidden" TYPE STRING)
 
+##
+## Parallel Build Setup.
+##
+VISIT_OPTION_DEFAULT(VISIT_PARALLEL ON TYPE BOOL)
+## (configured w/ mpi compiler wrapper)
+VISIT_OPTION_DEFAULT(VISIT_MPI_COMPILER /opt/local/bin/mpic++ TYPE FILEPATH)
+
+##############################################################
 ##
 ## Database reader plugin support libraries
 ##
+## The HDF4, HDF5 and NetCDF libraries must be first so that
+## their libdeps are defined for any plugins that need them.
+##
+## For libraries with LIBDEP settings, order matters.
+## Libraries with LIBDEP settings that depend on other
+## Library's LIBDEP settings must come after them.
 ##############################################################
+##
+
+##
+## Python
+##
+VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR /opt/local/Library/Frameworks/Python.framework/Versions/2.7)
+VISIT_OPTION_DEFAULT(PYTHON_INCLUDE_PATH /opt/local/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 )
+VISIT_OPTION_DEFAULT(PYTHON_LIBRARY /opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib)
+VISIT_OPTION_DEFAULT(PYTHON_LIBRARY_DIR /opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib)
+VISIT_OPTION_DEFAULT(PYTHON_VERSION 2.7)
+
+##
+## Qt
+##
+VISIT_OPTION_DEFAULT(QT_QTUITOOLS_INCLUDE_DIR /opt/local/include/QtUiTools)
+VISIT_OPTION_DEFAULT(VISIT_QT_BIN /opt/local/bin)
+
+##
+## VTK
+##
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/5.8.0.a.no.mesa/${VISITARCH})
 
 ##
 ## Boxlib
 ##
-VISIT_OPTION_DEFAULT(VISIT_BOXLIB2D_DIR ${VISITHOME}/boxlib/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_BOXLIB3D_DIR ${VISITHOME}/boxlib/${VISITARCH})
-
-##
-## CFITSIO
-##
-VISIT_OPTION_DEFAULT(VISIT_CFITSIO_DIR ${VISITHOME}/cfitsio/3006/${VISITARCH})
-
-##
-## CGNS
-##
-VISIT_OPTION_DEFAULT(VISIT_CGNS_DIR ${VISITHOME}/cgns/2.4/${VISITARCH})
-
-##
-## Exodus
-##
-VISIT_OPTION_DEFAULT(VISIT_EXODUSII_DIR ${VISITHOME}/exodus/4.46/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_EXODUSII_LIBDEP NETCDF_LIBRARY_DIR netcdf)
-
-##
-## GDAL
-##
-VISIT_OPTION_DEFAULT(VISIT_GDAL_DIR ${VISITHOME}/gdal/1.6.1/${VISITARCH})
-
-##
-## H5Part
-##
-VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR ${VISITHOME}/h5part/1.4.2/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_BOXLIB_DIR ${VISITHOME}/boxlib/1.0.7/${VISITARCH})
 
 ##
 ## FastBit
 ##
-VISIT_OPTION_DEFAULT(VISIT_FASTBIT_DIR ${VISITHOME}/fastbit/1.0.9/linux-x86_64_gcc-4.3)
+VISIT_OPTION_DEFAULT(VISIT_FASTBIT_DIR ${VISITHOME}/fastbit/1.2.0/${VISITARCH})
 
 ##
-## HDF4
+## Ice-T
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF4_DIR ${VISITHOME}/hdf4/4.2.1/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_HDF4_LIBDEP /usr/lib jpeg)
+VISIT_OPTION_DEFAULT(VISIT_ICET_DIR ${VISITHOME}/icet/1.0.0/${VISITARCH})
 
 ##
 ## HDF5
 ##
-VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.3/${VISITARCH})
-VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz)
+VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR /opt/local)
+VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP /opt/local/lib sz /opt/local/lib z)
 
 ##
-## Mili
+## H5Part
 ##
-#not done -- DEFAULT_VISIT_MILI_DIR = $VISITHOME/mili/1.10.0/$VISITARCH
+VISIT_OPTION_DEFAULT(VISIT_H5PART_DIR /opt/local)
+VISIT_OPTION_DEFAULT(VISIT_H5PART_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
 ## NetCDF
 ##
-VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/3.6.3/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR /opt/local)
 
 ##
 ## SZIP
 ##
-SET(SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
-
-##
-## CCMIO
-##
-VISIT_OPTION_DEFAULT(VISIT_CCMIO_DIR ${VISITHOME}/ccmio/2.6.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(SZIP_DIR /opt/local)
 
 ##
 ## Silo
 ##
-VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.7/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_SILO_DIR /opt/local)
 VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP})
 
 ##
+## PySide
+##
+VISIT_OPTION_DEFAULT(VISIT_PYSIDE_DIR /opt/local)
