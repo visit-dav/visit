a8215c6c8cec145acdb35c2d68b212f7119c9d52
Dave Pugmire <pugmire@ornl.gov>
2013-02-26 21:40:53 +0000

    Mark cycles as valid for ensembles.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@20350 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/Database/Formats/avtMTMDFileFormatInterface.C
+++ src/avt/Database/Formats/avtMTMDFileFormatInterface.C
@@ -402,12 +402,15 @@ avtMTMDFileFormatInterface::SetDatabaseMetaData(avtDatabaseMetaData *md,
             cycles.insert(cycles.end(),tmp.begin(),tmp.end());
         }
         bool cyclesLookGood = true;
-        for (i = 0; i < cycles.size(); i++)
+        if (!isEnsemble)
         {
-            if ((i != 0) && (cycles[i] <= cycles[i-1]))
+            for (i = 0; i < cycles.size(); i++)
             {
-                cyclesLookGood = false;
-                break;
+                if ((i != 0) && (cycles[i] <= cycles[i-1]))
+                {
+                    cyclesLookGood = false;
+                    break;
+                }
             }
         }
         if (cycles.size() != nTotalTimesteps)
@@ -543,12 +546,15 @@ avtMTMDFileFormatInterface::SetCycleTimeInDatabaseMetaData(
         cycles.insert(cycles.end(),tmp.begin(),tmp.end());
     }
     bool cyclesLookGood = true;
-    for (int i = 0; i < cycles.size(); i++)
+    if (!isEnsemble)
     {
-        if ((i != 0) && (cycles[i] <= cycles[i-1]))
+        for (int i = 0; i < cycles.size(); i++)
         {
-            cyclesLookGood = false;
-            break;
+            if ((i != 0) && (cycles[i] <= cycles[i-1]))
+            {
+                cyclesLookGood = false;
+                break;
+            }
         }
     }
     if (cycles.size() != nTotalTimesteps)
