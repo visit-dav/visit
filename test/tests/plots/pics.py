706bf04637e8e694d139a9a7caf11fde9f9a4579
Kathleen Bonnell <bonnell2@llnl.gov>
2013-06-13 19:19:31 +0000

    Update path to pics data
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@21137 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/plots/pics.py
+++ test/tests/plots/pics.py
@@ -1,4 +1,4 @@
-# ----------------------------------------------------------------------------
+#c ----------------------------------------------------------------------------
 #  CLASSES: nightly
 #
 #  Test Case:  pics.py
@@ -40,7 +40,7 @@ SetView3D(View3DAtts)
 
 for i in range(len(databases)):
   TestSection(databases[i])
-  db=data_path("%s.pics") %(databases[i])
+  db=data_path("pics_test_data/%s.pics") %(databases[i])
   OpenDatabase(db)
   for j in range(len(algs)):
      str="Varying time type with parallelization algorithm = %s" %(algs_strings[j])
