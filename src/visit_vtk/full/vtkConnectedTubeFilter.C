37a2df8003f30e4117deca1143af95e85a7ca825
Hank Childs <hank@uoregon.edu>
2013-07-07 20:16:23 +0000

    Check in change from Jean Favre that improves tube generation.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.6RC@21303 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/visit_vtk/full/vtkConnectedTubeFilter.C
+++ src/visit_vtk/full/vtkConnectedTubeFilter.C
@@ -99,6 +99,8 @@ vtkConnectedTubeFilter::PointSequence::~PointSequence()
 //    Hank Childs (for Jeremy Meredith), Mon Apr  7 11:46:51 PDT 2003
 //    Increased size of cellindex.
 //
+//    Jean Favre, Tue May  7 16:38:37 CEST 2013
+//    Used vtkIdType where needed
 // ****************************************************************************
 void
 vtkConnectedTubeFilter::PointSequence::Init(int maxlen)
@@ -109,8 +111,8 @@ vtkConnectedTubeFilter::PointSequence::Init(int maxlen)
         delete[] cellindex;
 
     length = 0;
-    index  = new int[maxlen];
-    cellindex = new int[maxlen];
+    index  = new vtkIdType[maxlen];
+    cellindex = new vtkIdType[maxlen];
 }
 
 // ****************************************************************************
@@ -122,9 +124,13 @@ vtkConnectedTubeFilter::PointSequence::Init(int maxlen)
 //  Programmer:  Jeremy Meredith
 //  Creation:    November  1, 2002
 //
+//  Modifications:
+//
+//    Jean Favre, Tue May  7 16:38:37 CEST 2013
+//    Used vtkIdType where needed
 // ****************************************************************************
 void
-vtkConnectedTubeFilter::PointSequence::Add(int i, int ci)
+vtkConnectedTubeFilter::PointSequence::Add(vtkIdType i, vtkIdType ci)
 {
     index[length]     = i;
     cellindex[length] = ci;
@@ -194,6 +200,10 @@ vtkConnectedTubeFilter::PointSequenceList::~PointSequenceList()
 //  Programmer:  Jeremy Meredith
 //  Creation:    November  1, 2002
 //
+//  Modifications:
+//
+//    Jean Favre, Tue May  7 16:38:37 CEST 2013
+//    Used vtkIdType where needed
 // ****************************************************************************
 bool 
 vtkConnectedTubeFilter::PointSequenceList::Build(vtkPoints *points,
@@ -201,10 +211,10 @@ vtkConnectedTubeFilter::PointSequenceList::Build(vtkPoints *points,
 {
     pts             = points;
     len             = points->GetNumberOfPoints();
-    numneighbors    = new int[len];
-    connectivity[0] = new int[len];
-    connectivity[1] = new int[len];
-    cellindex       = new int[len];
+    numneighbors    = new vtkIdType[len];
+    connectivity[0] = new vtkIdType[len];
+    connectivity[1] = new vtkIdType[len];
+    cellindex       = new vtkIdType[len];
 
     vtkIdType *cells = lines->GetPointer();
 
@@ -295,6 +305,12 @@ vtkConnectedTubeFilter::PointSequenceList::InitTraversal()
 //    that have 2 neighbors.  Previously it would have considered points
 //    with no neighbors, which caused it to reference uninitialized memory.
 //
+//    Jean Favre, Tue May  7 16:38:37 CEST 2013
+//    I modified the calls to GetPoint() to use the other variant of the call
+//    with two arguments. The previous version would never succeed in the test
+//    to remove sequential identical points.
+//    Used vtkIdType where needed
+//
 // ****************************************************************************
 bool
 vtkConnectedTubeFilter::PointSequenceList::GetNextSequence(PointSequence &seq)
@@ -306,24 +322,26 @@ vtkConnectedTubeFilter::PointSequenceList::GetNextSequence(PointSequence &seq)
         if (((lookforloops && numneighbors[index] == 2) ||
               numneighbors[index] == 1) && !visited[index])
         {
-            int current = index;
-            int previous = -1;
+            vtkIdType current = index;
+            vtkIdType previous = -1;
             seq.Init(len);
             seq.Add(current, cellindex[current]);
             visited[current] = true;
             while (true)
             {
-                int n1       = connectivity[0][current];
-                int n2       = connectivity[1][current];
-                int next     = (n1 == previous) ? n2 : n1;
+                vtkIdType n1       = connectivity[0][current];
+                vtkIdType n2       = connectivity[1][current];
+                vtkIdType next     = (n1 == previous) ? n2 : n1;
                 previous = current;
                 current = next;
 
  
                 // we must skip any sequential identical points:
                 // 1) they are useless, and 2) they mess up calculations
-                double *prePt = pts->GetPoint(previous);
-                double *curPt = pts->GetPoint(current);
+                double prePt[3];
+                double curPt[3];
+                pts->GetPoint(previous, prePt);
+                pts->GetPoint(current, curPt);
                 if (prePt[0] != curPt[0] ||
                     prePt[1] != curPt[1] ||
                     prePt[2] != curPt[2])
@@ -472,6 +490,14 @@ bool vtkConnectedTubeFilter::BuildConnectivityArrays()
 //    Kathleen Biagas, Thu Sep 6 11:15:29 MST 2012
 //    Preserve coordinate data type.
 //
+//    Jean Favre, Tue May  7 16:38:37 CEST 2013
+//    I modified the calls to GetPoint() to use the other variant of the call
+//    with two arguments. The previous version would fail getting the right values.
+//    Used vtkIdType where needed
+//
+//    Jean Favre, Tue May 28 13:28:41 CEST 2013
+//    I added a quaternion interpolation to smoothly interpolate the direction vectors
+//    used to construct the tube. This eliminates abrupt twists along the tube.
 // ****************************************************************************
 void vtkConnectedTubeFilter::Execute()
 {
@@ -545,33 +571,81 @@ void vtkConnectedTubeFilter::Execute()
             bool lastPoint  = (i==seq.length-1);
 
             // Get the current, previous, and next indices
-            int ix  = seq.index[i];
-            int ix1 = (firstPoint ?  seq.index[i] : seq.index[i-1]);
-            int ix2 = (lastPoint  ?  seq.index[i] : seq.index[i+1]);
+            vtkIdType ix  = seq.index[i];
+            vtkIdType ix1 = (firstPoint ?  seq.index[i] : seq.index[i-1]);
+            vtkIdType ix2 = (lastPoint  ?  seq.index[i] : seq.index[i+1]);
 
             // Use a centered difference approximation for direction
-            double dir[3];
-            vtkMath::Subtract(inPts->GetPoint(ix2),inPts->GetPoint(ix1), dir);
+            double v0[3], v1[3], v2[3], dir[3], pt[3];
+            double prev_dir[3], prev_v1[3], prev_v2[3];
+            inPts->GetPoint(ix2, v2);
+            inPts->GetPoint(ix1, v1);
+            vtkMath::Subtract(v2, v1, dir);
 
             // If our centered difference was zero, do a forward
             // difference instead.  We ensured no sequential points
             // are identical, so this can't fail.
             if (dir[0]==0 && dir[1]==0 && dir[2]==0)
             {
-               vtkMath::Subtract(inPts->GetPoint(ix2),inPts->GetPoint(ix),dir);
+               inPts->GetPoint(ix, v0);
+               vtkMath::Subtract(v2, v0, dir);
+            }
+            vtkMath::Cross(dir, v1, v2);
+            vtkMath::Normalize(v2);
+
+            if(firstPoint)
+            {
+              // Get a couple vectors orthogonal to our direction
+              //double v1[3], v2[3];
+              vtkMath::Perpendiculars(dir, v1,v2, 0.0);
+              vtkMath::Normalize(v1);
+              vtkMath::Normalize(v2);
+            }
+            else
+            {
+              // we can't just get two new orthogonal vectors each time because of abrupt changes
+              // We will construct the matrix which xforms the "previous dir" vector
+              // into this new dir vector, and then use this rigid body motion to
+              // xform v1 and v2 from the previous point
+              double Xform[3][3];
+              double vec[3];
+              vtkMath::Cross(prev_dir, dir, vec);
+              double costheta = vtkMath::Dot(prev_dir, dir);
+              double sintheta = vtkMath::Norm(vec);
+              double theta = atan2(sintheta, costheta);
+              if (sintheta != 0)
+                {
+                vec[0] /= sintheta;
+                vec[1] /= sintheta;
+                vec[2] /= sintheta;
+                }
+              // convert to quaternion
+              costheta = cos(0.5*theta);
+              sintheta = sin(0.5*theta);
+              double quat[4];
+              quat[0] = costheta;
+              quat[1] = vec[0]*sintheta;
+              quat[2] = vec[1]*sintheta;
+              quat[3] = vec[2]*sintheta;
+              // convert to matrix
+              vtkMath::QuaternionToMatrix3x3(quat, Xform); // Xform will xform prev_dir into dir
+              // now use it
+              vtkMath::Multiply3x3 (Xform, prev_v1, v1);
+              vtkMath::Multiply3x3 (Xform, prev_v2, v2);
+              vtkMath::Normalize(v1);
+              vtkMath::Normalize(v2);
             }
 
-            // Get a couple vectors orthogonal to our direction
-            double v1[3], v2[3];
-            vtkMath::Perpendiculars(dir, v1,v2, 0.0);
-            vtkMath::Normalize(v1);
-            vtkMath::Normalize(v2);
+            prev_dir[0] = dir[0]; prev_dir[1] = dir[1]; prev_dir[2] = dir[2];
+            prev_v1[0]  = v1[0];  prev_v1[1]  = v1[1];  prev_v1[2]  = v1[2];
+            prev_v2[0]  = v2[0];  prev_v2[1]  = v2[1];  prev_v2[2]  = v2[2];
 
             // Hang on to the first point index we create; we need it 
             // to create the cells
             vtkIdType firstIndex = newPts->GetNumberOfPoints();
     
-            double *pt = inPts->GetPoint(ix);
+            //double *pt = inPts->GetPoint(ix);
+            inPts->GetPoint(ix, pt);
             for (int j = 0 ; j < NumberOfSides ; j++)
             {
                 double q = (j * 2. * vtkMath::Pi()) / double(NumberOfSides);
