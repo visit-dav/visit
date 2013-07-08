890abd8dbe6f8c6c7c1979525c0679007c371c7a
Kathleen Bonnell <bonnell2@llnl.gov>
2013-07-08 21:57:21 +0000

    Add generated java files that were part of the last release. These can be auto-regenerated using the script regeneratejava.py
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.6RC@21318 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ src/java/regeneratejava.py
@@ -0,0 +1,66 @@
+#!/usr/local/apps/bin/python
+import sys, os 
+
+#
+# Get the output of a command as a tuple of strings.
+#
+def GetFilenames(command):
+    p = os.popen(command)
+    files = p.readlines()
+    p.close()
+    return files
+
+#
+# Execute a command. It's wrapped for debugging.
+#
+def systemCommand(command):
+    print command
+    return os.system(command)
+
+###############################################################################
+#
+# Purpose: This program regenerates all of the java state objects in this 
+#          directory by first finding the XML files and then calling xml2java.
+#
+# Programmer: Kathleen Biagas 
+# Date:       June 26, 2013
+#
+# Modifications:
+#
+###############################################################################
+
+def main(exeDir):
+    # Get the names of the XML files in the common/state directory.
+    files = GetFilenames("ls ../common/state/*.xml")
+
+    # regenerate the java files
+    for f in files:
+        command = "%s/xml2java -clobber %s" % (exeDir, f)
+        systemCommand(command)
+
+    # Get the names of the XML files in the avt/DBAtts/MetaData directory.
+    files = GetFilenames("ls ../avt/DBAtts/MetaData/*.xml")
+
+    # regenerate the java files
+    for f in files:
+        command = "%s/xml2java -clobber %s" % (exeDir, f)
+        systemCommand(command)
+
+    # Get the names of the XML files in the viewer/rpc directory.
+    files = GetFilenames("ls ../viewer/rpc/*.xml")
+
+    # regenerate the java files
+    for f in files:
+        command = "%s/xml2java -clobber %s" % (exeDir, f)
+        systemCommand(command)
+
+    return 0
+
+#
+# Call the main function
+#
+if len(sys.argv) > 1:
+    main(sys.argv[1])
+else:
+    print "usage: regeneratejava.py /path/to/xml2java"
+
