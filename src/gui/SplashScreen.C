56f5adc73a9dccc1bc4835e76067013af239b345
Eric Brugger <brugger1@llnl.gov>
2013-07-10 18:21:23 +0000

    I changed the date on the splash screen to July 2013.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@21352 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/gui/SplashScreen.C
+++ src/gui/SplashScreen.C
@@ -177,6 +177,9 @@
 //    Eric Brugger, Wed Jan 23 09:06:55 PST 2013
 //    Changed the date on the splash screen to January 2013.
 //
+//    Eric Brugger, Wed Jul 10 10:56:47 PDT 2013
+//    Changed the date on the splash screen to July 2013.
+//
 // ****************************************************************************
 
 SplashScreen::SplashScreen(bool cyclePictures) 
