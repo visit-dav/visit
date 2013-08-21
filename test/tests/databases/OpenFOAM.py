7d5d83c07e35ddcc854da5024acd4ae85c0902bb
Kathleen Bonnell <bonnell2@llnl.gov>
2013-08-21 00:21:50 +0000

    Rebaselines and test updates due to reader changes
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@21662 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/databases/OpenFOAM.py
+++ test/tests/databases/OpenFOAM.py
@@ -10,13 +10,15 @@
 #    Jeremy Meredith, Tue Jul 15 10:43:58 EDT 2008
 #    Changed number of vectors in vector plot to match the old behavior.
 #    (We now account for how many domains there are.)
+#
+#    Kathleen Biagas, Tue Aug 20 14:06:46 PDT 2013
+#    Reader has been updated, default mesh is now 'internalMesh', and
+#    variables need mesh name.
 # ----------------------------------------------------------------------------
-
 
 OpenDatabase(data_path("OpenFOAM_test_data/bubbleColumn/system/controlDict"))
 
-
-AddPlot("Vector", "Ua")
+AddPlot("Vector", "internalMesh/Ua")
 v = VectorAttributes()
 v.nVectors = 400*5
 SetPlotOptions(v)
@@ -28,13 +30,13 @@ Test("OpenFOAM_02")
 TimeSliderNextState()
 Test("OpenFOAM_03")
 
-AddPlot("Mesh", "Mesh")
+AddPlot("Mesh", "internalMesh")
 DrawPlots()
 Test("OpenFOAM_04")
 
 DeleteAllPlots()
 
-AddPlot("Pseudocolor", "epsilon")
+AddPlot("Pseudocolor", "internalMesh/epsilon")
 DrawPlots()
 Test("OpenFOAM_05")
 
@@ -42,7 +44,7 @@ DeleteAllPlots()
 
 OpenDatabase(data_path("OpenFOAM_test_data/mixer2D_ascii/system/controlDict"))
 
-AddPlot("Vector", "U")
+AddPlot("Vector", "internalMesh/U")
 v = VectorAttributes()
 v.nVectors = 400*6
 SetPlotOptions(v)
@@ -54,13 +56,13 @@ Test("OpenFOAM_07")
 TimeSliderNextState()
 Test("OpenFOAM_08")
 
-AddPlot("Mesh", "Mesh")
+AddPlot("Mesh", "internalMesh")
 DrawPlots()
 Test("OpenFOAM_09")
 
 DeleteAllPlots()
 
-AddPlot("Pseudocolor", "p")
+AddPlot("Pseudocolor", "internalMesh/p")
 DrawPlots()
 Test("OpenFOAM_10")
 
