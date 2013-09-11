3f3447f2a126a9a8412edda56133e1d2cf74eb13
Kathleen Bonnell <bonnell2@llnl.gov>
2013-09-11 16:59:28 +0000

    Fix compile errors due to recent changes to ConfigManager.h
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@21818 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/databases/VLI/VLIFileManager.h
+++ src/databases/VLI/VLIFileManager.h
@@ -57,10 +57,12 @@ class VLIDataset;
 // Creation:   Fri Aug 10 11:48:00 EDT 2007 
 //
 // Modifications:
+//    Kathleen Biagas, Wed Sep 11 09:33:55 PDT 2013
+//    Remove 'STATE_API'.  Added stream versions of Read/WriteConfigFile.
 //
 // ****************************************************************************
 
-class STATE_API VLIFileManager : public ConfigManager
+class VLIFileManager : public ConfigManager
 {
 public:
                       VLIFileManager();
@@ -68,6 +70,9 @@ public:
 
     virtual bool      WriteConfigFile(const char *filename);
     virtual DataNode *ReadConfigFile(const char *filename);
+    virtual bool      WriteConfigFile(std::ostream &out);
+    virtual DataNode *ReadConfigFile(std::istream &in);
+
     VLIDataset       *getDataset();
     void              setDataset(VLIDataset *);
     std::string       getName();
@@ -97,7 +102,7 @@ private:
     int               nservers;
 };
    
-class STATE_API VLIAttribute
+class VLIAttribute
 {
 public:
                         VLIAttribute();
@@ -120,7 +125,7 @@ private:
     bool                isScaled;
 };
 
-class STATE_API VLIDataset
+class VLIDataset
 {
 public:
                           VLIDataset();
