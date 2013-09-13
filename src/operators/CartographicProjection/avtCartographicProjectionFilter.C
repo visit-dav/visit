f0d57af7c52ad93d2a58ab66488b9c0a972fd598
Eric Brugger <brugger1@llnl.gov>
2013-09-13 17:54:48 +0000

    I modified all copyright notices that had "2000 - 2012" to "2000 - 2013" in the src directory.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@21841 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/operators/CartographicProjection/avtCartographicProjectionFilter.C
+++ src/operators/CartographicProjection/avtCartographicProjectionFilter.C
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2000 - 2012, Lawrence Livermore National Security, LLC
+* Copyright (c) 2000 - 2013, Lawrence Livermore National Security, LLC
 * Produced at the Lawrence Livermore National Laboratory
 * LLNL-CODE-442911
 * All rights reserved.
