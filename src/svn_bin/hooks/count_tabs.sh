8a7223e0b6d6e0a68aebcee070b6af183a093a5b
Kathleen Bonnell <bonnell2@llnl.gov>
2013-09-26 18:50:45 +0000

    Allow tabs in files from windowsbuild, as they are mostly third party files
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@21934 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/svn_bin/hooks/count_tabs.sh
+++ src/svn_bin/hooks/count_tabs.sh
@@ -98,7 +98,7 @@ while read fline; do
     # Filter out other cases HandleCommonSkipCases doesn't catch
     #
     case $fname in
-        *.in|*.rc|*.html|*.doc|*.odt|*.odm|*.nib|*/third_party_builtin/*|*/common/icons/*|*.vcproj|*.sln|*.cmake|*.tcl|*/windowsbuild/ThirdParty/*)
+        *.in|*.rc|*.html|*.doc|*.odt|*.odm|*.nib|*/third_party_builtin/*|*/common/icons/*|*.vcproj|*.sln|*.cmake|*.tcl|*/windowsbuild/*)
             continue
             ;;
         */src/configure|*/src/aclocal.m4|*/bin/db_mktmpl)
