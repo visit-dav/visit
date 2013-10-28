e07301a09bc6447335e5dc6e5efbebcd123ba8b1
Cyrus Harrison <cyrush@llnl.gov>
2013-10-28 21:59:28 +0000

    external plugin dev fixes for vtk-6, add qt.confs to osx bundles via osxfixup
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.7RC@22249 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/osxfixup/osxfixup.py
+++ src/osxfixup/osxfixup.py
@@ -88,6 +88,33 @@ def find_libs(sdir):
         lib_maps[lib_basename] = full_lib.replace(sdir,"")   
     return lib_names,lib_maps
 
+def find_bundles(sdir):
+    """
+    Walk sdir and find all OSX bundles.
+    """
+    bundles = find_matches(sdir,"*.app")
+    return bundles
+
+def fixup_bundles(bundles):
+    """
+    Apply OSX bundle specific fixes.
+    """ 
+    add_qt_conf(bundles)
+
+def add_qt_conf(bundles):
+    """
+    Adds a qt.conf config file to all bundles to avoid using any bad built-in plugin 
+    paths. 
+    """
+    for bundle in bundles:
+        resources_dir = pjoin(bundle,"Contents","Resources")
+        if not os.path.isdir(resources_dir):
+            os.mkdir(resources_dir)
+        qtconf_fname = pjoin(resources_dir,"qt.conf")
+        print "[creating: %s]" % qtconf_fname
+        qtconf = open(qtconf_fname,"w")
+        qtconf.write("[Paths]\nPlugins=\n")
+
 def find_exes(sdir):
     """
     Walk sdir and find all exes.
@@ -97,6 +124,9 @@ def find_exes(sdir):
     for root, dirs, files in os.walk(sdir):
         for fname in  files:
           fname = pjoin(root,fname)
+          if not os.path.exists(fname):
+              print 'WARNING: not found %s -- skipped.'%(fname)
+              continue
           st = os.stat(fname)
           mode = st.st_mode
           if mode & exe_flags:
@@ -109,7 +139,7 @@ def find_exes(sdir):
 
 def fixup_items(items,lib_maps,prefix_path):
     """
-    fixup libs / exes with the proper paths.
+    Fixup libs / exes with the proper paths.
     """
     exe_rpaths = ["@executable_path/..", # standard exe
                   "@executable_path/../../../.." # bundle
@@ -164,19 +194,23 @@ def main():
     prefix_path = "darwin-x86_64"
     if len(sys.argv) > 1:
         prefix_path = sys.argv[1]
-    prefix_path = os.path.abspath(prefix_path) 
+    prefix_path = os.path.abspath(prefix_path)
     print "[Finding libraries @ %s]" % prefix_path
     lib_names,lib_maps = find_libs(prefix_path)
     print "[Found %d libraries]" % len(lib_names)
     print "[Finding executables @ %s]" % prefix_path
     exe_names = find_exes(prefix_path)
     print "[Found %d executables]" % len(exe_names)
+    print "[Finding bundles @ %s]" % prefix_path
+    bundle_names = find_bundles(prefix_path)
+    print "[Found %d bundles]" % len(bundle_names)
     print "[Fixing Libraries...]"
     fixup_items(lib_names,lib_maps,prefix_path)
     print "[Fixing Executables...]"
     fixup_items(exe_names,lib_maps,prefix_path)
+    print "[Fixing Bundles...]"
+    fixup_bundles(bundle_names)
 
 
 if __name__ == "__main__":
     main()
-
