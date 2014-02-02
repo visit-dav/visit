25eb9a59e49c0a0ce8c1f9b0faedbf1dbdf24acd
Burlen Loring <bloring@lbl.gov>
2014-02-02 22:04:15 +0000

    experimental ctest support
    change the cdash drop site from my.cdash.org to missmarple.lbl.gov
    
    
    
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@22780 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/CTestConfig.cmake
+++ src/CTestConfig.cmake
@@ -38,6 +38,6 @@
 set(CTEST_PROJECT_NAME "VisIt")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_SITE "missmarple.lbl.gov/CDash") # my.cdash.org
 set(CTEST_DROP_LOCATION "/submit.php?project=VisIt")
 set(CTEST_DROP_SITE_CDASH TRUE)
