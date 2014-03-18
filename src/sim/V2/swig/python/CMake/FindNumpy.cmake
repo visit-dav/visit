c7f468bfab076b4d41f1390326678e3b879af192
Burlen Loring <bloring@lbl.gov>
2014-03-18 23:30:16 +0000

    libsim numpy
    only use numpy if version geater than 1.5, move FindNumpy
    into sim/V2 so we don't accidentally pull VisIt's incompatible
    CMake module overrides in.
    
    
    
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@23033 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/sim/V2/swig/python/CMake/FindNumpy.cmake
+++ src/sim/V2/swig/python/CMake/FindNumpy.cmake
@@ -42,6 +42,7 @@
 # This module defines
 #  NUMPY_FOUND, set TRUE if numpy and c-api are available
 #  NUMPY_INCLUDE_DIR, where to find c-api headers
+#  NUMPY_VERSION, numpy release version
 set(_TMP_PY_OUTPUT)
 set(_TMP_PY_RETURN)
 exec_program("${PYTHON_EXECUTABLE}"
@@ -55,7 +56,22 @@ else()
   set(_TMP_PY_OUTPUT)
 endif()
 set(NUMPY_INCLUDE_DIR "${_TMP_PY_OUTPUT}")
+
+set(_TMP_PY_OUTPUT)
+set(_TMP_PY_RETURN)
+exec_program("${PYTHON_EXECUTABLE}"
+  ARGS "-c 'import numpy; print numpy.version.version'"
+  OUTPUT_VARIABLE _TMP_PY_OUTPUT
+  RETURN_VALUE _TMP_PY_RETURN)
+set(NUMPY_VERSION_FOUND FALSE)
+if(NOT _TMP_PY_RETURN)
+  set(NUMPY_VERSION_FOUND TRUE)
+else()
+  set(_TMP_PY_OUTPUT)
+endif()
+set(NUMPY_VERSION "${_TMP_PY_OUTPUT}")
+
 #set(NUMPY_INCLUDE_DIR "${_TMP_PY_OUTPUT}" CACHE PATH "Numpy C API headers")
 #mark_as_advanced(NUMPY_INCLUDE_DIR)
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NUMPY DEFAULT_MSG NUMPY_INCLUDE_FOUND)
+find_package_handle_standard_args(NUMPY DEFAULT_MSG NUMPY_INCLUDE_FOUND NUMPY_VERSION_FOUND)
