43e6023da6442e757eb5d311c575a98d9c6bb0be
Allen Sanderson <allen@sci.utah.edu>
2014-04-16 17:12:51 +0000

    added dt to make computing faster
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@23176 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/IVP/avtIVPVTKTimeVaryingField.h
+++ src/avt/IVP/avtIVPVTKTimeVaryingField.h
@@ -103,12 +103,12 @@ class IVP_API avtIVPVTKTimeVaryingField : public avtIVPField
     // avtIVPField interface
     virtual Result    operator()(const double& t,
                                  const avtVector &pt,
-                                 avtVector &retV) const;
+                                       avtVector &retV) const;
 
     virtual Result    operator()(const double& t,
                                  const avtVector &pt,
                                  const avtVector &vel,
-                                 avtVector &retV) const
+                                       avtVector &retV) const
     { return FAIL; }
 
     virtual avtVector ConvertToCartesian(const avtVector& pt) const;
@@ -144,7 +144,7 @@ class IVP_API avtIVPVTKTimeVaryingField : public avtIVPField
     std::vector<std::string>         sclDataName;
     vtkDataArray*          sclData[2][256];
     bool                   sclCellBased[256];
-    double                 t0, t1;
+    double                 t0, t1, dt;
 
     mutable avtVector               lastPos;
     mutable vtkIdType               lastCell;
