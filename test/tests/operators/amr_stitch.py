1b5af363ecffc436ce4ae6da7c6997a1e41b804c
Gunther Weber <ghweber@lbl.gov>
2014-04-30 17:43:32 +0000

    Added missing data_path call to amr_stitch test. Replaced blank baseline figures with test results.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@23246 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/operators/amr_stitch.py
+++ test/tests/operators/amr_stitch.py
@@ -15,7 +15,7 @@ TurnOffAllAnnotations(a)
 
 # Files used for tests
 filename_template = "AMRStitchCell_test_data/AMRStitchCellTest.%d.no_ghost.phi.%dd.hdf5"
-test_case_filenames = [ filename_template % ( case, dim ) for dim in range(2,4) for case in range(1,5) ]
+test_case_filenames = [ data_path(filename_template % ( case, dim )) for dim in range(2,4) for case in range(1,5) ]
 
 # Setup plots and run first test
 OpenDatabase(test_case_filenames[0])
@@ -25,13 +25,13 @@ DrawPlots()
 Test("amr_stitch_01")
 
 # Perform remaining tests
-for test_no, test_filename in enumerate(test_case_filenames[1:], 2):
-    OpenDatabase(test_filename)
-    ReplaceDatabase(test_filename)
+for test_no, ds in enumerate(test_case_filenames[1:], 2):
+    ReplaceDatabase(ds)
     Test("amr_stitch_%02d" % test_no)
 
 # Clean-up
 DeleteAllPlots()
-CloseDatabase(ds)
+for ds in test_case_filenames:
+    CloseDatabase(ds)
 
 Exit()
