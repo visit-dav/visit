87193bb304096994348fde24bdc3762701a30e0a
Eric Brugger <brugger1@llnl.gov>
2014-06-04 15:01:47 +0000

    1) I updated the version number to 2.7.3.
    2) I changed the date on the splash screen.
    
    3) I cleaned up the release notes.
    
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.7RC@23444 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/gui/SplashScreen.C
+++ src/gui/SplashScreen.C
@@ -189,6 +189,9 @@
 //    Eric Brugger, Thu Mar 13 09:50:50 PDT 2014
 //    Changed the date on the splash screen to March 2014.
 //
+//    Eric Brugger, Wed Jun  4 07:34:26 PDT 2014
+//    Changed the date on the splash screen to June 2014.
+//
 // ****************************************************************************
 
 SplashScreen::SplashScreen(bool cyclePictures) 
@@ -313,7 +316,7 @@ SplashScreen::SplashScreen(bool cyclePictures)
            << tr("October")
            << tr("November")
            << tr("December");
-    int currentMonth = 3;
+    int currentMonth = 6;
     lLayout->addWidget(new QLabel(versionText, this));
     lLayout->addWidget(new QLabel(months[currentMonth-1] + " 2014", this));
 
