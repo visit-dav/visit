2198277127fc11a94f278daec6dfa64c32cba7df
Mark Miller <miller86@llnl.gov>
2014-07-09 00:49:57 +0000

    fixing compilation warning fixes
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@23675 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/databases/AMRTest/avtAMRTestFileFormat.C
+++ src/databases/AMRTest/avtAMRTestFileFormat.C
@@ -200,7 +200,6 @@ avtAMRTestFileFormat::PopulateDomainNesting()
         dn->SetLevelCellSizes(level, cs);
     }
 
-    //int iChildPatch = 0;
     for (int patch = 0; patch < totalPatches; patch++)
     {
         //
