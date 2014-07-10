fe9aead81dbe87b131ef1c0281336e5c52b98fd4
Burlen Loring <bloring@lbl.gov>
2014-07-10 17:13:19 +0000

    ctest support: change nightly svn update time from 6pm pacific to 8pm pacific.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@23698 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/CTestConfig.cmake
+++ src/CTestConfig.cmake
@@ -36,7 +36,7 @@
 #
 #*****************************************************************************
 set(CTEST_PROJECT_NAME "VisIt")
-set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+set(CTEST_NIGHTLY_START_TIME "03:00:00 UTC")
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cdash.visit.lbl.gov")
 set(CTEST_DROP_LOCATION "/submit.php?project=VisIt")
