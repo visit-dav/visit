387d9a12983348e9c0f85d2dfca0526b35f7903d
Allen Sanderson <allen@sci.utah.edu>
2014-09-22 19:40:10 +0000

    fixed the version check to include both 10.8 and 10.9
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@24530 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/svn_bin/bv_support/bv_qt.sh
+++ src/svn_bin/bv_support/bv_qt.sh
@@ -126,9 +126,10 @@ function bv_qt_depends_on
 
 function bv_qt_info
 {
-# if we are on osx 10.9, we need to use 4.8.6   
+# if we are on osx 10.8 or later, we need to use 4.8.6   
     if [[ "$OPSYS" == "Darwin" ]]; then
-        if [[ "${MACOSX_DEPLOYMENT_TARGET}" == "10.8" ]]; then
+        if [[ "${MACOSX_DEPLOYMENT_TARGET}" == "10.8" ||
+              "${MACOSX_DEPLOYMENT_TARGET}" == "10.9" ]]; then
             export QT_FILE=${QT_FILE:-"qt-everywhere-opensource-src-4.8.6.tar.gz"}
             export QT_VERSION=${QT_VERSION:-"4.8.6"}
             export QT_MD5_CHECKSUM="2edbe4d6c2eff33ef91732602f3518eb"
