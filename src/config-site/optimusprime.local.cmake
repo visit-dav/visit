7572627f35186acaf6efb6593f83d63d9111a40b
Brad Whitlock <bjw@ilight.com>
2014-10-21 20:53:06 +0000

    Turn off SimV2 reader for static builds for now because we don't want the engine linked with SimV2. That would bring in unwanted simV2runtime dependencies.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@24843 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/optimusprime.local.cmake
+++ src/config-site/optimusprime.local.cmake
@@ -1,3 +1,95 @@
+IF(VISIT_STATIC)
+#/Users/bjw/Development/thirdparty_static/2.9.0/cmake/3.0.2/i386-apple-darwin12_gcc-4.2/bin/cmake
+##
+## ./build_visit generated host.cmake
+## created: Tue Oct 21 11:27:25 PDT 2014
+## system: Darwin optimusprime.local 12.5.0 Darwin Kernel Version 12.5.0: Sun Sep 29 13:33:47 PDT 2013; root:xnu-2050.48.12~1/RELEASE_X86_64 x86_64
+## by: bjw
+
+##
+## Setup VISITHOME & VISITARCH variables.
+##
+SET(VISITHOME /Users/bjw/Development/thirdparty_static/2.9.0)
+SET(VISITARCH i386-apple-darwin12_gcc-4.2)
+
+## Compiler flags.
+##
+VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc TYPE FILEPATH)
+VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++ TYPE FILEPATH)
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS "-Wall -fno-common -fexceptions" TYPE STRING)
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS "-Wall -fno-common -fexceptions" TYPE STRING)
+
+##
+## VisIt Thread Option
+##
+VISIT_OPTION_DEFAULT(VISIT_THREAD OFF TYPE BOOL)
+
+##
+## VisIt Boost Option.
+##
+VISIT_OPTION_DEFAULT(VISIT_USE_BOOST ON TYPE BOOL)
+
+##############################################################
+##
+## Database reader plugin support libraries
+##
+## The HDF4, HDF5 and NetCDF libraries must be first so that
+## their libdeps are defined for any plugins that need them.
+##
+## For libraries with LIBDEP settings, order matters.
+## Libraries with LIBDEP settings that depend on other
+## Library's LIBDEP settings must come after them.
+##############################################################
+##
+
+##
+## Python
+##
+VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.7.6/${VISITARCH})
+
+##
+## Qt
+##
+VISIT_OPTION_DEFAULT(VISIT_QT_BIN ${VISITHOME}/qt/4.8.6/${VISITARCH}/bin)
+
+##
+## VTK
+##
+SETUP_VTK_VERSION(6.1.0)
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/${VTK_VERSION}/${VISITARCH})
+##
+
+##
+## MPICH
+##
+
+# Give VisIt information so it can install MPI into the binary distribution.
+VISIT_OPTION_DEFAULT(VISIT_MPICH_DIR ${VISITHOME}/mpich/3.0.4/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_MPICH_INSTALL ON)
+
+# Tell VisIt the parallel compiler so it can deduce parallel flags
+VISIT_OPTION_DEFAULT(VISIT_MPI_COMPILER ${VISIT_MPICH_DIR}/bin/mpicc)
+VISIT_OPTION_DEFAULT(VISIT_PARALLEL ON)
+
+##
+## SZIP
+##
+VISIT_OPTION_DEFAULT(VISIT_SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
+
+##
+## HDF5
+##
+VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.7/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz /usr/lib z TYPE STRING)
+
+##
+## Silo
+##
+VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.10.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP} TYPE STRING)
+
+
+ELSE(VISIT_STATIC)
 #/Users/bjw/Development/thirdparty_shared/2.8.0/cmake/2.8.12.2/i386-apple-darwin12_gcc-4.2/bin/cmake
 ##
 ## ./build_visit generated host.cmake
@@ -134,3 +226,4 @@ VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP
 VISIT_OPTION_DEFAULT(VISIT_XDMF_DIR ${VISITHOME}/Xdmf/2.1.1/${VISITARCH})
 VISIT_OPTION_DEFAULT(VISIT_XDMF_LIBDEP HDF5_LIBRARY_DIR hdf5  VTK_LIBRARY_DIRS vtklibxml2-${VTK_MAJOR_VERSION}.${VTK_MINOR_VERSION}  TYPE STRING)
 
+ENDIF(VISIT_STATIC)
