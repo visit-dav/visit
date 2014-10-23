1e38cc9678936ab129980ffa7fd3b797eae020b5
Mark Miller <miller86@llnl.gov>
2014-10-23 23:06:58 +0000

    2.8RC->Trunk; made zone-type labels be 3-chars long; added support for more zonetypes
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@24879 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/Expressions/General/avtZoneTypeExpression.C
+++ src/avt/Expressions/General/avtZoneTypeExpression.C
@@ -102,7 +102,12 @@ avtZoneTypeExpression::~avtZoneTypeExpression()
 //    Mark C. Miller, Wed Apr  2 09:46:47 PDT 2008
 //    Added case statement to set character for zone type.
 //
+//    Mark C. Miller, Thu Oct 23 15:59:17 PDT 2014
+//    Made the output 3 chars long instead of one. Added cases for more
+//    element types.
 // ****************************************************************************
+
+#define SET_VAL(STR) strncpy(val, #STR, sizeof(val))
 
 vtkDataArray *
 avtZoneTypeExpression::DeriveVariable(vtkDataSet *in_ds, int currentDomainsIndex)
@@ -110,33 +115,73 @@ avtZoneTypeExpression::DeriveVariable(vtkDataSet *in_ds, int currentDomainsIndex
     vtkIdType ncells = in_ds->GetNumberOfCells();
 
     vtkIntArray *rv = vtkIntArray::New();
+    rv->SetNumberOfComponents(4);
     rv->SetNumberOfTuples(ncells);
     for (vtkIdType i = 0 ; i < ncells ; i++)
     {
-        int val = (int) '?';
+        char val[3];
+        SET_VAL(" ? ");
         switch (in_ds->GetCellType(i))
         {
             // 2D cell types, lower case letters
-            case VTK_EMPTY_CELL:     val = (int) 'e'; break;
-            case VTK_VERTEX:         val = (int) 'v'; break;
-            case VTK_POLY_VERTEX:    val = (int) 'w'; break;
-            case VTK_LINE:           val = (int) 'l'; break;
-            case VTK_POLY_LINE:      val = (int) 'm'; break;
-            case VTK_TRIANGLE:       val = (int) 't'; break;
-            case VTK_TRIANGLE_STRIP: val = (int) 's'; break;
-            case VTK_POLYGON:        val = (int) 'p'; break;
-            case VTK_PIXEL:          val = (int) 'x'; break;
-            case VTK_QUAD:           val = (int) 'q'; break;
+            case VTK_EMPTY_CELL:     SET_VAL(emt); break;
+            case VTK_VERTEX:         SET_VAL(vtx); break;
+            case VTK_POLY_VERTEX:    SET_VAL(pvx); break;
+            case VTK_LINE:           SET_VAL(lin); break;
+            case VTK_POLY_LINE:      SET_VAL(pln); break;
+            case VTK_TRIANGLE:       SET_VAL(tri); break;
+            case VTK_TRIANGLE_STRIP: SET_VAL(tst); break;
+            case VTK_POLYGON:        SET_VAL(ply); break;
+            case VTK_PIXEL:          SET_VAL(pix); break;
+            case VTK_QUAD:           SET_VAL(qad); break;
 
             // 3D cell types, upper case letters
-            case VTK_TETRA:          val = (int) 'T'; break;
-            case VTK_VOXEL:          val = (int) 'V'; break;
-            case VTK_HEXAHEDRON:     val = (int) 'H'; break;
-            case VTK_WEDGE:          val = (int) 'W'; break;
-            case VTK_PYRAMID:        val = (int) 'P'; break;
+            case VTK_TETRA:          SET_VAL(TET); break;
+            case VTK_VOXEL:          SET_VAL(VOX); break;
+            case VTK_HEXAHEDRON:     SET_VAL(HEX); break;
+            case VTK_WEDGE:          SET_VAL(WDG); break;
+            case VTK_PYRAMID:        SET_VAL(PYR); break;
+
+            // Quadratic, isoparametric cells
+            case VTK_QUADRATIC_EDGE:                   SET_VAL(Qln); break;
+            case VTK_QUADRATIC_TRIANGLE:               SET_VAL(Qtr); break;
+            case VTK_QUADRATIC_QUAD:                   SET_VAL(Qqd); break;
+            case VTK_QUADRATIC_TETRA:                  SET_VAL(QTT); break;
+            case VTK_QUADRATIC_HEXAHEDRON:             SET_VAL(QHX); break;
+            case VTK_QUADRATIC_WEDGE:                  SET_VAL(QWD); break;
+            case VTK_QUADRATIC_PYRAMID:                SET_VAL(QPY); break;
+            case VTK_BIQUADRATIC_QUAD:                 SET_VAL(2Qq); break;
+            case VTK_TRIQUADRATIC_HEXAHEDRON:          SET_VAL(3QH); break;
+            case VTK_QUADRATIC_LINEAR_QUAD:            SET_VAL(QLq); break;
+            case VTK_QUADRATIC_LINEAR_WEDGE:           SET_VAL(QLW); break;
+            case VTK_BIQUADRATIC_QUADRATIC_WEDGE:      SET_VAL(2QW); break;
+            case VTK_BIQUADRATIC_QUADRATIC_HEXAHEDRON: SET_VAL(3QH); break;
+            case VTK_BIQUADRATIC_TRIANGLE:             SET_VAL(2Qt); break;
+
+            case VTK_CUBIC_LINE:       SET_VAL(3ln); break;
+            case VTK_CONVEX_POINT_SET: SET_VAL(CPS); break;
+            case VTK_POLYHEDRON:       SET_VAL(PLY); break;
+
+            // Higher order cells in parametric form
+            case VTK_PARAMETRIC_CURVE:        SET_VAL(Pcu); break;
+            case VTK_PARAMETRIC_SURFACE:      SET_VAL(Psu); break;
+            case VTK_PARAMETRIC_TRI_SURFACE:  SET_VAL(Pts); break;
+            case VTK_PARAMETRIC_QUAD_SURFACE: SET_VAL(Pqs); break;
+            case VTK_PARAMETRIC_TETRA_REGION: SET_VAL(PTR); break;
+            case VTK_PARAMETRIC_HEX_REGION:   SET_VAL(PHR); break;
+
+            // Higher order cells
+            case VTK_HIGHER_ORDER_EDGE:        SET_VAL(HOl); break;
+            case VTK_HIGHER_ORDER_TRIANGLE:    SET_VAL(HOt); break;
+            case VTK_HIGHER_ORDER_QUAD:        SET_VAL(HOq); break;
+            case VTK_HIGHER_ORDER_POLYGON:     SET_VAL(HOp); break;
+            case VTK_HIGHER_ORDER_TETRAHEDRON: SET_VAL(HOT); break;
+            case VTK_HIGHER_ORDER_WEDGE:       SET_VAL(HOW); break;
+            case VTK_HIGHER_ORDER_PYRAMID:     SET_VAL(HOP); break;
+            case VTK_HIGHER_ORDER_HEXAHEDRON:  SET_VAL(HOH); break;
         }            
 
-        rv->SetTuple1(i, val);
+        rv->SetTuple4(i, (int) val[0], (int) val[1], (int) val[2], (int) '\0');
     }
 
     return rv;
