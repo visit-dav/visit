3a77c645726f0f32b5f57ad610d05f8340774ba7
Harinarayan(Hari) Krishnan <hkrishnan@lbl.gov>
2014-12-03 03:38:12 +0000

    minor fix to keep initial handshake open
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@25139 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/viewer/main/SharedDaemon.C
+++ src/viewer/main/SharedDaemon.C
@@ -498,7 +498,8 @@ void SharedDaemon::AddNewClient(const std::string &host, const stringVector &arg
         tsocket->disconnectFromHost();
         if(tsocket->state() != QAbstractSocket::UnconnectedState)
             tsocket->waitForDisconnected();
-        tsocket->deleteLater();
+        //HKTODO: Do not delete connection (test fix for ORNL machines)
+        //tsocket->deleteLater();
     }
     else
     {
