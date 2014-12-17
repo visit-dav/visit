1c62d459cde8c4f69955ec5caa7dea9a7cc3f7b4
Eric Brugger <brugger1@llnl.gov>
2014-12-17 21:37:12 +0000

    I added an avtexamples directory and added an example avt program that opens a file, applies a transform filter, a second transform filter, and a contour filter and then writes out the result in a silo file.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@25255 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ src/tools/avtexamples/avtFilterExample.C
@@ -0,0 +1,217 @@
+#include <VisItInit.h>
+
+#include <DatabasePluginInfo.h>
+#include <DatabasePluginManager.h>
+#include <OperatorPluginManager.h>
+#include <PlotPluginManager.h>
+
+#include <avtContourFilter.h>
+#include <avtDatabase.h>
+#include <avtDatabaseFactory.h>
+#include <avtDatabaseWriter.h>
+#include <avtLinearTransformFilter.h>
+#include <avtOriginatingSource.h>
+#include <avtParallel.h>
+
+#include <VisItException.h>
+#include <visitstream.h>
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+//
+// Things to note:
+//
+// 1) If you specify VTK_1.0 to the visit writer the program will crash.
+//
+// 2) If you specify more than one contour level the program will crash.
+//
+// 3) If you specify the contour levels with NLevels the program will crash.
+//
+// 4) The Update calls after each filter is added may be extraneous and
+//    may be causing the network to execute unnecessarily.
+//
+// 5) The operator and plot managers are not used.
+//
+// 6) Parallel has never been tested and probably doesn't work.
+//
+
+int
+main(int argc, char *argv[])
+{
+    //
+    // Initialize VisIt.
+    //
+    cerr << "Initializing VisIt." << endl;
+    bool parallel = false;
+#ifdef PARALLEL
+    parallel = true;
+#endif
+
+    PAR_Init(argc, argv);
+
+    VisItInit::SetComponentName("engine");
+    VisItInit::Initialize(argc, argv);
+
+    //
+    // Initialize the plugin readers.
+    //
+    cerr << "Creating the plugin managers." << endl;
+    DatabasePluginManager *dbmgr = new DatabasePluginManager;
+    OperatorPluginManager  *omgr = new OperatorPluginManager;
+    PlotPluginManager      *pmgr = new PlotPluginManager;
+
+    dbmgr->Initialize(DatabasePluginManager::Engine, parallel);
+    omgr->Initialize(DatabasePluginManager::Engine, parallel);
+    pmgr->Initialize(DatabasePluginManager::Engine, parallel);
+
+#if 0
+    dbmgr->LoadPluginsOnDemand();
+    omgr->LoadPluginsOnDemand();
+    pmgr->LoadPluginsOnDemand();
+#else
+    dbmgr->LoadPluginsNow();
+    omgr->LoadPluginsNow();
+    pmgr->LoadPluginsNow();
+#endif
+
+    //
+    // Instantiate the database.
+    //
+    cerr << "Opening the file." << endl;
+    const char *filename = "/usr/gapps/visit/data/rect2d.silo";
+    avtDatabase *db = NULL;
+    vector<string> pluginList;
+    TRY
+    {
+        db = avtDatabaseFactory::FileList(dbmgr, &filename, 1, 0, pluginList);
+    }
+    CATCHALL
+    {
+        if (PAR_Rank() == 0)
+            cerr << "The file " << filename << " does not exist or could "
+                 << "not be opened." << endl;
+        PAR_Exit();
+        exit(EXIT_FAILURE);
+    }
+    ENDTRY
+
+    if (db == NULL)
+    {
+        if (PAR_Rank() == 0)
+            cerr << "Could not open file " << argv[1] << ".  Tried using plugins ";
+        for (size_t i = 0 ; i < pluginList.size() ; i++)
+        {
+            if (PAR_Rank() == 0)
+            {
+                cerr << pluginList[i];
+                if (i != pluginList.size()-1)
+                    cerr << ", ";
+                else
+                    cerr << endl;
+            }
+        }
+
+        PAR_Exit();
+        exit(EXIT_FAILURE);
+    }
+
+    //
+    // Get the database object.
+    //
+    avtDataObject_p dob = db->GetOutput("d", 0);
+
+    avtOriginatingSource  *src = dob->GetOriginatingSource();
+    avtDataRequest_p ds  = src->GetFullDataRequest();
+    avtContract_p contractForDB = new avtContract(ds, 0);
+
+    //
+    // Apply a linear transform.
+    //
+    cerr << "Applying a linear transform." << endl;
+    avtLinearTransformFilter *filter = new avtLinearTransformFilter;
+    filter->SetInput(dob);
+    LinearTransformAttributes atts;
+    atts.SetM00(2.); atts.SetM01(0.); atts.SetM02(0.); atts.SetM03(0.);
+    atts.SetM10(0.); atts.SetM11(4.); atts.SetM12(0.); atts.SetM13(0.);
+    atts.SetM20(0.); atts.SetM21(0.); atts.SetM22(1.); atts.SetM23(0.);
+    atts.SetM30(0.); atts.SetM31(0.); atts.SetM32(0.); atts.SetM33(1.);
+    filter->SetAtts(&atts);
+    filter->Update(contractForDB);
+    avtDataObject_p output = filter->GetOutput();
+
+    //
+    // Apply a second linear transform.
+    //
+    cerr << "Applying a second linear transform." << endl;
+    avtLinearTransformFilter *filter2 = new avtLinearTransformFilter;
+    filter2->SetInput(output);
+    LinearTransformAttributes atts2;
+    atts2.SetM00(3.); atts2.SetM01(0.); atts2.SetM02(0.); atts2.SetM03(0.);
+    atts2.SetM10(0.); atts2.SetM11(2.); atts2.SetM12(0.); atts2.SetM13(0.);
+    atts2.SetM20(0.); atts2.SetM21(0.); atts2.SetM22(1.); atts2.SetM23(0.);
+    atts2.SetM30(0.); atts2.SetM31(0.); atts2.SetM32(0.); atts2.SetM33(1.);
+    filter2->SetAtts(&atts2);
+    filter2->Update(contractForDB);
+    avtDataObject_p output2 = filter2->GetOutput();
+
+    //
+    // Apply a contour filter.
+    //
+    cerr << "Applying a contour filter." << endl;
+    ContourOpAttributes atts3;
+    doubleVector levels;
+    levels.push_back(0.5);
+    atts3.SetContourPercent(levels);
+    avtContourFilter *filter3 = new avtContourFilter(atts3);
+    filter3->SetInput(output2);
+    filter3->Update(contractForDB);
+    avtDataObject_p output3 = filter3->GetOutput();
+
+    //
+    // Write out the output.
+    //
+    cerr << "Writing the output." << endl;
+    EngineDatabasePluginInfo *edpi = dbmgr->GetEnginePluginInfo("Silo_1.0");
+
+    if (edpi == NULL)
+    {
+        PAR_Exit();
+        exit(EXIT_FAILURE);
+    }
+
+    avtDatabaseWriter *wrtr = edpi->GetWriter();
+
+    if (wrtr == NULL)
+    {
+        PAR_Exit();
+        exit(EXIT_FAILURE);
+    }
+
+    const avtDatabaseMetaData *md = db->GetMetaData(0);
+
+    vector<string> vars;
+    vars.push_back("d");
+    wrtr->SetInput(output3);
+    wrtr->Write("", "output", md, vars);
+
+    //
+    // Clean up and exit.
+    //
+    cerr << "Cleaning up." << endl;
+    delete dbmgr;
+    delete omgr;
+    delete pmgr;
+
+    delete filter;
+    delete filter2;
+    delete filter3;
+
+    cerr << "Exiting." << endl;
+    PAR_Exit();
+
+    return 0;
+}
