1b32189390fdbaa15ca019b2249bd86fb14aea74
Allen Sanderson <allen@sci.utah.edu>
2015-02-12 18:09:19 +0000

    added new bounds checks to allow any vector to be decomposed
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@25743 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/Expressions/General/avtVectorDecomposeExpression.C
+++ src/avt/Expressions/General/avtVectorDecomposeExpression.C
@@ -211,11 +211,6 @@ avtVectorDecomposeExpression::DeriveVariable(vtkDataSet *in_ds, int currentDomai
 
     if (twoDVector)
     {
-        // if ((which_comp > 1) || (which_comp < 0))
-        // {
-        //     EXCEPTION2(ExpressionException, outputVariableName, 
-        //                "The only valid indices for 2D vectors are 0 and 1.");
-        // }
         if (arr->GetNumberOfComponents() == 3)
         {
             rv->SetNumberOfComponents(1);
@@ -273,6 +268,11 @@ avtVectorDecomposeExpression::DeriveVariable(vtkDataSet *in_ds, int currentDomai
                 rv->SetTuple3(i, val1, val2, 0.);
             }
         }
+        else if ((which_comp < 0) || (arr->GetNumberOfComponents() <= which_comp))
+        {
+            EXCEPTION2(ExpressionException, outputVariableName, 
+                       "the indices for the 2D vector is out of range.");
+        }
         else
         {
             rv->SetNumberOfComponents(1);
@@ -291,11 +291,6 @@ avtVectorDecomposeExpression::DeriveVariable(vtkDataSet *in_ds, int currentDomai
     }
     else
     {
-        // if ((which_comp > 2) || (which_comp < 0))
-        // {
-        //     EXCEPTION2(ExpressionException, outputVariableName, 
-        //               "The only valid indices for 3D vectors are 0, 1, and 2");
-        // }
         if (arr->GetNumberOfComponents() == 3)
         {
             //
@@ -360,6 +355,11 @@ avtVectorDecomposeExpression::DeriveVariable(vtkDataSet *in_ds, int currentDomai
                 double val3 = arr->GetComponent(i, 3*which_comp+2);
                 rv->SetTuple3(i, val1, val2, val3);
             }
+        }
+        else if ((which_comp < 0) || (arr->GetNumberOfComponents() <= which_comp))
+        {
+            EXCEPTION2(ExpressionException, outputVariableName, 
+                       "the indices for the 3D vector is out of range.");
         }
         else
         {
