fa60f3c83b89ef2dd6332ca6ba9a005f72458eac
Kathleen Bonnell <bonnell2@llnl.gov>
2015-02-13 00:01:27 +0000

    Fix pick bug with arbpoly data. Resolves #2126. update my config-site file for 2.9
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@25767 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/Queries/Pick/avtPickQuery.h
+++ src/avt/Queries/Pick/avtPickQuery.h
@@ -58,79 +58,79 @@ class avtExpressionEvaluatorFilter;
 //  Class: avtPickQuery
 //
 //  Purpose:
-//    This query gathers information about a cell picked by the user. 
+//    This query gathers information about a cell picked by the user.
 //
-//  Programmer: Kathleen Bonnell 
-//  Creation:   November 15, 2002 
+//  Programmer: Kathleen Bonnell
+//  Creation:   November 15, 2002
 //
 //  Modifications:
-//    Kathleen Bonnell, Thu Apr 17 09:39:19 PDT 2003  
+//    Kathleen Bonnell, Thu Apr 17 09:39:19 PDT 2003
 //    Added member invTransform.
 //
-//    Kathleen Bonnell, Wed May  7 13:24:37 PDT 2003 
-//    Renamed 'LocateCell' as 'LocatorFindCell'. Added 'RGridFindCell'. 
+//    Kathleen Bonnell, Wed May  7 13:24:37 PDT 2003
+//    Renamed 'LocateCell' as 'LocatorFindCell'. Added 'RGridFindCell'.
 //
-//    Kathleen Bonnell, Wed Jun 18 18:07:55 PDT 2003   
+//    Kathleen Bonnell, Wed Jun 18 18:07:55 PDT 2003
 //    Add member ghostType.
 //
-//    Kathleen Bonnell, Fri Jun 27 17:06:27 PDT 2003  
+//    Kathleen Bonnell, Fri Jun 27 17:06:27 PDT 2003
 //    Add methods RetrieveNodes, RetrieveZones, DeterminePickedNode,
 //    SetRealIds, RetrieveVarInfo -- to support Node Pick and make code
 //    more legible.
 //
-//    Kathleen Bonnell, Thu Nov 13 09:05:14 PST 2003 
+//    Kathleen Bonnell, Thu Nov 13 09:05:14 PST 2003
 //    Remove RgridFindCell, LocatorFindCell -- functionality now resides
-//    in vtkVisItUtility. 
-//    
+//    in vtkVisItUtility.
+//
 //    Hank Childs, Thu Feb  5 17:11:06 PST 2004
 //    Moved inlined destructor definition to .C file because certain compilers
 //    have problems with them.
 //
-//    Kathleen Bonnell, Mon Mar  8 15:39:15 PST 2004 
-//    Added SetNeedTransform / SetTransform and setUseSet. 
-//    
-//    Kathleen Bonnell, Thu Apr  1 09:21:22 PST 2004 
-//    Inherit from avtVariableQuery, moved common methods to parent class 
-//    (RetrieveNodes, RetrieveZones, RetrieveVarInfo). 
-// 
-//    Kathleen Bonnell, Thu Jul 29 17:10:48 PDT 2004 
-//    No longer inherit from avtVariableQuery, moved common methods back to
-//    this class: (RetrieveNodes, RetrieveZones, RetrieveVarInfo). 
+//    Kathleen Bonnell, Mon Mar  8 15:39:15 PST 2004
+//    Added SetNeedTransform / SetTransform and setUseSet.
 //
-//    Kathleen Bonnell, Wed Aug 11 09:21:07 PDT 2004 
+//    Kathleen Bonnell, Thu Apr  1 09:21:22 PST 2004
+//    Inherit from avtVariableQuery, moved common methods to parent class
+//    (RetrieveNodes, RetrieveZones, RetrieveVarInfo).
+//
+//    Kathleen Bonnell, Thu Jul 29 17:10:48 PDT 2004
+//    No longer inherit from avtVariableQuery, moved common methods back to
+//    this class: (RetrieveNodes, RetrieveZones, RetrieveVarInfo).
+//
+//    Kathleen Bonnell, Wed Aug 11 09:21:07 PDT 2004
 //    Added RetrieveVarInfo method with extra args.  Added two
-//    GetCurrentZoneForOriginal methods. 
-// 
+//    GetCurrentZoneForOriginal methods.
+//
 //    Kathleen Bonnell, Mon Aug 30 17:56:29 PDT 2004
 //    Added skippedLocate flag, and Set method.
-// 
-//    Kathleen Bonnell, Fri Sep  3 08:36:58 PDT 2004 
-//    Removed VerifyInput. 
-// 
-//    Kathleen Bonnell, Thu Sep 23 17:38:15 PDT 2004 
-//    Removed SetRealIds. 
-// 
-//    Kathleen Bonnell, Wed Dec 15 09:19:39 PST 2004 
-//    Add SetGlobalIds. 
-// 
-//    Kathleen Bonnell, Wed Dec 15 17:16:17 PST 2004 
+//
+//    Kathleen Bonnell, Fri Sep  3 08:36:58 PDT 2004
+//    Removed VerifyInput.
+//
+//    Kathleen Bonnell, Thu Sep 23 17:38:15 PDT 2004
+//    Removed SetRealIds.
+//
+//    Kathleen Bonnell, Wed Dec 15 09:19:39 PST 2004
+//    Add SetGlobalIds.
+//
+//    Kathleen Bonnell, Wed Dec 15 17:16:17 PST 2004
 //    Add GetCurrentNodeForOriginal.
-// 
-//    Kathleen Bonnell, Wed May 11 17:50:53 PDT 2005 
+//
+//    Kathleen Bonnell, Wed May 11 17:50:53 PDT 2005
 //    Added ConvertElNamesToGlobal.
 //
-//    Kathleen Bonnell, Tue Jun 28 10:47:35 PDT 2005 
-//    Re-added SetRealIds. 
-// 
+//    Kathleen Bonnell, Tue Jun 28 10:47:35 PDT 2005
+//    Re-added SetRealIds.
+//
 //    Kathleen Bonnell, Tue Nov  8 10:45:43 PST 2005
 //    Added avtDataAttributes arg to Preparation.
-//    
-//    Kathleen Bonnell, Tue Oct 24 18:59:27 PDT 2006 
-//    Add SetPickAttsForTimeQuery. 
-//    
-//    Kathleen Bonnell, Tue Jan 30 13:45:43 PST 2007 
+//
+//    Kathleen Bonnell, Tue Oct 24 18:59:27 PDT 2006
+//    Add SetPickAttsForTimeQuery.
+//
+//    Kathleen Bonnell, Tue Jan 30 13:45:43 PST 2007
 //    Added RetrieveVarInfo with ds and int args.
-//    
+//
 //    Mark C. Miller, Tue Mar 27 08:39:55 PDT 2007
 //    Added support for node origin
 //
@@ -154,14 +154,14 @@ class QUERY_API avtPickQuery : public avtDatasetQuery
 
     virtual bool                    OriginalData(void) { return true; };
 
-    // There are two distinct stages from the database, plus one for the query 
+    // There are two distinct stages from the database, plus one for the query
     // itself, so pretend we have three filters.
     virtual int                     GetNFilters(void) { return 3; };
 
     void                            SetPickAtts(const PickAttributes *pa);
     void                            SetPickAttsForTimeQuery(const PickAttributes *pa);
     const PickAttributes *          GetPickAtts(void);
-    virtual void                    SetTransform(const avtMatrix *m){}; 
+    virtual void                    SetTransform(const avtMatrix *m){};
     virtual void                    SetInvTransform(const avtMatrix *m){};
     void                            SetNeedTransform(const bool b)
                                         { needTransform = b; };
@@ -181,7 +181,7 @@ class QUERY_API avtPickQuery : public avtDatasetQuery
     // Query-specific code that needs to be defined.
     virtual void                    PreExecute(void);
     virtual void                    PostExecute(void);
-    virtual avtDataObject_p         ApplyFilters(avtDataObject_p);   
+    virtual avtDataObject_p         ApplyFilters(avtDataObject_p);
 
     // Pick-specific code
     virtual void                    Preparation(const avtDataAttributes &){};
@@ -191,19 +191,19 @@ class QUERY_API avtPickQuery : public avtDatasetQuery
 
     void                            RetrieveVarInfo(vtkDataSet *);
     void                            RetrieveVarInfo(vtkDataSet *, const int);
-    void                            RetrieveVarInfo(vtkDataSet *, const int, 
-                                                    const intVector &);   
-    bool                            RetrieveNodes(vtkDataSet *, int);   
-    bool                            RetrieveZones(vtkDataSet *, int);   
+    void                            RetrieveVarInfo(vtkDataSet *, const int,
+                                                    const intVector &);
+    bool                            RetrieveNodes(vtkDataSet *, int, bool = false);
+    bool                            RetrieveZones(vtkDataSet *, int, bool = false);
 
-    int                             GetCurrentNodeForOriginal(vtkDataSet *, 
+    int                             GetCurrentNodeForOriginal(vtkDataSet *,
                                                               const int);
-    int                             GetCurrentZoneForOriginal(vtkDataSet *, 
+    int                             GetCurrentZoneForOriginal(vtkDataSet *,
                                                               const int);
-    intVector                       GetCurrentZoneForOriginal(vtkDataSet *, 
+    intVector                       GetCurrentZoneForOriginal(vtkDataSet *,
                                                               const intVector&);
     void                            SetGlobalIds(vtkDataSet *, int);
-                                                              
+
     void                            ConvertElNamesToGlobal(void);
     void                            SetRealIds(vtkDataSet *);
 
