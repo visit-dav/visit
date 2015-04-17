4a5c33103da678c058a5086754c1fdb13d9f323c
Kathleen Bonnell <bonnell2@llnl.gov>
2015-04-17 19:10:19 +0000

    Remove no-longer needed BOOST_INCLUDE_DIR
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@26313 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/databases/Chombo/avtChomboFileFormat.C
+++ src/databases/Chombo/avtChomboFileFormat.C
@@ -94,7 +94,6 @@
 #define H5_USE_16_API
 #include <hdf5.h>
 #include <visit-hdf5.h>
-#include <cstdint>
 using     std::string;
 
 
@@ -2347,7 +2346,7 @@ avtChomboFileFormat::GetLevelAndLocalPatchNumber(int global_patch,
 class LookUpOrderCmp
 {
     public:
-        LookUpOrderCmp(const int32_t *d1, const int32_t *d2) : order1Var(d1), order2Var(d2) {}
+        LookUpOrderCmp(const int *d1, const int *d2) : order1Var(d1), order2Var(d2) {}
         bool operator()(vtkIdType a, vtkIdType b)
         {
             return order1Var[a] < order1Var[b] ||
@@ -2355,8 +2354,8 @@ class LookUpOrderCmp
         }
 
     private:
-        const int32_t *order1Var;
-        const int32_t *order2Var;
+        const int *order1Var;
+        const int *order2Var;
 };
 
 vtkDataSet *
@@ -2756,8 +2755,8 @@ avtChomboFileFormat::GetMesh(int patch, const char *meshname)
             }
         }
 
-        int32_t *particleOrder = 0;
-        int32_t *polymerNo = 0;
+        int *particleOrder = 0;
+        int *polymerNo = 0;
 
         if (connectParticles &&
             std::find(
@@ -2780,8 +2779,8 @@ avtChomboFileFormat::GetMesh(int patch, const char *meshname)
                         H5Sget_simple_extent_dims(dataSpace, &nParticlesCheck, NULL);
                         if (nParticles == nParticlesCheck)
                         {
-                            particleOrder = new int32_t[nParticles];
-                            H5Dread(dataSet, H5T_NATIVE_INT32, H5S_ALL, H5S_ALL, H5P_DEFAULT, particleOrder);
+                            particleOrder = new int[nParticles];
+                            H5Dread(dataSet, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, particleOrder);
                         }
                     }
                 }
@@ -2800,8 +2799,8 @@ avtChomboFileFormat::GetMesh(int patch, const char *meshname)
                         H5Sget_simple_extent_dims(dataSpace, &nParticlesCheck, NULL);
                         if (nParticles == nParticlesCheck)
                         {
-                            polymerNo = new int32_t[nParticles];
-                            H5Dread(dataSet, H5T_NATIVE_INT32, H5S_ALL, H5S_ALL, H5P_DEFAULT, polymerNo);
+                            polymerNo = new int[nParticles];
+                            H5Dread(dataSet, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, polymerNo);
                         }
                     }
                 }
