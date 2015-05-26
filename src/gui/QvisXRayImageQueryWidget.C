41e6bab437fe4e780f6a37ef787f4e60fa36a384
Eric Brugger <brugger1@llnl.gov>
2015-05-26 23:01:57 +0000

    1) I updated the XRay Image query window in the GUI to use the new    view description and support the recently added background intensity    parameter. This resolves #2280.
    2) I modified the image_pan parameter to the XRay Image query to no longer
       truncate the values to integer values.
    
    
    
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@26552 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/gui/QvisXRayImageQueryWidget.C
+++ src/gui/QvisXRayImageQueryWidget.C
@@ -70,6 +70,10 @@
 //   Kathleen Biagas, Wed Oct 17 12:12:29 PDT 2012
 //   Added upVector.
 //
+//   Eric Brugger, Fri May 22 15:52:32 PDT 2015
+//   I updated the window to use the new view description and support the
+//   recently added background intensity parameter.
+//
 // ****************************************************************************
 
 QvisXRayImageQueryWidget::QvisXRayImageQueryWidget(QWidget *parent,
@@ -96,65 +100,107 @@ QvisXRayImageQueryWidget::QvisXRayImageQueryWidget(QWidget *parent,
     imageFormat->setCurrentIndex(2);
     topLayout->addWidget(imageFormat, 0, 1);
 
+    //
+    // Divide emissivity by absorptivity
+    //
     divideFlag = new QCheckBox(tr("Divide Emis by Absorb"));
     divideFlag->setChecked(0);
     topLayout->addWidget(divideFlag, 1, 0, 1, 2);
 
     // 
-    // origin
+    // Background intensities
     // 
-    topLayout->addWidget(new QLabel(tr("Origin")), 2, 0);
-    origin = new QLineEdit();
-    origin->setText("0 0 0");
-    topLayout->addWidget(origin, 2, 1);
+    topLayout->addWidget(new QLabel(tr("background intensities")), 2, 0);
+    backgroundIntensities = new QLineEdit();
+    backgroundIntensities->setText("0");
+    topLayout->addWidget(backgroundIntensities, 2, 1);
 
     // 
-    // upVector
+    // Normal
     // 
-    topLayout->addWidget(new QLabel(tr("Up Vector")), 3, 0);
-    upVector = new QLineEdit();
-    upVector->setText("0 1 0");
-    topLayout->addWidget(upVector, 3, 1);
+    topLayout->addWidget(new QLabel(tr("Normal")), 3, 0);
+    normal = new QLineEdit();
+    normal->setText("0 0 1");
+    topLayout->addWidget(normal, 3, 1);
 
     // 
-    // Theta
+    // Focus
     // 
-    topLayout->addWidget(new QLabel(tr("Theta")), 4, 0);
-    theta= new QLineEdit();
-    theta->setText("0");
-    topLayout->addWidget(theta, 4, 1);
+    topLayout->addWidget(new QLabel(tr("Focus")), 4, 0);
+    focus = new QLineEdit();
+    focus->setText("0 0 0");
+    topLayout->addWidget(focus, 4, 1);
 
     // 
-    // Phi
+    // View up
     // 
-    topLayout->addWidget(new QLabel(tr("Phi")), 5, 0);
-    phi = new QLineEdit();
-    phi->setText("0");
-    topLayout->addWidget(phi, 5, 1);
+    topLayout->addWidget(new QLabel(tr("View up")), 5, 0);
+    viewUp = new QLineEdit();
+    viewUp->setText("0 1 0");
+    topLayout->addWidget(viewUp, 5, 1);
 
     // 
-    // Width
+    // View angle
     // 
-    topLayout->addWidget(new QLabel(tr("Image Width")), 6, 0);
-    width= new QLineEdit();
-    width->setText("1");
-    topLayout->addWidget(width, 6, 1);
+    topLayout->addWidget(new QLabel(tr("View angle")), 6, 0);
+    viewAngle = new QLineEdit();
+    viewAngle->setText("30");
+    topLayout->addWidget(viewAngle, 6, 1);
 
     // 
-    // Height
+    // Parallel scale
     // 
-    topLayout->addWidget(new QLabel(tr("Image Height")), 7, 0);
-    height = new QLineEdit();
-    height->setText("1");
-    topLayout->addWidget(height, 7, 1);
+    topLayout->addWidget(new QLabel(tr("Parallel scale")), 7, 0);
+    parallelScale = new QLineEdit();
+    parallelScale->setText("10");
+    topLayout->addWidget(parallelScale, 7, 1);
 
     // 
-    // Pixel Size
+    // Near plane
     // 
-    topLayout->addWidget(new QLabel(tr("Image Pixel Size")), 8, 0);
+    topLayout->addWidget(new QLabel(tr("Near plane")), 8, 0);
+    nearPlane = new QLineEdit();
+    nearPlane->setText("-20");
+    topLayout->addWidget(nearPlane, 8, 1);
+
+    // 
+    // Far plane
+    // 
+    topLayout->addWidget(new QLabel(tr("Far plane")), 9, 0);
+    farPlane = new QLineEdit();
+    farPlane->setText("20");
+    topLayout->addWidget(farPlane, 9, 1);
+
+    // 
+    // Image pan
+    // 
+    topLayout->addWidget(new QLabel(tr("Image pan")), 10, 0);
+    imagePan = new QLineEdit();
+    imagePan->setText("0 0");
+    topLayout->addWidget(imagePan, 10, 1);
+
+    // 
+    // Image zoom
+    // 
+    topLayout->addWidget(new QLabel(tr("Image zoom")), 11, 0);
+    imageZoom = new QLineEdit();
+    imageZoom->setText("1");
+    topLayout->addWidget(imageZoom, 11, 1);
+
+    //
+    // Perspective
+    //
+    perspective = new QCheckBox(tr("Perspective"));
+    perspective->setChecked(0);
+    topLayout->addWidget(perspective, 12, 0, 1, 2);
+
+    // 
+    // Image size
+    // 
+    topLayout->addWidget(new QLabel(tr("Image Size")), 13, 0);
     imageSize = new QLineEdit();
-    imageSize->setText("200 200");
-    topLayout->addWidget(imageSize, 8, 1);
+    imageSize->setText("500 500");
+    topLayout->addWidget(imageSize, 13, 1);
 }
 
 // ****************************************************************************
@@ -184,6 +230,49 @@ QvisXRayImageQueryWidget::~QvisXRayImageQueryWidget()
 //
 // Arguments:
 //   whichWidget     : The text field to parse.
+//   pt    : The array in which the values will be stored.
+//
+// Returns:    True if it worked.
+//
+// Programmer: Eric Brugger
+// Creation:   May 22, 2015
+//
+// Modifications:
+//
+// ****************************************************************************
+
+bool 
+QvisXRayImageQueryWidget::GetDoubleValues(int whichWidget, doubleVector &pt)
+{
+    QString temp;
+  
+    if (whichWidget == 0) // Background intensities
+    {
+        temp = backgroundIntensities->displayText().simplified();
+    }
+    bool okay = !temp.isEmpty();
+
+    if(okay)
+    {
+        QStringList s = temp.split(" ", QString::SkipEmptyParts);
+        for (int i = 0; okay && i < s.size(); ++i)
+        {
+            double val = s[i].toDouble(&okay);
+            if (okay) pt.push_back(val);
+        }
+    }
+
+    return okay;
+}
+
+// ****************************************************************************
+// Method: QvisXRayImageQueryWidget::GetDoubleValues
+//
+// Purpose:
+//   Gets double values from a text field.
+//
+// Arguments:
+//   whichWidget     : The text field to parse.
 //   n     : The number of values expected
 //   pt    : The array in which the values will be stored.
 //
@@ -196,6 +285,10 @@ QvisXRayImageQueryWidget::~QvisXRayImageQueryWidget()
 //   Kathleen Biagas, Wed Oct 17 12:12:29 PDT 2012
 //   Added upVector.
 //
+//   Eric Brugger, Fri May 22 15:52:32 PDT 2015
+//   I updated the window to use the new view description and support the
+//   recently added background intensity parameter.
+//
 // ****************************************************************************
 
 bool 
@@ -203,29 +296,41 @@ QvisXRayImageQueryWidget::GetDoubleValues(int whichWidget, int n, double *pt)
 {
     QString temp;
   
-    if (whichWidget == 0) // Origin
+    if (whichWidget == 1) // Normal
     {
-        temp = origin->displayText().simplified();
+        temp = normal->displayText().simplified();
     }
-    else if (whichWidget == 1) // upVector
+    else if (whichWidget == 2) // Focus
     {
-        temp = upVector->displayText().simplified();
+        temp = focus->displayText().simplified();
     }
-    else if (whichWidget == 2) // Theta
+    else if (whichWidget == 3) // View up
     {
-        temp = theta->displayText().simplified();
+        temp = viewUp->displayText().simplified();
     }
-    else if (whichWidget == 3) // Phi
+    else if (whichWidget == 4) // View angle
     {
-        temp = phi->displayText().simplified();
+        temp = viewAngle->displayText().simplified();
     }
-    else if (whichWidget == 4) // Image Width
+    else if (whichWidget == 5) // Parallel scale
     {
-        temp = width->displayText().simplified();
+        temp = parallelScale->displayText().simplified();
     }
-    else if (whichWidget == 5) // Image Height
+    else if (whichWidget == 6) // Near plane
     {
-        temp = height->displayText().simplified();
+        temp = nearPlane->displayText().simplified();
+    }
+    else if (whichWidget == 7) // Far plane
+    {
+        temp = farPlane->displayText().simplified();
+    }
+    else if (whichWidget == 8) // Image pan
+    {
+        temp = imagePan->displayText().simplified();
+    }
+    else if (whichWidget == 9) // Image zoom
+    {
+        temp = imageZoom->displayText().simplified();
     }
     bool okay = !temp.isEmpty();
 
@@ -255,7 +360,7 @@ QvisXRayImageQueryWidget::GetDoubleValues(int whichWidget, int n, double *pt)
 // Method: QvisXRayImageQueryWidget::GetIntValues
 //
 // Purpose:
-//   Gets a point from the text fiels containing ints.
+//   Gets a point from the text fields containing ints.
 //
 // Arguments:
 //   whichWidget : which widget to retrieve from
@@ -266,6 +371,11 @@ QvisXRayImageQueryWidget::GetDoubleValues(int whichWidget, int n, double *pt)
 // Programmer: Kathleen Biagas 
 // Creation:   June 17, 2011
 //
+// Modifications:
+//   Eric Brugger, Fri May 22 15:52:32 PDT 2015
+//   I updated the window to use the new view description and support the
+//   recently added background intensity parameter.
+//
 // ****************************************************************************
 
 bool 
@@ -273,7 +383,7 @@ QvisXRayImageQueryWidget::GetIntValues(int whichWidget, int *pt)
 {
     QString temp;
 
-    if (whichWidget == 3) // Image Pixel Size
+    if (whichWidget == 10) // Image size
     {
         temp = imageSize->displayText().simplified();
     }
@@ -307,55 +417,85 @@ QvisXRayImageQueryWidget::GetIntValues(int whichWidget, int *pt)
 // Creation:   June 17, 2011
 //
 // Modifications:
-//    Kathleen Biagas, Wed Sep  7 08:40:22 PDT 2011
-//    Return output_type as string instead of int.
+//   Kathleen Biagas, Wed Sep  7 08:40:22 PDT 2011
+//   Return output_type as string instead of int.
 // 
 //   Kathleen Biagas, Wed Oct 17 12:12:29 PDT 2012
 //   Added upVector.
+//
+//   Eric Brugger, Fri May 22 15:52:32 PDT 2015
+//   I updated the window to use the new view description and support the
+//   recently added background intensity parameter.
 //
 // ****************************************************************************
 bool
 QvisXRayImageQueryWidget::GetQueryParameters(MapNode &params)
 {
-    doubleVector origin(3);
-    doubleVector upVector(3);
-    double       t, p, w, h;
-    intVector    is(2);
+    doubleVector backgroundIntensities;
+    doubleVector normal(3);
+    doubleVector focus(3);
+    doubleVector viewUp(3);
+    double       viewAngle, parallelScale, nearPlane, farPlane;
+    doubleVector imagePan(2);
+    double       imageZoom;
+    intVector    imageSize(2);
 
     bool noerrors = true;
 
-    if (!GetDoubleValues(0, 3, &origin[0]))
+    if (!GetDoubleValues(0, backgroundIntensities))
+        noerrors = false;
+    if (backgroundIntensities.size() == 0)
         noerrors = false;
 
-    if (!GetDoubleValues(1, 3, &upVector[0]))
+    if (noerrors && !GetDoubleValues(1, 3, &normal[0]))
         noerrors = false;
 
-    if (noerrors && !GetDoubleValues(2, 1, &t))
+    if (noerrors && !GetDoubleValues(2, 3, &focus[0]))
         noerrors = false;
 
-    if (noerrors && !GetDoubleValues(3, 1, &p))
+    if (noerrors && !GetDoubleValues(3, 3, &viewUp[0]))
         noerrors = false;
 
-    if (noerrors && !GetDoubleValues(4, 1, &w))
+    if (noerrors && !GetDoubleValues(4, 1, &viewAngle))
         noerrors = false;
 
-    if (noerrors && !GetDoubleValues(5, 1, &h))
+    if (noerrors && !GetDoubleValues(5, 1, &parallelScale))
         noerrors = false;
 
-    if (noerrors && !GetIntValues(3, &is[0]))
+    if (noerrors && !GetDoubleValues(6, 1, &nearPlane))
+        noerrors = false;
+
+    if (noerrors && !GetDoubleValues(7, 1, &farPlane))
+        noerrors = false;
+
+    if (noerrors && !GetDoubleValues(8, 2, &imagePan[0]))
+        noerrors = false;
+
+    if (noerrors && !GetDoubleValues(9, 1, &imageZoom))
+        noerrors = false;
+
+    if (noerrors && !GetIntValues(10, &imageSize[0]))
         noerrors = false;
 
     if (noerrors)
     {
         params["output_type"] = imageFormat->currentText().toStdString();
         params["divide_emis_by_absorb"] = (int)divideFlag->isChecked();
-        params["origin"] = origin;
-        params["up_vector"] = upVector;
-        params["theta"] = t;
-        params["phi"] = p;
-        params["width"] = w;
-        params["height"] = h;
-        params["image_size"] = is;
+        if (backgroundIntensities.size() == 1)
+            params["background_intensity"] = backgroundIntensities[0];
+        else
+            params["background_intensities"] = backgroundIntensities;
+        params["normal"] = normal;
+        params["focus"] = focus;
+        params["view_up"] = viewUp;
+        params["view_angle"] = viewAngle;
+        params["parallel_scale"] = parallelScale;
+        params["near_plane"] = nearPlane;
+        params["far_plane"] = farPlane;
+        params["image_pan"] = imagePan;
+        params["image_zoom"] = imageZoom;
+        params["perspective"] = (int)perspective->isChecked();;
+        params["image_size"] = imageSize;
     }
     return noerrors; 
 }
