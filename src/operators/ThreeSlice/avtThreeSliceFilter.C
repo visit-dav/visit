8485f0b089044481774371ce37896cda0960c573
Eric Brugger <brugger1@llnl.gov>
2015-08-25 16:31:35 +0000

    I corrected a bug with the ThreeSlice operator where it would give an error if any of the domains being slided resulted in no geometry. This resolved #2363.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@27024 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/operators/ThreeSlice/avtThreeSliceFilter.C
+++ src/operators/ThreeSlice/avtThreeSliceFilter.C
@@ -179,6 +179,11 @@ avtThreeSliceFilter::Equivalent(const AttributeGroup *a)
 //    Eric Brugger, Tue Aug 19 09:30:56 PDT 2014
 //    Modified the class to work with avtDataRepresentation.
 //
+//    Eric Brugger, Tue Aug 25 09:12:36 PDT 2015
+//    Modified the routine to return NULL if the slice created a data set
+//    with no cells. This fixes a bug where an error would be generated if
+//    any of the domains being sliced resulted in no geometry.
+//
 // ****************************************************************************
 
 avtDataRepresentation *
@@ -392,17 +397,14 @@ avtThreeSliceFilter::ExecuteData(avtDataRepresentation *in_dr)
         }
     }    
     
-    if (rv->GetNumberOfCells() == 0)
+    avtDataRepresentation *out_dr = NULL;
+    if (rv->GetNumberOfCells() > 0)
     {
-        rv->Delete();
-        rv = NULL;
+        out_dr = new avtDataRepresentation(rv,
+            in_dr->GetDomain(), in_dr->GetLabel());
     }
 
-    avtDataRepresentation *out_dr = new avtDataRepresentation(rv,
-        in_dr->GetDomain(), in_dr->GetLabel());
-
-    if (rv != NULL)
-        rv->Delete();
+    rv->Delete();
 
     slicer->Delete();
     merger->Delete();
