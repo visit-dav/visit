b41c3752e769566a14ccc24a5d80dac87ddbc6cf
Allen Sanderson <allen@sci.utah.edu>
2015-09-04 18:44:25 +0000

    splitting the LCS tests
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@27108 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/operators/lcs_ic.py
+++ test/tests/operators/lcs_ic.py
@@ -18,19 +18,21 @@
 db=data_path("pics_test_data/ftle_double_gyre_1_domain.pics")
 OpenDatabase(db)
 AddPlot("Pseudocolor", "operators/LCS/velocity")
+AddOperator("IntegralCurve")
 
 LCSAtts = LCSAttributes()
 LCSAtts.Resolution = (101, 51, 1)
 LCSAtts.integrationDirection = LCSAtts.Forward  # Forward, Backward, Both
 LCSAtts.auxiliaryGridSpacing = 0.005
 LCSAtts.maxSteps = 1000000
-LCSAtts.operationType = LCSAtts.Lyapunov  # IntegrationTime, ArcLength, AverageDistanceFromSeed, EigenValue, EigenVector, Lyapunov
+LCSAtts.operationType = LCSAtts.EigenVector  # IntegrationTime, ArcLength, AverageDistanceFromSeed, EigenValue, EigenVector, Lyapunov
 LCSAtts.cauchyGreenTensor = LCSAtts.Right  # Left, Right
-LCSAtts.eigenComponent = LCSAtts.Largest  # Smallest, Intermediate, Largest, PosShearVector, NegShearVector, PosLambdaShearVector, NegLambdaShearVector
+LCSAtts.eigenComponent = LCSAtts.PosLambdaShearVector  # Smallest, Intermediate, Largest, PosShearVector, NegShearVector, PosLambdaShearVector, NegLambdaShearVector
+LCSAtts.eigenWeight = 0.98
 LCSAtts.operatorType = LCSAtts.BaseValue  # BaseValue, Gradient
 LCSAtts.terminationType = LCSAtts.Time  # Time, Distance, Size
 LCSAtts.terminateByTime = 1
-LCSAtts.termTime = 4
+LCSAtts.termTime = 10
 LCSAtts.maxStepLength = 0.001
 LCSAtts.integrationType = LCSAtts.AdamsBashforth  # Euler, Leapfrog, DormandPrince, AdamsBashforth, RK4, M3DC12DIntegrator
 LCSAtts.parallelizationAlgorithmType = LCSAtts.ParallelStaticDomains  # LoadOnDemand, ParallelStaticDomains, MasterSlave, VisItSelects
@@ -39,15 +41,29 @@ LCSAtts.pathlinesCMFE = LCSAtts.CONN_CMFE  # CONN_CMFE, POS_CMFE
 SetOperatorOptions(LCSAtts, 0)
 
 
+IntegralCurveAtts = IntegralCurveAttributes()
+IntegralCurveAtts.sourceType = IntegralCurveAtts.Point  # Point, PointList, Line_, Circle, Plane, Sphere, Box, Selection, FieldData
+IntegralCurveAtts.pointSource = (1.23053, 0.624189, 0)
+IntegralCurveAtts.dataValue = IntegralCurveAtts.SeedPointID  # Solid, SeedPointID, Speed, Vorticity, ArcLength, TimeAbsolute, TimeRelative, AverageDistanceFromSeed, CorrelationDistance, Difference, Variable
+IntegralCurveAtts.integrationDirection = IntegralCurveAtts.ForwardDirectionless  # Forward, Backward, Both, ForwardDirectionless, BackwardDirectionless, BothDirectionless
+IntegralCurveAtts.maxSteps = 2000
+IntegralCurveAtts.maxStepLength = 0.001
+IntegralCurveAtts.integrationType = IntegralCurveAtts.AdamsBashforth  # Euler, Leapfrog, DormandPrince, AdamsBashforth, RK4, M3DC12DIntegrator
+IntegralCurveAtts.parallelizationAlgorithmType = IntegralCurveAtts.ParallelStaticDomains  # LoadOnDemand, ParallelStaticDomains, MasterSlave, VisItSelects
+SetOperatorOptions(IntegralCurveAtts, 0)
+
+
 databases=["ftle_double_gyre_1_domain", "ftle_double_gyre_2_domains"]
 
 src_type=[LCSAtts.RegularGrid, LCSAtts.NativeMesh]
 src_type_str=["RegularGrid", "NativeMesh"]
 
-aux_grid=[LCSAtts.None, LCSAtts.TwoDim]
-aux_grid_str=["NoAuxGrid", "2DAuxGrid"]
+aux_grid=[LCSAtts.TwoDim]
+aux_grid_str=["2DAuxGrid"]
 
-TestSection("Basic FTLE function")
+# FTLE with integral curve
+TestSection("Basic FTLE function with integral curve operator")
+
 for i in range(len(databases)):
   db=data_path("pics_test_data/%s.pics") %(databases[i])
   str="Testing database = %s" %(db)
@@ -66,42 +82,12 @@ for i in range(len(databases)):
      for k in range(len(aux_grid)):
         str="Testing auxiliary grid = %s" %(aux_grid_str[k])
         TestSection(str)
-        LCSAtts.auxiliaryGrid = aux_grid[k]  # None, TwoDim
+        LCSAtts.auxiliaryGrid = aux_grid[k]  # TwoDim
         SetOperatorOptions(LCSAtts, 0)
-        DrawPlots()
-        str="lcs_%s_%s_%s" %(databases[i], src_type_str[j], aux_grid_str[k])
-        Test(str)
-
-
-
-# FTLE with smallest exponent
-LCSAtts.eigenComponent = LCSAtts.Smallest  # Smallest, Intermediate, Largest, PosShearVector, NegShearVector, PosLambdaShearVector, NegLambdaShearVector
-SetOperatorOptions(LCSAtts, 0)
-DrawPlots()
-str="lcs_%s_%s_%s_Smallest" %(databases[i], src_type_str[j], aux_grid_str[k])
-Test(str)
-
-
-
-# FTLE with left Cauchy Green Tensor
-LCSAtts.cauchyGreenTensor = LCSAtts.Left  # Left, Right
-LCSAtts.eigenComponent = LCSAtts.Largest  # Smallest, Intermediate, Largest, PosShearVector, NegShearVector, PosLambdaShearVector, NegLambdaShearVector
-SetOperatorOptions(LCSAtts, 0)
-DrawPlots()
-str="lcs_%s_%s_%s_Left" %(databases[i], src_type_str[j], aux_grid_str[k])
-Test(str)
-
-
-
-# FTLE with eigen value
-LCSAtts.operationType = LCSAtts.EigenValue  # IntegrationTime, ArcLength, AverageDistanceFromSeed, EigenValue, EigenVector, Lyapunov
-LCSAtts.cauchyGreenTensor = LCSAtts.Right  # Left, Right
-LCSAtts.eigenComponent = LCSAtts.Largest  # Smallest, Intermediate, Largest, PosShearVector, NegShearVector, PosLambdaShearVector, NegLambdaShearVector
-SetOperatorOptions(LCSAtts, 0)
-DrawPlots()
-str="lcs_%s_%s_%s_EigenValue" %(databases[i], src_type_str[j], aux_grid_str[k])
-Test(str)
-
+        if (i==0) | (i==1 & j==1):
+           DrawPlots()
+           str="lcs_%s_%s_%s_IntegralCurve" %(databases[i], src_type_str[j], aux_grid_str[k])
+           Test(str)
 
 Exit()
 
@@ -113,95 +99,71 @@ Exit()
 db="/Projects/VisIt/trunk/build/data/pics_test_data/%s.pics" %(databases[i])
 OpenDatabase(db)
 AddPlot("Pseudocolor", "operators/LCS/velocity", 1, 0)
-LCSAtts = LCSAttributes()
+AddOperator("IntegralCurve")
+
 
 LCSAtts = LCSAttributes()
 LCSAtts.Resolution = (101, 51, 1)
 LCSAtts.integrationDirection = LCSAtts.Forward  # Forward, Backward, Both
 LCSAtts.auxiliaryGridSpacing = 0.005
 LCSAtts.maxSteps = 1000000
-LCSAtts.operationType = LCSAtts.Lyapunov  # IntegrationTime, ArcLength, AverageDistanceFromSeed, EigenValue, EigenVector, Lyapunov
+LCSAtts.operationType = LCSAtts.EigenVector  # IntegrationTime, ArcLength, AverageDistanceFromSeed, EigenValue, EigenVector, Lyapunov
 LCSAtts.cauchyGreenTensor = LCSAtts.Right  # Left, Right
-LCSAtts.eigenComponent = LCSAtts.Largest  # Smallest, Intermediate, Largest, PosShearVector, NegShearVector, PosLambdaShearVector, NegLambdaShearVector
+LCSAtts.eigenComponent = LCSAtts.PosLambdaShearVector  # Smallest, Intermediate, Largest, PosShearVector, NegShearVector, PosLambdaShearVector, NegLambdaShearVector
+LCSAtts.eigenWeight = 0.98
 LCSAtts.operatorType = LCSAtts.BaseValue  # BaseValue, Gradient
 LCSAtts.terminationType = LCSAtts.Time  # Time, Distance, Size
 LCSAtts.terminateByTime = 1
-LCSAtts.termTime = 4
+LCSAtts.termTime = 10
 LCSAtts.maxStepLength = 0.001
 LCSAtts.integrationType = LCSAtts.AdamsBashforth  # Euler, Leapfrog, DormandPrince, AdamsBashforth, RK4, M3DC12DIntegrator
 LCSAtts.parallelizationAlgorithmType = LCSAtts.ParallelStaticDomains  # LoadOnDemand, ParallelStaticDomains, MasterSlave, VisItSelects
 LCSAtts.pathlines = 1
 LCSAtts.pathlinesCMFE = LCSAtts.CONN_CMFE  # CONN_CMFE, POS_CMFE
 SetOperatorOptions(LCSAtts, 0)
-#DrawPlots()
+
+
+IntegralCurveAtts = IntegralCurveAttributes()
+IntegralCurveAtts.sourceType = IntegralCurveAtts.Point  # Point, PointList, Line_, Circle, Plane, Sphere, Box, Selection, FieldData
+IntegralCurveAtts.pointSource = (1.23053, 0.624189, 0)
+IntegralCurveAtts.dataValue = IntegralCurveAtts.SeedPointID  # Solid, SeedPointID, Speed, Vorticity, ArcLength, TimeAbsolute, TimeRelative, AverageDistanceFromSeed, CorrelationDistance, Difference, Variable
+IntegralCurveAtts.integrationDirection = IntegralCurveAtts.ForwardDirectionless  # Forward, Backward, Both, ForwardDirectionless, BackwardDirectionless, BothDirectionless
+IntegralCurveAtts.maxSteps = 2000
+IntegralCurveAtts.maxStepLength = 0.001
+IntegralCurveAtts.integrationType = IntegralCurveAtts.AdamsBashforth  # Euler, Leapfrog, DormandPrince, AdamsBashforth, RK4, M3DC12DIntegrator
+IntegralCurveAtts.parallelizationAlgorithmType = IntegralCurveAtts.ParallelStaticDomains  # LoadOnDemand, ParallelStaticDomains, MasterSlave, VisItSelects
+SetOperatorOptions(IntegralCurveAtts, 0)
+
 
 databases=["ftle_double_gyre_1_domain", "ftle_double_gyre_2_domains"]
 
 src_type=[LCSAtts.RegularGrid, LCSAtts.NativeMesh]
 src_type_str=["RegularGrid", "NativeMesh"]
 
-aux_grid=[LCSAtts.None, LCSAtts.TwoDim]
-aux_grid_str=["NoAuxGrid", "Two2DAuxGrid"]
+aux_grid=[LCSAtts.TwoDim]
+aux_grid_str=["Two2DAuxGrid"]
 
 
-
+# FTLE with integral curve
 for i in range(len(databases)):
   db="/Projects/VisIt/trunk/build/data/pics_test_data/%s.pics" %(databases[i])
   OpenDatabase(db)
+  ReplaceDatabase(db)
   for j in range(len(src_type)):
      str="Testing sample source = %s" %(src_type_str[j])
      LCSAtts.sourceType = src_type[j]  # NativeMesh, RegularGrid
      for k in range(len(aux_grid)):
         str="Testing auxiliary grid = %s" %(aux_grid_str[k])
-        DeleteAllPlots()
-        AddPlot("Pseudocolor", "operators/LCS/velocity", 1, 0)
-        LCSAtts.auxiliaryGrid = aux_grid[k]  # None, TwoDim
+        TestSection(str)
+        LCSAtts.auxiliaryGrid = aux_grid[k]  # TwoDim
         SetOperatorOptions(LCSAtts, 0)
-        #DrawPlots()
-        str="lcs_%s_%s_%s" %(databases[i], src_type_str[j], aux_grid_str[k])
+        DrawPlots()
+        str="lcs_%s_%s_%s_IntegralCurve" %(databases[i], src_type_str[j], aux_grid_str[k])
         swatts = SaveWindowAttributes()
         swatts.family = 0
         swatts.fileName = "/Projects/tmp/lcs/ser/%s" %(str)
         SetSaveWindowAttributes(swatts)
         SaveWindow()
-
-
-# FTLE with smallest exponent
-LCSAtts.eigenComponent = LCSAtts.Smallest  # Smallest, Intermediate, Largest, PosShearVector, NegShearVector, PosLambdaShearVector, NegLambdaShearVector
-SetOperatorOptions(LCSAtts, 0)
-#DrawPlots()
-str="lcs_%s_%s_%s_Smallest" %(databases[i], src_type_str[j], aux_grid_str[k])
-swatts = SaveWindowAttributes()
-swatts.family = 0
-swatts.fileName = "/Projects/tmp/lcs/ser/%s" %(str)
-SetSaveWindowAttributes(swatts)
-SaveWindow()
-
-# FTLE with left Cauchy Green Tensor
-LCSAtts.cauchyGreenTensor = LCSAtts.Left  # Left, Right
-LCSAtts.eigenComponent = LCSAtts.Largest  # Smallest, Intermediate, Largest, PosShearVector, NegShearVector, PosLambdaShearVector, NegLambdaShearVector
-SetOperatorOptions(LCSAtts, 0)
-#DrawPlots()
-str="lcs_%s_%s_%s_Left" %(databases[i], src_type_str[j], aux_grid_str[k])
-swatts = SaveWindowAttributes()
-swatts.family = 0
-swatts.fileName = "/Projects/tmp/lcs/ser/%s" %(str)
-SetSaveWindowAttributes(swatts)
-SaveWindow()
-
-# FTLE with right eigen value
-LCSAtts.operationType = LCSAtts.EigenValue  # IntegrationTime, ArcLength, AverageDistanceFromSeed, EigenValue, EigenVector, Lyapunov
-LCSAtts.cauchyGreenTensor = LCSAtts.Right  # Left, Right
-LCSAtts.eigenComponent = LCSAtts.Largest  # Smallest, Intermediate, Largest, PosShearVector, NegShearVector, PosLambdaShearVector, NegLambdaShearVector
-SetOperatorOptions(LCSAtts, 0)
-#DrawPlots()
-str="lcs_%s_%s_%s_EigenValue" %(databases[i], src_type_str[j], aux_grid_str[k])
-swatts = SaveWindowAttributes()
-swatts.family = 0
-swatts.fileName = "/Projects/tmp/lcs/ser/%s" %(str)
-SetSaveWindowAttributes(swatts)
-SaveWindow()
-
 
 # 1 processor:
 
