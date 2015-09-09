9237112c216324a41948a8bb7efced61304a9ff2
Mark Miller <miller86@llnl.gov>
2015-09-09 20:16:53 +0000

    adding some spark examples of VisIt queries
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@27132 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ src/tools/hdfs_export/spark_examples/MinMax.py
@@ -0,0 +1,40 @@
+import sys
+from pyspark import SparkContext
+from pyspark import SparkConf
+
+def min(a,b):
+    if a[1] <= b[1]:
+        return a
+    return b
+
+def max(a,b):
+    if a[1] >= b[1]:
+        return a
+    return b
+
+#
+# Perform the equiv. of a VisIt MinMax query
+#
+if __name__ == "__main__":
+  if len(sys.argv) < 4:
+    print >> sys.stderr, "Usage: MinMax <dbfile> <mesh> <var>"
+    exit(-1)
+
+  sc = SparkContext()
+  var_kv = sc.textFile("file:/home/training/vdbroot.silo/miller86/%s/*/%s/*/%s.txt.bz2"%\
+     (sys.argv[1],sys.argv[2],sys.argv[3])).map(lambda line: (line[0:18], float(line[19:])))
+  #
+  # we compute min reduce and max reduce separately. More execution time but less memory
+  #
+
+  # find foo -name v.txt.bz2 -exec bzcat {} \; | sort -n -k 2 -t',' -r | tail -n 1
+  # 0.39 seconds in shell
+  min_kv = var_kv.reduce(min)
+
+  # find foo -name v.txt.bz2 -exec bzcat {} \; | sort -n -k 2 -t',' | tail -n 1
+  # 0.39 seconds in shell
+  max_kv = var_kv.reduce(max)
+
+  # 9 seconds on laptop VM
+  print "Minimum value at %s is %g\n"%min_kv
+  print "Maximum value at %s is %g\n"%max_kv
---
+++ src/tools/hdfs_export/spark_examples/NumNodes.py
@@ -0,0 +1,30 @@
+import sys
+from pyspark import SparkContext
+from pyspark import SparkConf
+
+#
+# Performs equivalent of a VisIt NumNodes query over all blocks of specified mesh
+# Returns total node count and count of just ghost nodes
+#
+if __name__ == "__main__":
+  if len(sys.argv) < 3:
+    print >> sys.stderr, "Usage: NumNodes <dbfile> <mesh>"
+    exit(-1)
+
+  sc = SparkContext()
+  coord_data = sc.textFile("file:/home/training/vdbroot.silo/miller86/%s/*/%s/*/coords.txt.bz2"%\
+     (sys.argv[1],sys.argv[2]))
+
+  # 0.087 seconds on laptop VM
+  # find foo -name coords.txt.bz2 -exec bzcat {} \; | wc -l
+  # count of all nodes
+  all_node_count = coord_data.count()
+
+  # .203 seconds
+  # find foo -name coords.txt.bz2 -exec bzcat {} \; | grep '0[a-zA-Z0-9#%]*,1,' | wc -l
+  # count of just ghost nodes
+  ghost_node_count = coord_data.filter(lambda line: line[19]=='1').count()
+
+  # 9.2 seconds (on laptop VM running pyspark) cating output to /dev/null
+  print "Number of nodes is", all_node_count
+  print "Number of ghost nodes is", ghost_node_count
