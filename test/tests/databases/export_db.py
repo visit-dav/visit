b5695683885fc2ff3b8b95aa3d533e62a11b5f1b
Brad Whitlock <bjw@ilight.com>
2015-09-14 18:39:21 +0000

    Improved tests for exporting databases
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@27172 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/databases/export_db.py
+++ test/tests/databases/export_db.py
@@ -34,59 +34,350 @@
 #    was properly exported.
 #
 # ----------------------------------------------------------------------------
+import string
 
+def test0():
+    OpenDatabase(silo_data_path("globe.silo"))
+    AddPlot("Pseudocolor", "t")
+    DrawPlots()
 
+    # Set the export database attributes.
+    e = ExportDBAttributes()
+    e.db_type = "VTK"
+    e.filename = "test_ex_db"
+    ExportDatabase(e)
 
-OpenDatabase(silo_data_path("globe.silo"))
+    e.variables = ("u", "v")
+    e.filename = "test_ex_db2"
+    e.db_type = "Silo"
+    ExportDatabase(e)
 
-AddPlot("Pseudocolor", "t")
-DrawPlots()
+    DeleteAllPlots()
+    OpenDatabase("test_ex_db.vtk")
+    AddPlot("Pseudocolor", "t")
+    DrawPlots()
+    Test("export_db_01")
 
-# Set the export database attributes.
-e = ExportDBAttributes()
-e.db_type = "VTK"
-e.filename = "test_ex_db"
-ExportDatabase(e)
+    DeleteAllPlots()
+    OpenDatabase("test_ex_db2.silo")
+    AddPlot("Pseudocolor", "u")
+    DrawPlots()
+    Test("export_db_02")
 
-e.variables = ("u", "v")
-e.filename = "test_ex_db2"
-e.db_type = "Silo"
-ExportDatabase(e)
+    DeleteAllPlots()
+    OpenDatabase(silo_data_path("wave.visit"))
 
-DeleteAllPlots()
-OpenDatabase("test_ex_db.vtk")
-AddPlot("Pseudocolor", "t")
-DrawPlots()
-Test("export_db_01")
+    DefineScalarExpression("cmfe", "conn_cmfe(coord(<%s:quadmesh>)[1], quadmesh)" % cmfe_silo_data_path("wave0020.silo"))
+    AddPlot("Pseudocolor", "pressure")
+    DrawPlots()
+    e.variables = ("cmfe")
+    e.filename = "test_ex_db3"
+    e.db_type = "VTK"
+    ExportDatabase(e)
+    DeleteAllPlots()
 
-DeleteAllPlots()
-OpenDatabase("test_ex_db2.silo")
-AddPlot("Pseudocolor", "u")
-DrawPlots()
-Test("export_db_02")
+    DeleteExpression("cmfe")
+    OpenDatabase("test_ex_db3.vtk")
+    AddPlot("Pseudocolor", "cmfe")
+    DrawPlots()
+    Test("export_db_03")
 
-DeleteAllPlots()
-OpenDatabase(silo_data_path("wave.visit"))
+    # Ensure meshname was written by creating mesh plot with old db's meshname
+    AddPlot("Mesh", "quadmesh")
+    DrawPlots()
+    Test("export_db_04")
+    DeleteAllPlots()
 
-DefineScalarExpression("cmfe", "conn_cmfe(coord(<%s:quadmesh>)[1], quadmesh)" % cmfe_silo_data_path("wave0020.silo"))
-AddPlot("Pseudocolor", "pressure")
-DrawPlots()
-e.variables = ("cmfe")
-e.filename = "test_ex_db3"
-e.db_type = "VTK"
-ExportDatabase(e)
-DeleteAllPlots()
+def VTK_check_binary(filename):
+    f = open(filename, "rt")
+    line = ""
+    for i in (0,1,2):
+        line = f.readline()
+    b = "BINARY" == line[0:6]
+    return b
 
-DeleteExpression("cmfe")
-OpenDatabase("test_ex_db3.vtk")
-AddPlot("Pseudocolor", "cmfe")
-DrawPlots()
-Test("export_db_03")
+def TestExportedData(datafile, testname, setupCB):
+    AddWindow()
+    OpenDatabase(datafile)
+    setupCB()
+    Test(testname)
+    DeleteWindow()
 
-# Ensure meshname was written by creating mesh plot with old db's meshname
-AddPlot("Mesh", "quadmesh")
-DrawPlots()
-Test("export_db_04")
+def cleanup_files():
+    exts = ("*.ply", "*.raw", "*.stl", "*.silo", "*.tec", "*.okc", "*.visit", "*.xyz", "*.obj")
+    files = os.listdir(".")
+    for f in files:
+        for e in exts:
+            pattern = e[1:]
+            if f[-len(pattern):] == pattern:
+                os.unlink(f)
+                break
 
+def test1():
+    TestSection("Test export of some surfaces.")
+    OpenDatabase(silo_data_path("multi_rect3d.silo"))
+    DefineScalarExpression("rad", "magnitude(coords(mesh1))")
+    AddPlot("Pseudocolor", "u")
+    AddOperator("Isosurface")
+    iso = IsosurfaceAttributes()
+    iso.contourValue = (0.55, 1.455)
+    iso.contourMethod = iso.Value
+    iso.variable = "rad"
+    SetOperatorOptions(iso)
+    DrawPlots()
+
+    v = GetView3D()
+    v.viewNormal = (-0.467474, 0.301847, 0.830877)
+    v.focus = (0.5, 0.5, 0.5)
+    v.viewUp = (0.140705, 0.953323, -0.267166)
+    v.viewAngle = 30
+    v.parallelScale = 0.866025
+    v.nearPlane = -1.73205
+    v.farPlane = 1.73205
+    v.imagePan = (-0.0154649, 0.027457)
+    v.imageZoom = 1.14276
+    v.perspective = 1
+    v.eyeAngle = 2
+    v.centerOfRotationSet = 0
+    v.centerOfRotation = (0.5, 0.5, 0.5)
+    v.axis3DScaleFlag = 0
+    v.axis3DScales = (1, 1, 1)
+    v.shear = (0, 0, 1)
+    v.windowValid = 1
+    SetView3D(v)
+    Test("export_db_1_00")
+
+    # Test VTK binary
+    e = ExportDBAttributes()
+    e.db_type = "VTK"
+    e.filename = "binary_VTK"
+    opts = GetExportOptions("VTK")
+    opts['Binary format'] = 1
+    ExportDatabase(e, opts)
+    line = "The binary_VTK.0.vtk file is NOT binary.\n\n"
+    visitfile = string.join(open("binary_VTK.visit").readlines())
+    if VTK_check_binary("binary_VTK.0.vtk"):
+        line = "The binary_VTK.0.vtk file is binary.\n\n"
+    s = line + visitfile
+    TestText("export_db_1_01", s)
+
+    a = GetAnnotationAttributes()
+
+    AddWindow()
+    OpenDatabase("binary_VTK.visit")
+    AddPlot("Pseudocolor", "u")
+    DrawPlots()
+    t = CreateAnnotationObject("Text2D")
+    t.text = "VTK"
+    t.position = (0.01, 0.91)
+    t.height = 0.07
+    SetView3D(v)
+    SetAnnotationAttributes(a)
+    Test("export_db_1_02")
+    DeleteWindow()
+
+    # Test exporting some surfaces in other formats and plotting the results.
+    formats = {
+"PLY":  ("export_PLY",  "export_PLY.ply",   "export_db_1_03", "Subset", "PLY_mesh"),
+"RAW":  ("export_RAW",  "export_RAW.raw",   "export_db_1_04", "Subset", "mesh"),
+"STL":  ("export_STL",  "export_STL.stl",   "export_db_1_05", "Subset", "STL_mesh"),
+"Silo": ("export_Silo", "export_Silo.silo", "export_db_1_06", "Pseudocolor", "u"),
+"Tecplot": ("export_Tecplot", "export_Tecplot.tec", "export_db_1_07", "Pseudocolor", "u"),
+"WavefrontOBJ": ("export_OBJ", "export_OBJ.obj", "export_db_1_08", "Subset", "OBJMesh"),
+"XYZ":  ("export_XYZ", "export_XYZ.xyz", "export_db_1_09", "Subset", "mesh"),
+"Xmdv": ("export_Xmdv", "export_Xmdv.visit", "export_db_1_10", "Pseudocolor", "u")
+}
+    keys = sorted(formats.keys())
+    for f in keys:
+        e.db_type = f
+        e.db_type_fullname = f + "_1.0"
+        e.filename = formats[f][0]
+        ExportDatabase(e)
+        AddWindow()
+        if OpenDatabase(formats[f][1]):
+            md = GetMetaData(formats[f][1])
+            AddPlot(formats[f][3], formats[f][4])
+            DrawPlots()
+        else:
+            files = os.listdir(".")
+            files.sort()
+            s = ""
+            for fn in files:
+                if formats[f][0] in fn:
+                    s = fn + "\n"
+            TestText("files", s)
+        t = CreateAnnotationObject("Text2D")
+        t.text = f
+        t.position = (0.01, 0.91)
+        t.height = 0.07
+        SetView3D(v)
+        SetAnnotationAttributes(a)
+        Test(formats[f][2])
+        DeleteWindow()
+    DeleteAllPlots()
+    cleanup_files()
+
+def test2(writeGroupSize):
+    TestSection("Test export with write groups (parallel).")
+    OpenDatabase(silo_data_path("multi_rect3d.silo"))
+    DefineScalarExpression("rad", "magnitude(coords(mesh1))")
+    AddPlot("Pseudocolor", "u")
+    AddOperator("Isosurface")
+    iso = IsosurfaceAttributes()
+    iso.contourValue = (0.55, 1.455)
+    iso.contourMethod = iso.Value
+    iso.variable = "rad"
+    SetOperatorOptions(iso)
+    DrawPlots()
+
+    v = GetView3D()
+    v.viewNormal = (-0.467474, 0.301847, 0.830877)
+    v.focus = (0.5, 0.5, 0.5)
+    v.viewUp = (0.140705, 0.953323, -0.267166)
+    v.viewAngle = 30
+    v.parallelScale = 0.866025
+    v.nearPlane = -1.73205
+    v.farPlane = 1.73205
+    v.imagePan = (-0.0154649, 0.027457)
+    v.imageZoom = 1.14276
+    v.perspective = 1
+    v.eyeAngle = 2
+    v.centerOfRotationSet = 0
+    v.centerOfRotation = (0.5, 0.5, 0.5)
+    v.axis3DScaleFlag = 0
+    v.axis3DScales = (1, 1, 1)
+    v.shear = (0, 0, 1)
+    v.windowValid = 1
+    SetView3D(v)
+    Test("export_db_2_00")
+
+    a = GetAnnotationAttributes()
+
+    # Test exporting some surfaces in other formats and plotting the results.
+    formats = {
+"PLY":  ("wg_PLY",  "*.ply",   "export_db_2_01", "Subset", "PLY_mesh"),
+"RAW":  ("wg_RAW",  "*.raw",   "export_db_2_02", "Subset", "mesh"),
+"STL":  ("wg_STL",  "*.stl",   "export_db_2_03", "Subset", "STL_mesh"),
+"Silo": ("wg_Silo", "wg_Silo.silo", "export_db_2_04", "Pseudocolor", "u"),
+"Tecplot": ("wg_Tecplot", "*.tec", "export_db_2_05", "Pseudocolor", "u"),
+"VTK":  ("wg_VTK", "wg_VTK.visit", "export_db_2_06", "Pseudocolor", "u"),
+"WavefrontOBJ": ("wg_OBJ", "*.obj", "export_db_2_07", "Subset", "OBJMesh"),
+"XYZ":  ("wg_XYZ", "*.xyz", "export_db_2_08", "Subset", "mesh"),
+"Xmdv": ("wg_Xmdv", "wg_Xmdv.visit", "export_db_2_09", "Pseudocolor", "u")
+}
+    keys = sorted(formats.keys())
+    for f in keys:
+        e = ExportDBAttributes()
+        e.db_type = f
+        e.db_type_fullname = f + "_1.0"
+        e.filename = formats[f][0]
+        e.writeUsingGroups = 1
+        e.groupSize = writeGroupSize
+        ExportDatabase(e)
+        AddWindow()
+        pattern = formats[f][1]
+        filelist=""
+        if pattern[0] == '*':
+            # Read all of the filenames
+            files = sorted(os.listdir("."))
+            ext = pattern[2:]
+            for datafile in files:
+                if datafile[-len(ext):] == ext:
+                    OpenDatabase(datafile)
+                    AddPlot(formats[f][3], formats[f][4])
+                    filelist = filelist + datafile + "\n"
+        else:
+            if OpenDatabase(formats[f][1]):
+                md = GetMetaData(formats[f][1])
+                AddPlot(formats[f][3], formats[f][4])
+                filelist = filelist + formats[f][1] + "\n"
+            else:
+                filelist = "ERROR: " + string.join(os.listdir("."), "\n")
+        DrawPlots()
+        t = CreateAnnotationObject("Text2D")
+        t.text = f
+        t.position = (0.01, 0.91)
+        t.height = 0.07
+        SetView3D(v)
+        SetAnnotationAttributes(a)
+        Test(formats[f][2])
+        TestText(formats[f][2] + "fn", filelist)
+        DeleteWindow()
+    DeleteAllPlots()
+    cleanup_files()
+
+def test3():
+    TestSection("Test Tecplot multiblock export.")
+    OpenDatabase(silo_data_path("multi_rect3d.silo"))
+    DefineScalarExpression("rad", "magnitude(coords(mesh))")
+    AddPlot("Pseudocolor", "u")
+    DrawPlots()
+
+    v = GetView3D()
+    v.viewNormal = (-0.467474, 0.301847, 0.830877)
+    v.focus = (0.5, 0.5, 0.5)
+    v.viewUp = (0.140705, 0.953323, -0.267166)
+    v.viewAngle = 30
+    v.parallelScale = 0.866025
+    v.nearPlane = -1.73205
+    v.farPlane = 1.73205
+    v.imagePan = (-0.0154649, 0.027457)
+    v.imageZoom = 1.14276
+    v.perspective = 1
+    v.eyeAngle = 2
+    v.centerOfRotationSet = 0
+    v.centerOfRotation = (0.5, 0.5, 0.5)
+    v.axis3DScaleFlag = 0
+    v.axis3DScales = (1, 1, 1)
+    v.shear = (0, 0, 1)
+    v.windowValid = 1
+    SetView3D(v)
+    Test("export_db_3_00")
+    a = GetAnnotationAttributes()
+
+    e = ExportDBAttributes()
+    e.db_type = "Tecplot"
+    e.db_type_fullname = "Tecplot_1.0"
+    e.filename = "rectTecplot"
+    e.writeUsingGroups = 0
+    e.variables = ("default", "v", "rad")
+    ExportDatabase(e)
+    AddWindow()
+    OpenDatabase("rectTecplot.tec")
+    AddPlot("Pseudocolor", "u")
+    DrawPlots()
+    SetAnnotationAttributes(a)
+    SetView3D(v)
+    Test("export_db_3_01")
+    ChangeActivePlotsVar("v")
+    Test("export_db_3_02")
+    ChangeActivePlotsVar("rad")
+    Test("export_db_3_03")
+
+    # Make sure we have volume data
+    AddOperator("ThreeSlice")
+    atts = ThreeSliceAttributes()
+    atts.y = 0.5
+    atts.x = 0.5
+    atts.z = 0.5
+    SetOperatorOptions(atts)
+    DrawPlots()
+    Test("export_db_3_04")
+
+    DeleteWindow()
+    DeleteAllPlots()
+
+def main():
+    test0()
+    test1()
+    if GetEngineProperties(GetEngineList()[0]).numProcessors > 1:
+        # We just use 2 processors normally so let's set the write group size to 1
+        # so each rank will write its own data using a different write group. For
+        # certain formats, there will be 1 data file per write group, or in this case
+        # 2 output files.
+        test2(1)
+    test3()
+
+main()
 Exit()
 
