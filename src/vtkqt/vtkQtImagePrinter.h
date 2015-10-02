ea2fa47233b957abf302a0e4d4dc432f705f8ab7
Burlen Loring <bloring@lbl.gov>
2015-10-02 23:44:26 +0000

    don't implement copy constructor and assignment operator.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@27362 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/vtkqt/vtkQtImagePrinter.h
+++ src/vtkqt/vtkQtImagePrinter.h
@@ -58,7 +58,9 @@
 // Creation:   Wed Feb 20 15:46:31 PST 2002
 //
 // Modifications:
-//   
+//   Burlen Loring, Thu Aug 13 10:13:23 PDT 2015
+//   delete the copy constructor and assignment operator
+//
 // ****************************************************************************
 
 class VTKQT_API vtkQtImagePrinter : public vtkImageWriter
@@ -71,8 +73,8 @@ public:
 protected:
   vtkQtImagePrinter();
   virtual ~vtkQtImagePrinter() {};
-  vtkQtImagePrinter(const vtkQtImagePrinter&) {};
-  void operator=(const vtkQtImagePrinter&) {};
+  vtkQtImagePrinter(const vtkQtImagePrinter&); // not implemented
+  void operator=(const vtkQtImagePrinter&); // not implemented
 
   virtual void WriteFile(ofstream *file, vtkImageData *data, int ext[6], int wext[6]);
   virtual void WriteFileHeader(ofstream *, vtkImageData *, int [6]) { };
