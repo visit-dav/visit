db2c8a695f8485d9e5b88312234815dc6e8825f9
Burlen Loring <bloring@lbl.gov>
2016-01-26 17:12:07 +0000

    move the cdash server off site
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@27955 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/CTestConfig.cmake
+++ src/CTestConfig.cmake
@@ -38,6 +38,6 @@
 set(CTEST_PROJECT_NAME "VisIt")
 set(CTEST_NIGHTLY_START_TIME "03:00:00 UTC")
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "cdash.visit.lbl.gov")
+set(CTEST_DROP_SITE "cdash.hpcvis.com")
 set(CTEST_DROP_LOCATION "/submit.php?project=VisIt")
 set(CTEST_DROP_SITE_CDASH TRUE)
