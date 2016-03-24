48e6d50d568fc321d1de176e4d9327017ef41278
Kathleen Bonnell <bonnell2@llnl.gov>
2016-03-24 01:50:56 +0000

    Fix engine crash with pick-by-global-id when dataset contains empty domains.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@28304 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/Database/Database/avtGenericDatabase.C
+++ src/avt/Database/Database/avtGenericDatabase.C
@@ -8086,6 +8086,9 @@ avtGenericDatabase::MaterialSelect(avtDatasetCollection &ds,
 //    Kathleen Bonnell, Wed Dec 15 08:41:17 PST 2004
 //    Changed 'vector<int>' to 'intVector'.
 //
+//    Kathleen Biagas, Wed Mar 23 18:06:29 PDT 2016
+//    Account for some domains being empty (NULL vtkDataSet).
+//
 // ****************************************************************************
 
 void
@@ -8097,9 +8100,13 @@ avtGenericDatabase::CreateGlobalZones(avtDatasetCollection &ds,
     src->DatabaseProgress(0, 0, progressString);
     for (int i = 0 ; i < ds.GetNDomains() ; i++)
     {
-        vtkDataArray *arr = GetGlobalZoneIds(domains[i], spec->GetVariable(),
-                                             spec->GetTimestep());
-        ds.GetDataset(i, 0)->GetCellData()->AddArray(arr);
+        if (ds.GetDataset(i,0) != NULL)
+        {
+            vtkDataArray *arr = GetGlobalZoneIds(domains[i],
+                                                 spec->GetVariable(),
+                                                 spec->GetTimestep());
+            ds.GetDataset(i, 0)->GetCellData()->AddArray(arr);
+        }
         src->DatabaseProgress(i, ds.GetNDomains(), progressString);
     }
     src->DatabaseProgress(1, 0, progressString);
@@ -8119,6 +8126,9 @@ avtGenericDatabase::CreateGlobalZones(avtDatasetCollection &ds,
 //    Kathleen Bonnell, Wed Dec 15 08:41:17 PST 2004
 //    Changed 'vector<int>' to 'intVector'.
 //
+//    Kathleen Biagas, Wed Mar 23 18:06:29 PDT 2016
+//    Account for some domains being empty (NULL vtkDataSet).
+//
 // ****************************************************************************
 
 void
@@ -8130,9 +8140,13 @@ avtGenericDatabase::CreateGlobalNodes(avtDatasetCollection &ds,
     src->DatabaseProgress(0, 0, progressString);
     for (int i = 0 ; i < ds.GetNDomains() ; i++)
     {
-        vtkDataArray *arr = GetGlobalNodeIds(domains[i], spec->GetVariable(),
-                                             spec->GetTimestep());
-        ds.GetDataset(i, 0)->GetPointData()->AddArray(arr);
+        if (ds.GetDataset(i,0) != NULL)
+        {
+            vtkDataArray *arr = GetGlobalNodeIds(domains[i],
+                                                 spec->GetVariable(),
+                                                 spec->GetTimestep());
+            ds.GetDataset(i, 0)->GetPointData()->AddArray(arr);
+        }
         src->DatabaseProgress(i, ds.GetNDomains(), progressString);
     }
     src->DatabaseProgress(1, 0, progressString);
