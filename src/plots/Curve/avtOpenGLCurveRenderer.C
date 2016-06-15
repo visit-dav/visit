a51f7a77d77569141b22008d4e757dcc939d8164
Mark Miller <miller86@llnl.gov>
2016-06-15 17:12:41 +0000

    2.10RC->Trunk: Fix #2631: curve symbol sizes dont change except for symbol type of point
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@28710 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/plots/Curve/avtOpenGLCurveRenderer.C
+++ src/plots/Curve/avtOpenGLCurveRenderer.C
@@ -603,6 +603,10 @@ avtOpenGLCurveRenderer::GetAspect(int &bin_x_n, double &bin_x_size, double &bin_
 //    Moved some code to another method so that it could be reused in the
 //    RenderBall method.
 //
+//    Mark C. Miller, Wed Jun 15 10:06:51 PDT 2016
+//    I modified the REDUCE_SCALE macro to include atts.PointSize() so that
+//    these symbol sizes could be adjusted also. Previosly only point symbols
+//    could have their size adjusted.
 // ****************************************************************************
 
 void
@@ -661,7 +665,7 @@ avtOpenGLCurveRenderer::DrawCurveAsSymbols()
 
 #define SWAP(a,b) { int tmp = a; a = b; b = tmp; }
 #define ONE_THIRD_ROUND (2. * M_PI / 3.)
-#define REDUCE_SCALE 0.8
+#define REDUCE_SCALE (0.8 * atts.GetPointSize())
 #define MAX_SYMBOL_VERTS 25
 
     double symbolPoints[MAX_SYMBOL_VERTS][2];
