b29052e586a2abaeab1ae0f18e364bcbebe62181
Kathleen Bonnell <bonnell2@llnl.gov>
2016-08-25 19:14:33 +0000

    Add #include <algorithm> to avtAxisAligned Slice4DFilter.C to fix compile error on Windows. Also updated the copyright date on all the files and added category
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@29136 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/operators/AxisAlignedSlice4D/avtAxisAlignedSlice4DFilter.C
+++ src/operators/AxisAlignedSlice4D/avtAxisAlignedSlice4DFilter.C
@@ -1,6 +1,6 @@
 /*****************************************************************************
 *
-* Copyright (c) 2000 - 2015, Lawrence Livermore National Security, LLC
+* Copyright (c) 2000 - 2016, Lawrence Livermore National Security, LLC
 * Produced at the Lawrence Livermore National Laboratory
 * LLNL-CODE-442911
 * All rights reserved.
@@ -42,6 +42,7 @@
 
 #include <avtAxisAlignedSlice4DFilter.h>
 
+#include <algorithm>
 #include <float.h>
 
 #ifdef PARALLEL
