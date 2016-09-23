ca7025b97d286cdb0e74f163b931457adb4b2da5
Kevin Griffin <griffin28@llnl.gov>
2016-09-23 01:05:21 +0000

    Bug #2310: Added data checks to XRay Image Query with more verbose messages
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.11RC@29337 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/Filters/avtXRayFilter.C
+++ src/avt/Filters/avtXRayFilter.C
@@ -2375,6 +2375,15 @@ avtXRayFilter::CheckDataSets(int nDataSets, vtkDataSet **dataSets)
 {
     int numBins;
     cellDataType = -1;
+    
+    if(nDataSets <= 0)
+    {
+        char msg[256];
+        SNPRINTF(msg,256, "Variables %s and %s resulted in no data being selected.",
+                 absVarName.c_str(), emisVarName.c_str());
+        EXCEPTION1(VisItException, msg);
+    }
+    
     for (int i = 0; i < nDataSets; i++)
     {
         vtkDataArray *abs  = dataSets[i]->GetCellData()->GetArray(absVarName.c_str());
@@ -2391,8 +2400,8 @@ avtXRayFilter::CheckDataSets(int nDataSets, vtkDataSet **dataSets)
                                   absVarName.c_str());
             }
             else
-                SNPRINTF(msg,256, "Variable %s not found.",
-                                  absVarName.c_str());
+                SNPRINTF(msg,256, "Variable %s not found.", absVarName.c_str());
+            
             EXCEPTION1(VisItException, msg);
         }
         if (emis == NULL)
