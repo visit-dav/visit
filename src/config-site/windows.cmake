a4f64501eaff020a159159e34c4ff8ac1b7568d6
Kathleen Bonnell <bonnell2@llnl.gov>
2016-10-24 23:49:25 +0000

    Turn off use of 7-zip on my windows machine where I don't have sufficient privilege to create symlinks.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.11RC@29483 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/windows.cmake
+++ src/config-site/windows.cmake
@@ -36,6 +36,7 @@ SET(VISITHOME ${VISIT_WINDOWS_DIR}/${VISIT_MSVC_VERSION})
 # Create the NSIS installer package (override default from root CMakeLists.txt
 OPTION(VISIT_MAKE_NSIS_INSTALLER "Create an installer package using NSIS." ON)
 VISIT_OPTION_DEFAULT(VISIT_QT5 false TYPE BOOL)
+VISIT_OPTION_DEFAULT(VISIT_USE_7ZIP true TYPE BOOL)
 
 ##############################################################
 ##
