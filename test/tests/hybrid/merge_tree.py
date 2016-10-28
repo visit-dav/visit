a2f29d17adc310c3bf307aa97001edb8f6239cf0
Cyrus Harrison <cyrush@llnl.gov>
2016-10-28 20:30:11 +0000

    add merge tree test script
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@29526 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ test/tests/hybrid/merge_tree.py
@@ -0,0 +1,63 @@
+# ----------------------------------------------------------------------------
+#  CLASSES: nightly
+#
+#  Test Case:  merge_tree.py
+#
+#
+#  Programmer: Cyrus Harrison
+#  Date:       Friday October 28, 2016
+#
+#  Modifiations:
+#
+# ----------------------------------------------------------------------------
+
+
+test_dbs = { "rect2d" : {"var":  "d",
+                         "path": silo_data_path("rect2d.silo")} ,
+             "noise2d" : {"var": "shepardglobal",
+                          "path" :silo_data_path("noise2d.silo")} ,
+
+             "noise3d" : {"var": "shepardglobal",
+                          "path": silo_data_path("noise.silo")}
+           }
+
+for db_name,db_info in test_dbs.items():
+    test_db_file = db_info["path"]
+    OpenDatabase(test_db_file)
+
+    # test merge tree, split tree and local threshold exprs
+
+    DefineScalarExpression("mtree", "merge_tree(%s)" % db_info["var"])
+    AddPlot("Pseudocolor", "mtree")
+    DrawPlots()
+
+    Test(db_name + "_mtree")
+    DeleteAllPlots()
+
+    DefineScalarExpression("stree", "split_tree(%s)" % db_info["var"])
+    AddPlot("Pseudocolor", "stree")
+    DrawPlots()
+    
+    Test(db_name + "_stree")
+    DeleteAllPlots()
+    
+    DefineScalarExpression("localt_mt", "local_threshold(%s,mtree)" % db_info["var"])
+
+    AddPlot("Pseudocolor", "localt_mt")
+    DrawPlots()
+    
+    Test(db_name + "_localt_mt")
+    DeleteAllPlots()
+
+    DefineScalarExpression("localt_st", "local_threshold(%s,stree)" % db_info["var"])
+
+    AddPlot("Pseudocolor", "localt_st")
+    DrawPlots()
+    
+    Test(db_name + "_localt_st")
+    DeleteAllPlots()
+
+    
+    CloseDatabase(test_db_file)
+
+Exit()
