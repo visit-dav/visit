9f270b616e834e4fe7c2cc165c12356c908615e8
Kathleen Bonnell <bonnell2@llnl.gov>
2016-11-11 15:50:37 +0000

    Update location of TP libs for 2.12
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.12RC@29776 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/config-site/vulcanlac4-compute.cmake
+++ src/config-site/vulcanlac4-compute.cmake
@@ -8,7 +8,7 @@
 ##
 ## Setup VISITHOME & VISITARCH variables.
 ##
-SET(VISITHOME /usr/gapps/visit/thirdparty_static/2.11.0)
+SET(VISITHOME /usr/workspace/wsa/visit/visit/thirdparty_static/2.12.0)
 SET(VISITARCH linux-ppc64_gcc-4.4_BGQ)
 
 ## Compiler flags.
