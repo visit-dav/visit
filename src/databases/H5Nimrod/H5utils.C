3e4f4f8dd31d8db096a55abc20b1c5a5f0f47f7d
Allen Sanderson <allen@sci.utah.edu>
2016-12-15 18:38:54 +0000

    removed suprious output and properly closed dataset
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@30021 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/databases/H5Nimrod/H5utils.C
+++ src/databases/H5Nimrod/H5utils.C
@@ -45,7 +45,7 @@ _normalize_h5_type (hid_t type)
         default:
             ;                                /* NOP */
     }
-    printf ("Unknown type %d", (int) type);
+    printf ("H5Utils:H5NIMROD Unknown type %d", (int) type);
 
     return -1;
 }
@@ -203,7 +203,7 @@ H5NIMROD_read_float32_array (hid_t parent_id,
     dataset = H5Dopen (parent_id, dataset_name);
     if (dataset < 0)
     {
-        printf ("could not open dataset %s\n", dataset_name);
+        printf ("H5Utils:H5NIMROD Could not open dataset %s\n", dataset_name);
         return H5NIMROD_ERR;
     }
     if (offset == NULL)
