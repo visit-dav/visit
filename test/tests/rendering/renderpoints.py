ea67b3fdb89657c8600a1b7f0fabb59fdf39a267
Kathleen Bonnell <bonnell2@llnl.gov>
2016-12-15 17:40:29 +0000

    Flesh out testing for glyphed points: enusre all glyph types are tested for all plots that support it. Test scaling glyphs by variable. Added testing for restoring session files that utilize point glyphing.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@30019 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/rendering/renderpoints.py
+++ test/tests/rendering/renderpoints.py
@@ -17,6 +17,10 @@
 #
 #    Mark C. Miller, Wed Jan 20 07:37:11 PST 2010
 #    Added ability to swtich between Silo's HDF5 and PDB data.
+#
+#    Kathleen Biagas, Thu Dec 15 09:16:39 PST 2016
+#    Added tests for other glyph types, scaling by variable and restoring
+#    session files.
 # ----------------------------------------------------------------------------
 
 #
@@ -90,7 +94,7 @@ def test0():
 #
 # General testing function #2
 #
-def TestPlot2(plotName, plotVar, atts, cases):
+def TestPlot1(plotName, plotVar, atts, cases):
     AddPlot(plotName, plotVar)
     if atts == None:
         atts = eval(plotName + "Attributes()")
@@ -128,14 +132,14 @@ def TestPlot2(plotName, plotVar, atts, cases):
 def test1():
     TestSection("Check points to glyph transition")
     # Test Pseudocolor
-    TestPlot2("Pseudocolor", "Matnos", None, ("_1_00", "_1_01"))
+    TestPlot1("Pseudocolor", "Matnos", None, ("_1_00", "_1_01"))
     # Test FilledBoundary
-    TestPlot2("FilledBoundary", "stars", None, ("_1_02", "_1_03"))
+    TestPlot1("FilledBoundary", "stars", None, ("_1_02", "_1_03"))
 
 #
 # General testing function #3
 #
-def TestPlot3(plotName, plotVar, atts, cases):
+def TestPlot2(plotName, plotVar, atts, cases):
     AddPlot(plotName, plotVar)
     if atts == None:
         atts = eval(plotName + "Attributes()")
@@ -176,10 +180,10 @@ def test2():
     atts = MeshAttributes()
     atts.meshColor = (100,150,255)
     atts.foregroundFlag = 0
-    TestPlot3("Mesh", "StarMesh", atts, ("_2_00", "_2_01"))
+    TestPlot2("Mesh", "StarMesh", atts, ("_2_00", "_2_01"))
 
     # Test Pseudocolor
-    TestPlot3("Pseudocolor", "Matnos", None, ("_2_02", "_2_03"))
+    TestPlot2("Pseudocolor", "Matnos", None, ("_2_02", "_2_03"))
 
     # Test Scatter
     atts = ScatterAttributes()
@@ -188,13 +192,134 @@ def test2():
     atts.var3Role = atts.Coordinate2
     atts.var4 = "Matnos"
     atts.var4Role = atts.Color
-    TestPlot3("Scatter", "vx", atts, ("_2_04", "_2_05"))
+    TestPlot2("Scatter", "vx", atts, ("_2_04", "_2_05"))
 
     # Test FilledBoundary
-    TestPlot3("FilledBoundary", "stars", None, ("_2_06", "_2_07"))
+    TestPlot2("FilledBoundary", "stars", None, ("_2_06", "_2_07"))
 
     # Test Subset
-    TestPlot3("Subset", "domains", None, ("_2_08", "_2_09"))
+    TestPlot2("Subset", "domains", None, ("_2_08", "_2_09"))
+
+def TestPlot3(plotName, plotVar, atts, cases):
+    AddPlot(plotName, plotVar)
+    if atts == None:
+        atts = eval(plotName + "Attributes()")
+    atts.pointType = atts.Axis
+    SetPlotOptions(atts)
+    DrawPlots()
+
+    Test("renderpoint" + cases[0])
+
+    atts.pointType = atts.Icosahedron
+    atts.pointSize = 0.1
+    SetPlotOptions(atts)
+    Test("renderpoint" + cases[1])
+
+    atts.pointType = atts.Octahedron
+    SetPlotOptions(atts)
+    Test("renderpoint" + cases[2])
+
+    atts.pointSize = 0.05
+    atts.pointType = atts.Tetrahedron
+    SetPlotOptions(atts)
+    Test("renderpoint" + cases[3])
+
+    atts.pointType = atts.SphereGeometry
+    SetPlotOptions(atts)
+    Test("renderpoint" + cases[4])
+
+    DeleteActivePlots()
+
+#
+# Test that all plots that support sphere point texturing can do it.
+#
+def test3():
+    TestSection("Check glyph types for Mesh plot")
+    atts = MeshAttributes()
+    atts.meshColor = (152,203,0)
+    atts.foregroundFlag = 0
+    TestPlot3("Mesh", "StarMesh", atts, ("_3_00", "_3_01", "_3_02", "_3_03", "_3_04"))
+
+    TestSection("Check glyph types for Pseudocolor plot")
+    # Test Pseudocolor
+    TestPlot3("Pseudocolor", "Matnos", None, ("_3_05", "_3_06", "_3_07", "_3_08", "_3_09"))
+
+    TestSection("Check glyph types for Scatter plot")
+    # Test Scatter
+    atts = ScatterAttributes()
+    atts.var2 = "vy"
+    atts.var3 = "vz"
+    atts.var3Role = atts.Coordinate2
+    atts.var4 = "Matnos"
+    atts.var4Role = atts.Color
+    TestPlot3("Scatter", "vx", atts, ("_3_10", "_3_11", "_3_12", "_3_13", "_3_14"))
+
+    TestSection("Check glyph types for FilledBoundary plot")
+    # Test FilledBoundary
+    TestPlot3("FilledBoundary", "stars", None, ("_3_15", "_3_16", "_3_17", "_3_18", "_3_19"))
+
+    # Test Subset
+    TestSection("Check all glyph types for Subset plot")
+    TestPlot3("Subset", "domains", None, ("_3_20", "_3_21", "_3_22", "_3_23", "_3_24"))
+
+def TestPlot4(plotName, plotVar, atts, case):
+    AddPlot(plotName, plotVar)
+    if atts == None:
+        atts = eval(plotName + "Attributes()")
+    atts.pointType = atts.SphereGeometry
+    atts.pointSize = 0.007
+    atts.pointSizeVarEnabled = 1
+    atts.pointSizeVar = "Matnos"
+    SetPlotOptions(atts)
+    DrawPlots()
+
+    Test("renderpoint" + case)
+
+    DeleteActivePlots()
+
+#
+# Test that all plots that support scaling glyphs by a variable can do it.
+#
+def test4():
+    TestSection("Check scaling glyphs by a variable")
+    atts = MeshAttributes()
+    atts.meshColor = (152,203,0)
+    atts.foregroundFlag = 0
+    TestPlot4("Mesh", "StarMesh", atts, "_4_00")
+
+    # Test Pseudocolor
+    TestPlot4("Pseudocolor", "Matnos", None, "_4_01")
+
+    # Test FilledBoundary
+    TestPlot4("FilledBoundary", "stars", None, "_4_02")
+
+    # Test Subset
+    TestPlot4("Subset", "domains", None, "_4_03")
+
+#
+# Test that glyphed plots can be restored from session files
+#
+def test5():
+    TestSection("Check glyphed plots restored from session files")
+    DeleteAllPlots()
+    RestoreSessionWithDifferentSources(tests_path("rendering", "filledBoundaryStars.session"), 0, silo_data_path("galaxy0000.silo"))
+    Test("renderpoint_5_00")
+
+    DeleteAllPlots()
+    RestoreSessionWithDifferentSources(tests_path("rendering", "meshNoisePoint.session"), 0, silo_data_path("noise.silo"))
+    Test("renderpoint_5_01")
+
+    DeleteAllPlots()
+    RestoreSessionWithDifferentSources(tests_path("rendering", "pcNoisePoint.session"), 0, silo_data_path("noise.silo"))
+    Test("renderpoint_5_02")
+
+    DeleteAllPlots()
+    RestoreSessionWithDifferentSources(tests_path("rendering", "scatterNoiseHG.session"), 0, silo_data_path("noise.silo"))
+    Test("renderpoint_5_03")
+
+    DeleteAllPlots()
+    RestoreSessionWithDifferentSources(tests_path("rendering", "subsetNoisePoint.session"), 0, silo_data_path("noise.silo"))
+    Test("renderpoint_5_04")
 
 
 def main():
@@ -203,10 +328,15 @@ def main():
 
     OpenDatabase(silo_data_path("galaxy0000.silo"))
 
-
     test0()
     test1()
     test2()
+    test3()
+    test4()
+
+    CloseDatabase(silo_data_path("galaxy0000.silo"))
+
+    test5()
 
 main()
 Exit()
