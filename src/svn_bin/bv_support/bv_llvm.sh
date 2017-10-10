c977c8eb499feba0037c981e71560d1d98c36891
Eric Brugger <brugger1@llnl.gov>
2017-10-10 18:19:41 +0000

    1) I made another fix to FindVisItQt5 to include libQt5Svg in a distribution.
    2) I rebuilt the third party libraries for the 2.13.0 release on a bunch of
       machines and updated the config site files.
    
    3) I corrected a couple of typos in the 2.13.0 release notes.
    
    4) I modified bv_llvm to prepend "BV_" to some of the environment variable
       names that caused clash with Mesa. Specifically LLVM_FILE to BV_LLVM_FILE,
       LLVM_VERSION to BV_LLVM_VERSION and LLVM_BUILD to BV_LLVM_BUILD. It turns
       out Mesa's configure script looks for LLVM_VERSION.
    
    5) I modified the closed visit build and install scripts for use with the
       new third party libraries.
    
    
    
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@31696 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/svn_bin/bv_support/bv_llvm.sh
+++ src/svn_bin/bv_support/bv_llvm.sh
@@ -22,19 +22,19 @@ function bv_llvm_depends_on
 
 function bv_llvm_info
 {
-    export LLVM_FILE=${LLVM_FILE:-"llvm-4.0.0.src.tar.gz"}
-    export LLVM_VERSION=${LLVM_VERSION:-"4.0.0"}
-    export LLVM_BUILD_DIR=${LLVM_BUILD_DIR:-"llvm-4.0.0.src"}
+    export BV_LLVM_FILE=${BV_LLVM_FILE:-"llvm-4.0.0.src.tar.gz"}
+    export BV_LLVM_VERSION=${BV_LLVM_VERSION:-"4.0.0"}
+    export BV_LLVM_BUILD_DIR=${BV_LLVM_BUILD_DIR:-"llvm-4.0.0.src"}
     export LLVM_MD5_CHECKSUM="7cbcd974e214d08928d53df90bf57221"
     export LLVM_SHA256_CHECKSUM=""
 }
 
 function bv_llvm_print
 {
-    printf "%s%s\n" "LLVM_FILE=" "${LLVM_FILE}"
-    printf "%s%s\n" "LLVM_VERSION=" "${LLVM_VERSION}"
+    printf "%s%s\n" "BV_LLVM_FILE=" "${BV_LLVM_FILE}"
+    printf "%s%s\n" "BV_LLVM_VERSION=" "${BV_LLVM_VERSION}"
     printf "%s%s\n" "LLVM_TARGET=" "${LLVM_TARGET}"
-    printf "%s%s\n" "LLVM_BUILD_DIR=" "${LLVM_BUILD_DIR}"
+    printf "%s%s\n" "BV_LLVM_BUILD_DIR=" "${BV_LLVM_BUILD_DIR}"
 }
 
 function bv_llvm_print_usage
@@ -49,13 +49,13 @@ function bv_llvm_host_profile
         echo "##" >> $HOSTCONF
         echo "## LLVM" >> $HOSTCONF
         echo "##" >> $HOSTCONF
-        echo "VISIT_OPTION_DEFAULT(VISIT_LLVM_DIR \${VISITHOME}/llvm/$LLVM_VERSION/\${VISITARCH})" >> $HOSTCONF
+        echo "VISIT_OPTION_DEFAULT(VISIT_LLVM_DIR \${VISITHOME}/llvm/$BV_LLVM_VERSION/\${VISITARCH})" >> $HOSTCONF
     fi
 }
 
 function bv_llvm_initialize_vars
 {
-    export VISIT_LLVM_DIR=${VISIT_LLVM_DIR:-"$VISITDIR/llvm/${LLVM_VERSION}/${VISITARCH}"}
+    export VISIT_LLVM_DIR=${VISIT_LLVM_DIR:-"$VISITDIR/llvm/${BV_LLVM_VERSION}/${VISITARCH}"}
     LLVM_INCLUDE_DIR="${VISIT_LLVM_DIR}/include"
     LLVM_LIB_DIR="${VISIT_LLVM_DIR}/lib"
     if [[ "$DO_STATIC_BUILD" == "yes" ]]; then
@@ -80,7 +80,7 @@ function bv_llvm_ensure
 {
     if [[ "$DO_DBIO_ONLY" != "yes" ]]; then
         if [[ "$DO_LLVM" == "yes" ]] ; then
-            ensure_built_or_ready "llvm"   $LLVM_VERSION   $LLVM_BUILD_DIR   $LLVM_FILE
+            ensure_built_or_ready "llvm"   $BV_LLVM_VERSION   $BV_LLVM_BUILD_DIR   $BV_LLVM_FILE
             if [[ $? != 0 ]] ; then
                 return 1
             fi
@@ -100,7 +100,7 @@ function build_llvm
     #
     # prepare build dir
     #
-    prepare_build_dir $LLVM_BUILD_DIR $LLVM_FILE
+    prepare_build_dir $BV_LLVM_BUILD_DIR $BV_LLVM_FILE
     untarred_llvm=$?
     if [[ $untarred_llvm == -1 ]] ; then
         warn "Unable to prepare LLVM build directory. Giving Up!"
@@ -114,13 +114,13 @@ function build_llvm
     #
     # LLVM must be built with an out of source build.
     #
-    LLVM_SRC_DIR=${LLVM_BUILD_DIR}
-    LLVM_BUILD_DIR="${LLVM_SRC_DIR}-build"
-    if [[ ! -d ${LLVM_BUILD_DIR} ]] ; then
-        info "Making build directory ${LLVM_BUILD_DIR}"
-        mkdir ${LLVM_BUILD_DIR}
+    BV_LLVM_SRC_DIR=${BV_LLVM_BUILD_DIR}
+    BV_LLVM_BUILD_DIR="${BV_LLVM_SRC_DIR}-build"
+    if [[ ! -d ${BV_LLVM_BUILD_DIR} ]] ; then
+        info "Making build directory ${BV_LLVM_BUILD_DIR}"
+        mkdir ${BV_LLVM_BUILD_DIR}
     fi
-    cd ${LLVM_BUILD_DIR} || error "Couldn't cd to llvm build dir."
+    cd ${BV_LLVM_BUILD_DIR} || error "Couldn't cd to llvm build dir."
 
     #
     # Remove any CMakeCache.txt files just to be safe.
@@ -129,7 +129,7 @@ function build_llvm
 
     info "Configuring LLVM . . ."
     ${CMAKE_COMMAND} \
-        -DCMAKE_INSTALL_PREFIX:PATH="${VISITDIR}/llvm/${LLVM_VERSION}/${VISITARCH}" \
+        -DCMAKE_INSTALL_PREFIX:PATH="${VISITDIR}/llvm/${BV_LLVM_VERSION}/${VISITARCH}" \
         -DCMAKE_BUILD_TYPE:STRING="${VISIT_BUILD_MODE}" \
         -DCMAKE_BUILD_WITH_INSTALL_RPATH:BOOL=ON \
         -DBUILD_SHARED_LIBS:BOOL=OFF \
@@ -140,7 +140,7 @@ function build_llvm
         -DLLVM_TARGETS_TO_BUILD=X86 \
         -DLLVM_ENABLE_RTTI:BOOL=ON \
         -DLLVM_BUILD_LLVM_DYLIB:BOOL=ON \
-        ../${LLVM_SRC_DIR}
+        ../${BV_LLVM_SRC_DIR}
     if [[ $? != 0 ]] ; then
         warn "LLVM cmake failed.  Giving up"
         return 1
@@ -179,7 +179,7 @@ function bv_llvm_is_enabled
 
 function bv_llvm_is_installed
 {
-    check_if_installed "llvm" $LLVM_VERSION
+    check_if_installed "llvm" $BV_LLVM_VERSION
     if [[ $? == 0 ]] ; then
         return 1
     fi
@@ -193,7 +193,7 @@ function bv_llvm_build
     #
     cd "$START_DIR"
     if [[ "$DO_LLVM" == "yes" ]] ; then
-        check_if_installed "llvm" $LLVM_VERSION
+        check_if_installed "llvm" $BV_LLVM_VERSION
         if [[ $? == 0 ]] ; then
             info "Skipping LLVM build.  LLVM is already installed."
         else
