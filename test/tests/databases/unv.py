98a51d44a6d779fca177e89e7e93ad0b6ce6c886
Mark Miller <miller86@llnl.gov>
2017-12-18 21:11:19 +0000

    adding updated tests from Olivier
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@32121 18c085ea-50e0-402c-830e-de6fd14e8384

--- test/tests/databases/unv.py
+++ test/tests/databases/unv.py
@@ -118,4 +118,53 @@ SetView3D(View3DAtts)
 DrawPlots()
 Test("unv_05")
 
+CloseDatabase(pjoin(datapath,"small_vol_pyr_bor.unv"), 0)
+OpenDatabase(pjoin(datapath, "small_sur.unv"))
+
+AddPlot("Vector", "normals2d", 1, 1)
+DrawPlots()
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.554906, -0.684409, 0.472931)
+View3DAtts.focus = (0, 0, 2.5)
+View3DAtts.viewUp = (-0.448111, 0.233062, 0.863063)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 3.4641
+View3DAtts.nearPlane = -6.9282
+View3DAtts.farPlane = 6.9282
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 2.5)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+SaveWindowAtts = SaveWindowAttributes()
+SaveWindowAtts.outputToCurrentDirectory = 1
+SaveWindowAtts.outputDirectory = "."
+SaveWindowAtts.fileName = "small_sur_normals2d"
+SaveWindowAtts.family = 1
+SaveWindowAtts.format = SaveWindowAtts.PNG  # BMP, CURVE, JPEG, OBJ, PNG, POSTSCRIPT, POVRAY, PPM, RGB, STL, TIFF, ULTRA, VTK, PLY
+SaveWindowAtts.width = 1024
+SaveWindowAtts.height = 1024
+SaveWindowAtts.screenCapture = 0
+SaveWindowAtts.saveTiled = 0
+SaveWindowAtts.quality = 80
+SaveWindowAtts.progressive = 0
+SaveWindowAtts.binary = 0
+SaveWindowAtts.stereo = 0
+SaveWindowAtts.compression = SaveWindowAtts.PackBits  # None, PackBits, Jpeg, Deflate
+SaveWindowAtts.forceMerge = 0
+SaveWindowAtts.resConstraint = SaveWindowAtts.ScreenProportions  # NoConstraint, EqualWidthHeight, ScreenProportions
+SaveWindowAtts.advancedMultiWindowSave = 0
+SetSaveWindowAttributes(SaveWindowAtts)
+SaveWindow()
+Test("unv_06", SaveWindowAtts)
+
 Exit()
