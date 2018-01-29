60c2b82744dc7ddf748e5bcc864162f425511c5a
Allen Sanderson <allen@sci.utah.edu>
2018-01-29 18:42:07 +0000

    back ported when reseting the view use the user specified bounding box
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.12RC@32285 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/avt/VisWindow/Colleagues/VisWinAxes3D.C
+++ src/avt/VisWindow/Colleagues/VisWinAxes3D.C
@@ -483,31 +483,32 @@ VisWinAxes3D::SetBounds(double bounds[6], double scales[3])
         currentScaleFactors[1] = scales[1];
         currentScaleFactors[2] = scales[2];
         
-
+        //
+        // Add a fudge-factor to prevent axes from being obscured by plots
+        // that fill their full extents.
+        //
+        
         double fudgeX;
         double fudgeY;
         double fudgeZ;
 
-       //
-       // Add a fudge-factor to prevent axes from being obscured by plots
-       // that fill their full extents.
-       //
-       // if the limits are initialized to +- DBL_MAX then doing math
-       // with them will cause an excepton so check them first.
-       if ( (bounds[1] == -DBL_MAX  && bounds[0] ==  DBL_MAX) ||
-            (bounds[3] == -DBL_MAX  && bounds[2] ==  DBL_MAX) ||
-            (bounds[5] == -DBL_MAX  && bounds[4] ==  DBL_MAX))
-       {
-         fudgeX = 0.001;
-         fudgeY = 0.001;
-         fudgeZ = 0.001;
-        }
+        // If the limits are initialized to +- DBL_MAX, negative, or
+        // zero then doing math with them will cause an excepton.       
+        if (bounds[1] > bounds[0])
+          fudgeX = (bounds[1] - bounds[0]) * 0.001;
         else
-        {
-           fudgeX = (bounds[1] - bounds[0]) * 0.001;
-           fudgeY = (bounds[3] - bounds[2]) * 0.001;
-           fudgeZ = (bounds[5] - bounds[4]) * 0.001;
-        }
+          fudgeX = 0.00001;
+          
+        if (bounds[3] > bounds[2])
+          fudgeY = (bounds[3] - bounds[2]) * 0.001;
+        else
+          fudgeY = 0.00001;
+
+        if (bounds[5] > bounds[4])
+          fudgeZ = (bounds[5] - bounds[4]) * 0.001;
+        else
+          fudgeZ = 0.00001;
+        
         //
         // Add a fudge-factor to prevent axes from being obscured by plots
         // that fill their full extents. 
@@ -1485,3 +1486,39 @@ VisWinAxes3D::Set3DAxisScalingFactors(bool scale,
     }
 }
 
+// ****************************************************************************
+// Method:  VisWinAxes3D::GetBoundsOverridden
+//
+// Purpose:
+//   Returns whether the user has set a specific bounding box
+//
+// Arguments:
+//
+// Programmer:  Allen Sanderson
+// Creation:    Jan 25, 2017
+//
+// ****************************************************************************
+bool
+VisWinAxes3D::GetBoundsOverridden() const
+{
+  return boundsOverridden;
+}
+
+// ****************************************************************************
+// Method:  VisWinAxes3D::GetOverrideBounds
+//
+// Purpose:
+//   Returns the user specific bounding box
+//
+// Arguments:
+//
+// Programmer:  Allen Sanderson
+// Creation:    Jan 25, 2017
+//
+// ****************************************************************************
+void
+VisWinAxes3D::GetOverrideBounds( double *bounds ) const
+{
+  for( int i=0; i<6; ++i )
+    bounds[i] = overrideBounds[i];
+}
