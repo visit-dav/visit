c96917289bd3fb82c016faffe9ea09bab628b418
Allen Sanderson <allen@sci.utah.edu>
2018-02-12 19:51:59 +0000

    added flag debug replacememnt
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@32357 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/CMake/VisItMacros.cmake
+++ src/CMake/VisItMacros.cmake
@@ -61,23 +61,25 @@ IF (WIN32)
   ENDMACRO(REPLACE_FLAG)
 
   # Remove /_DEBUG From debug builds
-  REPLACE_FLAG("/D_DEBUG" "" CMAKE_CXX_FLAGS_DEBUG
-               "Flags used by the compiler during debug builds")
-  REPLACE_FLAG("/D_DEBUG" "" CMAKE_C_FLAGS_DEBUG
-               "Flags used by the compiler during debug builds")
-  REPLACE_FLAG("/D_DEBUG" "" CMAKE_EXE_LINKER_FLAGS_DEBUG
-               "Flags used by the linker during debug builds")
-  REPLACE_FLAG("/D_DEBUG" "" CMAKE_MODULE_LINKER_FLAGS_DEBUG
-               "Flags used by the linker during debug builds")
+  if (VISIT_REPLACE_DEBUG_FLAGS)
+    REPLACE_FLAG("/D_DEBUG" "" CMAKE_CXX_FLAGS_DEBUG
+                 "Flags used by the compiler during debug builds")
+    REPLACE_FLAG("/D_DEBUG" "" CMAKE_C_FLAGS_DEBUG
+                 "Flags used by the compiler during debug builds")
+    REPLACE_FLAG("/D_DEBUG" "" CMAKE_EXE_LINKER_FLAGS_DEBUG
+                 "Flags used by the linker during debug builds")
+    REPLACE_FLAG("/D_DEBUG" "" CMAKE_MODULE_LINKER_FLAGS_DEBUG
+                 "Flags used by the linker during debug builds")
   # Change /MDd to /MD for debug builds
-  REPLACE_FLAG("/MDd" "/MD" CMAKE_CXX_FLAGS_DEBUG
-               "Flags used by the compiler during debug builds")
-  REPLACE_FLAG("/MDd" "/MD" CMAKE_C_FLAGS_DEBUG
-               "Flags used by the compiler during debug builds")
-  REPLACE_FLAG("/MDd" "/MD" CMAKE_EXE_LINKER_FLAGS_DEBUG
-               "Flags used by the linker during debug builds")
-  REPLACE_FLAG("/MDd" "/MD" CMAKE_MODULE_LINKER_FLAGS_DEBUG
-               "Flags used by the linker during debug builds")
+    REPLACE_FLAG("/MDd" "/MD" CMAKE_CXX_FLAGS_DEBUG
+                 "Flags used by the compiler during debug builds")
+    REPLACE_FLAG("/MDd" "/MD" CMAKE_C_FLAGS_DEBUG
+                 "Flags used by the compiler during debug builds")
+    REPLACE_FLAG("/MDd" "/MD" CMAKE_EXE_LINKER_FLAGS_DEBUG
+                 "Flags used by the linker during debug builds")
+    REPLACE_FLAG("/MDd" "/MD" CMAKE_MODULE_LINKER_FLAGS_DEBUG
+                 "Flags used by the linker during debug builds")
+  endif ()
 ENDIF (WIN32)
 
 FUNCTION(ADD_TARGET_INCLUDE target)
