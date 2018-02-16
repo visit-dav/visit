35760ce5ae9298feea36fece88f3598cd27c7f9c
Brad Whitlock <bjw@ilight.com>
2018-02-16 00:27:03 +0000

    OpenEXR read test.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@32404 18c085ea-50e0-402c-830e-de6fd14e8384

---
+++ test/tests/databases/openexr.py
@@ -0,0 +1,52 @@
+# ----------------------------------------------------------------------------
+#  CLASSES: nightly
+#
+#  Test Case:  openexr.py 
+#
+#  Tests:      openexr reading 
+#
+#  Programmer: Brad Whitlock
+#  Date:       Thu Feb 15 14:16:45 PST 2018
+#
+#  Modifications:
+# 
+# ----------------------------------------------------------------------------
+
+def test0():
+    DefineScalarExpression("masked_value", "if(gt(A, 0), value, 0)")
+
+    db = data_path("OpenEXR_test_data/noise_rgbazlv.exr")
+    OpenDatabase(db)
+    AddPlot("Pseudocolor", "R")
+    DrawPlots()
+    v = GetView2D()
+    v.viewportCoords = (0, 1, 0, 1)
+    SetView2D(v)
+    Test("openexr_0_R")
+
+    ChangeActivePlotsVar("G")
+    Test("openexr_0_G")
+
+    ChangeActivePlotsVar("B")
+    Test("openexr_0_B")
+
+    ChangeActivePlotsVar("A")
+    Test("openexr_0_A")
+
+    ChangeActivePlotsVar("Z")
+    Test("openexr_0_Z")
+
+    ChangeActivePlotsVar("L")
+    Test("openexr_0_L")
+
+    ChangeActivePlotsVar("masked_value")
+    Test("openexr_0_value")
+
+    DeleteAllPlots()
+    CloseDatabase(db)
+
+def main():
+    test0()
+
+main()
+Exit()
