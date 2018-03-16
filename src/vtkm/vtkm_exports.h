de2519d5785aec25a07ffb7a249e99d18becd703
Matt Larsen <larsen30@llnl.gov>
2018-03-16 16:40:47 +0000

    fixing visit vtkm exports
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.13RC@32691 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/vtkm/vtkm_exports.h
+++ src/vtkm/vtkm_exports.h
@@ -40,7 +40,7 @@
 #define VTKM_EXPORTS_H
 
 #if defined(_WIN32)
-#if defined(VTKM_EXPORTS) || defined(vtkm_EXPORTS)
+#if defined(VISIT_VTKM_EXPORTS) || defined(visit_vtkm_EXPORTS)
 #define VTKM_API __declspec(dllexport)
 #else
 #define VTKM_API __declspec(dllimport)
@@ -54,7 +54,7 @@
 #pragma warning(disable:4786)
 #endif
 #else
-# if __GNUC__ >= 4 && (defined(VTKM_EXPORTS) || defined(vtkm_EXPORTS))
+# if __GNUC__ >= 4 && (defined(VISIT_VTKM_EXPORTS) || defined(visit_vtkm_EXPORTS))
 #   define VTKM_API __attribute__ ((visibility("default")))
 # else
 #   define VTKM_API /* hidden by default */
