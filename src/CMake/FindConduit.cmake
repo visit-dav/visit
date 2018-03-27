6d24ae4ecbc372dcc4b497fa6b8c23b32b343740
Cyrus Harrison <cyrush@llnl.gov>
2018-03-27 03:47:48 +0000

    fix install of conduit_relay_mpi
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/2.13RC@32817 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/CMake/FindConduit.cmake
+++ src/CMake/FindConduit.cmake
@@ -45,5 +45,11 @@ INCLUDE(${VISIT_SOURCE_DIR}/CMake/SetUpThirdParty.cmake)
 
 SET_UP_THIRD_PARTY(CONDUIT lib include/conduit conduit conduit_relay conduit_blueprint)
 
+# par engine needs conduit relay's mpi lib, find it using setup_third_party
+# so it will be installed properly
+if(VISIT_PARALLEL)
+    set(CONDUIT_MPI_DIR ${CONDUIT_DIR})
+    SET_UP_THIRD_PARTY(CONDUIT_MPI lib include/conduit conduit_relay_mpi)
+endif()
 
 
