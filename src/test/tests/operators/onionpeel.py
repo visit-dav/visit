6445578301684fd6161a6af1726ece19f3fbd4e5
Eric Brugger <brugger1@llnl.gov>
2018-12-20 18:17:39 +0000

    I updated one of the onion peel operator tests. The test suite should now pass again.
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/trunk@33968 18c085ea-50e0-402c-830e-de6fd14e8384

--- src/test/tests/operators/onionpeel.py
+++ src/test/tests/operators/onionpeel.py
@@ -109,9 +109,6 @@ def TestUCD():
     ResetView()
     SetViewExtentsType("actual")
     AddOperator("OnionPeel")
-    op = OnionPeelAttributes()
-    op.index = 1
-    SetOperatorOptions(op)
     DrawPlots()
     Test("ops_onionpeel_04")
 
