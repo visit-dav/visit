d256806c3af2dd4ad0a11b829b17023716f09232
Alister Maguire <maguire7@llnl.gov>
2019-02-12 10:57:47 -0800

    updating mili tests

--- src/test/tests/databases/mili.py
+++ src/test/tests/databases/mili.py
@@ -8,8 +8,8 @@
 #
 #  Defect ID:  none
 #
-#  Programmer: Hank Childs
-#  Date:       March 31, 2005
+#  Programmer: Alister Maguire
+#  Date:       
 #
 #  Modifications:
 #
@@ -32,164 +32,117 @@
 # ----------------------------------------------------------------------------
 RequiredDatabasePlugin("Mili")
 
-OpenDatabase(data_path("mili_test_data/m_plot.mili"))
-
-AddPlot("FilledBoundary", "materials1_no_free_nodes(mesh1_no_free_nodes)")
-DrawPlots()
-
-v = GetView3D()
-v.viewNormal = (-0.31, 0.35, 0.88)
-SetView3D(v)
-
-Test("mili_01")
-
-SetTimeSliderState(101)
-v = GetView3D()
-v.viewNormal = (0.38, 0.34, 0.85)
-v.focus = (14.1, 0, 0)
-v.viewUp = (0, 1, 0)
-SetView3D(v)
-Test("mili_02")
-
-thres = ThresholdAttributes()
-thres.lowerBounds = (0.5)
-thres.listedVarNames = ("sand")
-SetDefaultOperatorOptions(thres)
-
-AddOperator("Threshold")
-DrawPlots()
-Test("mili_03")
-
-DeleteAllPlots()
-# We have to test a *lot* of scalar quantities, because they correspond to
-# a lot of different expressions.  On the plus side, we are getting a lot
-# of code coverage out of this.
-AddPlot("Pseudocolor", "derived/acceleration/mag")
-AddOperator("Threshold")
-DrawPlots()
-Test("mili_04")
-
-ChangeActivePlotsVar("derived/pressure")
-Test("mili_05")
-ChangeActivePlotsVar("derived/stress/x")
-Test("mili_06")
-ChangeActivePlotsVar("derived/stress/z")
-Test("mili_07")
-ChangeActivePlotsVar("derived/stress/xy")
-Test("mili_08")
-ChangeActivePlotsVar("derived/stress/yz")
-Test("mili_09")
-ChangeActivePlotsVar("derived/stress/xz")
-Test("mili_10")
-ChangeActivePlotsVar("derived/eff_stress")
-Test("mili_11")
-ChangeActivePlotsVar("derived/prin_dev_stress/1")
-Test("mili_12")
-ChangeActivePlotsVar("derived/prin_dev_stress/2")
-Test("mili_13")
-ChangeActivePlotsVar("derived/prin_dev_stress/3")
-Test("mili_14")
-ChangeActivePlotsVar("derived/max_shear_stress")
-Test("mili_15")
-ChangeActivePlotsVar("derived/prin_stress/1")
-Test("mili_16")
-ChangeActivePlotsVar("derived/prin_stress/2")
-Test("mili_17")
-ChangeActivePlotsVar("derived/prin_stress/3")
-Test("mili_18")
-
-DeleteAllPlots()
-
-AddPlot("Mesh", "mesh1")
-AddOperator("Threshold")
-DrawPlots()
-Test("mili_19")
-
-SetTimeSliderState(60)
-Test("mili_20")
-
-AddPlot("Vector", "bend")
-DrawPlots()
-Test("mili_21")
-
-v = VectorAttributes()
-v.autoScale = 0
-SetPlotOptions(v)
-Test("mili_22")
-
-DeleteAllPlots()
+def TestComponentVis():
+    OpenDatabase("/usr/workspace/wsrzd/maguire7/MiliTestData/new/d3samp6new.plt.mili")
+    v = GetView3D()
+    v.viewNormal = (0.9, 0.35, -0.88)
+    SetView3D(v)
+    SetTimeSliderState(90)
+    
+    
+    AddPlot("Pseudocolor", "Primal/brick/edrate")
+    DrawPlots()
+    Test("mili_brick_comp")
+    
+    ChangeActivePlotsVar("Primal/beam/svec/svec_x")
+    Test("mili_beam_comp")
+    ChangeActivePlotsVar("Primal/node/nodacc/ax")
+    Test("mili_nodacc_comp")
+    
+    DeleteAllPlots()
 
 
-AddPlot("Tensor", "stress")
-AddOperator("Threshold")
-DrawPlots()
-Test("mili_23")
+def TestElementSetComponents():
+    OpenDatabase("/usr/workspace/wsrzd/maguire7/MiliTestData/new/d3samp6new.plt.mili")
+    v = GetView3D()
+    v.viewNormal = (0.9, 0.35, -0.88)
+    SetView3D(v)
+    SetTimeSliderState(90)
 
-t = TensorAttributes()
-t.useStride = 1
-t.colorTableName = "calewhite"
-SetPlotOptions(t)
-Test("mili_24")
+    AddPlot("Pseudocolor", "Primal/shell/strain/exy")
+    DrawPlots()
+    Test("mili_es_comp_01")
+    ChangeActivePlotsVar("Primal/beam/stress/sz")
+    Test("mili_es_comp_02")
 
-s = SILRestriction()
-s.TurnOffSet(3)
-s.TurnOffSet(5)
-s.TurnOffSet(6)
-SetPlotSILRestriction(s)
-Test("mili_25")
+    DeleteAllPlots()
 
-DeleteAllPlots()
 
-AddPlot("Mesh", "mesh1")
-AddOperator("Threshold")
-AddPlot("Mesh", "mesh1_free_nodes")
-matts = MeshAttributes()
-matts.pointType = matts.Icosahedron
-matts.pointSize = 0.5
-SetPlotOptions(matts)
-DrawPlots()
-Test("mili_26")
+def TestMaterialVar():
+    OpenDatabase("/usr/workspace/wsrzd/maguire7/MiliTestData/new/d3samp6new.plt.mili")
+    v = GetView3D()
+    v.viewNormal = (0.9, 0.35, -0.88)
+    SetView3D(v)
+    SetTimeSliderState(90)
 
-AddPlot("Vector","free_nodes/nodvel")
-DrawPlots()
-Test("mili_27")
+    AddPlot("Pseudocolor", "Primal/mat/matcgy")
+    DrawPlots()
+    Test("mili_mat_var_01")
+    ChangeActivePlotsVar("Primal/mat/matke")
+    Test("mili_mat_var_02")
+    DeleteAllPlots()
 
-SetTimeSliderState(90)
-Test("mili_28")
 
-DeleteAllPlots()
-DefineScalarExpression("ratio", "volume(mesh1_no_free_nodes) / conn_cmfe(volume(<[0]i:mesh1_no_free_nodes>), mesh1_no_free_nodes)")
-AddPlot("Pseudocolor", "ratio")
-DrawPlots()
-Test("mili_29")
-sil = SILRestriction()
-sil.TurnOffAll()
-sil.TurnOnSet(12)
-SetPlotSILRestriction(sil)
-Test("mili_30")
+def TestMaterials():
+    OpenDatabase("/usr/workspace/wsrzd/maguire7/MiliTestData/new/d3samp6new.plt.mili")
+    v = GetView3D()
+    v.viewNormal = (0.9, 0.35, -0.88)
+    SetView3D(v)
+    SetTimeSliderState(90)
 
-DeleteAllPlots()
-CloseDatabase(data_path("mili_test_data/m_plot.mili"))
+    AddPlot("FilledBoundary", "materials1")
+    DrawPlots()
+    Test("mili_materials_01")
 
-#
-# Test a mili database with time-invariant nodal positions
-#
-OpenDatabase(data_path("mili_test_data/m1_plot.mili"))
+    DeleteAllPlots()
 
-SetTimeSliderState(5)
-AddPlot("Pseudocolor","temp")
-ResetView()
-DrawPlots()
-Test("mili_31")
-SetTimeSliderState(10)
-Test("mili_32")
-DeleteAllPlots()
-AddPlot("Mesh","mesh1")
-DrawPlots()
-Test("mili_33")
-SetTimeSliderState(6)
-Test("mili_34")
 
+def TestTensors():
+    OpenDatabase("/usr/workspace/wsrzd/maguire7/MiliTestData/new/d3samp6new.plt.mili")
+    v = GetView3D()
+    v.viewNormal = (0.9, 0.35, -0.88)
+    SetView3D(v)
+    SetTimeSliderState(90)
+
+    AddPlot("Tensor", "Primal/brick/stress")
+    DrawPlots()
+    Test("mili_tensors_01")
+
+    ChangeActivePlotsVar("Primal/brick/stress")
+    Test("mili_tensors_02")
+    
+    ChangeActivePlotsVar("Primal/shell/stress")
+    Test("mili_tensors_03")
+    DeleteAllPlots()
+
+
+def TestVectors():
+    OpenDatabase("/usr/workspace/wsrzd/maguire7/MiliTestData/new/d3samp6new.plt.mili")
+    v = GetView3D()
+    v.viewNormal = (0.9, 0.35, -0.88)
+    SetView3D(v)
+    SetTimeSliderState(90)
+
+    AddPlot("Vector", "Primal/node/nodpos")
+    DrawPlots()
+    Test("mili_vectors_01")
+    
+    ChangeActivePlotsVar("Primal/shell/bend")
+    Test("mili_vectors_02")
+    
+    ChangeActivePlotsVar("Primal/beam/svec")
+    Test("mili_vectors_03")
+    DeleteAllPlots()
+
+
+def Main():
+    TestComponentVis()    
+    TestElementSetComponents()
+    TestMaterialVar()
+    TestMaterials()
+    TestTensors()
+    TestVectors()
+
+Main()
 Exit()
-
 
