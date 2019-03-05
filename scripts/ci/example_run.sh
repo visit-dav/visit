52caacb355f6ef3073d5e5f727dae548e69fb265
Cyrus Harrison <cyrush@llnl.gov>
2019-03-05 09:47:41 -0800

    masonry refactor, start of docker ci build scripts

---
+++ scripts/ci/example_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# exec docker run to create a container from our image
+echo "docker run -t -i visit-ci-ubuntu:current"
+docker run -t -i visit-ci-ubuntu:current /bin/bash
+
+
--- src/tools/dev/masonry/masonry_view_log.py
+++ src/tools/dev/masonry/masonry_view_log.py
@@ -4,9 +4,7 @@ import os
 
 from os.path import join as pjoin
 
-sys.path.append(pjoin(os.path.split(os.path.abspath(__file__))[0],"build/lib"))
-
 from masonry import *
 
 if __name__ == "__main__":
-    log.view(sys.argv[1])
+    view_log(sys.argv[1])
