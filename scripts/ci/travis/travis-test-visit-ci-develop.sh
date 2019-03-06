71b8b5a41f54b1991c835f482b45ceb38f0f092b
Cyrus Harrison <cyrush@llnl.gov>
2019-03-06 09:09:49 -0800

    here we go again ...

--- scripts/ci/travis/travis-test-visit-ci-develop.sh
+++ scripts/ci/travis/travis-test-visit-ci-develop.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ev
-cd /home/build
+export PATH=$PATH:/masonry/build-mb-develop-ci-smoke/thirdparty_shared/third_party/cmake/3.9.3/ci/bin/
+cd /visit-clone
 mkdir travis-build
 cd travis-build
 cmake -DVISIT_CONFIG_SITE:PATH="/visit-ci-cfg.cmake" ../src
-make -j 2
+make -j 4
