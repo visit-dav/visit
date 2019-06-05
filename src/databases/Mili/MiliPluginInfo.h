5fd8276deb6180c2a30d618af0b4943e1614a8e6
Alister <maguire7@llnl.gov>
2019-06-05 08:35:28 -0700

    Task/maguire7/2019 01 mili plugin rewrite 3.0 rc (#3512)
    * adding the baseline re-write
    
    * beginning cleanup and noting issue
    
    * cleanup the mili meta data
    
    * Added handling of Nan values and minor mili cleanup
    
    * refactoring subrecord handling and general cleanup
    
    * refactored subrec retrieval process and performed some clean-up
    
    * mapping element set names to their standard variable names
    
    * simplified md creation
    
    * working through comments and cleanup
    
    * Setting material colors in mili, and updating Mat MD
    
    * fixed mem leak, updated doc strings, and saved space
    
    * handling global vectors
    
    * cleaning up mem errors/leaks
    
    * updating mili tests
    
    * preparing for sand variables
    
    * Creating generic buffer read for sand prep
    
    * handling sanded elements
    
    * handle variable components in sand mesh
    
    * simplifying sand mesh creation
    
    * adding some new mili test data
    
    * updating tests
    
    * working on fixing archived data error
    
    * updating mili test data
    
    * bug fixes and minor changes for multi-proc
    
    * handling particles and state format records
    
    * adding multi-proc data
    
    * updated mili test data material colors
    
    * updating tests and adding silo fix
    
    * adding new baselines
    
    * optimizing label generation time
    
    * Fixing offset and SR association issues
    
    * beginning refactor of labels
    
    * refactored mili labels
    
    * re-enabling node labels
    
    * cleanup
    
    * working on label map
    
    * Integrating label mapping
    
    * fixing node label query
    
    * working out issues with no nodepos and adding shared
    
    * adding to test suite and fixing issue with static node ds
    
    * refactored some memory management
    
    * beginning refactor for shared ES
    
    * adding framework for shared element sets
    
    * progess on shared element sets
    
    * progress on shared element sets
    
    * completing shared element sets
    
    * updating test data
    
    * resolving segmented subrec issues
    
    * adding a test for non-shared portions of es
    
    * re-enabling labels
    
    * adding baseline
    
    * cleaning up
    
    * cleaning up
    
    * updating tests and handling of old ES data
    
    * updating baselines for pick
    
    * updating pick tests
    
    * updating test suite
    
    * removing extra space
    
    * resolving space nonsense
    
    * completing doc
    
    * updating nan checks and fixing issues for windows compiler
    
    * release updates
    
    * adding the new makemili
    
    * updating release doc
    
    * fixing MS compile errors and adding missing baseline
    
    * fixing MS compile errors
    
    * resloving TODOs
    
    * adding label test
    
    * update doc

--- src/databases/Mili/MiliPluginInfo.h
+++ src/databases/Mili/MiliPluginInfo.h
@@ -82,6 +82,7 @@ class MiliCommonPluginInfo : public virtual CommonDatabasePluginInfo, public vir
     virtual DatabaseType              GetDatabaseType();
     virtual avtDatabase              *SetupDatabase(const char * const *list,
                                                     int nList, int nBlock);
+    virtual std::string               GetLicense() const;
 };
 
 class MiliMDServerPluginInfo : public virtual MDServerDatabasePluginInfo, public virtual MiliCommonPluginInfo
