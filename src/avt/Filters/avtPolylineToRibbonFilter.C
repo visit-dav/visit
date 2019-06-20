87e383ad467e693db34863b5db4663a7657aa09f
Kathleen Biagas <biagas2@llnl.gov>
2019-06-20 16:10:59 -0700

    Add support for cell-scalars in the filters used by Pseudocolor plot's line geometry features. (#3625)
    * Add support for cell-scalars in the filters used by Pseudocolor
    plot's line Geometry features (tube, ribbons, endpoints).
    Also changed how 'line' cells are removed after tube or ribbon
    filters applied, the append filter seems to do strange things
    with cell-data, but removing the lines before appending fixes
    the problem.  Resolves #3581.
    
    * Update release notes.

--- src/avt/Filters/avtPolylineToRibbonFilter.C
+++ src/avt/Filters/avtPolylineToRibbonFilter.C
@@ -44,7 +44,7 @@
 
 #include <vtkRibbonFilter.h>
 #include <vtkAppendPolyData.h>
-#include <vtkDataSet.h>
+#include <vtkCellData.h>
 #include <vtkPointData.h>
 #include <vtkPolyData.h>
 
@@ -96,8 +96,17 @@ avtPolylineToRibbonFilter::~avtPolylineToRibbonFilter()
 //
 //  Modifications:
 //    Eric Brugger, Thu Oct 20 14:51:51 PDT 2016
-//    I added code to remove duplicate points from the lines since the 
+//    I added code to remove duplicate points from the lines since the
 //    vtkRibbonFilter exits on any lines that have duplicate points.
+//
+//    Kathleen Biagas, Thu Jun 20 11:37:25 PDT 2019
+//    1) Handle cell-centered data.
+//    2) Only use the append filter if input contained more than just lines,
+//       and remove the lines before adding to append.
+//    3) Change how widthVar is sent to the vtk filter to avoid mucking with
+//       ActiveScalars.
+//    4) Removed CleanFilter as the input has already been cleaned before this
+//       filter is called.
 //
 // ****************************************************************************
 
@@ -121,13 +130,11 @@ avtPolylineToRibbonFilter::ExecuteData(avtDataRepresentation *inDR)
         return inDR;
     }
 
-    vtkDataArray *activeScalars = inDS->GetPointData()->GetScalars();
-
     vtkPolyData *data = vtkPolyData::SafeDownCast(inDS);
 
     // Create the ribbon polydata.
     vtkRibbonFilter *ribbonFilter = vtkRibbonFilter::New();
-
+    ribbonFilter->SetInputData(data);
     ribbonFilter->SetWidth( width );
     ribbonFilter->ReleaseDataFlagOn();
     ribbonFilter->SetVaryWidth( varyWidth );
@@ -136,38 +143,49 @@ avtPolylineToRibbonFilter::ExecuteData(avtDataRepresentation *inDR)
     if (varyWidth && widthVar != "" && widthVar != "\0")
     {
         if (widthVar != "default")
-            data->GetPointData()->SetActiveScalars(widthVar.c_str());
+        {
+            int fieldAssociation = vtkDataObject::FIELD_ASSOCIATION_POINTS;
+            if (data->GetCellData()->HasArray(widthVar.c_str()))
+            {
+                fieldAssociation = vtkDataObject::FIELD_ASSOCIATION_CELLS;
+            }
+            ribbonFilter->SetInputArrayToProcess(0, 0,0, fieldAssociation,
+                                                 widthVar.c_str());
+        }
+    }
+    ribbonFilter->Update();
+    vtkPolyData *outPD = NULL;
+
+    // Append the original data and ribbon polydata if needed
+    if (data->GetNumberOfLines() < data->GetNumberOfCells())
+    {
+        // seems to work better removing the lines before the append
+        // especially if avtPolylineAddEndPointsFilter has been used
+        // prior to this filter. The cell-data arrays get mixed up
+        // when lines are still in the data.
+        vtkPolyData *noLines = data->NewInstance();
+        noLines->ShallowCopy(data);
+        noLines->SetLines(NULL);
+        noLines->RemoveDeletedCells();
+
+        vtkAppendPolyData *append = vtkAppendPolyData::New();
+        append->AddInputData(noLines);
+        append->AddInputData(ribbonFilter->GetOutput());
+        append->Update();
+
+        outPD = append->GetOutput();
+        outPD->Register(NULL);
+        append->Delete();
+        noLines->Delete();
+    }
+    else
+    {
+        outPD = ribbonFilter->GetOutput();
+        outPD->Register(NULL);
     }
 
-    ribbonFilter->SetInputData(data);
-
-    ribbonFilter->Update();
-
-    // Append the original data and ribbon polydata
-    vtkAppendPolyData *append = vtkAppendPolyData::New();
-
-    append->AddInputData(data);
-    append->AddInputData(ribbonFilter->GetOutput());
-    
     ribbonFilter->Delete();
 
-    append->Update();
-
-    // Get the output.
-    vtkPolyData *outPD = append->GetOutput();
-    outPD->Register(NULL);
-    append->Delete();
-    
-    // Remove the lines.
-    outPD->SetLines(NULL);
-    outPD->RemoveDeletedCells();
-
-    // Restore the active scalars.
-    if (activeScalars)
-    {
-        data->GetPointData()->SetActiveScalars(activeScalars->GetName());
-        outPD->GetPointData()->SetActiveScalars(activeScalars->GetName());
-    }
 
     // Create the output data rep.
     avtDataRepresentation *outDR =
--- src/avt/Filters/avtPolylineToTubeFilter.C
+++ src/avt/Filters/avtPolylineToTubeFilter.C
@@ -42,12 +42,13 @@
 
 #include <avtPolylineToTubeFilter.h>
 
-#include <vtkTubeFilter.h>
 #include <vtkAppendPolyData.h>
+#include <vtkCellData.h>
 #include <vtkCleanPolyData.h>
-#include <vtkDataSet.h>
 #include <vtkPointData.h>
 #include <vtkPolyData.h>
+#include <vtkTubeFilter.h>
+
 
 // ****************************************************************************
 //  Method: avtPolylineToTubeFilter constructor
@@ -99,8 +100,17 @@ avtPolylineToTubeFilter::~avtPolylineToTubeFilter()
 //
 //  Modifications:
 //    Eric Brugger, Thu Oct 20 14:15:30 PDT 2016
-//    I added code to remove duplicate points from the lines since the 
+//    I added code to remove duplicate points from the lines since the
 //    vtkTubeFilter exits on any lines that have duplicate points.
+//
+//    Kathleen Biagas, Thu Jun 20 11:37:25 PDT 2019
+//    1) Handle cell-centered data.
+//    2) Only use the append filter if input contained more than just lines,
+//       and remove the lines before adding to append.
+//    3) Change how radiusVar is sent to the vtk filter to avoid mucking with
+//       ActiveScalars.
+//    4) Removed CleanFilter as the input has already been cleaned before this
+//       filter is called.
 //
 // ****************************************************************************
 
@@ -124,65 +134,64 @@ avtPolylineToTubeFilter::ExecuteData(avtDataRepresentation *inDR)
         return inDR;
     }
 
-    vtkDataArray *activeScalars = inDS->GetPointData()->GetScalars();
-
     vtkPolyData *data = vtkPolyData::SafeDownCast(inDS);
+    vtkPolyData *output = NULL;
 
-    // Clean duplicate points from the polydata.
-    vtkCleanPolyData *cleanFilter = vtkCleanPolyData::New();
-
-    cleanFilter->SetInputData(data);
-
-    cleanFilter->Update();
 
     // Create the tube polydata.
     vtkTubeFilter *tubeFilter = vtkTubeFilter::New();
-
+    tubeFilter->SetInputData(data);
     tubeFilter->SetRadius(radius);
     tubeFilter->SetNumberOfSides(numberOfSides);
     tubeFilter->SetCapping(1);
     tubeFilter->ReleaseDataFlagOn();
 
-    if (varyRadius && radiusVar != "" && radiusVar != "\0")
+    if (varyRadius)
     {
-        if (radiusVar != "default")
-            data->GetPointData()->SetActiveScalars(radiusVar.c_str());
-        
+        if (!radiusVar.empty() && radiusVar != "default")
+        {
+            int fieldAssociation = vtkDataObject::FIELD_ASSOCIATION_POINTS;
+            if (data->GetCellData()->HasArray(radiusVar.c_str()))
+            {
+                fieldAssociation = vtkDataObject::FIELD_ASSOCIATION_CELLS;
+            }
+            tubeFilter->SetInputArrayToProcess(0, 0,0, fieldAssociation,
+                                               radiusVar.c_str());
+        }
         tubeFilter->SetVaryRadiusToVaryRadiusByScalar();
         tubeFilter->SetRadiusFactor(radiusFactor);
     }
-
-    tubeFilter->SetInputData(cleanFilter->GetOutput());
-
-    cleanFilter->Delete();
-
     tubeFilter->Update();
 
-    // Append the original data and tube polydata
-    vtkAppendPolyData *append = vtkAppendPolyData::New();
+    vtkPolyData *outPD = NULL;
 
-    append->AddInputData(data);
-    append->AddInputData(tubeFilter->GetOutput());
-    
-    tubeFilter->Delete();
-
-    append->Update();
-
-    // Get the output.
-    vtkPolyData *outPD = append->GetOutput();
-    outPD->Register(NULL);
-    append->Delete();
-    
-    // Remove the lines.
-    outPD->SetLines(NULL);
-    outPD->RemoveDeletedCells();
-
-    // Restore the active scalars.
-    if (activeScalars)
+    if (data->GetNumberOfLines() < data->GetNumberOfCells())
     {
-        data->GetPointData()->SetActiveScalars(activeScalars->GetName());
-        outPD->GetPointData()->SetActiveScalars(activeScalars->GetName());
+        // seems to work better removing the lines before the append
+        // especially if avtPolylineAddEndPointsFilter has been used
+        // prior to this filter. The cell-data arrays get mixed up
+        // when lines are still in the data.
+        vtkPolyData *noLines = data->NewInstance();
+        noLines->ShallowCopy(data);
+        noLines->SetLines(NULL);
+        noLines->RemoveDeletedCells();
+
+        vtkAppendPolyData *append = vtkAppendPolyData::New();
+        append->AddInputData(data);
+        append->AddInputData(tubeFilter->GetOutput());
+        append->ReleaseDataFlagOn();
+        append->Update();
+        outPD = append->GetOutput();
+        outPD->Register(NULL);
+        append->Delete();
+        noLines->Delete(); 
     }
+    else
+    {
+        outPD = tubeFilter->GetOutput();
+        outPD->Register(NULL);
+    }
+    tubeFilter->Delete();
 
     // Create the output data rep.
     avtDataRepresentation *outDR =
