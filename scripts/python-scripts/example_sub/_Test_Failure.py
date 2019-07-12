d0d212b9e342e2e4e79514afec4435fceb907362
Eddie Rusu <epro24@gmail.com>
2019-07-12 09:50:36 -0700

    Simplified failing test

--- scripts/python-scripts/example_sub/_Test_Failure.py
+++ scripts/python-scripts/example_sub/_Test_Failure.py
@@ -12,15 +12,18 @@ class _TEST1PythonExpression(SimplePythonExpression):
         self.output_dimension = 1
     def derive_variable(self, ds_in, domain_id):
 
-        d = vnp.vtk_to_numpy(ds_in.GetCellData().GetArray(self.input_var_names[0]))
-        p = vnp.vtk_to_numpy(ds_in.GetCellData().GetArray(self.input_var_names[1]))
-        n = vnp.vtk_to_numpy(ds_in.GetCellData().GetArray(self.input_var_names[2]))
+        # d = vnp.vtk_to_numpy(ds_in.GetCellData().GetArray(self.input_var_names[0]))
+        # p = vnp.vtk_to_numpy(ds_in.GetCellData().GetArray(self.input_var_names[1]))
+        # n = vnp.vtk_to_numpy(ds_in.GetCellData().GetArray(self.input_var_names[2]))
         
-        # Perform calculation
-        out = d*p-n
+        d = ds_in.GetCellData().GetArray(self.input_var_names[0])
+        p = ds_in.GetCellData().GetArray(self.input_var_names[1])
+        n = ds_in.GetCellData().GetArray(self.input_var_names[2])
 
         # Convert result to vtk
-        res = vnp.numpy_to_vtk(out, deep=1)
+        res = vtk.vtkFloatArray()
+        res.SetNumberOfComponents(1)
+        res.SetNumberOfTuples(ds_in.GetNumberOfCells())
         return res
 
 py_filter = _TEST1PythonExpression
