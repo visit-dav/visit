276d5718f73eb4413667357aee91b6e51cb0c154
Eddie Rusu <rusu24edward@gmail.com>
2019-08-01 11:44:16 -0700

    Added function to check point data too

--- scripts/python-scripts/auto_py_filter_script.py
+++ scripts/python-scripts/auto_py_filter_script.py
@@ -1,4 +1,3 @@
-#!/usr/tce/bin/python
 
 import os
 import errno
@@ -10,11 +9,12 @@ import errno
 input_variables = [
     'd',
     'p',
+    'my_expr',
 ]
 
 expressions = [
-    '_TEST1 = d*p',
-    '_TEST2 = d-p',
+    """my_python_expr = my_expr-d+p*p""",
+    """my_python_expr_2 = d-p""",
 ]
 
 output_file_name = 'example.py'
@@ -44,6 +44,17 @@ import numpy as np
 import vtk.util.numpy_support as vnp
 """
 
+get_data_str = """
+def smart_get_data(ds_in, var_name):
+    out = ds_in.GetCellData().GetArray(var_name)
+    if out is None:
+        out = ds_in.GetPointData().GetArray(var_name)
+    if out is None:
+        raise Exception('Could not fetch %s' % var_name)
+    res = vnp.vtk_to_numpy(out)
+    return res
+"""
+
 class_body_str = """
     def __init__(self):
         SimplePythonExpression.__init__(self)
@@ -51,10 +62,12 @@ class_body_str = """
         self.description = "Auto-generated python expression from auto_py_filter_script.py"
         self.output_is_point_var = False
         self.output_dimension = 1
+        self.input_num_vars = """ + str(len(input_variables)) + """
+
     def derive_variable(self, ds_in, domain_id):
         # Globalize the names
         for var_name in self.input_var_names:
-            globals()[var_name] = vnp.vtk_to_numpy(ds_in.GetCellData().GetArray(var_name))
+            globals()[var_name] = smart_get_data(ds_in, var_name)
         
         # Perform calculation
         out = self.user_expression()
@@ -62,11 +75,10 @@ class_body_str = """
         # Convert result to vtk
         res = vnp.numpy_to_vtk(out, deep=1)
         return res
+
     def user_expression(self):
         # Expression created by user and written here
 """
-
-postamble = 'py_filter = AutoPythonExpression'
 
 
 if __name__ == "__main__":
@@ -131,12 +143,13 @@ if __name__ == "__main__":
         sub_file_str = sub_dir_name + '/' + out_var + '.py'
         sub_file = open(sub_file_str, 'w+')
         sub_file.write(imports_str)
-        sub_file.write('class ' + out_var + 'PythonExpression(SimplePythonExpression):\n')
+        sub_file.write(get_data_str + '\n')
+        sub_file.write('class ' + out_var + '_PythonExpression(SimplePythonExpression):')
         sub_file.write(class_body_str)
         sub_file.write('        ' + expr + '\n\n')
         sub_file.write('        #Return the expression\n')
         sub_file.write('        return ' + out_var + '\n\n')
-        sub_file.write('py_filter = ' + out_var + 'PythonExpression')
+        sub_file.write('py_filter = ' + out_var + '_PythonExpression')
 
         # Populate the controller file
         def_python_str = 'visit.DefinePythonExpression("' + out_var + '", input_variables, file = "' + sub_file_str + '")\n'
