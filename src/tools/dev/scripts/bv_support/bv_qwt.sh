eaf4f57861fb36074ca7e51507687c034d6abb94
biagas <biagas2@llnl.gov>
2019-08-02 10:10:56 -0700

    Added <path> to usage for --alt-qwt-dir.

--- src/tools/dev/scripts/bv_support/bv_qwt.sh
+++ src/tools/dev/scripts/bv_support/bv_qwt.sh
@@ -72,7 +72,7 @@ function bv_qwt_print
 function bv_qwt_print_usage
 {
     printf "%-20s %s\n" "--qwt" "Build with Qwt"
-    printf "%-20s %s [%s]\n" "--alt-qwt-dir" "Use Qwt from an alternative directory"
+    printf "%-20s %s []\n" "--alt-qwt-dir <path>" "Use Qwt from an alternative directory"
 }
 
 function bv_qwt_host_profile
