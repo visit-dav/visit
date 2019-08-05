e70ca98fac6c685c80913c68f7734fcdd078fae0
Eddie Rusu <rusu24edward@gmail.com>
2019-08-05 16:09:14 -0700

    Updated examples to use numpy syntax for square

--- scripts/python-scripts/auto_py_filter_script.py
+++ scripts/python-scripts/auto_py_filter_script.py
@@ -14,8 +14,8 @@ input_map = {
 
 expressions = [
     'my_python_1 = d*p',
-    'my_python_2 = my_expr^2 - my_expr',
-    'my_python_3 = my_python_2^2',
+    'my_python_2 = np.square(my_expr) - my_expr',
+    'my_python_3 = np.square(my_python_2)',
 ]
 
 output_file_name = 'example.py'
