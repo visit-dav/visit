2eba2c88e4da38372ba15c522b312825bec75208
Eddie Rusu <rusu1@llnl.gov>
2019-08-26 09:12:40 -0700

    Fixed segfault bug by initializing vtkCellTypes hanlder

--- src/databases/XYZ/avtXYZWriter.C
+++ src/databases/XYZ/avtXYZWriter.C
@@ -169,7 +169,7 @@ avtXYZWriter::WriteChunk(vtkDataSet *ds, int chunk)
     if (ds->GetPointData()->GetNumberOfArrays() == 0)
     {
         // There is no point data, so look for VTK_VERTEX only cells
-        vtkCellTypes *cellTypes;
+        vtkCellTypes *cellTypes = vtkCellTypes::New();
         ds->GetCellTypes(cellTypes);
         if (cellTypes->GetNumberOfTypes() == 1 && cellTypes->IsType(VTK_VERTEX) == 1)
         {
@@ -186,6 +186,7 @@ avtXYZWriter::WriteChunk(vtkDataSet *ds, int chunk)
                 "to include to the nodes.");
             dsa = ds->GetPointData();
         }
+        cellTypes->Delete();
     }
     else
     {
