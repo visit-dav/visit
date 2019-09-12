ca6de1f5aab62e11a8c580d6bedd631287190767
Alister Maguire <maguire7@llnl.gov>
2019-09-12 16:16:10 -0700

    resolving global id highlight issue

--- src/avt/Queries/Pick/avtPickByZoneQuery.C
+++ src/avt/Queries/Pick/avtPickByZoneQuery.C
@@ -113,6 +113,10 @@ avtPickByZoneQuery::~avtPickByZoneQuery()
 //    Matt Larsen, July 19 08:29:l2 PDT 2017
 //    Added support for picking by label
 //
+//    Alister Maguire, Thu Sep 12 15:34:02 PDT 2019
+//    Make sure that the highlight extractor gets the local id, not the
+//    global one. 
+//
 // ****************************************************************************
 
 void
@@ -323,7 +327,7 @@ avtPickByZoneQuery::Execute(vtkDataSet *ds, const int dom)
         pickAtts.SetPickPoint(center);
     }
     
-    this->ExtractZonePickHighlights(origPick, ds, dom);
+    this->ExtractZonePickHighlights(zoneid, ds, dom);
 }
 
 
