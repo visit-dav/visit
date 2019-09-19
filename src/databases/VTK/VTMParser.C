bba23c8d8ba88ad5fbbcfa5e36c1614062cb1f20
Kathleen Biagas <biagas2@llnl.gov>
2019-09-19 16:22:40 -0700

    Fix VTK reader bug parsing vtm files. (#3927)
    Merge from 3.0RC.

--- src/databases/VTK/VTMParser.C
+++ src/databases/VTK/VTMParser.C
@@ -21,8 +21,8 @@ using std::vector;
 // ****************************************************************************
 //  Method: VTMParser constructor
 //
-//  Programmer: Kathleen Biagas 
-//  Creation:   August 12, 2015 
+//  Programmer: Kathleen Biagas
+//  Creation:   August 12, 2015
 //
 // ****************************************************************************
 
@@ -38,12 +38,12 @@ VTMParser::VTMParser() : fileName(), baseDir(), blockExtension(),
 // ****************************************************************************
 //  Method: VTMParser destructor
 //
-//  Programmer: Kathleen Biagas 
-//  Creation:   August 12, 2015 
+//  Programmer: Kathleen Biagas
+//  Creation:   August 12, 2015
 //
 // ****************************************************************************
 
-VTMParser::~VTMParser() 
+VTMParser::~VTMParser()
 {
     groupIds.clear();
     blockNames.clear();
@@ -66,8 +66,8 @@ VTMParser::~VTMParser()
 //  Arguments:
 //    fn        The file name.
 //
-//  Programmer: Kathleen Biagas 
-//  Creation:   August 12, 2015 
+//  Programmer: Kathleen Biagas
+//  Creation:   August 12, 2015
 //
 // ****************************************************************************
 
@@ -88,8 +88,8 @@ VTMParser::SetFileName(const char *fn)
 //  Arguments:
 //      index   The block index.
 //
-//  Programmer: Kathleen Biagas 
-//  Creation:   August 12, 2015 
+//  Programmer: Kathleen Biagas
+//  Creation:   August 12, 2015
 //
 // ****************************************************************************
 
@@ -110,8 +110,8 @@ VTMParser::GetBlockFileName(int index)
 //
 //  Return:  The number of blocks.
 //
-//  Programmer: Kathleen Biagas 
-//  Creation:   August 12, 2015 
+//  Programmer: Kathleen Biagas
+//  Creation:   August 12, 2015
 //
 // ****************************************************************************
 
@@ -127,13 +127,13 @@ VTMParser::GetNumberOfBlocks()
 //
 //  Purpose:
 //    Parses the 'vtm' file for DataSet file names, and other information
-//    that VisIt might want like block and group names. 
+//    that VisIt might want like block and group names.
 //
 //  Note:
 //     A lot of error checking here, perhaps more than is truly needed.
 //
-//  Programmer: Kathleen Biagas 
-//  Creation:   August 12, 2015 
+//  Programmer: Kathleen Biagas
+//  Creation:   August 12, 2015
 //
 // ****************************************************************************
 
@@ -166,7 +166,7 @@ VTMParser::Parse()
     string dsType = root->GetAttribute("type");
     if (dsType != "vtkMultiBlockDataSet")
     {
-        errorMessage = string("VTKFile type: ") + 
+        errorMessage = string("VTKFile type: ") +
                       dsType + string(" currently not supported.");
         return false;
     }
@@ -181,7 +181,7 @@ VTMParser::Parse()
     int n = mbdsNode->GetNumberOfNestedElements();
 
     // Loop over nested attributes, assuming each represents a 'group'.
-    // Only top-level groups are accounted for with this code, sub-groups 
+    // Only top-level groups are accounted for with this code, sub-groups
     // are ignored.  (VTK's many_blocks.vtm example has sub-groups).
     vector<int> nBlocksPerGroup;
     for (int i = 0; i < n; ++i)
@@ -197,7 +197,7 @@ VTMParser::Parse()
         // associate each block with this group, via groupIds.
         for (int j = 0; j < nb; ++j)
             groupIds.push_back(i);
-        string elName(el->GetName()); 
+        string elName(el->GetName());
 
         if (elName  != "DataSet")
             groupPieceName = elName;
@@ -205,7 +205,7 @@ VTMParser::Parse()
     }
     nGroups = (int)nBlocksPerGroup.size();
 
-    // Sanity checks in case some blocks or datasets didn't have a 'name' 
+    // Sanity checks in case some blocks or datasets didn't have a 'name'
     // attribute
     if (nGroups == 1 || (int)groupNames.size() != nGroups)
         groupNames.clear();
@@ -215,9 +215,9 @@ VTMParser::Parse()
 
     // No guarantee that there is a separate name (or Title) for groups,
     // or blocks for that matter. If group and piece name are the same,
-    // give groupPieceName a generic name. 
-    if (!groupPieceName.empty() && 
-        !blockPieceName.empty() && 
+    // give groupPieceName a generic name.
+    if (!groupPieceName.empty() &&
+        !blockPieceName.empty() &&
         groupPieceName == blockPieceName)
     {
         groupPieceName = "Group";
@@ -230,11 +230,15 @@ VTMParser::Parse()
 //  Method: VTMParser::ParseForDS
 //
 //  Purpose:
-//    Recursively checks the passed vtkXMLDataElement for 'DataSet' tag. 
+//    Recursively checks the passed vtkXMLDataElement for 'DataSet' tag.
 //    Retrieves and stores 'file' and 'name' attributes.
 //
-//  Programmer: Kathleen Biagas 
-//  Creation:   August 12, 2015 
+//  Programmer: Kathleen Biagas
+//  Creation:   August 12, 2015
+//
+//  Modifications:
+//    Kathleen Biagas, Thu Sep 19 14:11:28 PDT 2019
+//    Don't process the 'DataSet' tag if it doesn't have a 'file' attribute.
 //
 // ****************************************************************************
 
@@ -244,33 +248,36 @@ VTMParser::ParseForDS(vtkXMLDataElement *de, int &n)
     string name(de->GetName());
     if (name == "DataSet")
     {
-        n++;
-        string bfn(de->GetAttribute("file"));
-        if (blockExtension.empty())
+        if (de->GetAttribute("file"))
         {
-            size_t pos = bfn.find_last_of('.');
-            if (pos == std::string::npos)
-                blockExtension = "none";
-            else
-                blockExtension = bfn.substr(pos+1);
-        }
-        blockFileNames.push_back(bfn);
-        if (de->GetAttribute("name") != NULL) 
-        {
-            blockNames.push_back(string(de->GetAttribute("name")));
-        }
-        if (blockPieceName.empty())
-        {
-            vtkXMLDataElement *parent = de->GetParent();
-            if(parent)
-{
-                blockPieceName = parent->GetName();
-}
+            n++;
+            string bfn(de->GetAttribute("file"));
+            if (blockExtension.empty())
+            {
+                size_t pos = bfn.find_last_of('.');
+                if (pos == std::string::npos)
+                    blockExtension = "none";
+                else
+                    blockExtension = bfn.substr(pos+1);
+            }
+            blockFileNames.push_back(bfn);
+            if (de->GetAttribute("name") != NULL)
+            {
+                blockNames.push_back(string(de->GetAttribute("name")));
+            }
+            if (blockPieceName.empty())
+            {
+                vtkXMLDataElement *parent = de->GetParent();
+                if(parent)
+                {
+                    blockPieceName = parent->GetName();
+                }
+            }
         }
     }
     else
     {
         for (int i = 0; i < de->GetNumberOfNestedElements(); ++i)
             ParseForDS(de->GetNestedElement(i), n);
-    } 
+    }
 }
