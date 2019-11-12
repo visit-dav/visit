ce0dbf5df3c000850fdddd9c7c8293cfeff45a9f
Eric Brugger <brugger1@llnl.gov>
2019-11-11 16:05:43 -0800

    Merge the ADIOS2 changes from the 3.0RC to the 3.1RC. (#4037)

--- src/databases/ADIOS2/avtSpecFEMFileFormat.h
+++ src/databases/ADIOS2/avtSpecFEMFileFormat.h
@@ -1,6 +1,40 @@
-// Copyright (c) Lawrence Livermore National Security, LLC and other VisIt
-// Project developers.  See the top-level LICENSE file for dates and other
-// details.  No copyright assignment is required to contribute to VisIt.
+/*****************************************************************************
+*
+* Copyright (c) 2000 - 2019, Lawrence Livermore National Security, LLC
+* Produced at the Lawrence Livermore National Laboratory
+* LLNL-CODE-442911
+* All rights reserved.
+*
+* This file is  part of VisIt. For  details, see https://visit.llnl.gov/.  The
+* full copyright notice is contained in the file COPYRIGHT located at the root
+* of the VisIt distribution or at http://www.llnl.gov/visit/copyright.html.
+*
+* Redistribution  and  use  in  source  and  binary  forms,  with  or  without
+* modification, are permitted provided that the following conditions are met:
+*
+*  - Redistributions of  source code must  retain the above  copyright notice,
+*    this list of conditions and the disclaimer below.
+*  - Redistributions in binary form must reproduce the above copyright notice,
+*    this  list of  conditions  and  the  disclaimer (as noted below)  in  the
+*    documentation and/or other materials provided with the distribution.
+*  - Neither the name of  the LLNS/LLNL nor the names of  its contributors may
+*    be used to endorse or promote products derived from this software without
+*    specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT  HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR  IMPLIED WARRANTIES, INCLUDING,  BUT NOT  LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND  FITNESS FOR A PARTICULAR  PURPOSE
+* ARE  DISCLAIMED. IN  NO EVENT  SHALL LAWRENCE  LIVERMORE NATIONAL  SECURITY,
+* LLC, THE  U.S.  DEPARTMENT OF  ENERGY  OR  CONTRIBUTORS BE  LIABLE  FOR  ANY
+* DIRECT,  INDIRECT,   INCIDENTAL,   SPECIAL,   EXEMPLARY,  OR   CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT  LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF  USE, DATA, OR PROFITS; OR  BUSINESS INTERRUPTION) HOWEVER
+* CAUSED  AND  ON  ANY  THEORY  OF  LIABILITY,  WHETHER  IN  CONTRACT,  STRICT
+* LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY  WAY
+* OUT OF THE  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+* DAMAGE.
+*
+*****************************************************************************/
 
 // ************************************************************************* //
 //                            avtADIOSFileFormat.h                           //
@@ -13,11 +47,11 @@
 #include <vector>
 #include <string>
 #include <map>
-#include <memory>
 #include <adios2.h>
 
 class avtFileFormatInterface;
 class vtkRectilinearGrid;
+
 
 // ****************************************************************************
 //  Class: avtSpecFEMFileFormat
@@ -35,33 +69,16 @@ class vtkRectilinearGrid;
 class avtSpecFEMFileFormat : public avtMTMDFileFormat
 {
   public:
-    static bool Identify(const std::string &fname,
-                         const std::map<std::string, adios2::Params> &vars,
-                         const std::map<std::string, adios2::Params> &attrs);
-
-    // interface creator with first file already opened
+    static bool        Identify(const char *fname);
     static avtFileFormatInterface *CreateInterface(const char *const *list,
                                                    int nList,
-                                                   int nBlock,
-                                                   std::shared_ptr<adios2::ADIOS> adios,
-                                                   adios2::Engine &reader,
-                                                   adios2::IO &io,
-                                                   const std::map<std::string, adios2::Params> &vars,
-                                                   const std::map<std::string, adios2::Params> &attrs);
-
+                                                   int nBlock);
     static bool        GenerateFileNames(const std::string &nm,
                                          std::string &meshNm, std::string &dataNm);
     static bool        IsMeshFile(const std::string &fname);
     static bool        IsDataFile(const std::string &fname);
 
-    avtSpecFEMFileFormat(const char *fname);
-    avtSpecFEMFileFormat(std::shared_ptr<adios2::ADIOS> adios,
-                         adios2::Engine &reader,
-                         adios2::IO &io,
-                         const std::map<std::string, adios2::Params> &vars,
-                         const std::map<std::string, adios2::Params> &attrs,
-                         const char *fname);
-
+    avtSpecFEMFileFormat(const char *);
     virtual  ~avtSpecFEMFileFormat();
 
     virtual void        GetCycles(std::vector<int> &);
