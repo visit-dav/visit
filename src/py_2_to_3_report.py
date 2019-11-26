0af9d852dec0541616b28dbddcb76d84dd6d7ca1
Cyrus Harrison <cyrush@llnl.gov>
2019-11-26 09:18:47 -0800

    tweaks to py 2to3 helper

--- src/py_2_to_3_report.py
+++ src/py_2_to_3_report.py
@@ -1,4 +1,5 @@
 
+import sys
 import os
 import subprocess
 from os.path import join as pjoin
@@ -18,19 +19,36 @@ def sexe(cmd,ret_output=False,echo = True):
     else:
         return subprocess.call(cmd,shell=True)
 
-py_files = []
-# traverse root directory, and list directories as dirs and files as files
-for root, dirs, files in os.walk("."):
-    path = root.split(os.sep)
-    path_str = os.sep.join(path)
-    #print((len(path) - 1) * '---', os.path.basename(root))
-    for file in files:
-        if file.endswith(".py"):
-            #print(len(path) * '---', file)
-            py_files.append(pjoin(path_str,file))
 
-for p in py_files:
-    print(p)
-    sexe("2to3 {}".format(p))
+def main(dcheck,do_xform=False):
+    print(dcheck,do_xform)
+    py_files = []
+    # traverse root directory, and list directories as dirs and files as files
+    for root, dirs, files in os.walk(dcheck):
+        path = root.split(os.sep)
+        path_str = os.sep.join(path)
+        #print((len(path) - 1) * '---', os.path.basename(root))
+        for file in files:
+            if file.endswith(".py"):
+                #print(len(path) * '---', file)
+                py_files.append(pjoin(path_str,file))
     
+    xform = ""
+    if do_xform:
+        xform = "-w -n"
+    for p in py_files:
+        print(p)
+        sexe("2to3 {} {}".format(xform,p))
+    
+
+
+if __name__ == "__main__":
+    dcheck = "."
+    do_xform = False
+    for val in sys.argv[1:]:
+        if not val.startswith("--"):
+            dcheck = val
+        else:
+            do_xform = True
+    main(dcheck,do_xform)
 
