10cf1d600aaa10d1c76b9b917c62482dbca93fca
Cyrus Harrison <cyrush@llnl.gov>
2019-12-11 21:55:32 -0800

    refactor marcos to use common helper (needs more testing)

--- src/CMake/VisItMacros.cmake
+++ src/CMake/VisItMacros.cmake
@@ -120,20 +120,29 @@ MACRO(VISIT_PLUGIN_TARGET_RTOD type)
 ENDMACRO(VISIT_PLUGIN_TARGET_RTOD)
 
 
-##############################################################################
-# Function that creates XML tools C++ Code Gen Target 
-##############################################################################
-FUNCTION(ADD_CPP_GEN_TARGET gen_name
-                            src_dir
-                            dest_dir)
 
-    set(gen_target_name "gen_cpp_${gen_name}")
+##############################################################################
+# Function that creates a generic XML tool Code Gen Target 
+# (helper for functions below, don't call directly )
+##############################################################################
+FUNCTION(ADD_XML_TOOLS_GEN_TARGET gen_name
+                                  src_dir
+                                  dest_dir
+                                  tool_name
+                                  gen_type)
+
+    set(gen_target_name "gen_${gen_type}_${gen_name}")
 
     if(VISIT_CMAKE_VERBOSE_GEN_TARGET_MESSAGES)
-        MESSAGE(STATUS "Adding xml tools C++ generation target: ${gen_target_name}")
+        MESSAGE(STATUS "Adding ${tool_name} generation target: ${gen_target_name}")
     endif()
 
-    set(xml_gen_tool ${CMAKE_BINARY_DIR}/bin/xml2atts)
+    if(WIN32)
+        # need to test this in the future
+        set(xml_gen_tool ${CMAKE_BINARY_DIR}/bin/visit.exe -${tool_name})
+    else()
+        set(xml_gen_tool ${CMAKE_BINARY_DIR}/bin/${tool_name})
+    endif()
 
     # construct path to source file, we need to run 
     # in the dir where we want the code to gen
@@ -147,17 +156,32 @@ FUNCTION(ADD_CPP_GEN_TARGET gen_name
                       COMMAND ${xml_gen_tool} -clobber ${xml_input}
                       DEPENDS  ${xml_file} xml2atts
                       WORKING_DIRECTORY ${dest_dir}
-                      COMMENT "Generating C++ Code for ${gen_name}" VERBATIM)
+                      COMMENT "Running ${tool_name} on ${gen_name}" VERBATIM)
 
     # connect this target to roll up target for all python gen
-    if(NOT TARGET gen_cpp_all)
-        add_custom_target("gen_cpp_all")
+    set(top_level_target "gen_${gen_type}_all")
+    if(NOT TARGET ${top_level_target})
+        add_custom_target(${top_level_target})
     endif()
     
-    add_dependencies(gen_cpp_all ${gen_target_name})
+    add_dependencies(${top_level_target} ${gen_target_name})
+
+ENDFUNCTION(ADD_XML_TOOLS_GEN_TARGET)
+
+#############################################################################
+# Function that creates XML tools C++ Code Gen Target 
+##############################################################################
+FUNCTION(ADD_CPP_GEN_TARGET gen_name
+                            src_dir
+                            dest_dir)
+
+    ADD_XML_TOOLS_GEN_TARGET(${gen_name}
+                             ${src_dir}
+                             ${dest_dir}
+                             "xml2atts"
+                             "cpp")
 
 ENDFUNCTION(ADD_CPP_GEN_TARGET)
-
 
 ##############################################################################
 # Function that creates XML tools Python Code Gen Target 
@@ -166,34 +190,11 @@ FUNCTION(ADD_PYTHON_GEN_TARGET gen_name
                                src_dir
                                dest_dir)
 
-    set(gen_target_name "gen_python_${gen_name}")
-
-    if(VISIT_CMAKE_VERBOSE_GEN_TARGET_MESSAGES)
-        MESSAGE(STATUS "Adding xml tools Python generation target: ${gen_target_name}")
-    endif()
-
-    set(xml_gen_tool ${CMAKE_BINARY_DIR}/bin/xml2python)
-
-    # construct path to source file, we need to run 
-    # in the dir where we want the code to gen
-    set(xml_input ${src_dir}/${gen_name}.xml)
-
-    if(NOT IS_ABSOLUTE ${xml_input})
-        set(xml_input "${CMAKE_CURRENT_SOURCE_DIR}/${xml_input}")
-    endif()
-
-    add_custom_target(${gen_target_name}
-                      COMMAND ${xml_gen_tool} -clobber ${xml_input}
-                      DEPENDS  ${xml_file} xml2python 
-                      WORKING_DIRECTORY ${dest_dir}
-                      COMMENT "Generating Python Wrapping Code for ${gen_name}" VERBATIM)
-
-    # connect this target to roll up target for all python gen
-    if(NOT TARGET gen_python_all)
-        add_custom_target("gen_python_all")
-    endif()
-
-    add_dependencies(gen_python_all ${gen_target_name})
+   ADD_XML_TOOLS_GEN_TARGET(${gen_name}
+                            ${src_dir}
+                            ${dest_dir}
+                            "xml2python"
+                            "python")
 
 ENDFUNCTION(ADD_PYTHON_GEN_TARGET)
 
@@ -204,35 +205,11 @@ FUNCTION(ADD_JAVA_GEN_TARGET gen_name
                              src_dir
                              dest_dir)
 
-    set(gen_target_name "gen_java_${gen_name}")
-
-    if(VISIT_CMAKE_VERBOSE_GEN_TARGET_MESSAGES)
-        MESSAGE(STATUS "Adding xml tools Java generation target: ${gen_target_name}")
-    endif()
-
-    set(xml_gen_tool ${CMAKE_BINARY_DIR}/bin/xml2java)
-
-    # construct path to source file, we need to run 
-    # in the dir where we want the code to gen
-    set(xml_input ${src_dir}/${gen_name}.xml)
-
-    if(NOT IS_ABSOLUTE ${xml_input})
-        set(xml_input "${CMAKE_CURRENT_SOURCE_DIR}/${xml_input}")
-    endif()
-    
-
-    add_custom_target(${gen_target_name}
-                      COMMAND ${xml_gen_tool} -clobber ${xml_input}
-                      DEPENDS  ${xml_file} xml2java 
-                      WORKING_DIRECTORY ${dest_dir}
-                      COMMENT "Generating Java Code for ${gen_name}" VERBATIM)
-
-    # connect this target to roll up target for all python gen
-    if(NOT TARGET gen_java_all)
-        add_custom_target("gen_java_all")
-    endif()
-
-    add_dependencies(gen_java_all ${gen_target_name})
+     ADD_XML_TOOLS_GEN_TARGET(${gen_name}
+                              ${src_dir}
+                              ${dest_dir}
+                              "xml2java"
+                              "java")
 
 ENDFUNCTION(ADD_JAVA_GEN_TARGET)
 
@@ -243,34 +220,11 @@ FUNCTION(ADD_INFO_GEN_TARGET gen_name
                              src_dir
                              dest_dir)
 
-    set(gen_target_name "gen_info_${gen_name}")
-
-    if(VISIT_CMAKE_VERBOSE_GEN_TARGET_MESSAGES)
-        MESSAGE(STATUS "Adding xml tools Info generation target: ${gen_target_name}")
-    endif()
-
-    set(xml_gen_tool ${CMAKE_BINARY_DIR}/bin/xml2info)
-
-    # construct path to source file, we need to run 
-    # in the dir where we want the code to gen
-    set(xml_input ${src_dir}/${gen_name}.xml)
-
-    if(NOT IS_ABSOLUTE ${xml_input})
-        set(xml_input "${CMAKE_CURRENT_SOURCE_DIR}/${xml_input}")
-    endif()
-
-    add_custom_target(${gen_target_name}
-                      COMMAND ${xml_gen_tool} -clobber ${xml_input}
-                      DEPENDS  ${xml_file} xml2info
-                      WORKING_DIRECTORY ${dest_dir}
-                      COMMENT "Generating Info Code Gen for ${gen_name}" VERBATIM)
-
-    # connect this target to roll up target for all python gen
-    if(NOT TARGET gen_info_all)
-        add_custom_target("gen_info_all")
-    endif()
-
-    add_dependencies(gen_info_all ${gen_target_name})
+    ADD_XML_TOOLS_GEN_TARGET(${gen_name}
+                             ${src_dir}
+                             ${dest_dir}
+                             "xml2info"
+                             "info")
 
 ENDFUNCTION(ADD_INFO_GEN_TARGET)
 
@@ -281,34 +235,11 @@ FUNCTION(ADD_CMAKE_GEN_TARGET gen_name
                               src_dir
                               dest_dir)
 
-    set(gen_target_name "gen_cmake_${gen_name}")
-
-    if(VISIT_CMAKE_VERBOSE_GEN_TARGET_MESSAGES)
-        MESSAGE(STATUS "Adding xml tools Info generation target: ${gen_target_name}")
-    endif()
-
-    set(xml_gen_tool ${CMAKE_BINARY_DIR}/bin/xml2cmake)
-
-    # construct path to source file, we need to run 
-    # in the dir where we want the code to gen
-    set(xml_input ${src_dir}/${gen_name}.xml)
-
-    if(NOT IS_ABSOLUTE ${xml_input})
-        set(xml_input "${CMAKE_CURRENT_SOURCE_DIR}/${xml_input}")
-    endif()
-
-    add_custom_target(${gen_target_name}
-                      COMMAND ${xml_gen_tool} -clobber ${xml_input}
-                      DEPENDS  ${xml_file} xml2cmake
-                      WORKING_DIRECTORY ${dest_dir}
-                      COMMENT "Generating Info Code Gen for ${gen_name}" VERBATIM)
-
-    # connect this target to roll up target for all python gen
-    if(NOT TARGET gen_cmake_all)
-        add_custom_target("gen_cmake_all")
-    endif()
-
-    add_dependencies(gen_cmake_all ${gen_target_name})
+    ADD_XML_TOOLS_GEN_TARGET(${gen_name}
+                             ${src_dir}
+                             ${dest_dir}
+                             "xml2cmake"
+                             "cmake")
 
 ENDFUNCTION(ADD_CMAKE_GEN_TARGET)
 
