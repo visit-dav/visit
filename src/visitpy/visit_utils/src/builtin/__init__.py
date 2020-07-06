c11f8a0b659a24e2979b1e4ff1f4983345029c10
Cyrus Harrison <cyrush@llnl.gov>
2020-07-06 11:29:05 -0700

    add missing __init__.py

---
+++ src/visitpy/visit_utils/src/builtin/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) Lawrence Livermore National Security, LLC and other VisIt
+# Project developers.  See the top-level LICENSE file for dates and other
+# details.  No copyright assignment is required to contribute to VisIt.
+
+"""
+ file: __init__.py
+ author: Cyrus Harrison <cyrush@llnl.gov>
+ created: 7/6/2020
+ description:
+     Init file for 'visit_utils.builtin' module.
+"""
+
+from .evalfuncs import *
+from .writescript import WriteScript
