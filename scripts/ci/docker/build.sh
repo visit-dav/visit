c6e633a232c69acb9a56c29a01a7db24c3347873
Cyrus Harrison <cyrush@llnl.gov>
2020-09-04 14:35:52 -0700

    fixes

---
+++ scripts/ci/docker/build.sh
@@ -0,0 +1,8 @@
+# Copyright (c) Lawrence Livermore National Security, LLC and other VisIt
+# Project developers.  See the top-level LICENSE file for dates and other
+# details.  No copyright assignment is required to contribute to VisIt.
+date
+
+python build_docker_visit_ci.py
+
+date
