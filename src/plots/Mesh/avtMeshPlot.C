f927a31f9c44f20c329c2bf98ab69aab87945a39
Mark C. Miller <miller86@llnl.gov>
2020-12-16 12:37:30 -0800

    rc2dev: fix cell count multiplier logic in mesh plot (#5309)

--- src/plots/Mesh/avtMeshPlot.C
+++ src/plots/Mesh/avtMeshPlot.C
@@ -232,6 +232,11 @@ avtMeshPlot::Create()
 //    Mark C. Miller, Mon Aug 23 20:24:31 PDT 2004
 //    Changed to the Set... method (Get is now done in avtPlot.C)
 //
+//    Mark C. Miller, Tue Dec 15 19:50:23 PST 2020
+//    Fix logic to set non-unit multipler only when...
+//       a) topo dim is zero...meaning its a point mesh and a glyphed plot
+//       b) spatial dim is 2 or 3...meaning glyphs are sets of 3D faces
+//    Set multipler to count of faces of each 2 or 3D glyph type.
 // ****************************************************************************
 
 void
@@ -239,11 +244,53 @@ avtMeshPlot::SetCellCountMultiplierForSRThreshold(const avtDataObject_p dob)
 {
     if (*dob)
     {
-        int dim = dob->GetInfo().GetAttributes().GetSpatialDimension();
-        if (dim == 0)
-            cellCountMultiplierForSRThreshold = 6.0;
-        else
+        int tdim = dob->GetInfo().GetAttributes().GetTopologicalDimension();
+
+        if (tdim > 0)
+        {
             cellCountMultiplierForSRThreshold = 1.0;
+            return;
+        }
+
+        int sdim = dob->GetInfo().GetAttributes().GetSpatialDimension();
+        if (sdim < 2)
+        {
+            cellCountMultiplierForSRThreshold = 1.0;
+            return;
+        }
+
+        // 2D glyphs are polydata often comprised of multiple tris or quads
+        // but can include lines and points.
+        if (sdim == 2)
+        {
+            switch (atts.GetPointType())
+            {
+                case Box:            cellCountMultiplierForSRThreshold = 1.0; break;
+                case Axis:           cellCountMultiplierForSRThreshold = 1.5; break;
+                case Icosahedron:    cellCountMultiplierForSRThreshold = 10.0; break;
+                case Octahedron:     cellCountMultiplierForSRThreshold = 5.0; break;
+                case Tetrahedron:    cellCountMultiplierForSRThreshold = 1.0; break;
+                case SphereGeometry: cellCountMultiplierForSRThreshold = 1.0; break;
+                case Point:          cellCountMultiplierForSRThreshold = 1.0; break;
+                case Sphere:         cellCountMultiplierForSRThreshold = 1.0; break;
+            }
+            return;
+        }
+
+        if (sdim == 3)
+        {
+            switch (atts.GetPointType())
+            {
+                case Box:            cellCountMultiplierForSRThreshold = 6.0; break;
+                case Axis:           cellCountMultiplierForSRThreshold = 3.0; break;
+                case Icosahedron:    cellCountMultiplierForSRThreshold = 20.0; break;
+                case Octahedron:     cellCountMultiplierForSRThreshold = 8.0; break;
+                case Tetrahedron:    cellCountMultiplierForSRThreshold = 4.0; break;
+                case SphereGeometry: cellCountMultiplierForSRThreshold = 1.0; break;
+                case Point:          cellCountMultiplierForSRThreshold = 1.0; break;
+                case Sphere:         cellCountMultiplierForSRThreshold = 1.0; break;
+            }
+        }
     }
 }
 
