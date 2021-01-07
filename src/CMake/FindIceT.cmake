43dd8b80f768ec01b02b2e470777309b0dc88fd7
Kathleen Biagas <biagas2@llnl.gov>
2021-01-07 10:59:11 -0800

    Fix typos and excessive space.

--- src/CMake/FindIceT.cmake
+++ src/CMake/FindIceT.cmake
@@ -19,12 +19,10 @@
 #    INCLUDE(${VISIT_ICET_DIR}/lib/ICETConfig.cmake)
 #ENDIF(VISIT_ICET_DIR)
 
-
-
 # Use the ICET_DIR hint from the config-site .cmake file 
 # Except on windows, where it is part of the repo.
 
-if(VISIT_ICET_DIR
+if(VISIT_ICET_DIR)
     include(${VISIT_SOURCE_DIR}/CMake/SetUpThirdParty.cmake)
     SET_UP_THIRD_PARTY(ICET lib include IceTCore IceTGL IceTMPI)
 endif()
