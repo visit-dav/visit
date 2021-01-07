c59fb78dc78b4562822800ce24e2f97e25e47b6b
biagas <biagas2@llnl.gov>
2021-01-07 10:42:00 -0800

    Only setup icet if VISIT_ICET_DIR is set.

--- src/CMake/FindIceT.cmake
+++ src/CMake/FindIceT.cmake
@@ -7,6 +7,9 @@
 #   Kathleen Biagas, Tues Oct 1 09:33:47 MST 2013
 #   Removed logic handling windows differently than other platforms.
 #
+#   Kathleen Biagas, Thu Jan 7, 2021
+#   Only process if VISIT_ICET_DIR is set.
+#
 #****************************************************************************/
 
 # Ice-T provides an ICETConfig.cmake file that we could include but it appears 
@@ -16,10 +19,13 @@
 #    INCLUDE(${VISIT_ICET_DIR}/lib/ICETConfig.cmake)
 #ENDIF(VISIT_ICET_DIR)
 
+
+
 # Use the ICET_DIR hint from the config-site .cmake file 
 # Except on windows, where it is part of the repo.
 
-INCLUDE(${VISIT_SOURCE_DIR}/CMake/SetUpThirdParty.cmake)
-
-SET_UP_THIRD_PARTY(ICET lib include IceTCore IceTGL IceTMPI)
+if(VISIT_ICET_DIR
+    include(${VISIT_SOURCE_DIR}/CMake/SetUpThirdParty.cmake)
+    SET_UP_THIRD_PARTY(ICET lib include IceTCore IceTGL IceTMPI)
+endif()
 
