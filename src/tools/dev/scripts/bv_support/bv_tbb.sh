db1a14ad99c94ab61a07c1ed5c44e0333a130b83
biagas <biagas2@llnl.gov>
2021-01-13 09:48:29 -0800

    Change SETUP_APP_VERISON(TBB to set(TBB_VERSION)
    Fixes CMake warnings when using newer cmake.
    Also modified bv_tbb to change how config-site entry is written,
    SETUP_APP_VERSION not needed.

--- src/tools/dev/scripts/bv_support/bv_tbb.sh
+++ src/tools/dev/scripts/bv_support/bv_tbb.sh
@@ -79,9 +79,8 @@ function bv_tbb_host_profile
         #   TBB related projects. I want to keep it set here since I am afraid of breaking other packages
         #   other than ospray
         if [[ "$USE_SYSTEM_TBB" == "no" ]]; then
-            echo "SETUP_APP_VERSION(TBB ${TBB_VERSION})" >> $HOSTCONF
-            echo "VISIT_OPTION_DEFAULT(TBB_ROOT \${VISITHOME}/tbb/\${TBB_VERSION}/\${VISITARCH})" >> $HOSTCONF
-            echo "VISIT_OPTION_DEFAULT(VISIT_TBB_DIR \${VISITHOME}/tbb/\${TBB_VERSION}/\${VISITARCH})" >> $HOSTCONF
+            echo "VISIT_OPTION_DEFAULT(TBB_ROOT \${VISITHOME}/tbb/${TBB_VERSION}/\${VISITARCH})" >> $HOSTCONF
+            echo "VISIT_OPTION_DEFAULT(VISIT_TBB_DIR \${VISITHOME}/tbb/${TBB_VERSION}/\${VISITARCH})" >> $HOSTCONF
         else
             echo "VISIT_OPTION_DEFAULT(TBB_ROOT ${TBB_INSTALL_DIR})" >> $HOSTCONF
             echo "VISIT_OPTION_DEFAULT(VISIT_TBB_DIR ${TBB_INSTALL_DIR})" >> $HOSTCONF
