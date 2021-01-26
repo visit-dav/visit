6697e9db649c879faffc5b438aa3300336c41edb
Eric Brugger <brugger1@llnl.gov>
2021-01-26 13:56:12 -0800

    Fixed the handling of arguments with python queries.

--- src/test/tests/queries/py_queries.py
+++ src/test/tests/queries/py_queries.py
@@ -31,8 +31,7 @@ PythonQuery(file=script_path("py_query_script_00.vpq"))
 res = GetQueryOutputString()
 TestText("py_queries_00",res + "\n")
 
-#PythonQuery(file=script_path("py_query_script_00.vpq"),vars=["default","p"],args=["test",0.0,[1,2.0,3]])
-PythonQuery(file=script_path("py_query_script_01.vpq"),vars=["default","p"])
+PythonQuery(file=script_path("py_query_script_01.vpq"),vars=["default","p"],args=["test",0.0,[1,2.0,3]])
 res = GetQueryOutputString()
 TestText("py_queries_01",res + "\n")
 
