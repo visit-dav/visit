ad027dbf4550fb8d05f0d9fdf4bf7b1ca354cfb3
Cyrus Harrison <cyrush@llnl.gov>
2021-06-04 12:04:33 -0700

    check for rotten eggs

--- src/osxfixup/osxcheckup.py
+++ src/osxfixup/osxcheckup.py
@@ -8,6 +8,7 @@ import sys
 import subprocess
 import glob
 import json
+import zipfile
 
 from os.path import join as pjoin
 
@@ -81,16 +82,56 @@ def find_suspect_libs(path):
     print(json.dumps(suspect_libs,indent=2))
     return suspect_libs, deps_to_libs_map
 
+def find_suspect_eggs(path):
+    # if our python modules are eggs that are zipfiles
+    # that will pose a problem for our signing.
+    #so check for egg zips 
+    # full traverse of the dir
+    eggs = []
+    suspect_eggs = {}
+    for path, directories, files in os.walk(path):
+         for f in files:
+              if f.endswith(".egg"):
+                  eggs.append(pjoin(path,f))
+         # we know these are ok, but still want to find some eggs
+         for d in directories:
+             if d.endswith(".egg"):
+                 eggs.append(pjoin(path,d))
+    for egg in eggs:
+        rcode, rout = shexe("file {0}".format(egg),ret_output=True)
+        if rcode != 0:
+            print("[error running egg file check!]")
+            sys.exit(-1)
+        if rout.lower().count("zip") > 0:
+            # list contents of the zip file
+            z = zipfile.ZipFile(egg)
+            # list available files in the container
+            z_items = z.namelist()
+            z_libs = []
+            for z_item in z_items:
+                if z_item.endswith(".so") or  z_item.endswith(".dylib"):
+                    z_libs.append(z_item)
+            if len(z_libs) > 0:
+                suspect_eggs[egg] = z_libs
+    return suspect_eggs
 
 def main():
     path = sys.argv[1]
+    ok = True
     suspect_libs, deps_to_libs_map = find_suspect_libs(path)
     # if we have suspect libs, return error
     if len(suspect_libs) > 0:
+        ok = False
         for suspect_lib in suspect_libs:
             print("suspect lib: {0}".format(suspect_lib))
             print("used by:")
             print(json.dumps(deps_to_libs_map[suspect_lib],indent=2))
+    suspect_eggs = find_suspect_eggs(path)
+    if len(suspect_eggs) > 0:
+        print("Error: python egg zip files with shared libs detected!")
+        print(json.dumps(suspect_eggs,indent=2))
+        ok = False
+    if not ok:
         sys.exit(-1)
 
 if __name__ == "__main__":
