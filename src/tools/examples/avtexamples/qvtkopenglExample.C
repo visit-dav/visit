7f8910052882f6873f610ad5c8e4a2c0032a24a3
Eric Brugger <brugger1@llnl.gov>
2021-06-21 14:24:23 -0700

    Add some Qt VTK examples.

---
+++ src/tools/examples/avtexamples/qvtkopenglExample.C
@@ -0,0 +1,46 @@
+// Copyright (c) Lawrence Livermore National Security, LLC and other VisIt
+// Project developers.  See the top-level LICENSE file for dates and other
+// details.  No copyright assignment is required to contribute to VisIt.
+
+#include <GUIWindow.h>
+
+#include <QApplication>
+
+#include <QVTKOpenGLWidget.h>
+
+#include <iostream>
+
+int
+MCVMain(int argc, char *argv[])
+{
+    int retval = 0;
+
+    //
+    // Setting the default QSurfaceFormat required with QVTKOpenGLwidget.
+    // This causes Qt to create an OpenGL 3.2 context.
+    //
+    auto surfaceFormat = QVTKOpenGLWidget::defaultFormat();
+    surfaceFormat.setSamples(0);
+    surfaceFormat.setAlphaBufferSize(0);
+    QSurfaceFormat::setDefaultFormat(surfaceFormat);
+
+    //
+    // Create the QApplication. This sets the qApp pointer.
+    //
+    QApplication *mainApp = new QApplication(argc, argv, true);
+
+    GUIWindow *guiWin = new GUIWindow();
+
+    guiWin->show();
+    guiWin->raise();
+
+    retval = mainApp->exec();
+
+    return retval;
+}
+
+int
+main(int argc, char **argv)
+{
+    return MCVMain(argc, argv);
+}
