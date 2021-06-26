c6378ad30400806347c1d0877c5ce339cc781480
miller86 <miller86@llnl.gov>
2021-06-25 17:31:18 -0700

    add new apropos CLI method

---
+++ src/visitpy/visit_utils/src/builtin/apropos.py
@@ -0,0 +1,33 @@
+import re
+
+try:
+    import visit
+except:
+    pass
+
+#
+# Return the list of names of all methods where the specified regex
+# matches either in the method name or its doc string or a strinified
+# version of the object. Use a set to accumulate only unique results
+# but return the result as a list. The matching is performed all in
+# lower case.
+#
+def apropos(key):
+    retval = set()
+    for v in dir(visit):
+        if re.search(key, v, re.IGNORECASE):
+            retval.add(v)
+            continue
+        meth = getattr(visit, v) 
+        if 'Attributes' in v:
+            try:
+                if re.search(key, str(meth()), re.IGNORECASE):
+                    retval.add(v)
+                    continue
+            except:
+                pass
+        if hasattr(meth,'__doc__'): 
+             if meth.__doc__ is not None:
+                 if re.search(key, meth.__doc__, re.IGNORECASE):
+                     retval.add(v)
+    return list(retval)
