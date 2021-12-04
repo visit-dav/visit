cc92e2958369ad30c4e6f44e1867cf55f150bbb6
ARSanderson <allen@sci.utah.edu>
2021-12-03 17:24:01 -0700

    complete rewrite mininal checks needed now

--- src/CMake/FindOSPRay.cmake
+++ src/CMake/FindOSPRay.cmake
@@ -2,149 +2,52 @@
 # Project developers.  See the top-level LICENSE file for dates and other
 # details.  No copyright assignment is required to contribute to VisIt.
 
-#****************************************************************************
-# Modifications:
-#   Kevin Griffin, Fri May 3 18:44:57 PDT 2019
-#   Add logic to install libraries on OSX correctly.
-#
-#   Kathleen Biagas, Thu Jul 18 10:41:50 PDT 2019
-#   Add special handling of tbb and embree installs for linux.
-#
-#*****************************************************************************
+# Use the OSPRAY_DIR hint from the config-site .cmake file
 
-IF(VISIT_OSPRAY)
+#MESSAGE(STATUS "  OSPRAY_VERSION=${OSPRAY_VERSION}")
+#MESSAGE(STATUS "  VISIT_OSPRAY_DIR=${VISIT_OSPRAY_DIR}")
 
-    # -- this is a hack for TBB_ROOT
-    IF(NOT DEFINED TBB_ROOT)
-      SET(TBB_ROOT ${VISIT_TBB_ROOT})
-    ENDIF()
-    SET(embree_DIR ${EMBREE_DIR})
-    MARK_AS_ADVANCED(ospray_DIR)
-    MARK_AS_ADVANCED(embree_DIR)
+# The libraries as of ospray 2.8
+SET(OSPRAY_LIBRARIES ospray
+                     ospray_imgui
+                     ospray_module_ispc)
 
-    # setup ospray
-    # we have to search for the osprayConfig.cmake file in the ospray install
-    # folder. Because ospray sometimes install its library in <...>/lib64
-    # folder instead of the <...>/lib folder, we have to check both
-    # possibilities
-    FIND_PACKAGE(ospray REQUIRED
-                 PATHS 
-                     ${OSPRAY_DIR}/lib/cmake/ospray-${OSPRAY_VERSION}
-                     ${OSPRAY_DIR}/lib64/cmake/ospray-${OSPRAY_VERSION}
-                 NO_DEFAULT_PATH)
-    ADD_DEFINITIONS(-DVISIT_OSPRAY)
-    # append additional module libraries
-    IF(NOT APPLE)
-        list(APPEND OSPRAY_LIBRARIES 
-            ${LIBRARY_PATH_PREFIX}ospray_module_ispc${LIBRARY_SUFFIX}
-	     )
+If( EXISTS ${VISIT_OSPRAY_DIR} )
+
+    IF(EXISTS ${VISIT_OSPRAY_DIR}/lib64)
+        SET(LIB lib64)
     ELSE()
-        list(APPEND OSPRAY_LIBRARIES
-            ${LIBRARY_PATH_PREFIX}ospray.${OSPRAY_MAJOR_VERSION}${LIBRARY_SUFFIX}
-            ${LIBRARY_PATH_PREFIX}ospray_imgui.${OSPRAY_MAJOR_VERSION}${LIBRARY_SUFFIX}
-            ${LIBRARY_PATH_PREFIX}ospray_module_ispc.${OSPRAY_MAJOR_VERSION}${LIBRARY_SUFFIX}
-	     )
+        SET(LIB lib)
     ENDIF()
 
-    # ospray tries to dlopen the ispc libs at runtime
-    # so we need ot make sure those libs exist in
-    # ${VISIT_BINARY_DIR}/lib/
-    # so developer builds can load them
-
-    IF( NOT WIN32 )
-        FOREACH(ospray_lib ${OSPRAY_LIBRARIES})
-            IF( "${ospray_lib}" MATCHES "ispc")
-                execute_process(COMMAND ${CMAKE_COMMAND} -E copy
-                                        ${ospray_lib}
-                                        ${VISIT_BINARY_DIR}/lib/)
-            ENDIF()
-        ENDFOREACH()
+    IF(EXISTS ${VISIT_OSPRAY_DIR}/${LIB}/cmake/ospray-${OSPRAY_VERSION}/OSPRAYConfig.cmake)
+        SET(OSPRAY_DIR ${VISIT_OSPRAY_DIR})
     ENDIF()
 
+    MESSAGE(STATUS "Checking for ospray in ${OSPRAY_DIR}/${LIB}/cmake/ospray-${OSPRAY_VERSION}")
 
-    # install ospray libs follow visit's standard
-    #
-    # -- OSPRAY_LIBRARIES contains also libtbb and libembree, so we dont
-    #    have to handle embree and tbb seperately. As for ISPC, since ISPC
-    #    is only a compiler and will only be needed when we are compiling
-    #    ospray source, we dont have to install ISPC at all here for VisIt.
-    #
-    # on linux ospray have libraries in a form of:
-    #      libospray[xxx].so
-    #      libospray[xxx].so.[major_version]
-    #      libospray[xxx].so.[version]
-    #
-    # on mac ospray have libraries in a form of:
-    #      libospray[xxx].dylib
-    #      libospray[xxx].[major_version].dylib
-    #      libospray[xxx].[version].dylib
-    #
-    # on windows
-    #      libospray[xxx].lib
-    #      libospray[xxx].dll
-    #
-    # -- installing headers
-    IF(VISIT_INSTALL_THIRD_PARTY AND NOT VISIT_HEADERS_SKIP_INSTALL)
-      INSTALL(DIRECTORY ${OSPRAY_INCLUDE_DIR}/ospray
-        DESTINATION
-        DESTINATION ${VISIT_INSTALLED_VERSION_INCLUDE}
-        FILE_PERMISSIONS OWNER_WRITE OWNER_READ
-                         GROUP_WRITE GROUP_READ
-                         WORLD_READ
-        DIRECTORY_PERMISSIONS OWNER_WRITE OWNER_READ OWNER_EXECUTE
-                              GROUP_WRITE GROUP_READ GROUP_EXECUTE
-                                          WORLD_READ WORLD_EXECUTE
-        PATTERN ".svn" EXCLUDE)
-    ENDIF()
-    # -- installing libs
-    IF(NOT VISIT_OSPRAY_SKIP_INSTALL)
-      FOREACH(l ${OSPRAY_LIBRARIES})
-        # here because the CMake variable OSPRAY_LIBRARIES contains words
-        # like 'optimized' and 'debug', we should skip them
-        IF( (NOT "${l}" STREQUAL "optimized") AND
-            (NOT "${l}" STREQUAL "debug"))
-          GET_FILENAME_COMPONENT(_name_ ${l} NAME_WE)
-          IF(LINUX AND
-              (NOT "${_name_}" STREQUAL "libtbb_debug") AND
-              (NOT "${_name_}" STREQUAL "libtbbmalloc_debug") AND
-              (NOT "${_name_}" STREQUAL "libtbb") AND
-              (NOT "${_name_}" STREQUAL "libtbbmalloc") AND
-              (NOT "${_name_}" STREQUAL "libembree3") )
-            THIRD_PARTY_INSTALL_LIBRARY(${l}.${OSPRAY_MAJOR_VERSON})
-          ENDIF()
+    # Find ospray
+    set(CMAKE_PREFIX_PATH ${VISIT_OSPRAY_DIR}/${LIB}/cmake/ospray-${OSPRAY_VERSION} ${CMAKE_PREFIX_PATH})
+    set(CMAKE_LIBRARY_PATH ${VISIT_OSPRAY_DIR}/${ospray}/LIB ${CMAKE_LIBRARY_PATH})
 
-          if(LINUX AND ("${_name_}" MATCHES "embree" OR
-                        "${_name_}" MATCHES "tbb"))
-              # embree and tbb are listed with their full extensions of
-              # .so.[version]. The simple .so is also needed in order for
-              # plugins to link correctly against an install.  If the .so
-              # is a symlink to the full version (as with embree) the install
-              # library logic will correctly install both the full verison and
-              # the .so symlink, so only the .so is needed to be sent to the
-              # function.
-              get_filename_component(_tmp_name ${l} NAME)
-              get_filename_component(_tmp_path ${l} PATH)
-              GET_FILENAME_SHORTEXT(_tmp_ext ${_tmp_name})
-              string(REPLACE "${_tmp_ext}" "" _tmp_name ${_tmp_name})
-              THIRD_PARTY_INSTALL_LIBRARY(${_tmp_path}/${_tmp_name})
-              # tbb's .so isn't a symlink, so install full version too
-              if("${_name_}" MATCHES "tbb")
-                  THIRD_PARTY_INSTALL_LIBRARY(${l})
-              endif()
-          else()
-              THIRD_PARTY_INSTALL_LIBRARY(${l})
-          endif()
-        ENDIF()
-      ENDFOREACH()
-    ENDIF()
+    FIND_PACKAGE(ospray)
 
-    # debug
-    MESSAGE(STATUS "OSPRAY_INCLUDE_DIR: " ${OSPRAY_INCLUDE_DIR})
-    MESSAGE(STATUS "OSPRAY_LIBRARIES: " ${OSPRAY_LIBRARIES})
-    MESSAGE(STATUS "OSPRay for VisIt: ON")
+#    MESSAGE(STATUS "  OSPRAY_INCLUDE_DIRS=${OSPRAY_INCLUDE_DIRS}")
+#    MESSAGE(STATUS "  OSPRAY_LIBRARY_DIRS=${OSPRAY_LIBRARY_DIRS}")
+#    MESSAGE(STATUS "  OSPRAY_DEFINITIONS=${OSPRAY_DEFINITIONS}")
+ENDIF()
 
-ELSE(VISIT_OSPRAY)
-    MESSAGE(STATUS "OSPRay for VisIt: OFF")
-ENDIF(VISIT_OSPRAY)
+INCLUDE(${VISIT_SOURCE_DIR}/CMake/SetUpThirdParty.cmake)
 
+SET_UP_THIRD_PARTY(OSPRAY
+    ${LIB}
+    include/ospray
+    ${OSPRAY_LIBRARIES}
+    )
+
+IF(OSPRAY_FOUND)
+    SET(HAVE_OSPRAY true CACHE BOOL "Have ospray lib")
+else()
+    unset(OSPRAY_LIBRARIES)
+ENDIF(OSPRAY_FOUND)
 
