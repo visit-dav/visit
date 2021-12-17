7dd2cc368f829977579a964bd50fac7ca8cfb214
miller86 <miller86@llnl.gov>
2021-12-16 20:38:02 -0800

    adding dir() test

--- src/test/tests/unit/default_methods.py
+++ src/test/tests/unit/default_methods.py
@@ -22,14 +22,16 @@ TestValueEQ("GetDebugLevel()", GetDebugLevel(), 0)
 DeleteWindow()
 TestValueEQ("GetLastError()", GetLastError(), "Can't delete the last window.")
 
-# This version number test requires always 3, single digits
+# This version number test assumes 3, single digits
 TestValueGE("Version()", int(Version().replace('.','')), 321)
 
 TestValueEQ("LocalNameSpace()", LocalNameSpace(), None)
 
 # The long file name test is relevant only on Windows
+# But, we should really have an example of a short file name
+# there that LongFileName expands and we confirm matches expected
 if sys.platform.startswith("win"):
-    TestValueEQ("LongFileName()", LongFileName("myfile.txt"))
+    TestValueEQ("LongFileName()", LongFileName("myfile.txt"), "myfile.txt")
 
 # Test two ways of using WriteScript
 f = open('foo.py', 'wt')
@@ -38,5 +40,14 @@ f.close()
 WriteScript('bar.py')
 TestValueEQ("WriteScript('foo.py') == WriteScript(f)", filecmp.cmp('foo.py','bar.py'), True)
 TestValueGT("WriteScript() file size", os.path.getsize('foo.py'), 3000)
+
+# Test dir() on some objects
+dout = dir(SILRestriction())
+TestValueIN("SetsInCategory in dir(SILAttributes())", dout, 'SetsInCategory')
+TestValueIN("SetIndex in dir(SILAttributes())", dout, 'SetIndex')
+dout = dir(View3DAttributes())
+TestValueIN("RotateAxis in dir(View3Dattributes())", dout, 'RotateAxis')
+TestValueIN("GetImageZoom in dir(View3Dattributes())", dout, 'GetImageZoom')
+TestValueIN("SetImagePan in dir(View3Dattributes())", dout, 'SetImagePan')
 
 Exit()
