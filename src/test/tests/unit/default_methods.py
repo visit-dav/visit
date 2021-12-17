1e8cd4e730a33d4bd41c14d6e5a262edab97e599
miller86 <miller86@llnl.gov>
2021-12-16 16:43:02 -0800

    convert to TestValueXX type tests

--- src/test/tests/unit/default_methods.py
+++ src/test/tests/unit/default_methods.py
@@ -7,14 +7,25 @@
 #
 #  Cyrus Harrison, Mon Jul 20 11:32:42 PDT 2020
 # ----------------------------------------------------------------------------
+import sys
 
 #
 # Simply Call These
 #
-print(GetDebugLevel())
-print(GetLastError())
-print(Version())
-print(LocalNameSpace())
-print(LongFileName("myfile.txt"))
+TestValueEQ("GetDebugLevel()", GetDebugLevel(), 0)
+
+# Do something to create an error (not a python exception) so that we
+# can get something other than the empty string from GetLastError()
+DeleteWindow()
+TestValueEQ("GetLastError()", GetLastError(), "Can't delete the last window.")
+
+# This version number test requires always 3, single digits
+TestValueGE("Version()", int(Version().replace('.','')), 321)
+
+TestValueEQ("LocalNameSpace()", LocalNameSpace(), None)
+
+# The long file name test is relevant only on Windows
+if sys.platform.startswith("win"):
+    TestValueEQ("LongFileName()", LongFileName("myfile.txt"))
 
 Exit()
