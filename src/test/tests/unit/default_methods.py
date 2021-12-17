e7329d0840c6fa4510875a8b2ee1fa455962889b
miller86 <miller86@llnl.gov>
2021-12-16 17:28:20 -0800

    test WriteScript

--- src/test/tests/unit/default_methods.py
+++ src/test/tests/unit/default_methods.py
@@ -6,12 +6,15 @@
 #  Tests:      Tests default method interface of the visit module
 #
 #  Cyrus Harrison, Mon Jul 20 11:32:42 PDT 2020
+#
+#  Modifications
+#    Mark C. Miller, Thu Dec 16 16:43:39 PST 2021
+#    Wrap in TestValueXX calls so that we really do test behavior and not
+#    just that something doesn't fail due to exception. Add tests for
+#    WriteScript.
 # ----------------------------------------------------------------------------
-import sys
+import os, sys, filecmp
 
-#
-# Simply Call These
-#
 TestValueEQ("GetDebugLevel()", GetDebugLevel(), 0)
 
 # Do something to create an error (not a python exception) so that we
@@ -27,5 +30,13 @@ TestValueEQ("LocalNameSpace()", LocalNameSpace(), None)
 # The long file name test is relevant only on Windows
 if sys.platform.startswith("win"):
     TestValueEQ("LongFileName()", LongFileName("myfile.txt"))
+
+# Test two ways of using WriteScript
+f = open('foo.py', 'wt')
+WriteScript(f)
+f.close()
+WriteScript('bar.py')
+TestValueEQ("WriteScript('foo.py') == WriteScript(f)", filecmp.cmp('foo.py','bar.py'), True)
+TestValueGT("WriteScript() file size", os.path.getsize('foo.py'), 3000)
 
 Exit()
