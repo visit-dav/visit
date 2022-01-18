2e4b2cd28e9d8ed77a08780c0fd752240f3c508d
Christopher Laganella <cdl@ilight.com>
2022-01-18 11:14:38 -0500

    Add flatten() to the cli

---
+++ src/visitpy/visit_utils/src/builtin/flatten.py
@@ -0,0 +1,50 @@
+# Copyright (c) Lawrence Livermore National Security, LLC and other VisIt
+# Project developers.  See the top-level LICENSE file for dates and other
+# details.  No copyright assignment is required to contribute to VisIt.
+
+###############################################################################
+# file: flatten.py
+# Purpose: Expose tabular mesh data to the CLI
+#
+#
+# Programmer: Chris Laganella
+# Creation: Tue Jan 18 11:05:21 EST 2022
+#
+#
+# Modifications:
+#
+#
+###############################################################################
+
+try:
+    import visit
+except:
+    pass
+
+def flatten(vars):
+    """flatten(vars) -> dict
+
+Synopsis:
+    Query the active plot for the data at each node/zone for the given
+    variables. Data is returned in as numpy compatible 2D arrays.
+
+Arguments:
+
+vars:
+    The names of the desired variables (tuple of strings).
+
+Returns:
+
+    Flatten returns a dictionary that contains different keys depending
+    on the data. If the output contains node centered data then there
+    will be 'nodeColumnNames' and 'nodeTable' entries. If the output contains
+    zone centered data then there will be 'zoneColumnNames' and 'zoneTable'
+    entries. If the query results in no output data, then an empty dictionary
+    is returned. The '*Table' entries are compatible with numpy via the
+    'numpy.asarray()' function.
+    """
+    flattenOpts = dict()
+    flattenOpts["vars"] = vars
+
+    visit.Query("Flatten", flattenOpts)
+    return visit.GetFlattenOutput()
