9dfc84cb9003220be80552bd1ae9406d14ddf366
James Kress <jameskress@users.noreply.github.com>
2022-02-16 21:23:33 -0500

    Updating vtkm and vtkh scripts (#17048)

--- src/tools/dev/scripts/bv_support/bv_vtkm.sh
+++ src/tools/dev/scripts/bv_support/bv_vtkm.sh
@@ -20,7 +20,7 @@ function bv_vtkm_alt_vtkm_dir
     bv_vtkm_enable
     USE_SYSTEM_VTKM="yes"
     VTKM_INSTALL_DIR="$1"
-    info "Using Alternate VTKM: $SYSTEM_VTKM_DIR"
+    info "Using Alternate VTKM: $VTKM_INSTALL_DIR"
 }
 
 function bv_vtkm_depends_on
@@ -39,11 +39,11 @@ function bv_vtkm_initialize_vars
 
 function bv_vtkm_info
 {
-    export VTKM_VERSION=${VTKM_VERSION:-"a3b852"}
-    export VTKM_FILE=${VTKM_FILE:-"vtkm-${VTKM_VERSION}.tar.gz"}
-    export VTKM_BUILD_DIR=${VTKM_BUILD_DIR:-"vtkm-${VTKM_VERSION}"}
-    export VTKM_MD5_CHECKSUM="ce173342dd433223879415af5a2713f1"
-    export VTKM_SHA256_CHECKSUM="358c0ab5f18e674ca147de51a08480009643b88b87b2b203fed721a746ef227f"
+    export VTKM_VERSION=${VTKM_VERSION:-"v1.7.0"}
+    export VTKM_FILE=${VTKM_FILE:-"vtk-m-${VTKM_VERSION}.tar.gz"}
+    export VTKM_BUILD_DIR=${VTKM_BUILD_DIR:-"vtk-m-${VTKM_VERSION}"}
+    export VTKM_MD5_CHECKSUM="8dcaf4472d2f4729a3f5ab2381e4d818"
+    export VTKM_SHA256_CHECKSUM="a86667ac22057462fc14495363cfdcc486da125b366cb568ec23c86946439be4"
 }
 
 function bv_vtkm_print
@@ -96,7 +96,6 @@ function bv_vtkm_dry_run
 #
 #
 # *************************************************************************** #
-
 function apply_patch_1
 {
    patch -p0 << \EOF
@@ -113,7 +112,7 @@ diff -c ./vtkm/cont/arg/TransportTagTopologyFieldIn.h.orig ./vtkm/cont/arg/Trans
         throw vtkm::cont::ErrorBadValue("Input array to worklet invocation the wrong size.");
 + #endif
       }
-  
+
       return object.PrepareForInput(Device(), token);
 EOF
 
@@ -160,7 +159,7 @@ function build_vtkm
         warn "Unable to prepare VTKm build directory. Giving Up!"
         return 1
     fi
-    
+
     #
     # Apply patches
     #
@@ -184,7 +183,7 @@ function build_vtkm
     # Configure VTKM
     #
     info "Configuring VTKm . . ."
-    
+
     CMAKE_BIN="${CMAKE_INSTALL}/cmake"
 
     # Make a build directory for an out-of-source build.. Change the
@@ -201,10 +200,13 @@ function build_vtkm
     vopts=""
     vopts="${vopts} -DCMAKE_INSTALL_PREFIX:PATH=${VISITDIR}/vtkm/${VTKM_VERSION}/${VISITARCH}"
     vopts="${vopts} -DVTKm_ENABLE_TESTING:BOOL=OFF"
+    vopts="${vopts} -DVTKm_ENABLE_TESTING_LIBRARY:BOOL=ON"
     vopts="${vopts} -DVTKm_ENABLE_RENDERING:BOOL=ON"
     vopts="${vopts} -DVTKm_USE_64BIT_IDS:BOOL=OFF"
     vopts="${vopts} -DVTKm_USE_DOUBLE_PRECISION:BOOL=ON"
+    vopts="${vopts} -DVTKm_USE_DEFAULT_TYPES_FOR_VTK:BOOL=ON"
     vopts="${vopts} -DCMAKE_BUILD_TYPE:STRING=${VISIT_BUILD_MODE}"
+    vopts="${vopts} -DBUILD_SHARED_LIBS:BOOL=OFF"
     # Disable CUDA support for now since it requires using the CUDA compiler
     # to build all of VisIt, which we don't want to do.
     #if [[ -d $CUDA_HOME ]]; then
@@ -250,7 +252,7 @@ function build_vtkm
 function bv_vtkm_is_enabled
 {
     if [[ $DO_VTKM == "yes" ]]; then
-        return 1    
+        return 1
     fi
     return 0
 }
