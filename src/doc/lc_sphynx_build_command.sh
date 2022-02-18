6d3a64bd4e196739b2af20a51e1d469e5e276b8a
Eric Brugger <brugger1@llnl.gov>
2022-02-18 10:47:04 -0800

    Update python path in lc_sphynx_build_command.sh.

--- src/doc/lc_sphynx_build_command.sh
+++ src/doc/lc_sphynx_build_command.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/gapps/conduit/software/ascent/2020-11-07/toss_3_x86_64_ib/openmp/gnu/python-install/bin/python /usr/gapps/conduit/software/ascent/2020-11-07/toss_3_x86_64_ib/openmp/gnu/python-install/bin/sphinx-build -b html . _build -a
+/usr/gapps/conduit/software/ascent/0.8.0/toss_3_x86_64_ib/openmp/gnu/python-install/bin/python /usr/gapps/conduit/software/ascent/0.8.0/toss_3_x86_64_ib/openmp/gnu/python-install/bin/sphinx-build -b html . _build -a
