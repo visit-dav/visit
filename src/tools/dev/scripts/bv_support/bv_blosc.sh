1e7e6b649ed1361fa71307aa80ea5f60b5b89d91
JustinPrivitera <justinprivitera@gmail.com>
2022-03-10 19:26:10 -0800

    added blosc to modules.xml and began work on build script for it

---
+++ src/tools/dev/scripts/bv_support/bv_blosc.sh
@@ -0,0 +1,42 @@
+# Initialize any variables you may want to export
+function bv_blosc_initialize
+{
+    # help!
+    export DO_BLOSC="maybe"
+}
+
+# Enables the module
+function bv_blosc_enable
+{
+    DO_BLOSC="yes"
+}
+
+# Disables the module
+function bv_blosc_disable
+{
+    DO_BLOSC="no"
+}
+
+# Where to get the module, the version, etc...
+function bv_blosc_info
+{
+
+}
+
+# Ensure the module has been downloaded and extracted properly. Set and check variables here..
+function bv_blosc_ensure
+{
+
+}
+
+# What other modules does this module depend on. Example "adios" returns string "mxml"
+function bv_blosc_depends_on
+{
+
+}
+
+# build the module
+function bv_blosc_build
+{
+
+}
