a2442c8f41977d1f6f108f6d995074a9ece8e5ef
miller86 <miller86@llnl.gov>
2022-03-12 17:41:25 -0800

    upd CSV doc & make it tested

--- src/doc/data_into_visit/PlainTextFormat.rst
+++ src/doc/data_into_visit/PlainTextFormat.rst
@@ -1,22 +1,27 @@
 .. _Data_Into_VisIt_PT:
 
-The PlainText file format
-=========================
+The ``PlainText`` file format
+=============================
 
-Plain text files are text files with tables of data.
-These are standard CSV (Comma Separated Values) files.
-The file can start with an arbitrary number of lines that may be skipped.
-The files can be used to represent the following types of data:
+Standard CSV (Comma Separated Values) files are read using the ``PlainText`` reader.
 
-* A collection of curves, all defined on the same domain.
+Plain text files are text files with columns of data.
+A single space, comma or tab character separates (e.g. *delimits*) individual values on each line of the file.
+The ``PlainText`` reader automatically detects the separator character being used.
+The file can include an arbitrary number of lines at the *beginning* of the file to be skipped.
+Following any skipped lines, the file may include an optional *header* line holding the names associated with each column.
+Plain text files can be used to represent the following types of data:
+
+* A collection of curves, all defined on the same (explicit or implicit) domain.
 * Points in 2D or 3D with variables defined on the points.
-* A single variable defined on a 2D regular grid.
+* A single variable defined on a 2D, uniform grid.
 
-Defining curves with a PlainText file
--------------------------------------
+Defining curves with a ``PlainText`` file
+-----------------------------------------
 
 The first line can be an optional list of variable names.
 The remaining lines consist of rows, where each row represents one point in each of the curves.
+In this example, the values on each row are separated by commas.
 
 Here are the first 10 lines of an example of a file representing curves.
 
@@ -25,21 +30,46 @@ Here are the first 10 lines of an example of a file representing curves.
 
 Here is the Python script that created the file.
 
-.. literalinclude:: data_examples/plain_text.py
-   :lines: 1-14
+.. literalinclude:: ../../test/tests/databases/plaintext.py
+   :language: Python 
+   :start-after: # Begin curve data gen logic
+   :end-before: # End curve data gen logic
 
-Here are the Plain Text reader options used to read the data.
+Here are the ``PlainText`` reader options used to read the data.
 
 .. figure:: images/PlainTextOptions1.png
 
 If you specify the column for the X coordinates, then that column will be used for the domain for all the curves.
 If you don't specify an X coordinate, then it will use the row index for the domain for all the curves.
 
+Here is the resulting data plotted in VisIt_
+
+.. figure:: ../../../test/baseline/databases/plaintext/PlainText_Curves.png
+
+Defining curves using row index for X coordinate
+------------------------------------------------
+
+Here are the first 10 lines of an example of a file representing curves.
+
+.. literalinclude:: data_examples/curves.csv
+   :lines: 1-10
+
+Here is the Python script that created the file.
+
+.. literalinclude:: ../../test/tests/databases/plaintext.py
+   :language: Python 
+   :start-after: # Begin curve noX data gen logic
+   :end-before: # End curve noX data gen logic
+
+Here is the resulting data plotted in VisIt_
+
+.. figure:: ../../../test/baseline/databases/plaintext/PlainText_Curves_noX.png
+
 Defining 2D or 3D points with variables
 ---------------------------------------
 
 The first line can be an optional list of variable names.
-The remaining lines consist of rows, where each row represents the values for a single point.
+The remaining lines consist of rows, where each row represents the coordinates and variable values for a single point.
 
 Here are the first 10 lines of an example of a file representing 3D points.
 
@@ -48,38 +78,52 @@ Here are the first 10 lines of an example of a file representing 3D points.
 
 Here is the Python script that created the file.
 
-.. literalinclude:: data_examples/plain_text.py
-   :lines: 16-32
+.. literalinclude:: ../../test/tests/databases/plaintext.py
+   :language: Python 
+   :start-after: # Begin 3D points with velocity & temp variable data gen logic
+   :end-before: # End 3D points with velocity & temp variable data gen logic
 
-Here are the Plain Text reader options used to read the data.
+Here are the ``PlainText`` reader options used to read the data.
 
 .. figure:: images/PlainTextOptions2.png
 
 If you specify the columns for the X and Y coordinates, the points will be defined in 2D space.
 If you specify the columns for the X, Y and Z coordinates, the points will be defined in 3D space.
 
-Defining a single variable on a 2D regular grid
+Here is the resulting data plotted in VisIt_
+
+.. figure:: ../../../test/baseline/databases/plaintext/PlainText_Points.png
+
+Defining a single variable on a 2D uniform grid
 -----------------------------------------------
 
-The variable is interpreted as a node centered variable and the X and Y coordinates are the indexes into the rows and columns.
+The data is interpreted as a node centered variable on a uniform mesh where the row and column indices define the X and Y coordinates.
 The rows represent values along the X direction and the rows get stacked in the Y direction.
+Each row further *down* in the file gets stacked *up* one upon the other in the visualized result in VisIt_.
+This means that the row-by-row *downward* direction in the file is the same as the *upward* (positive Y) direction in the visualized result in VisIt_.
 
 The first line can be an optional list of variable names.
-The first variable name will be used for the name of the variable.
-Note that you will need to provide the same number of variable names as there are columns, even though only the first one is used.
+The first column name will be used for the name of the variable.
+Other column names are ignored but nonetheless required to proper reading. 
 The remaining lines consist of rows, where each row represents the values for a single Y coordinate.
 
 Here is an example of a file representing 3D points.
 
-.. literalinclude:: data_examples/regulargrid2d.csv
+.. literalinclude:: data_examples/array.csv
 
 Here is the Python script that created the file.
 
-.. literalinclude:: data_examples/plain_text.py
-   :lines: 34-51
+.. literalinclude:: ../../test/tests/databases/plaintext.py
+   :language: Python 
+   :start-after: # Begin 2D array gen logic
+   :end-before: # End 2D array gen logic
 
-Here are the Plain Text reader options used to read the data.
+Here are the ``PlainText`` reader options used to read the data.
 
 .. figure:: images/PlainTextOptions3.png
 
 The columns for the X, Y and Z coordinates are not used.
+
+Here is the resulting data plotted in VisIt_
+
+.. figure:: ../../../test/baseline/databases/plaintext/PlainText_2DArray.png
---
+++ src/test/tests/databases/plaintext.py
@@ -0,0 +1,232 @@
+# ----------------------------------------------------------------------------
+#  CLASSES: nightly
+#
+#  Test Case:  plaintext.py
+#
+#  Tests:      multiple curves from a CSV file
+#              2D and 3D points with variables in a CSV file
+#              An array of data as a 2D regular grid
+#              A .visit file listing a bunch of arrays forming a 3D regular grid
+#
+#  Notes:
+#    - Code snippets from this file are literalinclude'd in our docs.
+#    - Test image results produced by this test are also literalinclude'd.
+#    - Input .csv files used here are also literalinclude'd.
+#    - If you ever need to change the .csv files, you can regen them using
+#      this script with 'genonly' CL arg and vanilla python (it won't make any
+#      VisIt CLI calls).
+#
+#  Mark C. Miller, Fri Mar 11 14:33:30 PST 2022
+#  Based on original code by Eric Brugger in our documentation
+# ----------------------------------------------------------------------------
+import copy, math, os, sys
+
+#
+# Example of PlainText open options dict...
+#
+# {'Data layout': '1D Columns # Options are: 1D Columns, 2D Array',
+#  'Lines to skip at beginning of file': 0,
+#  'First row has variable names': 0,
+#  'Column for X coordinate (or -1 for none)': -1,
+#  'Column for Y coordinate (or -1 for none)': -1,
+#  'Column for Z coordinate (or -1 for none)': -1}
+#
+
+#
+# Use this function with deepcopy to ensure we always start from a
+# prestine default state for open options.
+#
+def GetDefaultOpenOptions():
+    if not hasattr(GetDefaultOpenOptions, 'defaultOpenOptions'):
+        GetDefaultOpenOptions.defaultOpenOptions = copy.deepcopy(GetDefaultFileOpenOptions("PlainText"))
+    return copy.deepcopy(GetDefaultOpenOptions.defaultOpenOptions)
+
+def SetOpenOptionsForCurveTest():
+    plainTextOpenOptions = GetDefaultOpenOptions()
+    plainTextOpenOptions['First row has variable names'] = 1
+    plainTextOpenOptions['Column for X coordinate (or -1 for none)'] = 0
+    SetDefaultFileOpenOptions("PlainText", plainTextOpenOptions)
+
+def SetOpenOptionsForCurveTestNoX():
+    plainTextOpenOptions = GetDefaultOpenOptions()
+    plainTextOpenOptions['First row has variable names'] = 1
+    SetDefaultFileOpenOptions("PlainText", plainTextOpenOptions)
+
+def SetOpenOptionsForPointsTest():
+    plainTextOpenOptions = GetDefaultOpenOptions()
+    plainTextOpenOptions['First row has variable names'] = 1
+    plainTextOpenOptions['Column for X coordinate (or -1 for none)'] = 0
+    plainTextOpenOptions['Column for Y coordinate (or -1 for none)'] = 1
+    plainTextOpenOptions['Column for Z coordinate (or -1 for none)'] = 2
+    SetDefaultFileOpenOptions("PlainText", plainTextOpenOptions)
+
+def SetOpenOptionsForArrayTest():
+    plainTextOpenOptions = GetDefaultOpenOptions()
+    plainTextOpenOptions['First row has variable names'] = 1
+    plainTextOpenOptions['Data layout'] = '2D Array'
+    SetDefaultFileOpenOptions("PlainText", plainTextOpenOptions)
+
+def CreateCurvesDataFile(filename):
+
+    # Begin curve data gen logic
+    with open(filename, "wt") as f:
+        # create header
+        f.write("angle,sine,cosine\n")
+        npts = 73
+        for i in range(npts):
+            angle_deg = float(i) * (360. / float(npts-1))
+            angle_rad = angle_deg * (3.1415926535 / 180.)
+            sine = math.sin(angle_rad)
+            cosine = math.cos(angle_rad)
+            # write abscissa (x value) and ordinates (y-value(s))
+            f.write("%g,%g,%g\n" % (angle_deg, sine, cosine))
+    # End curve data gen logic
+
+def CreateCurvesDataFileWithNoXCoordinates(filename):
+
+    # Begin curve noX data gen logic
+    with open(filename, "wt") as f:
+        # create header
+        f.write("inverse sqrt quadratic\n")
+        npts = 100 
+        for i in range(npts):
+            inv = float(100) / (float(i)+1)
+            sqr = 10 * math.sqrt(i)
+            quad = float(i*i) / float(100)
+            f.write("%g %g %g\n" % (inv, sqr, quad))
+    # End curve noX data gen logic
+
+def Create3DPointsWithVariablesDataFile(filename):
+
+    # Begin 3D points with velocity & temp variable data gen logic
+    with open(filename, "wt") as f:
+        # write header
+        f.write("x y z velx vely velz temp\n")
+        n = 100
+        for i in range(n):
+            t = float(i) / float(n-1)
+            angle = t * (math.pi * 2.) * 5.
+            r = t * 10.
+            x = r * math.cos(angle)
+            y = r * math.sin(angle)
+            z = t * 10.
+            vx = math.sqrt(x*x + y*y)
+            vy = math.sqrt(y*y + z*z)
+            vz = math.sqrt(x*x + z*z)
+            temp = math.sqrt((t-0.5)*(t-0.5))
+            # write point and value(s)
+            f.write("%g %g %g %g %g %g %g\n" % (x,y,z,vx,vy,vz,temp))
+    # End 3D points with velocity & temp variable data gen logic
+
+def Create2DArrayDataFile(filename):
+
+    # Begin 2D array gen logic
+    with open(filename, "wt") as f:
+        # Only the first column name matters.
+        # The others are required but otherwise ignored.
+        f.write("density c2 c3 c4 c5 c6 c7 c8\n")
+        nx = 8
+        ny = 10
+        for iy in range(ny):
+            y = float(iy)
+            for ix in range(nx):
+                x = float(ix)
+                #dist = math.sqrt((x - 3.5) * (x - 3.5) + (y - 4.5) * (y - 4.5))
+                dist = math.sqrt(x*x + y*y)
+                if (ix < nx - 1):
+                   f.write("%g " % dist)
+                else:
+                   f.write("%g\n" % dist)
+    # End 2D array gen logic
+
+def TestCSVCurves(filename):
+    TestSection("CSV data as Curves")
+
+    CreateCurvesDataFile(filename)
+    SetOpenOptionsForCurveTest()
+
+    OpenDatabase(filename)
+    AddPlot("Curve","sine")
+    AddPlot("Curve","cosine")
+    DrawPlots()
+    ResetView()
+    Test("PlainText_Curves")
+    DeleteAllPlots()
+    CloseDatabase(filename)
+    os.unlink(filename)
+
+def TestCSVCurvesNoX(filename):
+    TestSection("CSV data as Curves inferred X coordinate")
+
+    CreateCurvesDataFileWithNoXCoordinates(filename)
+    SetOpenOptionsForCurveTestNoX()
+
+    OpenDatabase(filename)
+    AddPlot("Curve","inverse")
+    AddPlot("Curve","sqrt")
+    AddPlot("Curve","quadratic")
+    DrawPlots()
+    ResetView()
+    Test("PlainText_Curves_noX")
+    DeleteAllPlots()
+    CloseDatabase(filename)
+    os.unlink(filename)
+
+def TestCSV3DPointsAndVariables(filename):
+    TestSection("CSV data as 3D points with variable(s)")
+
+    Create3DPointsWithVariablesDataFile(filename)
+    SetOpenOptionsForPointsTest()
+
+    OpenDatabase(filename)
+    DefineVectorExpression("vel", "{velx,vely,velz}")
+    AddPlot("Pseudocolor", "temp")
+    AddPlot("Vector","vel")
+    DrawPlots()
+    ResetView()
+    Test("PlainText_Points")
+    DeleteAllPlots()
+    CloseDatabase(filename)
+    os.unlink(filename)
+
+def TestCSV2DArray(filename):
+    TestSection("CSV data as 2D array on uniform grid")
+
+    Create2DArrayDataFile(filename)
+    SetOpenOptionsForArrayTest()
+
+    OpenDatabase(filename)
+    AddPlot("Pseudocolor", "density")
+    DrawPlots()
+    ResetView()
+    Test("PlainText_2DArray")
+    DeleteAllPlots()
+    CloseDatabase(filename)
+    os.unlink(filename)
+
+def main():
+
+    if 'genonly' in sys.argv:
+        CreateCurvesDataFile('curves.csv')
+        CreateCurvesDataFileWithNoXCoordinates('curves_nox.csv')
+        Create3DPointsWithVariablesDataFile('points.csv')
+        Create2DArrayDataFile('array.csv')
+        sys.exit(0)
+
+    #
+    # Idiom here...we can't manipulate open options until *after*
+    # mdserver is running
+    #
+    OpenMDServer("localhost")
+
+    TestCSVCurves("curves.csv")
+
+    TestCSVCurvesNoX("curves_nox.csv")
+
+    TestCSV3DPointsAndVariables("points.csv")
+
+    TestCSV2DArray("array.csv")
+
+    Exit()
+
+main()
