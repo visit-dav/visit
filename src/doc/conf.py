ff512e342133940d9d2744804510ebdb6f046385
miller86 <miller86@llnl.gov>
2022-03-12 22:03:15 -0800

    undo change

--- src/doc/conf.py
+++ src/doc/conf.py
@@ -6,8 +6,8 @@
 
 # -- Path setup --------------------------------------------------------------
 
-#def setup(app):
-#    app.add_css_file('custom.css')
+def setup(app):
+    app.add_css_file('custom.css')
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
