626a2c7799f79eb4b4c9e9d976ccdba4f3218776
Justin Privitera <justin@rztopaz221.llnl.gov>
2022-03-14 18:25:34 -0700

    upgraded cmake version, updated instructions in comments, added blank blosc functions to fill in later

--- src/tools/dev/scripts/bv_support/bv_blosc.sh
+++ src/tools/dev/scripts/bv_support/bv_blosc.sh
@@ -20,23 +20,53 @@ function bv_blosc_disable
 # Where to get the module, the version, etc...
 function bv_blosc_info
 {
-
+    echo ""
 }
 
 # Ensure the module has been downloaded and extracted properly. Set and check variables here..
 function bv_blosc_ensure
 {
-
+    echo ""
 }
 
 # What other modules does this module depend on. Example "adios" returns string "mxml"
 function bv_blosc_depends_on
 {
-
+    echo ""
 }
 
 # build the module
 function bv_blosc_build
 {
+    echo ""
+}
 
+function bv_blosc_print
+{
+    printf ""
+}
+
+function bv_blosc_print_usage
+{
+    printf ""
+}
+
+function bv_blosc_host_profile
+{
+    printf ""
+}
+
+function bv_blosc_dry_run
+{
+    printf ""
+}
+
+function bv_blosc_is_installed
+{
+    printf ""
+}
+
+function bv_blosc_is_enabled
+{
+    printf ""
 }
