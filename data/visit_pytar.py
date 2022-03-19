a70f816f0279152cdb09431fa6e5a277598fe913
miller86 <miller86@llnl.gov>
2022-03-19 16:36:23 -0700

    simplify; add X option

--- data/visit_pytar.py
+++ data/visit_pytar.py
@@ -11,14 +11,18 @@ def usage(errmsg=""):
     print(\
 """
 %s
-Usage: Simple xz archiver using Python 3 tarfile module:
+Usage: Basic archiver with xz compression using Python 3 tarfile
+    module for VisIt test data archives:
 
-    Create: visit_pytar c <archive> <input dir>
-    List:   visit_pytar t <archive>
-    Expand: visit_pytar x <archive>
+    Create: visit_pytar c <batch>_test_data.tar.xz
+    List:   visit_pytar t <batch>_test_data.tar.xz
+    Expand: visit_pytar x|X <batch>_test_data.tar.xz (X=expand into)
 
-    <archive> is a file name of the form <dbformat>_test_data.tar.xz
-    <input dir> is a directory name of the form <dbformat>_test_data
+    Where <batch> is an arbitrary string naming a batch of related
+    data files. For Create, (c) option, a matching directory name
+    of the form <batch>_test_data must already exist. Expand with
+    lowercase x fails if <batch>_test_data already exists. Use X
+    to override.
 
     This tooling is provided only as a convenience.
     
@@ -37,17 +41,25 @@ elif 'c' in sys.argv[1]: # Create
         usage('"%s" must be of the form <dbformat>_test_data.tar.xz'%sys.argv[2])
     if os.path.exists(sys.argv[2]):
         usage('%s must not already exist'%sys.argv[2])
-    if not os.path.isdir(sys.argv[3]): 
-        usage('"%s" must be a directory'%sys.argv[3])
-    if not sys.argv[3].endswith('_test_data'):
-        usage('"%s" must be of the form <dbformat>_test_data'%sys.argv[3])
+    if not os.path.isdir(sys.argv[2][:-7]): 
+        usage('"%s" must be a pre-existing directory'%sys.argv[2][:-7])
     tf = tarfile.open(sys.argv[2], 'w:xz')
-    tf.add(sys.argv[3])
+    tf.add(sys.argv[2][:-7])
+    tf.close()
 elif 't' in sys.argv[1]: # List
+    if not os.path.exists(sys.argv[2]):
+        usage('%s does not exist'%sys.argv[2])
     tf = tarfile.open(sys.argv[2], 'r')
     tf.list()
-elif 'x' in sys.argv[1]: # Expand
+    tf.close()
+elif 'x' in sys.argv[1].lower(): # Expand
+    if not os.path.exists(sys.argv[2]):
+        usage('%s does not exist'%sys.argv[2])
+    if 'x' in sys.argv[1] and os.path.isdir(sys.argv[2][:-7]):
+        usage('%s already exists. Use uppercase X to expand into'%sys.argv[2][:-7])
     tf = tarfile.open(sys.argv[2], 'r')
     tf.extractall()
-tf.close()
+    tf.close()
+else:
+    usage('"%s" uncrecognized option'%sys.argv[1])
 sys.exit(0)
