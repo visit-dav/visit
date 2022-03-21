8b3e995b9a880b95edb2aca38879218ee60e08e6
miller86 <miller86@llnl.gov>
2022-03-21 10:42:11 -0700

    work with any tar cmd

--- data/visit_pytar.py
+++ data/visit_pytar.py
@@ -19,15 +19,17 @@ def usage(errmsg=""):
 Usage: Basic archiver with xz compression using Python 3 tarfile
     module for VisIt test data archives:
 
-    Create: visit_pytar c <batch>_test_data.tar.xz
-    List:   visit_pytar t <batch>_test_data.tar.xz
+    Create: visit_pytar c|C <batch>_test_data.tar.xz
+    List:   visit_pytar t   <batch>_test_data.tar.xz
     Expand: visit_pytar x|X <batch>_test_data.tar.xz
 
     Where <batch> is an arbitrary string naming a batch of related
     data files. For Create, (c) option, a matching directory name
-    of the form <batch>_test_data must already exist. Lowercase x
-    Expand fails if <batch>_test_data already exists. Uppercase X
-    Expand ignores this check.
+    of the form <batch>_test_data must already exist. Lowercase c
+    Create fails if <batc>_test_data.tar.xz already exists.
+    Uppercase C ignores this check. Lowercase x Expand fails if
+    <batch>_test_data already exists. Uppercase X Expand ignores
+    this check.
 
     This tooling is provided only as a convenience.
     
@@ -41,22 +43,25 @@ if len(sys.argv) == 1:
     usage()
 elif sys.version_info.major != 3:
     usage('Python version 3 is required')
-elif 'c' in sys.argv[1]: # Create
+
+elif 'c' in sys.argv[1].lower(): # Create
     if not sys.argv[2].endswith('_test_data.tar.xz'):
         usage('"%s" must be of the form <dbformat>_test_data.tar.xz'%sys.argv[2])
-    if os.path.exists(sys.argv[2]):
+    if 'c' in sys.argv[1] and os.path.exists(sys.argv[2]):
         usage('%s must not already exist'%sys.argv[2])
     if not os.path.isdir(sys.argv[2][:-7]): 
         usage('"%s" must be a pre-existing directory'%sys.argv[2][:-7])
     tf = tarfile.open(sys.argv[2], 'w:xz', preset=9)
     tf.add(sys.argv[2][:-7])
     tf.close()
+
 elif 't' in sys.argv[1]: # List
     if not os.path.exists(sys.argv[2]):
         usage('%s does not exist'%sys.argv[2])
     tf = tarfile.open(sys.argv[2], 'r')
     tf.list()
     tf.close()
+
 elif 'x' in sys.argv[1].lower(): # Expand
     if not os.path.exists(sys.argv[2]):
         usage('%s does not exist'%sys.argv[2])
@@ -65,6 +70,8 @@ elif 'x' in sys.argv[1].lower(): # Expand
     tf = tarfile.open(sys.argv[2], 'r')
     tf.extractall()
     tf.close()
+
 else:
     usage('"%s" uncrecognized option'%sys.argv[1])
+
 sys.exit(0)
