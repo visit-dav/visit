608e4de884aab6b296ac3d5d2d5feab1f2a873b9
Justin Privitera <privitera1@llnl.gov>
2022-04-11 13:07:36 -0700

    adios2 attempt, but doesn't work

--- src/tools/dev/scripts/bv_support/bv_adios2.sh
+++ src/tools/dev/scripts/bv_support/bv_adios2.sh
@@ -235,6 +235,15 @@ function build_adios2
         cfg_opts="${cfg_opts} -DCMAKE_CXX_FLAGS:STRING=\"${CXX_OPT_FLAGS}\""
         cfg_opts="${cfg_opts} -DADIOS2_USE_SST:BOOL=ON"
 
+        # Use Blosc?
+        if [[ "$DO_BLOSC" == "yes" ]] ; then
+            BLOSC_INCLUDE_DIR="${VISITDIR}/blosc/${BLOSC_VERSION}/${VISITARCH}/include"
+            BLOSC_LIBRARY="${VISITDIR}/blosc/${BLOSC_VERSION}/${VISITARCH}/lib64"
+            cfg_opts="${cfg_opts} -DADIOS2_USE_Blosc:BOOL=ON"
+            cfg_opts="${cfg_opts} -DBLOSC_INCLUDE_DIR=${BLOSC_INCLUDE_DIR}"
+            cfg_opts="${cfg_opts} -DBLOSC_LIBRARY=${BLOSC_LIBRARY}"
+        fi
+
         if [[ "$bt" == "ser" ]]; then
             cfg_opts="${cfg_opts} -DADIOS2_USE_MPI:BOOL=OFF"
             cfg_opts="${cfg_opts} -DCMAKE_C_COMPILER:STRING=${C_COMPILER}"
