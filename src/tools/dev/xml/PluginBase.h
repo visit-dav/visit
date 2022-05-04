bb61f2ef2642019ca3e4ebe23d7377012e6251e9
Kathleen Biagas <biagas2@llnl.gov>
2022-05-04 15:33:37 -0700

    Improvements to cmake gen code (#17633)
    * Consolidate plot and operator CMake code gen methods.
    
    Reduces duplication of code.
    Regenerate CMakeLists.txt for plots and operators.
    The only significant change is order of libraries for TARGET_LINK_LIBRARIES calls.
    
    * Regen plots' CMakeLists.txt. Minor whitespace change.
    
    * Consolidate engine-target code into one method.
    
    Regenerate databases CMakeLists.txt (for those that can be regened).
    
    * regenerate plots CMakeLists.txt.
    
    * Regenerate operator CMakeLists.txt
    
    * Update regenerateatts.py
    
    Fix for python 3.
    Add optional tool argument, xml2atts is default if not provided.
    Support using on Windows from bash.
    
    * Add support for component-specific CXXFLAGS.
    
    Update MOAB .xml file to use component-specific CXXFLAGS.
    Regenerate MOAB CMakeLists.txt.
    
    * Add ability for escaped quotes to be retained when parsing.
    
    Update uintah.xml to use escaped quotes.
    Regenerate uintah's CMakeList.txt.
    
    * Add component-specific LDFLAGS.
    
    * Fix target link directories white space.
    
    * Fix adios2.xml for regen of CMakeLists.txt
    
    * Add support for component-specific DEFINES
    
    * running gen_info_all picked up some minor info file changes.

--- src/tools/dev/xml/PluginBase.h
+++ src/tools/dev/xml/PluginBase.h
@@ -71,6 +71,9 @@
 //    For plugins that have custom code that isn't currently supported by
 //    generation tools.
 //
+//    Kathleen Biagas, Tue May 3, 2022
+//    Added support for component-specific CXXFLAGS, LDFLAGS and DEFINES.
+//
 // ****************************************************************************
 
 class PluginBase
@@ -123,14 +126,23 @@ public:
     std::vector<QString> wmfiles;    // mdserver files for windows
     bool custommlibs;
     std::vector<QString> mlibs;      // mdserver libs
+    std::vector<QString> mcxxflags;  // mdserver cxxflags
+    std::vector<QString> mldflags;   // mdserver ldflags
+    std::vector<QString> mdefs;      // mdserver defines
     bool customefiles;
     std::vector<QString> efiles;     // engine files
     bool customwefiles;
     std::vector<QString> wefiles;    // engine files for windows
     bool customelibsSer;
-    std::vector<QString> elibsSer;      // engine libs
+    std::vector<QString> elibsSer;      // serial engine libs
+    std::vector<QString> ecxxflagsSer;  // serial engine cxxflags
+    std::vector<QString> eldflagsSer;   // serial engine ldflags
+    std::vector<QString> edefsSer;      // serial engine defines
     bool customelibsPar;
-    std::vector<QString> elibsPar;      // engine libs
+    std::vector<QString> elibsPar;      // parallel engine libs
+    std::vector<QString> ecxxflagsPar;  // parallel engine cxxflags
+    std::vector<QString> eldflagsPar;   // parallel engine ldflags
+    std::vector<QString> edefsPar;      // parallel engine defines
     bool customwfiles;
     std::vector<QString> wfiles;     // widgets
     bool customvwfiles;
@@ -181,14 +193,23 @@ public:
           wmfiles(),
           custommlibs(false),
           mlibs(),
+          mcxxflags(),
+          mldflags(),
+          mdefs(),
           customefiles(false),
           efiles(),
           customwefiles(false),
           wefiles(),
           customelibsSer(false),
           elibsSer(),
+          ecxxflagsSer(),
+          eldflagsSer(),
+          edefsSer(),
           customelibsPar(false),
           elibsPar(),
+          ecxxflagsPar(),
+          eldflagsPar(),
+          edefsPar(),
           customwfiles(false),
           wfiles(),
           customvwfiles(false),
