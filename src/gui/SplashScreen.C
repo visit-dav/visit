7f3ae97effbcdf20b632b19db017ca84b105178e
Eric Brugger <brugger1@llnl.gov>
2022-05-27 10:04:05 -0700

    Update for release 3.3.

--- src/gui/SplashScreen.C
+++ src/gui/SplashScreen.C
@@ -253,6 +253,9 @@
 //    Eric Brugger, Wed Dec 15 09:48:55 PST 2021
 //    Changed the date on the splash screen to January 2022.
 //
+//    Eric Brugger, Thu May 26 15:47:49 PDT 2022
+//    Changed the date on the splash screen to June 2022.
+//
 // ****************************************************************************
 
 SplashScreen::SplashScreen(bool cyclePictures) : QFrame(0, Qt::SplashScreen)
@@ -373,7 +376,7 @@ SplashScreen::SplashScreen(bool cyclePictures) : QFrame(0, Qt::SplashScreen)
            << tr("October")
            << tr("November")
            << tr("December");
-    int currentMonth = 1;
+    int currentMonth = 6;
     lLayout->addWidget(new QLabel(versionText, this));
     lLayout->addWidget(new QLabel(months[currentMonth-1] + " 2022", this));
 
