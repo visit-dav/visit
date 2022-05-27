b2dea8013e3a16a3d8da960cbfe4513c2e608856
Justin Privitera <privitera1@llnl.gov>
2022-05-27 16:38:46 -0700

    made lots of changes to bp tests and added some new baselines

--- src/test/tests/databases/blueprint.py
+++ src/test/tests/databases/blueprint.py
@@ -84,22 +84,31 @@ braid_3d_meshes = ["points", "uniform", "rect", "struct", "tets","hexs"]
 braid_2d_meshes_0_8_2 = ["points", "uniform", "rect", "struct", "tris","quads", "points_implicit"]
 braid_3d_meshes_0_8_2 = ["points", "uniform", "rect", "struct", "tets","hexs", "points_implicit"]
 
-mfem_ex9_examples   = ["periodic_cube","star_q3","periodic_hexagon"]
+class mfem_ex9_example:
+    def __init__(self, name, dim):
+        self.name = name
+        self.dim = dim
+
+mfem_ex9_examples = []
+mfem_ex9_examples.append(mfem_ex9_example("periodic_cube", "3d"))
+mfem_ex9_examples.append(mfem_ex9_example("star_q3", "3d"))
+mfem_ex9_examples.append(mfem_ex9_example("periodic_hexagon", "2d"))
 mfem_ex9_protocols = ["json","conduit_bin","conduit_json","hdf5"]
 
 class devilray_mfem_example:
-    def __init__(self, name, number, pseudo_fields, vector_fields):
+    def __init__(self, name, number, pseudo_fields, vector_fields, dim):
         self.name = name
         self.number = number
         self.pseudo_fields = pseudo_fields
         self.vector_fields = vector_fields
+        self.dim = dim
 
 devilray_mfem_examples = []
-devilray_mfem_examples.append(devilray_mfem_example("esher", "000000", ["mesh_nodes_magnitude"], []))
-devilray_mfem_examples.append(devilray_mfem_example("laghos_tg.cycle", "000350", ["mesh_nodes_magnitude", "density", "specific_internal_energy", "velocity_magnitude"], ["velocity"]))
-devilray_mfem_examples.append(devilray_mfem_example("taylor_green.cycle", "001860", ["mesh_nodes_magnitude", "density", "specific_internal_energy", "velocity_magnitude"], ["velocity"]))
-devilray_mfem_examples.append(devilray_mfem_example("tri_beam", "000000", ["mesh_nodes_magnitude"], ["mesh_nodes"]))
-devilray_mfem_examples.append(devilray_mfem_example("warbly_cube", "000000", ["mesh_nodes_magnitude"], []))
+devilray_mfem_examples.append(devilray_mfem_example("esher", "000000", ["mesh_nodes_magnitude"], [], "3d"))
+devilray_mfem_examples.append(devilray_mfem_example("laghos_tg.cycle", "000350", ["mesh_nodes_magnitude", "density", "specific_internal_energy", "velocity_magnitude"], ["velocity"], "3d"))
+devilray_mfem_examples.append(devilray_mfem_example("taylor_green.cycle", "001860", ["mesh_nodes_magnitude", "density", "specific_internal_energy", "velocity_magnitude"], ["velocity"], "3d"))
+devilray_mfem_examples.append(devilray_mfem_example("tri_beam", "000000", ["mesh_nodes_magnitude"], ["mesh_nodes"], "2d"))
+devilray_mfem_examples.append(devilray_mfem_example("warbly_cube", "000000", ["mesh_nodes_magnitude"], [], "3d"))
 
 def full_mesh_name(mesh_name):
     return mesh_name + "_mesh"
@@ -157,70 +166,82 @@ def set_3d_poly_view():
 
 
 def set_test_view(tag_name):
-    if "3d" in tag_name and "poly" in tag_name:
-        set_3d_poly_view()
-    elif "3d" in tag_name:
-        set_3d_view()
+    if "3d" in tag_name:
+        if "poly" in tag_name:
+            set_3d_poly_view()
+        else:
+            set_3d_view()
     else:
         ResetView()
 
 def test(mesh_name,tag_name):
     #
     AddPlot("Mesh","%s" % full_mesh_name(mesh_name))
-    DrawPlots()
     set_test_view(tag_name)
+    DrawPlots()
     Test(tag_name + "_" +  mesh_name + "_mesh")
     DeleteAllPlots()
+    ResetView()
     #
     AddPlot("Pseudocolor", full_var_name(mesh_name,"braid"))
-    DrawPlots()
     set_test_view(tag_name)
+    DrawPlots()
     Test(tag_name + "_" +  mesh_name + "_braid")
-    #
     DeleteAllPlots()
+    ResetView()
+    #
     AddPlot("Pseudocolor", full_var_name(mesh_name,"radial"))
     set_test_view(tag_name)
     DrawPlots()
     Test(tag_name + "_" +  mesh_name + "_radial")
     DeleteAllPlots()
+    ResetView()
 
 def test_poly(tag_name):
     #
     AddPlot("Mesh", "mesh_topo")
-    DrawPlots()
     set_test_view(tag_name)
+    DrawPlots()
     Test(tag_name + "_mesh_topo_mesh")
     DeleteAllPlots()
+    ResetView()
     #
     AddPlot("Pseudocolor", "mesh_topo/level")
-    DrawPlots()
     set_test_view(tag_name)
+    DrawPlots()
     Test(tag_name + "_mesh_topo_level")
     DeleteAllPlots()
+    ResetView()
 
 def test_mfem(tag_name, example_name, protocol):
-    readOptions=GetDefaultFileOpenOptions("Blueprint")
+    readOptions = GetDefaultFileOpenOptions("Blueprint")
     readOptions["MFEM LOR Setting"] = "Legacy LOR"
     SetDefaultFileOpenOptions("Blueprint", readOptions)
 
-    dbfile =mfem_test_file(example_name,protocol)
+    dbfile = mfem_test_file(example_name,protocol)
     OpenDatabase(dbfile)
     #
     AddPlot("Pseudocolor", "mesh_main/solution")
+    set_test_view(tag_name)
     DrawPlots()
     Test(tag_name + "_" +  example_name + "_" + protocol + "_sol")
     #
     DeleteAllPlots()
+    ResetView()
     AddPlot("Pseudocolor", "mesh_main/element_coloring")
+    set_test_view(tag_name)
     DrawPlots()
     Test(tag_name + "_" +  example_name + "_" + protocol + "_ele_coloring")
     #
     DeleteAllPlots()
+    ResetView()
     AddPlot("Pseudocolor", "mesh_main/element_attribute")
+    set_test_view(tag_name)
     DrawPlots()
     Test(tag_name + "_" +  example_name + "_" + protocol + "_ele_att")
     #
     DeleteAllPlots()
+    ResetView()
 
     CloseDatabase(dbfile)
 
@@ -229,14 +250,16 @@ def test_mfem(tag_name, example_name, protocol):
     SetDefaultFileOpenOptions("Blueprint", readOptions)
 
 def test_mfem_lor_mesh(tag_name, example_name, protocol, devilray = False, number = "000000"):
-    readOptions = GetDefaultFileOpenOptions("Blueprint")
-    readOptions["MFEM LOR Setting"] = "MFEM LOR"
-    SetDefaultFileOpenOptions("Blueprint", readOptions)
-
+    dbfile = ""
     if (devilray):
         dbfile = devilray_mfem_test_file(example_name, number)
     else:
         dbfile = mfem_test_file(example_name, protocol)
+
+    # get default options
+    readOptions = GetDefaultFileOpenOptions("Blueprint")
+    readOptions["MFEM LOR Setting"] = "MFEM LOR"
+    SetDefaultFileOpenOptions("Blueprint", readOptions)
     OpenDatabase(dbfile)
 
     # we want to test a picture of a wireframe
@@ -245,15 +268,17 @@ def test_mfem_lor_mesh(tag_name, example_name, protocol, devilray = False, numbe
     SubsetAtts = SubsetAttributes()
     SubsetAtts.wireframe = 1
     SetPlotOptions(SubsetAtts)
+    set_test_view(tag_name)
     DrawPlots()
-
     Test(tag_name + "_" +  example_name + "_" + protocol + "_lor")
-
     DeleteAllPlots()
-
+    ResetView()
     CloseDatabase(dbfile)
 
+    ##############################
+
     # examine legacy
+    readOptions = GetDefaultFileOpenOptions("Blueprint")
     readOptions["MFEM LOR Setting"] = "Legacy LOR"
     SetDefaultFileOpenOptions("Blueprint", readOptions)
     OpenDatabase(dbfile)
@@ -263,15 +288,15 @@ def test_mfem_lor_mesh(tag_name, example_name, protocol, devilray = False, numbe
     SubsetAtts = SubsetAttributes()
     SubsetAtts.wireframe = 1
     SetPlotOptions(SubsetAtts)
+    set_test_view(tag_name)
     DrawPlots()
-
     Test(tag_name + "_" +  example_name + "_" + protocol + "_legacy_lor")
-
     DeleteAllPlots()
-
+    ResetView()
     CloseDatabase(dbfile)
 
     # restore default
+    readOptions = GetDefaultFileOpenOptions("Blueprint")
     readOptions["MFEM LOR Setting"] = "MFEM LOR"
     SetDefaultFileOpenOptions("Blueprint", readOptions)
 
@@ -289,9 +314,11 @@ def test_mfem_lor_field(tag_name, name, number, pseudocolor_fields, vector_field
         MultiresControlAtts = MultiresControlAttributes()
         MultiresControlAtts.resolution = 3
         SetOperatorOptions(MultiresControlAtts, 0, 1)
+        set_test_view(tag_name)
         DrawPlots()
         Test(tag_name + "_" + name + "_pseudocolor_" + field + "_lor")
         DeleteAllPlots()
+        ResetView()
 
     for field in vector_fields:
         AddPlot("Vector", "mesh_main/" + field, 1, 1)  
@@ -301,8 +328,10 @@ def test_mfem_lor_field(tag_name, name, number, pseudocolor_fields, vector_field
         MultiresControlAtts.resolution = 3
         SetOperatorOptions(MultiresControlAtts, 0, 1)
         DrawPlots()
+        set_test_view(tag_name)
         Test(tag_name + "_" + name + "_vector_" + field + "_lor")
         DeleteAllPlots()
+        ResetView()
 
     CloseDatabase(dbfile)
 
@@ -310,6 +339,7 @@ def test_venn(tag_name, venn_db_file):
     TestSection("Blueprint Matset Example Tests: {0} ".format(tag_name))
     OpenDatabase(venn_db_file)
     AddPlot("Pseudocolor", "mesh_topo/mat_check")
+    set_test_view(tag_name)
     DrawPlots()
     Test(tag_name + "_mat_check")
     # value check
@@ -322,6 +352,7 @@ def test_venn(tag_name, venn_db_file):
     TestValueEQ("testeq_t_mat_check_max",int(res["max"]),4320)
     TestValueEQ("testeq_mat_check_min",int(res["min"]),1)
     DeleteAllPlots()
+    ResetView()
 
 
     TestSection("Blueprint Matset Example Tests: {0} Matvf Exprs".format(tag_name))
@@ -335,6 +366,7 @@ def test_venn(tag_name, venn_db_file):
     for ename,edef in vf_exprs.items():
         DefineScalarExpression(ename,edef);
         AddPlot("Pseudocolor", ename)
+        set_test_view(tag_name)
         DrawPlots()
         Test(tag_name + "_" + ename)
         # value check
@@ -347,6 +379,7 @@ def test_venn(tag_name, venn_db_file):
         TestValueEQ("testeq_" + ename + "_max",res["max"],1.0)
         TestValueEQ("testeq_" + ename + "_min",res["min"],0.0)
         DeleteAllPlots()
+        ResetView()
 
     TestSection("Blueprint Matset Example Tests: {0} Val4mat Exprs".format(tag_name))
     
@@ -364,6 +397,7 @@ def test_venn(tag_name, venn_db_file):
     for ename,edef in v4m_exprs.items():
         DefineScalarExpression(ename,edef);
         AddPlot("Pseudocolor", ename)
+        set_test_view(tag_name)
         DrawPlots()
         Test(tag_name + "_" + ename)
         # value check
@@ -376,6 +410,7 @@ def test_venn(tag_name, venn_db_file):
         TestValueEQ("testeq_" + ename + "_max",int(res["max"]),v4m_test_vals[ename])
         TestValueEQ("testeq_" + ename + "_min",int(res["min"]),0)
         DeleteAllPlots()
+        ResetView()
     
 
     CloseDatabase(venn_db_file)
@@ -388,128 +423,132 @@ def test_paren_vars():
     #
     OpenDatabase(uniform_root)
     AddPlot("Pseudocolor", "mesh_topo/rho.Y_lp_CH2O_rp_")
+    set_test_view("3d")
     DrawPlots()
     Test("paren_vars_00")
 
     DeleteAllPlots()
+    ResetView()
     CloseDatabase(uniform_root)
 
 
-TestSection("2D Example JSON Mesh Files")
-OpenDatabase(braid_2d_json_root)
-for mesh_name in braid_2d_meshes:
-    test(mesh_name,"blueprint_2d_json")
-CloseDatabase(braid_2d_json_root)
+# TestSection("2D Example JSON Mesh Files")
+# OpenDatabase(braid_2d_json_root)
+# for mesh_name in braid_2d_meshes:
+#     test(mesh_name,"blueprint_2d_json")
+# CloseDatabase(braid_2d_json_root)
 
-TestSection("2D Example HDF5 Mesh Files")
-OpenDatabase(braid_2d_hdf5_root)
-for mesh_name in braid_2d_meshes:
-    test(mesh_name,"blueprint_2d_hdf5")
-CloseDatabase(braid_2d_hdf5_root)
+# TestSection("2D Example HDF5 Mesh Files")
+# OpenDatabase(braid_2d_hdf5_root)
+# for mesh_name in braid_2d_meshes:
+#     test(mesh_name,"blueprint_2d_hdf5")
+# CloseDatabase(braid_2d_hdf5_root)
 
-TestSection("2D Example Sidre HDF5 Mesh Files")
-OpenDatabase(braid_2d_sidre_root)
-for mesh_name in braid_2d_meshes:
-    test(mesh_name,"blueprint_2d_sidre_hdf5")
-CloseDatabase(braid_2d_sidre_root)
+# TestSection("2D Example Sidre HDF5 Mesh Files")
+# OpenDatabase(braid_2d_sidre_root)
+# for mesh_name in braid_2d_meshes:
+#     test(mesh_name,"blueprint_2d_sidre_hdf5")
+# CloseDatabase(braid_2d_sidre_root)
 
-TestSection("3D Example JSON Mesh Files")
-OpenDatabase(braid_3d_json_root)
-for mesh_name in braid_3d_meshes:
-    test(mesh_name,"blueprint_3d_json")
-CloseDatabase(braid_3d_json_root)
+# TestSection("3D Example JSON Mesh Files")
+# OpenDatabase(braid_3d_json_root)
+# for mesh_name in braid_3d_meshes:
+#     test(mesh_name,"blueprint_3d_json")
+# CloseDatabase(braid_3d_json_root)
 
-TestSection("3D Example HDF5 Mesh Files")
-OpenDatabase(braid_3d_hdf5_root)
-for mesh_name in braid_3d_meshes:
-    test(mesh_name,"blueprint_3d_hdf5")
-CloseDatabase(braid_3d_hdf5_root)
+# TestSection("3D Example HDF5 Mesh Files")
+# OpenDatabase(braid_3d_hdf5_root)
+# for mesh_name in braid_3d_meshes:
+#     test(mesh_name,"blueprint_3d_hdf5")
+# CloseDatabase(braid_3d_hdf5_root)
 
-TestSection("3D Example Sidre HDF5 Mesh Files")
-OpenDatabase(braid_3d_sidre_root)
-for mesh_name in braid_3d_meshes:
-    test(mesh_name,"blueprint_3d_sidre_hdf5")
-CloseDatabase(braid_3d_sidre_root)
+# TestSection("3D Example Sidre HDF5 Mesh Files")
+# OpenDatabase(braid_3d_sidre_root)
+# for mesh_name in braid_3d_meshes:
+#     test(mesh_name,"blueprint_3d_sidre_hdf5")
+# CloseDatabase(braid_3d_sidre_root)
 
-TestSection("MFEM Blueprint Example Data Tests")
-for example_name in mfem_ex9_examples:
-    for protocol in mfem_ex9_protocols:
-        test_mfem("blueprint_mfem", example_name, protocol)
+# TestSection("MFEM Blueprint Example Data Tests")
+# for example in mfem_ex9_examples:
+#     for protocol in mfem_ex9_protocols:
+#         test_mfem("blueprint_mfem_" + example.dim, example.name, protocol)
 
 TestSection("MFEM LOR Mesh Blueprint Tests")
-for example_name in mfem_ex9_examples:
+for example in mfem_ex9_examples:
     for protocol in mfem_ex9_protocols:
-        test_mfem_lor_mesh("blueprint_mfem", example_name, protocol)
-for example in devilray_mfem_examples:
-    test_mfem_lor_mesh("blueprint_mfem", example.name, "", devilray = True, number = example.number)
+        test_mfem_lor_mesh("blueprint_mfem_" + example.dim, example.name, protocol)
+# for example in devilray_mfem_examples:
+#     test_mfem_lor_mesh("blueprint_mfem_" + example.dim, example.name, "", devilray = True, number = example.number)
 
-TestSection("MFEM LOR Field Blueprint Tests")
-for example in devilray_mfem_examples:
-    test_mfem_lor_field("blueprint_mfem", example.name, example.number, example.pseudo_fields, example.vector_fields)
+# TestSection("MFEM LOR Field Blueprint Tests")
+# for example in devilray_mfem_examples:
+#     test_mfem_lor_field("blueprint_mfem_" + example.dim, example.name, example.number, example.pseudo_fields, example.vector_fields)
 
-TestSection("Blueprint Expressions")
-OpenDatabase(braid_2d_json_root)
-AddPlot("Pseudocolor", "uniform_mesh/scalar_expr")
-AddPlot("Vector", "uniform_mesh/vector_expr")
-DrawPlots()
-Test("blueprint_expressions")
-DeleteAllPlots()
-CloseDatabase(braid_2d_json_root)
+# TestSection("Blueprint Expressions")
+# OpenDatabase(braid_2d_json_root)
+# AddPlot("Pseudocolor", "uniform_mesh/scalar_expr")
+# AddPlot("Vector", "uniform_mesh/vector_expr")
+# DrawPlots()
+# set_test_view("2d")
+# Test("blueprint_expressions")
+# DeleteAllPlots()
+# ResetView()
+# CloseDatabase(braid_2d_json_root)
 
-test_paren_vars()
+# test_paren_vars()
 
-test_venn("venn_small_full", venn_full_root)
-test_venn("venn_small_sparse_by_element", venn_s_by_e_root)
-test_venn("venn_small_sparse_by_material", venn_s_by_m_root)
+# test_venn("venn_small_full", venn_full_root)
+# test_venn("venn_small_sparse_by_element", venn_s_by_e_root)
+# test_venn("venn_small_sparse_by_material", venn_s_by_m_root)
 
-test_venn("venn_small_full_yaml", venn_full_yaml_root)
-test_venn("venn_small_sparse_by_element_yaml", venn_s_by_e_yaml_root)
-test_venn("venn_small_sparse_by_material_yaml", venn_s_by_m_yaml_root)
+# test_venn("venn_small_full_yaml", venn_full_yaml_root)
+# test_venn("venn_small_sparse_by_element_yaml", venn_s_by_e_yaml_root)
+# test_venn("venn_small_sparse_by_material_yaml", venn_s_by_m_yaml_root)
 
-TestSection("2D Example HDF5 Mesh Files, 0.8.2")
-OpenDatabase(braid_2d_0_8_2_hdf5_root)
-for mesh_name in braid_2d_meshes_0_8_2:
-    test(mesh_name,"blueprint_2d_hdf5_0_8_2")
-CloseDatabase(braid_2d_0_8_2_hdf5_root)
+# TestSection("2D Example HDF5 Mesh Files, 0.8.2")
+# OpenDatabase(braid_2d_0_8_2_hdf5_root)
+# for mesh_name in braid_2d_meshes_0_8_2:
+#     test(mesh_name,"blueprint_2d_hdf5_0_8_2")
+# CloseDatabase(braid_2d_0_8_2_hdf5_root)
 
-TestSection("2D Example YAML Mesh Files, 0.8.2")
-OpenDatabase(braid_2d_0_8_2_yaml_root)
-for mesh_name in braid_2d_meshes_0_8_2:
-    test(mesh_name,"blueprint_2d_yaml_0_8_2")
-CloseDatabase(braid_2d_0_8_2_yaml_root)
+# TestSection("2D Example YAML Mesh Files, 0.8.2")
+# OpenDatabase(braid_2d_0_8_2_yaml_root)
+# for mesh_name in braid_2d_meshes_0_8_2:
+#     test(mesh_name,"blueprint_2d_yaml_0_8_2")
+# CloseDatabase(braid_2d_0_8_2_yaml_root)
 
-TestSection("3D Example HDF5 Mesh Files, 0.8.2")
-OpenDatabase(braid_3d_0_8_2_hdf5_root)
-for mesh_name in braid_3d_meshes_0_8_2:
-    test(mesh_name,"blueprint_3d_hdf5_0_8_2")
-CloseDatabase(braid_3d_0_8_2_hdf5_root)
+# TestSection("3D Example HDF5 Mesh Files, 0.8.2")
+# OpenDatabase(braid_3d_0_8_2_hdf5_root)
+# for mesh_name in braid_3d_meshes_0_8_2:
+#     test(mesh_name,"blueprint_3d_hdf5_0_8_2")
+# CloseDatabase(braid_3d_0_8_2_hdf5_root)
 
-TestSection("3D Example YAML Mesh Files, 0.8.2")
-OpenDatabase(braid_3d_0_8_2_yaml_root)
-for mesh_name in braid_3d_meshes_0_8_2:
-    test(mesh_name,"blueprint_3d_yaml_0_8_2")
-CloseDatabase(braid_3d_0_8_2_yaml_root)
+# TestSection("3D Example YAML Mesh Files, 0.8.2")
+# OpenDatabase(braid_3d_0_8_2_yaml_root)
+# for mesh_name in braid_3d_meshes_0_8_2:
+#     test(mesh_name,"blueprint_3d_yaml_0_8_2")
+# CloseDatabase(braid_3d_0_8_2_yaml_root)
 
-# test polygonal mesh
-TestSection("Polygonal 2D Example HDF5 Mesh Files, 0.8.2")
-OpenDatabase(poly_2d_hdf5_root)
-test_poly("blueprint_poly_2d_hdf5_0_8_2")
-CloseDatabase(poly_2d_hdf5_root)
+# # test polygonal mesh
+# TestSection("Polygonal 2D Example HDF5 Mesh Files, 0.8.2")
+# OpenDatabase(poly_2d_hdf5_root)
+# test_poly("blueprint_poly_2d_hdf5_0_8_2")
+# CloseDatabase(poly_2d_hdf5_root)
 
-TestSection("Polygonal 2D Example YAML Mesh Files, 0.8.2")
-OpenDatabase(poly_2d_yaml_root)
-test_poly("blueprint_poly_2d_yaml_0_8_2")
-CloseDatabase(poly_2d_yaml_root)
+# TestSection("Polygonal 2D Example YAML Mesh Files, 0.8.2")
+# OpenDatabase(poly_2d_yaml_root)
+# test_poly("blueprint_poly_2d_yaml_0_8_2")
+# CloseDatabase(poly_2d_yaml_root)
 
-# test 3d polygonal mesh
-TestSection("Polygonal 3D Example HDF5 Mesh Files, 0.8.2")
-OpenDatabase(poly_3d_hdf5_root)
-test_poly("blueprint_poly_3d_hdf5_0_8_2")
-CloseDatabase(poly_3d_hdf5_root)
+# # test 3d polygonal mesh
+# TestSection("Polygonal 3D Example HDF5 Mesh Files, 0.8.2")
+# OpenDatabase(poly_3d_hdf5_root)
+# test_poly("blueprint_poly_3d_hdf5_0_8_2")
+# CloseDatabase(poly_3d_hdf5_root)
 
-TestSection("Polygonal 3D Example YAML Mesh Files, 0.8.2")
-OpenDatabase(poly_3d_yaml_root)
-test_poly("blueprint_poly_3d_yaml_0_8_2")
-CloseDatabase(poly_3d_yaml_root)
+# TestSection("Polygonal 3D Example YAML Mesh Files, 0.8.2")
+# OpenDatabase(poly_3d_yaml_root)
+# test_poly("blueprint_poly_3d_yaml_0_8_2")
+# CloseDatabase(poly_3d_yaml_root)
 
 Exit()
