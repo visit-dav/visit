4710bb0415969b075154fa674f16a4faa71bb898
miller86 <miller86@llnl.gov>
2022-06-20 08:48:55 -0700

    fix newline isue

--- src/doc/functions_to_method_doc.py
+++ src/doc/functions_to_method_doc.py
@@ -222,8 +222,8 @@ if __name__ == '__main__':
         c_output_name  = top_level + '/src/visitpy/common/MethodDoc.C'
 
     func_file   = open(func_file_name, 'r')
-    h_output    = open(h_output_name,'wb')
-    c_output    = open(c_output_name,'wb')
+    h_output    = open(h_output_name,'w',newline='')
+    c_output    = open(c_output_name,'w',newline='')
     
     h_output.write(copyright)
     h_output.write(note)
