38aa013e1f72a1307baeec35b5881befcae42589
Kathleen Biagas <biagas2@llnl.gov>
2022-08-02 14:03:41 -0700

    Add specific version test to FindVisItVTK.cmake
    Only 8.1 in the 8.x series is supported, and only 9.1 in the 9.x series, so make this explicit until newer versions of 9.x are supported.

--- src/CMake/FindVisItVTK.cmake
+++ src/CMake/FindVisItVTK.cmake
@@ -6,12 +6,12 @@
 # Modifications:
 #****************************************************************************/
 
-if(VTK_MAJOR_VERSION EQUAL 9)
+if(VTK_VERSION VERSION_EQUAL "9.1.0")
     include(${VISIT_SOURCE_DIR}/CMake/FindVTK9.cmake)
-elseif(VTK_MAJOR_VERSION EQUAL 8)
+elseif(VTK_VERSION VERSION_EQUAL "8.1.0")
     include(${VISIT_SOURCE_DIR}/CMake/FindVTK8.cmake)
 else()
-    message(FATAL_ERROR "Only VTK versions 8.1 and 9.1 are supported")
+    message(FATAL_ERROR "Only VTK versions 8.1.0 and 9.1.0 are supported")
 endif()
 
 if(VTK_FOUND)
