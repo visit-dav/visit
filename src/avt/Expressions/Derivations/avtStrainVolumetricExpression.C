13b2a024addf276ddc743f9b0a185b9190d240e7
Justin Privitera <35237779+JustinPrivitera@users.noreply.github.com>
2022-09-19 13:30:32 -0700

    Add relative volume and volumetric strain derived variables to mili database (#18110) (#18114)

---
+++ src/avt/Expressions/Derivations/avtStrainVolumetricExpression.C
@@ -0,0 +1,68 @@
+// Copyright (c) Lawrence Livermore National Security, LLC and other VisIt
+// Project developers.  See the top-level LICENSE file for dates and other
+// details.  No copyright assignment is required to contribute to VisIt.
+
+// ************************************************************************* //
+//                       avtStrainVolumetricExpression.C                     //
+// ************************************************************************* //
+
+#include <avtStrainVolumetricExpression.h>
+#include <math.h>
+
+#include <vtkCellType.h>
+#include <vtkDataArray.h>
+#include <vtkDataSet.h>
+#include <vtkIdList.h>
+#include <vtkPointData.h>
+#include <vtkUnstructuredGrid.h>
+
+#include <ExpressionException.h>
+
+#include <vector>
+
+// ****************************************************************************
+//  Method: avtStrainVolumetricExpression constructor
+//
+//  Programmer: Justin Privitera
+//  Creation:   Fri Sep  9 10:37:12 PDT 2022
+//
+// ****************************************************************************
+
+avtStrainVolumetricExpression::avtStrainVolumetricExpression()
+{
+    ;
+}
+
+// ****************************************************************************
+//  Method: avtStrainVolumetricExpression destructor
+//
+//  Programmer: Justin Privitera
+//  Creation:   Fri Sep  9 10:37:12 PDT 2022
+//
+// ****************************************************************************
+
+avtStrainVolumetricExpression::~avtStrainVolumetricExpression()
+{
+    ;
+}
+
+// ****************************************************************************
+//  Method: avtStrainVolumetricExpression::DeriveVariable
+//
+//  Purpose:
+//      Calls base class volumetric strain calculation.
+//
+//  Programmer: Justin Privitera
+//  Creation:   Fri Sep  9 10:37:12 PDT 2022
+//
+//  Modifications:
+//
+// ****************************************************************************
+
+vtkDataArray *
+avtStrainVolumetricExpression::DeriveVariable (vtkDataSet *in_ds, 
+                                             int currentDomainsIndex)
+{
+    auto do_vol_strain{true};
+    return avtStrainExpression::CalculateEvolOrRelvol(in_ds, do_vol_strain);
+}
