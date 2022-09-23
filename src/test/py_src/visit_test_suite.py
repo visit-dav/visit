909ca1f56df7412022f30fd41813acd1e932cf83
biagas <biagas2@llnl.gov>
2022-09-23 11:35:44 -0700

    Fix regression skip_file path.
    skip_file is set based on a skip_def (default skip path), which isn't being set correctly.
    Added a kludge that if skip_file isn't found, to use a relative path from tests_dir, which should be set correctly by the time we try to read skip_file.
    
    Modified skip.json to add xrayimage test, and a few flatten cases to Windows skip list until the issues with those tests can be resolved.

--- src/test/py_src/visit_test_suite.py
+++ src/test/py_src/visit_test_suite.py
@@ -508,12 +508,17 @@ def finalize_options(opts):
     if isinstance(opts["classes"],str):
         opts["classes"]  = opts["classes"].split(",")
     opts["skip_list"]    = None
-    if not opts["skip_file"] is None and os.path.isfile(opts["skip_file"]):
-        try:
-            if opts["no_skip"] == False:
-                opts["skip_list"] = json_load(opts["skip_file"])
-        except:
-            opts["skip_list"] = None
+
+    if not opts["skip_file"] is None:
+        if not os.path.isfile(opts["skip_file"]):
+            opts["skip_file"] = abs_path(opts["tests_dir"], "../", "skip.json")
+        if os.path.isfile(opts["skip_file"]):
+            try:
+                if opts["no_skip"] == False:
+                    opts["skip_list"] = json_load(opts["skip_file"])
+            except:
+                opts["skip_list"] = None
+
 
 # ----------------------------------------------------------------------------
 #  Method: parse_args
@@ -1079,6 +1084,7 @@ def main(opts,tests):
     """
     Main entry point for the test suite.
     """
+    print("visit_test_suite.py :main")
     finalize_options(opts)
     Log("[[VisIt Test Suite]]")
     if opts["check_data"]:
