1d2a861024472d8a1c9da62c292d44daffceaac9
Kathleen Biagas <biagas2@llnl.gov>
2022-09-23 18:04:04 -0700

    Fix compile error introduced in previous commit. (#18145)

--- src/plots/FilledBoundary/PyFilledBoundaryAttributes.C
+++ src/plots/FilledBoundary/PyFilledBoundaryAttributes.C
@@ -1717,7 +1717,7 @@ PyFilledBoundaryAttributes_setattr(PyObject *self, char *name, PyObject *args)
 #if VISIT_OBSOLETE_AT_VERSION(3,3,2)
 #error This code is obsolete in this version. Please remove it.
 #else
-        else if(strcmp(name, "lineStyle") == 0)
+        if(strcmp(name, "lineStyle") == 0)
         {
             PyErr_WarnEx(NULL, "'lineStyle' is obsolete. It is being ignored.", 3);
             Py_INCREF(Py_None);
