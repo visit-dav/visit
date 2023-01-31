32e0cc37f57958478e6afb5cc41a8b089818a36e
Kathleen Biagas <biagas2@llnl.gov>
2023-01-30 16:48:39 -0800

    Update qtssh's putty to version 0.78. (#18452)
    Remove qtssh build support for unix.

--- src/tools/third_party/qtssh/pinger.c
+++ src/tools/third_party/qtssh/pinger.c
@@ -1,71 +1,71 @@
 /*
- * pinger.c: centralised module that deals with sending TS_PING
+ * pinger.c: centralised module that deals with sending SS_PING
  * keepalives, to avoid replicating this code in multiple backends.
  */
 
 #include "putty.h"
 
-struct pinger_tag {
+struct Pinger {
     int interval;
-    int pending;
-    unsigned long next;
-    Backend *back;
-    void *backhandle;
+    bool pending;
+    unsigned long when_set, next;
+    Backend *backend;
 };
 
-static void pinger_schedule(Pinger pinger);
+static void pinger_schedule(Pinger *pinger);
 
 static void pinger_timer(void *ctx, unsigned long now)
 {
-    Pinger pinger = (Pinger)ctx;
+    Pinger *pinger = (Pinger *)ctx;
 
     if (pinger->pending && now == pinger->next) {
-	pinger->back->special(pinger->backhandle, TS_PING);
-	pinger->pending = FALSE;
-	pinger_schedule(pinger);
+        backend_special(pinger->backend, SS_PING, 0);
+        pinger->pending = false;
+        pinger_schedule(pinger);
     }
 }
 
-static void pinger_schedule(Pinger pinger)
+static void pinger_schedule(Pinger *pinger)
 {
-    int next;
+    unsigned long next;
 
     if (!pinger->interval) {
-	pinger->pending = FALSE;       /* cancel any pending ping */
-	return;
+        pinger->pending = false;       /* cancel any pending ping */
+        return;
     }
 
     next = schedule_timer(pinger->interval * TICKSPERSEC,
-			  pinger_timer, pinger);
-    if (!pinger->pending || next < pinger->next) {
-	pinger->next = next;
-	pinger->pending = TRUE;
+                          pinger_timer, pinger);
+    if (!pinger->pending ||
+        (next - pinger->when_set) < (pinger->next - pinger->when_set)) {
+        pinger->next = next;
+        pinger->when_set = timing_last_clock();
+        pinger->pending = true;
     }
 }
 
-Pinger pinger_new(Conf *conf, Backend *back, void *backhandle)
+Pinger *pinger_new(Conf *conf, Backend *backend)
 {
-    Pinger pinger = snew(struct pinger_tag);
+    Pinger *pinger = snew(Pinger);
 
     pinger->interval = conf_get_int(conf, CONF_ping_interval);
-    pinger->pending = FALSE;
-    pinger->back = back;
-    pinger->backhandle = backhandle;
+    pinger->pending = false;
+    pinger->backend = backend;
     pinger_schedule(pinger);
 
     return pinger;
 }
 
-void pinger_reconfig(Pinger pinger, Conf *oldconf, Conf *newconf)
+void pinger_reconfig(Pinger *pinger, Conf *oldconf, Conf *newconf)
 {
     int newinterval = conf_get_int(newconf, CONF_ping_interval);
     if (conf_get_int(oldconf, CONF_ping_interval) != newinterval) {
-	pinger->interval = newinterval;
-	pinger_schedule(pinger);
+        pinger->interval = newinterval;
+        pinger_schedule(pinger);
     }
 }
 
-void pinger_free(Pinger pinger)
+void pinger_free(Pinger *pinger)
 {
     expire_timer_context(pinger);
     sfree(pinger);
