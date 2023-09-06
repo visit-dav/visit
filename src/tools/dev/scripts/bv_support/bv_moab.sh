74d0088d67e94afc8c1240bc413eedad2b49f13c
Iulian Grindeanu <iulian787@users.noreply.github.com>
2023-09-06 12:14:50 -0500

    Filter dimension (#18812)
    * filter by dimension
    important for polyhedra meshes
    * update moab lcoation and version
    * fix parallel partition tags, neumann sets, dirichlet sets
    geometry sets and material sets too

--- src/tools/dev/scripts/bv_support/bv_moab.sh
+++ src/tools/dev/scripts/bv_support/bv_moab.sh
@@ -26,12 +26,12 @@ function bv_moab_depends_on
 
 function bv_moab_info
 {
-    export MOAB_VERSION=${MOAB_VERSION:-"5.4.1"}
+    export MOAB_VERSION=${MOAB_VERSION:-"5.5.0"}
     export MOAB_FILE=${MOAB_FILE:-"moab-${MOAB_VERSION}.tar.gz"}
-    export MOAB_URL=${MOAB_URL:-"ftp://ftp.mcs.anl.gov/pub/fathom"}
-    export MOAB_BUILD_DIR=${MOAB_BUILD_DIR:-"moab-5.4.1"}
-    export MOAB_MD5_CHECKSUM="620850a74d7af431599e7328a36f0440"
-    export MOAB_SHA256_CHECKSUM="3625e25321bf37f88d98438f5d56c280b2774172602d8b6eb6c34eedf37686fc"
+    export MOAB_URL=${MOAB_URL:-"https://web.cels.anl.gov/projects/sigma/downloads/moab"}
+    export MOAB_BUILD_DIR=${MOAB_BUILD_DIR:-"moab-5.5.0"}
+    export MOAB_MD5_CHECKSUM="c8043113d6e8ac584fb27fc9bda93dc6"
+    export MOAB_SHA256_CHECKSUM="58969f8a1b209ec9036c08c53a6b7078b368eb3bf99d0368a4de5a2f2a8db678"
 }
 
 function bv_moab_print
