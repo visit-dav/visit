78d3998f3412d3b41d87e08824822015ab303954
Justin Privitera <privitera1@llnl.gov>
2023-10-25 17:42:16 -0700

    cleanupo

--- src/databases/Blueprint/avtBlueprintFileFormat.C
+++ src/databases/Blueprint/avtBlueprintFileFormat.C
@@ -6,32 +6,6 @@
 // avtBlueprintFileFormat.C
 // ************************************************************************* //
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wall"
-#pragma GCC diagnostic ignored "-Wextra"
-#pragma GCC diagnostic ignored "-Wshadow"
-#pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
-#pragma GCC diagnostic ignored "-Wold-style-cast"
-#pragma GCC diagnostic ignored "-Wcast-function-type"
-#pragma GCC diagnostic ignored "-Wcast-align"
-#pragma GCC diagnostic ignored "-Wunused"
-#pragma GCC diagnostic ignored "-Woverloaded-virtual"
-#pragma GCC diagnostic ignored "-Wpedantic"
-#pragma GCC diagnostic ignored "-Wconversion"
-#pragma GCC diagnostic ignored "-Wsign-conversion"
-#pragma GCC diagnostic ignored "-Wfloat-conversion"
-#pragma GCC diagnostic ignored "-Wmisleading-indentation"
-#pragma GCC diagnostic ignored "-Wduplicated-cond"
-#pragma GCC diagnostic ignored "-Wduplicated-branches"
-#pragma GCC diagnostic ignored "-Wlogical-op"
-#pragma GCC diagnostic ignored "-Wnull-dereference"
-#pragma GCC diagnostic ignored "-Wuseless-cast"
-#pragma GCC diagnostic ignored "-Wdouble-promotion"
-#pragma GCC diagnostic ignored "-Wformat=2"
-#pragma GCC diagnostic ignored "-Wpragmas"
-#pragma GCC diagnostic ignored "-pedantic"
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-
 #include "avtBlueprintFileFormat.h"
 
 //-----------------------------------------------------------------------------
@@ -97,7 +71,6 @@
 
 #include <vtkDataArray.h>
 #include <algorithm>
-#pragma GCC diagnostic pop
 
 #ifdef _WIN32
 #define strcasecmp stricmp
