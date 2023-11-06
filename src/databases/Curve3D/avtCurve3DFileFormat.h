1c606fd2711900a1b1e3a9553b5799ee356a249c
Olivier Cessenat <cessenat@gmail.com>
2023-11-06 19:08:58 +0100

    Update avtCurve3DFileFormat.h following reviewer remarks

--- src/databases/Curve3D/avtCurve3DFileFormat.h
+++ src/databases/Curve3D/avtCurve3DFileFormat.h
@@ -24,37 +24,11 @@ class     vtkUnstructuredGrid;
 //  Purpose:
 //      A file format reader for 3D curves.
 //
-//  Programmer: Hank Childs
-//  Creation:   May 28, 2002
+//  Copied from Curve2D plugin as of Aug 31, 2018 and adjusted for 3D
+//  and make a global mesh with materials out of the figures.
 //
-//  Modifications:
-//
-//    Hank Childs, Fri Aug  1 21:16:55 PDT 2003
-//    Made the format be a STSD.
-//
-//    Kathleen Bonnell, Fri Oct 28 13:02:51 PDT 2005 
-//    Added methods GetTime, GetCycle, and members curveTime, curveCycle.
-//
-//    Kathleen Bonnell, Mon Jul 31 10:15:00 PDT 2006 
-//    Represent curve as 1D RectilinearGrid instead of PolyData. 
-//
-//    Kathleen Bonnell, Thu Aug  3 08:42:33 PDT 2006 
-//    Added dataExtents. 
-//
-//    Mark C. Miller, Tue Oct 31 20:33:29 PST 2006
-//    Added VALID_XVALUE token to support "zone-centered" curves
-//
-//    Kathleen Bonnell, Tue Jan 20 11:02:57 PST 2009
-//    Added spatialExtents. 
-//
-//    Kathleen Biagas, Tue Jul 15 14:16:07 MST 2014
-//    Change 'GetPoint' args from float to double.
-//
-//    Kathleen Biagas, Fri Aug 31 14:22:11 PDT 2018
-//    Added DBOptionsAttributes (currently unused).
-//
-//    Olivier Cessenat, Thu Oct  5 18:52:48 CEST 2023
-//    Go 3D and make a global mesh with materials out of the figures.
+//  Programmer: Olivier Cessenat
+//  Creation:   Oct 5, 2023
 //
 // ****************************************************************************
 
@@ -90,6 +64,7 @@ class avtCurve3DFileFormat : public avtSTSDFileFormat
   protected:
     std::string           filename;
     bool                  readFile;
+    bool                  gnuplotStyle;
 
     std::vector<vtkUnstructuredGrid *> curves;
     std::vector<std::string>   curveNames;
@@ -100,12 +75,10 @@ class avtCurve3DFileFormat : public avtSTSDFileFormat
     int                        curveCycle;
     double range[6] ; // geometrical mesh range
 
-    void                  ReadFile(void);
+    void                  ReadFile(bool const clearData);
     Curve3DToken          GetPoint(istream &, double &, double &, double &,
                                    std::string &);
 };
 
 
 #endif
-
-
