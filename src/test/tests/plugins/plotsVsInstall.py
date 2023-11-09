13eb023a8416baf192fb5d10dd52a56a2c6e5147
biagas <biagas2@llnl.gov>
2023-11-09 12:08:58 -0800

    Fix pluginVsInstall regressions. Updated FilterDependencies to account for VTK 9 and Qt6 target names. Fix a typo in bv_vtk.sh Fix a Qt version reference in FindVTK9.cmake Updated GenerateCMake to account for VTK lib vs target names when doing a non-dev build of a plugin. Added VTK_VERSION_HEX to PluginVsInstall.cmake.in so LIB_VERSION comparisons will work when building a plugin against installed VisIt. Fixed compilation error with avtCartographicProjectionFilter, it hadn't been completely updated for use with VTK 9. Replace Volume with Molecule plot in plotsVsInstall.py, because the new volume plot cannot auto-regenerate its CMakeLists.txt Updated baseline for plotsVsInstall.txt Updated release notes regarding changes to Volume plot.

--- src/test/tests/plugins/plotsVsInstall.py
+++ src/test/tests/plugins/plotsVsInstall.py
@@ -10,12 +10,18 @@
 #  Date:       November 8, 2018 
 #
 #  Modifications:
+#    Kathleen Biagas, Wed Nov  8 09:41:41 PST 2023
+#    Temporarily remove Volume from being tested. We need to use the VTK 9
+#    version, but it's CMakeLists.txt file currently cannot be
+#    auto-regenerated. See issue #19044.
+#    Add Molecule in it's place.
 #
 # ----------------------------------------------------------------------------
 
 Source(tests_path("plugins", "pluginVsInstallHelpers"))
 
-do_plugin_type("plots", ["Contour", "Label", "Tensor", "Volume"])
+do_plugin_type("plots", ["Contour", "Label", "Molecule", "Tensor"])
+
 # for future use
 #do_tests("plots", ["contour.py", "label.py", "tensor.py", "ray_trace.py or volumePlot.py"])
 
