a280086f125b4ebad42f8bb538f2ecc6c49599f1
Justin Privitera <privitera1@llnl.gov>
2023-12-21 14:47:33 -0800

    here's to hoping that this magically works

--- src/tools/dev/scripts/bv_support/bv_adios2.sh
+++ src/tools/dev/scripts/bv_support/bv_adios2.sh
@@ -66,13 +66,13 @@ function bv_adios2_initialize_vars
 
 function bv_adios2_info
 {
-    export ADIOS2_VERSION=${ADIOS2_VERSION:-"2.7.1"}
+    export ADIOS2_VERSION=${ADIOS2_VERSION:-"2.9.2"}
     export ADIOS2_FILE=${ADIOS2_FILE:-"adios2-${ADIOS2_VERSION}.tar.gz"}
     export ADIOS2_COMPATIBILITY_VERSION=${ADIOS2_COMPATIBILITY_VERSION:-"${ADIOS2_VERSION}"}
     export ADIOS2_URL=${ADIOS2_URL:-"https://github.com/ornladios/ADIOS2/archive/refs/tags/v${ADIOS2_VERSION}"}
     export ADIOS2_BUILD_DIR=${ADIOS2_BUILD_DIR:-"ADIOS2-"${ADIOS2_VERSION}}
-    export ADIOS2_MD5_CHECKSUM="b78e02946c4ff481679063220f9fc961"
-    export ADIOS2_SHA256_CHECKSUM="c8e237fd51f49d8a62a0660db12b72ea5067512aa7970f3fcf80b70e3f87ca3e"
+    export ADIOS2_MD5_CHECKSUM="bbbb53af749dd61ccf00de8b28027966"
+    export ADIOS2_SHA256_CHECKSUM="78309297c82a95ee38ed3224c98b93d330128c753a43893f63bbe969320e4979"
 }
 
 function bv_adios2_print
