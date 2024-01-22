7adcda0c06709f460c640687eeeb147f060af24c
Cyrus Harrison <cyrush@llnl.gov>
2024-01-22 09:29:59 -0800

    more updates

--- src/tools/dev/scripts/bv_support/bv_openexr.sh
+++ src/tools/dev/scripts/bv_support/bv_openexr.sh
@@ -122,9 +122,19 @@ function build_ilmbase
          EXTRA_AC_FLAGS="ac_cv_build=aarch64-unknown-linux-gnu"
     fi
 
+    # Open EXR does not suport c++17, a few examples of errors:
+    #
+    #  ISO C++17 does not allow dynamic exception specifications
+    #  ISO C++17 does not allow ‘register’ storage class specifier [-Wregister]
+    #
+    # If c++17 s the compiler default it will fail to build
+    # Use --std=c++14 to avoid problems while building/
+    #
+    OPENEXR_CXX_FLAGS="$CXXFLAGS $CXX_OPT_FLAGS --std=c++14"
+
     set -x
     ./configure ${OPTIONAL} CXX="$CXX_COMPILER" \
-                CC="$C_COMPILER" CFLAGS="$CFLAGS $C_OPT_FLAGS" CXXFLAGS="$CXXFLAGS $CXX_OPT_FLAGS" \
+                CC="$C_COMPILER" CFLAGS="$CFLAGS $C_OPT_FLAGS" CXXFLAGS="$OPENEXR_CXX_FLAGS" \
                 $EXTRA_AC_FLAGS --prefix="$VISITDIR/openexr/$ILMBASE_VERSION/$VISITARCH" $DISABLE_BUILDTYPE
     set +x
     if [[ $? != 0 ]] ; then
