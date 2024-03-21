dd6abc49a7a7681bd7d26228be4a07000f3f16e4
Kathleen Biagas <biagas2@llnl.gov>
2024-03-21 12:12:12 -0700

    Update docker files for 3.4.1 (#19406)
    * Update docker files for 3.4.1
    
    * Update Release notes.

---
+++ scripts/docker/run_build_visit-ubuntu20.sh
@@ -0,0 +1,1 @@
+echo "yes" | ./build_visit3_4_1 --required --optional --mesagl --qt6 --mpich --uintah --no-moab --no-visit --thirdparty-path /home/visit/third-party --makeflags -j4 ; python3 build_visit_docker_cleanup.py
