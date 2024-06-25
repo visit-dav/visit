f237c5af993748cd57c77da985f11f898923fad5
biagas <biagas2@llnl.gov>
2024-06-25 09:09:10 -0700

    Add import libraries for conduit and mfem.

--- src/CMake/FindConduit.cmake
+++ src/CMake/FindConduit.cmake
@@ -8,6 +8,9 @@
 #   Removed install logic for conduit python.  Now handled in
 #   lib/CMakeLists.txt with build/lib/site-packages/ directory install.
 #
+#   Kathleen Biagas, Tue June 25, 2024
+#   Create exportable library via blt_import_library.
+#
 #-----------------------------------------------------------------------------
 
 #
@@ -29,5 +32,66 @@ endif()
 
 if(CONDUIT_FOUND)
     set(HAVE_CONDUIT TRUE CACHE BOOL "Have Conduit libraries")
+
+    # create a list of full-path libraries to be used when linking
+    foreach(libdir ${CONDUIT_LIBRARY_DIR})
+        foreach(lib ${CONDUIT_LIB})
+            if(EXISTS ${libdir}/${lib})
+                list(APPEND CONDUIT_LIBRARIES
+                    $<BUILD_INTERFACE:${libdir}/${lib}>)
+            endif()
+        endforeach()
+    endforeach()
+
+    # create the import library
+    blt_import_library(
+        NAME       conduit
+        INCLUDES   $<BUILD_INTERFACE:${CONDUIT_INCLUDE_DIR}>
+                   $<INSTALL_INTERFACE:${VISIT_INSTALLED_VERSION_INCLUDE}/conduit/conduit>
+        LIBRARIES  ${CONDUIT_LIBRARIES}
+        EXPORTABLE ON)
+
+    # Add install interface for link libraries
+    foreach(lib ${CONDUIT_LIB})
+        target_link_libraries(conduit INTERFACE
+            $<INSTALL_INTERFACE:\${_IMPORT_PREFIX}/${VISIT_INSTALLED_VERSION_LIB}/${lib}>)
+    endforeach()
+
+    if(VISIT_PARALLEL)
+        # create a list of full-path libraries to be used when linking
+        foreach(libdir ${CONDUIT_MPI_LIBRARY_DIR})
+            foreach(lib ${CONDUIT_MPI_LIB})
+                if(EXISTS ${libdir}/${lib})
+                    list(APPEND CONDUIT_MPI_LIBRARIES
+                        $<BUILD_INTERFACE:${libdir}/${lib}>)
+                endif()
+            endforeach()
+        endforeach()
+
+        # Create import library
+        blt_import_library(
+            NAME       conduit_mpi
+            INCLUDES   $<BUILD_INTERFACE:${CONDUIT_INCLUDE_DIR}>
+                       $<INSTALL_INTERFACE:${VISIT_INSTALLED_VERSION_INCLUDE}/conduit/conduit>
+            LIBRARIES  ${CONDUIT_MPI_LIBRARIES}
+            EXPORTABLE ON)
+
+        # Add install interface for link libraries
+        foreach(lib ${CONDUIT_MPI_LIB})
+            if(WIN32 AND ${lib} MATCHES "msmpi")
+                continue()
+            endif()
+            target_link_libraries(conduit_mpi INTERFACE
+                $<INSTALL_INTERFACE:\${_IMPORT_PREFIX}/${VISIT_INSTALLED_VERSION_LIB}/${lib}>)
+        endforeach()
+    endif()
+
+   # install the export targets
+   if(VISIT_INSTALL_THIRD_PARTY)
+       visit_install_export_targets(conduit)
+       if(VISIT_PARALLEL)
+           visit_install_export_targets(conduit_mpi)
+       endif()
+   endif()
 endif()
 
