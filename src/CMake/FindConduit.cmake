9b25a35d7c4a6f7fce370c7d5d0d4a836e94c259
Kathleen Biagas <biagas2@llnl.gov>
2024-07-16 11:24:42 -0700

    Update mfem, conduit (#19624)
    * Remove avt/Preprocessor, no longer used.
    
    * Add import libraries for conduit and mfem.
    
    * Update avtmfem and avtblueprint.
    
    Utilize visit_add_library, and new conduit and mfem targets.
    
    * Minor fixes to FindMfem and FindConduit:
    remove duplicates from lib-dir and lib lists.
    Add more TP includes to MFEMs include dirs install interface.
    
    * Remove whitespace.

--- src/CMake/FindConduit.cmake
+++ src/CMake/FindConduit.cmake
@@ -8,10 +8,13 @@
 #   Removed install logic for conduit python.  Now handled in
 #   lib/CMakeLists.txt with build/lib/site-packages/ directory install.
 #
+#   Kathleen Biagas, Tue June 25, 2024
+#   Create exportable library via blt_import_library.
+#
 #-----------------------------------------------------------------------------
 
 #
-# Use the CONDUIT_DIR hint from the config-site .cmake file 
+# Use the CONDUIT_DIR hint from the config-site .cmake file
 #
 
 SET_UP_THIRD_PARTY(CONDUIT
@@ -29,5 +32,71 @@ endif()
 
 if(CONDUIT_FOUND)
     set(HAVE_CONDUIT TRUE CACHE BOOL "Have Conduit libraries")
+    list(REMOVE_DUPLICATES CONDUIT_LIBRARY_DIR)
+    list(REMOVE_DUPLICATES CONDUIT_LIB)
+    # create a list of full-path libraries to be used when linking
+    foreach(libdir ${CONDUIT_LIBRARY_DIR})
+        foreach(lib ${CONDUIT_LIB})
+            if(EXISTS ${libdir}/${lib})
+                list(APPEND CONDUIT_LIBRARIES
+                    $<BUILD_INTERFACE:${libdir}/${lib}>)
+            endif()
+        endforeach()
+    endforeach()
+
+    # create the import library
+    blt_import_library(
+        NAME       conduit
+        INCLUDES   $<BUILD_INTERFACE:${CONDUIT_INCLUDE_DIR}>
+                   $<INSTALL_INTERFACE:${VISIT_INSTALLED_VERSION_INCLUDE}/conduit/conduit>
+        LIBRARIES  ${CONDUIT_LIBRARIES}
+        EXPORTABLE ON)
+
+    unset(CONDUIT_LIBRARIES)
+    # Add install interface for link libraries
+    foreach(lib ${CONDUIT_LIB})
+        target_link_libraries(conduit INTERFACE
+            $<INSTALL_INTERFACE:\${_IMPORT_PREFIX}/${VISIT_INSTALLED_VERSION_LIB}/${lib}>)
+    endforeach()
+
+    if(VISIT_PARALLEL)
+        list(REMOVE_DUPLICATES CONDUIT_MPI_LIBRARY_DIR)
+        list(REMOVE_DUPLICATES CONDUIT_MPI_LIB)
+        # create a list of full-path libraries to be used when linking
+        foreach(libdir ${CONDUIT_MPI_LIBRARY_DIR})
+            foreach(lib ${CONDUIT_MPI_LIB})
+                if(EXISTS ${libdir}/${lib})
+                    list(APPEND CONDUIT_MPI_LIBRARIES
+                        $<BUILD_INTERFACE:${libdir}/${lib}>)
+                endif()
+            endforeach()
+        endforeach()
+
+        # Create import library
+        blt_import_library(
+            NAME       conduit_mpi
+            INCLUDES   $<BUILD_INTERFACE:${CONDUIT_INCLUDE_DIR}>
+                       $<INSTALL_INTERFACE:${VISIT_INSTALLED_VERSION_INCLUDE}/conduit/conduit>
+            LIBRARIES  ${CONDUIT_MPI_LIBRARIES}
+            EXPORTABLE ON)
+
+        # Add install interface for link libraries
+        foreach(lib ${CONDUIT_MPI_LIB})
+            if(WIN32 AND ${lib} MATCHES "msmpi")
+                continue()
+            endif()
+            target_link_libraries(conduit_mpi INTERFACE
+                $<INSTALL_INTERFACE:\${_IMPORT_PREFIX}/${VISIT_INSTALLED_VERSION_LIB}/${lib}>)
+        endforeach()
+        unset(CONDUIT_MPI_LIBRARIES)
+    endif()
+
+   # install the export targets
+   if(VISIT_INSTALL_THIRD_PARTY)
+       visit_install_export_targets(conduit)
+       if(VISIT_PARALLEL)
+           visit_install_export_targets(conduit_mpi)
+       endif()
+   endif()
 endif()
 
--- src/CMake/FindMFEM.cmake
+++ src/CMake/FindMFEM.cmake
@@ -2,7 +2,66 @@
 # Project developers.  See the top-level LICENSE file for dates and other
 # details.  No copyright assignment is required to contribute to VisIt.
 
+#-----------------------------------------------------------------------------
+# Modifications:
+#   Kathleen Biagas, Tue June 25, 2024
+#   Create exportable library via blt_import_library.
+#
+#-----------------------------------------------------------------------------
+
 # Use the MFEM_DIR hint from the config-site .cmake file
 
 SET_UP_THIRD_PARTY(MFEM LIBS mfem)
+
+if(MFEM_FOUND)
+    # create a list of full-path libraries to be used when linking
+    list(REMOVE_DUPLICATES MFEM_LIBRARY_DIR)
+    list(REMOVE_DUPLICATES MFEM_LIB)
+    foreach(libdir ${MFEM_LIBRARY_DIR})
+        foreach(lib ${MFEM_LIB})
+            if(EXISTS ${libdir}/${lib})
+                list(APPEND mll $<BUILD_INTERFACE:${libdir}/${lib}>)
+            endif()
+        endforeach()
+    endforeach()
+
+    # create include dirs for build and install interface
+    foreach(incdir ${MFEM_INCLUDE_DIR})
+        list(APPEND mfem_inc $<BUILD_INTERFACE:${incdir}>)
+        if(${incdir} MATCHES "mfem")
+            list(APPEND mfem_inc
+                $<INSTALL_INTERFACE:${VISIT_INSTALLED_VERSION_INCLUDE}/mfem/include>)
+        elseif(${incdir} MATCHES "conduit")
+            list(APPEND mfem_inc
+                $<INSTALL_INTERFACE:${VISIT_INSTALLED_VERSION_INCLUDE}/conduit/conduit>)
+        elseif(${incdir} MATCHES "fms")
+            list(APPEND mfem_inc
+                $<INSTALL_INTERFACE:${VISIT_INSTALLED_VERSION_INCLUDE}/fms/include>)
+        endif()
+    endforeach()
+
+    # create import library
+    blt_import_library(
+        NAME       mfem
+        INCLUDES   ${mfem_inc}
+        LIBRARIES  ${mll}
+        EXPORTABLE ON)
+
+    # Add install interface for link libraries
+    foreach(libdir ${MFEM_LIBRARY_DIR})
+        foreach(lib ${MFEM_LIB})
+            if(EXISTS ${libdir}/${lib})
+                target_link_libraries(mfem INTERFACE
+                    $<INSTALL_INTERFACE:\${_IMPORT_PREFIX}/${VISIT_INSTALLED_VERSION_LIB}/${lib}>)
+            endif()
+        endforeach()
+    endforeach()
+
+    # install export target
+    visit_install_export_targets(mfem)
+
+    unset(mll)
+    unset(mfem_inc)
+endif()
+
 
