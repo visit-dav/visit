28f907a34a8c51a36cc75f2bacfcf343060bc17b
Justin Privitera <privitera1@llnl.gov>
2024-10-16 14:56:35 -0700

    weewoo

--- src/avt/Expressions/Math/avtMaxReductionExpression.C
+++ src/avt/Expressions/Math/avtMaxReductionExpression.C
@@ -10,6 +10,7 @@
 
 #include <vtkDataArray.h>
 #include <vtkDataSet.h>
+#include <vtkCellData.h>
 
 
 // ****************************************************************************
@@ -72,41 +73,45 @@ void
 avtMaxReductionExpression::DoOperation(vtkDataArray *in, vtkDataArray *out,
                           int ncomponents, int ntuples, vtkDataSet *in_ds)
 {
-    // const int ncellArray = in_ds->GetCellData()->GetNumberOfArrays();
-    // bool has_ghosts = false;
-    // for (int cellArrayId = 0; cellArrayId < ncellArray; cellArrayId ++)
-    // {
-    //     vtkDataArray *currArray = in_ds->GetCellData()->GetArray(cellArrayId);
-    //     const std::string arrayName = currArray->GetName();
-    //     if (arrayName == "avtGhostZones")
-    //     {
-    //         has_ghosts = true;
-    //         break;
-    //     }
-    // }
+    if (centering == AVT_ZONECENT)
+    {
 
-    // vtkDataSet *filtered_ds = nullptr;
-    // vtkDataSetRemoveGhostCells *ghost_filters = nullptr;
-    
-    // // filter out any ghost cells
-    // if (has_ghosts)
-    // {
-    //     ghost_filters = vtkDataSetRemoveGhostCells::New();
-    //     ghost_filters->SetInputData(in_ds);
-    //     ghost_filters->Update();
-    //     filtered_ds = ghost_filters->GetOutput();
-    // }
-    // else
-    // {
-    //     filtered_ds = in_ds;
-    // }
+    }
+    else // node centered
+    {
 
-    // if (has_ghosts)
-    // {
-    //     ghost_filters->Delete();
-    // }
+    }
 
 
+    const int ncellArray = in_ds->GetCellData()->GetNumberOfArrays();
+    bool has_ghosts = false;
+    vtkDataArray *ghost_data = nullptr;
+    for (int cellArrayId = 0; cellArrayId < ncellArray; cellArrayId ++)
+    {
+        vtkDataArray *currArray = in_ds->GetCellData()->GetArray(cellArrayId);
+        const std::string arrayName = currArray->GetName();
+        std::cout << arrayName << std::endl;
+        if (arrayName == "avtGhostZones")
+        {
+            has_ghosts = true;
+            ghost_data = currArray;
+            break;
+        }
+    }
+
+    std::cout << "ntuples " << ntuples << std::endl;
+    std::cout << "ncomponents " << ncomponents << std::endl;
+
+    if (has_ghosts)
+    {
+        std::cout << "ghost_data->GetNumberOfTuples() " << ghost_data->GetNumberOfTuples() << std::endl;
+        std::cout << "ghost_data->GetNumberOfComponents() " << ghost_data->GetNumberOfComponents() << std::endl;
+    }
+
+    for (int tuple_id = 0; tuple_id < 100; tuple_id ++)
+    {
+        const int ghost = ghost_data->GetComponent(tuple_id, 0);
+    }
 
     for (int comp_id = 0; comp_id < ncomponents; comp_id ++)
     {
