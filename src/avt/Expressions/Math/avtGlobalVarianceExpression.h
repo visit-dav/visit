a3a90101b09e50ceb11e91353347107617c335cf
Justin Privitera <privitera1@llnl.gov>
2024-10-25 16:59:51 -0700

    inheritance for variance and last commit was stddev

--- src/avt/Expressions/Math/avtGlobalVarianceExpression.h
+++ src/avt/Expressions/Math/avtGlobalVarianceExpression.h
@@ -9,7 +9,7 @@
 #ifndef AVT_GLOBAL_VARIANCE_FILTER_H
 #define AVT_GLOBAL_VARIANCE_FILTER_H
 
-#include <avtUnaryMathExpression.h>
+#include <avtGhostAwareUnaryMathExpression.h>
 
 class     vtkDataArray;
 
@@ -27,7 +27,7 @@ class     vtkDataArray;
 //
 // ****************************************************************************
 
-class EXPRESSION_API avtGlobalVarianceExpression : public avtUnaryMathExpression
+class EXPRESSION_API avtGlobalVarianceExpression : public avtGhostAwareUnaryMathExpression
 {
   public:
                               avtGlobalVarianceExpression();
@@ -38,8 +38,13 @@ class EXPRESSION_API avtGlobalVarianceExpression : public avtUnaryMathExpression
                                               { return "Calculating variance across mesh"; };
 
   protected:
-    virtual void              DoOperation(vtkDataArray *in, vtkDataArray *out,
-                                          int ncomponents, int ntuples, vtkDataSet *in_ds);
+    virtual void              CalculateWithoutGhosts(vtkDataArray *in, vtkDataArray *out,
+                                                     int ncomponents, int ntuples);
+    virtual void              CalculateWithGhosts(vtkDataArray *in, vtkDataArray *out,
+                                                  int ncomponents, int ntuples,
+                                                  int (getPointValid)(vtkDataArray *, int *, int),
+                                                  vtkDataArray *ghostZones,
+                                                  int *nodeShouldBeIgnoredPtr);
 };
 
 
