2bd6303f45cdb22464dd43566d1a38427be61f35
Justin Privitera <privitera1@llnl.gov>
2024-10-25 17:02:37 -0700

    rms uses new inheritance

--- src/avt/Expressions/Math/avtGlobalRMSExpression.h
+++ src/avt/Expressions/Math/avtGlobalRMSExpression.h
@@ -9,7 +9,7 @@
 #ifndef AVT_GLOBAL_RMS_FILTER_H
 #define AVT_GLOBAL_RMS_FILTER_H
 
-#include <avtUnaryMathExpression.h>
+#include <avtGhostAwareUnaryMathExpression.h>
 
 class     vtkDataArray;
 
@@ -27,7 +27,7 @@ class     vtkDataArray;
 //
 // ****************************************************************************
 
-class EXPRESSION_API avtGlobalRMSExpression : public avtUnaryMathExpression
+class EXPRESSION_API avtGlobalRMSExpression : public avtGhostAwareUnaryMathExpression
 {
   public:
                               avtGlobalRMSExpression();
@@ -38,8 +38,13 @@ class EXPRESSION_API avtGlobalRMSExpression : public avtUnaryMathExpression
                                               { return "Calculating root mean square across mesh"; };
 
   protected:
-    virtual void              DoOperation(vtkDataArray *in, vtkDataArray *out,
-                                          int ncomponents, int ntuples, vtkDataSet *in_ds);
+    virtual void              CalculateWithoutGhosts(vtkDataArray *in, vtkDataArray *out,
+                                                     int ncomponents, int ntuples);
+    virtual void              CalculateWithGhosts(vtkDataArray *in, vtkDataArray *out,
+                                                  int ncomponents, int ntuples,
+                                                  int (getPointValid)(vtkDataArray *, int *, int),
+                                                  vtkDataArray *ghostZones,
+                                                  int *nodeShouldBeIgnoredPtr);
 };
 
 
