d4c1735ae6fd8f80fe00f5f85266082e5ee5e047
Mark C. Miller <miller86@llnl.gov>
2024-10-31 10:47:41 -0700

    Revert "ws change to trigger ci"
    This reverts commit bf35853658ec53dbfa037ec20a4da7ecab5a5085.

--- src/test/tests/unit/protocol.py
+++ src/test/tests/unit/protocol.py
@@ -7,7 +7,6 @@
 #
 #  Mark C. Miller, Tue Jan 11 10:19:23 PST 2011
 # ----------------------------------------------------------------------------
-
 tapp = visit_bin_path("visitprotocol")
 res = sexe(tapp,ret_output=True)
 if res["return_code"] == 0:
