8c7510d5ea4d05dc42a160534df10fa2954de86d
Justin Privitera <justinprivitera@gmail.com>
2024-11-26 13:41:33 -0800

    update config site files

--- src/config-site/tuolumne1001.cmake
+++ src/config-site/tuolumne1001.cmake
@@ -1,7 +1,7 @@
 #/usr/workspace/visit/visit/thirdparty_shared/3.4.2/toss4-cray-gfx942/cmake/3.24.3/linux-x86_64_gcc-12.2/bin/cmake
 ##
 ## ./build_visit generated host.cmake
-## created: Fri Nov  8 15:43:43 PST 2024
+## created: Tue Nov 26 11:42:14 PST 2024
 ## system: Linux tuolumne1001 4.18.0-553.22.1.1toss.t4.x86_64 #1 SMP Tue Sep 24 16:50:00 PDT 2024 x86_64 x86_64 x86_64 GNU/Linux
 ## by: justin
 
