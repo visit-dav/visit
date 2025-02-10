a8ae2d2a9cc17b6bdc61fef6ef9cd91583675cb9
Eric Samuel Brugger <brugger@rzwhippet18.llnl.gov>
2025-02-10 13:37:54 -0800

    Turn off wayland cli support in xkbcommon build.

--- src/tools/dev/scripts/bv_support/bv_xkbcommon.sh
+++ src/tools/dev/scripts/bv_support/bv_xkbcommon.sh
@@ -97,7 +97,7 @@ function build_xkbcommon
 
     export PATH=$MESON_INSTALL_DIR/bin:$NINJA_INSTALL_DIR/bin:$PATH
 
-    meson setup build --prefix $XKBCOMMON_INSTALL_DIR || error "Xkbcommon did not configure correctly. Giving up."
+    meson setup build --prefix $XKBCOMMON_INSTALL_DIR -Denable-wayland=false || error "Xkbcommon did not configure correctly. Giving up."
 
     #
     # Build xkbcommon
