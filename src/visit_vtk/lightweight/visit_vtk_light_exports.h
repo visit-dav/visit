4c17fc71b72abf86535fd3d5662d1ca6739901c1
Kathleen Biagas <biagas2@llnl.gov>
2025-02-11 10:00:38 -0800

    Remove warning disablement.

--- src/visit_vtk/lightweight/visit_vtk_light_exports.h
+++ src/visit_vtk/lightweight/visit_vtk_light_exports.h
@@ -15,14 +15,6 @@
 #else
 #define VISIT_VTK_LIGHT_API
 #endif
-#if defined(_MSC_VER)
-// Turn off warning about lack of DLL interface
-#pragma warning(disable:4251)
-// Turn off warning non-dll class is base for dll-interface class.
-#pragma warning(disable:4275)
-// Turn off warning about identifier truncation
-#pragma warning(disable:4786)
-#endif
 #else
 # if __GNUC__ >= 4 && (defined(VISIT_VTK_LIGHT_EXPORTS) || defined(lightweight_visit_vtk_EXPORTS))
 #   define VISIT_VTK_LIGHT_API __attribute__ ((visibility("default")))
