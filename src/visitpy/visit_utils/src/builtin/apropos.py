40b3230bdf69ad682a3c46a8f7b470b56bb94a86
Mark C. Miller <miller86@llnl.gov>
2025-03-17 11:27:04 -0700

    simplify, fix search bugs

--- src/visitpy/visit_utils/src/builtin/apropos.py
+++ src/visitpy/visit_utils/src/builtin/apropos.py
@@ -1,5 +1,6 @@
-import re, sys
+import inspect, re, sys
 from pydoc import help as python_help
+from contextlib import suppress
 
 try:
     import visit
@@ -12,34 +13,30 @@ except:
 # result if it would be useful.
 #
 def visit_help(thing):
-    if type(thing) == str:
-        apresult = apropos(thing,0,True)
-        if apresult:
-            sys.stdout.write("NOTE: The following VisIt functions and objects also mention '%s'...\n"%str(thing))
-            sys.stdout.write("%s\n"%apresult)
-    else:
+    if not isinstance(thing, str):
         try:
             python_help(thing)
         except:
             pass
+        # Return early w/o adding apropos if thing is a basic type
+        if isinstance(thing, (int,float,bool,complex,list,type)):
+            return
+    elif hasattr(visit, thing):
         try:
-            if hasattr(thing, "__name__"):
-                thingstr = thing.__name__
-            else:
-                thingstr = str(thing)
-        except:
-            thingstr = str(thing)
-        try:
-            dirthing = [x in dir(thing) and not (x.startswith('__') and x.endswith('__'))]
-            if dirthing:
-                sys.stdout.write("'%s' defines the following methods...\n"%thingstr)
-                sys.stdout.write("%s\n"%dirthing)
+            python_help(getattr(visit, thing))
         except:
             pass
-        apresult = apropos(thingstr,0,True)
-        if apresult:
-            sys.stdout.write("NOTE: The following VisIt functions and objects also mention '%s'...\n"%thingstr)
-            sys.stdout.write("%s\n"%apresult)
+    try:
+        if hasattr(thing, "__name__"):
+            thingstr = thing.__name__
+        else:
+            thingstr = str(thing)
+    except:
+        thingstr = str(thing)
+    apresult = apropos(key=thingstr,trunc=50)
+    if apresult:
+        sys.stdout.write("Using apropos(), here are some VisIt items that also mention '%s'...\n"%thingstr)
+        sys.stdout.write("%s\n"%apresult)
 
 #
 # Re-define python's main help method
@@ -47,20 +44,32 @@ def visit_help(thing):
 help = visit_help
 
 #
-# Return the list of names of all methods where the specified regex
-# matches either in the method name or its doc string or a strinified
-# version of the object. Use a set to accumulate only unique results
-# but return the result as a list. The matching is performed all in
-# lower case.
 #
-def apropos(key, truncate=30, called_from_help=False):
-    result = set()
+def apropos(key, match='all', trunc=30):
+    """Search function, object names and/or doc strings for matching text
 
-    #
-    # We want to search attrs only on objects we can safely *instance*
-    # and dir() here
-    #
-    dirableObjectNamesContain = ['Attribute', 'SILRestriction']
+       - key: is search text (can be a regex; help(re) for more info)
+       - match: is a catentated string of words controlling behavior...
+          - items: search VisIt CLI item names
+          - attrs: search attributes of VisIt CLI items
+          - docs: search doc strings of VisIt CLI items
+          - all: search all of the above (this is default behavior)
+          - case: honor case in searches. By default, case is ignored.
+       - trunc: truncate response list (30 is default; 0 disables).
+
+       Return the list of names of all items where the specified text
+       (or regular expression) matches either in the item name, the
+       item's attributes, if any, or the item's docstring, if any.
+    """
+    called_from_help = True if inspect.stack()[1].function == "visit_help" else False
+
+    ignore_case = False if re.search(r'case', match, re.IGNORECASE) else True
+    search_items = True if re.search(r'items|all', match, re.IGNORECASE) else False
+    search_attrs = True if re.search(r'attrs|all', match, re.IGNORECASE) else False
+    search_docs = True if re.search(r'docs|all', match, re.IGNORECASE) else False
+
+    reflags = re.IGNORECASE if ignore_case else re.RegexFlag(0)
+    result = set()
 
     #
     # We wanna search for a string
@@ -73,56 +82,50 @@ def apropos(key, truncate=30, called_from_help=False):
     #
     for v in dir(visit):
 
-        #
-        # First, match the name of the CLI method
-        #
-        if re.search(key, v, re.IGNORECASE):
+        # Stop loop if we're at truncated size
+        if trunc > 0 and len(result) > trunc:
+            break
+
+        # First, search the name of thing itself.
+        if search_items and re.search(key, v, reflags):
             result.add(v)
+
+        # If we're not searching anything else, continue.
+        if not search_attrs and not search_docs:
             continue
 
-        #
-        # Get the actual method from its name
-        #
+        # Get the actual thing handle from its name.
+        thing = None
         try:
             thing = getattr(visit, v) 
         except:
+            pass
+
+        # Nothing to do if we can't get a python handle for thing.
+        if not thing:
             continue
 
-        #
-        # If a docstring is defined for th method, search it
-        #
-        if hasattr(thing,'__doc__'): 
-             if thing.__doc__ is not None:
-                 if re.search(key, thing.__doc__, re.IGNORECASE):
-                     result.add(v)
-                     continue
+        if search_attrs and hasattr(thing, '__dict__'): 
+            members = [m for m in getattr(thing, '__dict__') \
+                       if not (m.startswith('__') and m.endswith('__'))]
+            if re.search(key, str(members), reflags):
+                result.add(v)
+                continue
 
-        #
-        # If the method, when called, instantiates a dirable object,
-        # search the methods it defines too.
-        #
-        for donc in dirableObjectNamesContain:
-            if donc in v:
-                try:
-                    for member in dir(thing()):
-                        # ignore what appear to be builtin members
-                        if member.startswith('__') and member.endswith('__'):
-                            continue
-                        if re.search(key, member, re.IGNORECASE):
-                            result.add(v)
-                            break
-                except:
-                    pass
+        if search_docs and hasattr(thing, '__doc__') and thing.__doc__ is not None:
+            if re.search(key, thing.__doc__, reflags):
+                result.add(v)
+                continue
 
     if not result and not called_from_help:
         sys.stdout.write("Nothing found in VisIt for '%s'\n"%key)
         return None
 
     retval = sorted(list(result))
-    if truncate > 0 and len(retval) > truncate:
-        sys.stdout.write("More than %d VisIt functions and objects mention '%s'.\n"%(truncate,key))
-        sys.stdout.write("To see the complete list, enter `apropos('%s',0)`.\n"%key)
-        sys.stdout.write("To narrow the results, consider a more restrictve regular expression.\n")
+    if trunc > 0 and len(retval) > trunc and not called_from_help:
+        sys.stdout.write("More than %d VisIt items mention '%s'.\n"%(trunc,key))
+        sys.stdout.write("To see a complete list, enter `apropos('%s',trunc=0)`.\n"%key)
+        sys.stdout.write("To narrow results, try a more restrictve regular expression.\n")
         sys.stdout.write("To learn more about regular expressions type `help(re)`.\n")
-        retval = retval[:truncate]
+        retval = retval[:trunc]
     return retval
