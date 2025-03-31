172ee395373be39f7e14c781d02e5d380e4bf372
Mark C. Miller <miller86@llnl.gov>
2025-03-31 11:49:18 -0700

    Update src/test/tests/unit/atts_assign.py
    Co-authored-by: Kathleen Biagas <biagas2@llnl.gov>

--- src/test/tests/unit/atts_assign.py
+++ src/test/tests/unit/atts_assign.py
@@ -50,7 +50,7 @@ def repr2(s):
 #
 # Class to facilitate stdout redirect while already in a redirected state.
 # There are potentially better ways to do this in python 3 but the test
-# harness complicates things because there is already some rediction going on.
+# harness complicates things because there is already some redirection going on.
 #
 class my_redirect_stdout(list):
     def __enter__(self):
