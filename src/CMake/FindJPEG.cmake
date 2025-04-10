e59992d39fa3a12954941ac076a6793368d51d91
biagas <biagas2@llnl.gov>
2025-04-10 14:24:52 -0700

    Import Jpeg.

--- src/CMake/FindJPEG.cmake
+++ src/CMake/FindJPEG.cmake
@@ -13,26 +13,83 @@
 #   Kathleen Biagas, Tue Mar 11 10:44:54 MST 2014
 #   Change how newer versions are handled.
 #
-#   Kathleen Biagas, Tue Aug 2, 2022
-#   Set JPEG_LIBRARY, needed for VTK 9.
+#   Kathleen Biagas, Thu Apr 10, 2025
+#   Modified to create an INTERFACE target via blt_import_library.
+#   This allows jpeg to be a native CMake target, and will also be accounted
+#   for in the generated export set.
 #
 #****************************************************************************/
 
-# Use the JPEG_DIR hint from the config-site .cmake file
+# Uses the JPEG_DIR hint from the config-site .cmake file
 
-IF (WIN32)
-  if (JPEG_LIBNAME_PREFIX_LIB)
-      SET_UP_THIRD_PARTY(JPEG LIBS libjpeg)
-  else()
-      SET_UP_THIRD_PARTY(JPEG LIBS jpeg)
-  endif()
-  # VTK's (9) Find needs this var set.
-  set(JPEG_LIBRARY ${JPEG_LIBRARY_DIR}/${JPEG_LIB})
-ELSE()
-  if (VISIT_JPEG_DIR)
-      SET_UP_THIRD_PARTY(JPEG LIBS jpeg)
-      # VTK's (9) Find needs this var set.
-      set(JPEG_LIBRARY ${JPEG_LIBRARY_DIR}/${JPEG_LIB})
-  endif()
-ENDIF (WIN32)
+if(JPEG_DIR)
+
+    find_path(_jpeg_INCLUDE_DIR jpeglib.h
+              PATHS ${JPEG_DIR}
+              PATH_SUFFIXES include
+              NO_DEFAULT_PATH)
+
+    find_library(_jpeg_LIBRARY
+             NAMES jpeg libjpeg
+             PATHS ${JPEG_DIR}
+             PATH_SUFFIXES lib lib64
+             NO_DEFAULT_PATH)
+
+    find_package_handle_standard_args(JPEG DEFAULT_MSG
+        _jpeg_INCLUDE_DIR
+        _jpeg_LIBRARY)
+
+    if(JPEG_FOUND)
+        get_filename_component(libjpeg ${_jpeg_LIBRARY} NAME)
+
+        ####
+        # VTK needs this var set in order to find our version of jpeg
+        set(JPEG_LIBRARY ${_jpeg_LIBRARY})
+        ####
+
+        blt_import_library(
+            NAME        jpeg
+            INCLUDES    $<BUILD_INTERFACE:${_jpeg_INCLUDE_DIR}>
+                        $<INSTALL_INTERFACE:${VISIT_INSTALLED_VERSION_INCLUDE}/jpeg/include>
+            LIBRARIES   $<BUILD_INTERFACE:${_jpeg_LIBRARY}>
+            EXPORTABLE  ON)
+
+
+        # CMake doesn't prepend ${_IMPORT_PREFIX} in the generated export
+        # set for INTERFACE libs, so "${IMPORT_PREFIX}" needs to be
+        # explicitly added to the INSTALL_INTERFACE, and escaped so it
+        # doesn't get evaluated.
+        #
+        # Also, it seems if the INSTALL_INTERFACE is used in the
+        # blt_import_library,  the ${_IMPORT_PREFIX} is stripped, even
+        # if it is escaped, so it appears to be getting evaluated.
+        # That's why it is added separately here instead of being added
+        # to the LIBRARIES above.
+        target_link_libraries(jpeg INTERFACE
+            $<INSTALL_INTERFACE:\${_IMPORT_PREFIX}/${VISIT_INSTALLED_VERSION_LIB}/${libjpeg}>)
+
+        # install and export
+        if(VISIT_INSTALL_THIRD_PARTY)
+            visit_install_export_targets(jpeg)
+            THIRD_PARTY_INSTALL_INCLUDE(jpeg ${_jpeg_INCLUDE_DIR})
+        endif()
+
+        if(WIN32)
+            # need to copy the dll to the build dir
+            cmake_path(REPLACE_EXTENSION _libjpeg dll OUTPUT_VARIABLE _jpeg_DLL)
+            if(EXISTS ${_jpeg_DLL})
+                cmake_path(SET _jpeg_DLL ${JPEG_LIBRARY_DIR}/${_jpeg_DLL})
+            else()    
+                cmake_path(SET _jpeg_DLL NORMALIZE ${JPEG_LIBRARY_DIR}/../bin/${_jpeg_DLL})
+            endif()
+            if(EXISTS ${_jpeg_DLL})
+                execute_process(COMMAND ${CMAKE_COMMAND} -E copy
+                                ${_jpeg_DLL}
+                                ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/ThirdParty)
+            endif()
+        endif()
+    else()
+        message(STATUS "Jpeg was requested and it could not be found. Tried JPEG_DIR: ${JPEG_DIR}")
+    endif()
+endif()
 
