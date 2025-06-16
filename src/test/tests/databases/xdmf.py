2ce69fe056b933f165f0ceb32353b0d586752664
Mark C. Miller <miller86@llnl.gov>
2025-06-16 00:21:55 -0700

    more commentary

--- src/test/tests/databases/xdmf.py
+++ src/test/tests/databases/xdmf.py
@@ -419,10 +419,16 @@ def test8(datapath):
 # This test is designed to manifest large differences if for any reason there
 # is a mixup in the dimensions of data in the Xdmf HDF5 file vs. the dimensions
 # of the Xdmf mesh in the .xmf file. The input data, `mydata`, in the HDF5 file
-# is the function x^2+y^2+z^2 which has gradient {2x,2y,2z}. So, we compute the
-# gradient of the input data and take the difference between it and an expression
-# for the expected gradient. If the dimension ordering is mixed up, the magnitude
-# of diffs will be large and the ValueLT test will fail.
+# has dimension sizes which are prime numbers together with deltas that define
+# a uniform grid. The data defines the function x^2+y^2+z^2 which has gradient
+# {2x,2y,2z}. So, we compute the gradient of the input data and take the difference
+# between it and an expression for the expected gradient. If the dimension ordering
+# is mixed up, the magnitude of diffs will be large and the ValueLT test will fail.
+#
+# Note that in both Xdmf `Dimensions` attributes and HDF5 datasets, dimensions are
+# ordered ZYX. That is, for the dimensions in order 5 7 11, Nz=5, Ny=7 and Nx=11.
+# In C code, an array of dimensions, int dims[3], dims[0] is for X, dims[1] for Y
+# and dims[2] for Z.
 #
 def test9(datapath):
     TestSection("Uniform Dimension Ordering")
