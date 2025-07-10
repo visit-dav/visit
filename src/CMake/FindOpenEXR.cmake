d7a1ba4236225877a8e13be090a9a666c3598e9d
biagas <biagas2@llnl.gov>
2025-07-10 15:16:09 -0700

    Update OpenEXR to version 3.3.4. Maintain support for 2.2.0 for now.

--- src/CMake/FindOpenEXR.cmake
+++ src/CMake/FindOpenEXR.cmake
@@ -2,6 +2,10 @@
 # Project developers.  See the top-level LICENSE file for dates and other
 # details.  No copyright assignment is required to contribute to VisIt.
 
+# Modifications:
+#   Kathleen Biagas, Thu July 10, 2025
+#   Add support for version 3.3.4.
+
 # Use the OPENEXR_DIR hint from the config-site .cmake file
 
 # Force SET_UP_THIRD_PARTY to make the OPENEXR_LIB variable contain full paths
@@ -9,22 +13,26 @@
 # all over the place in the build.
 SET(SET_UP_THIRD_PARTY_FULL_PATH ON)
 
-IF (WIN32)
-  SET_UP_THIRD_PARTY(OPENEXR
-     INCDIR include/OpenEXR
-     LIBS IlmImf-2_2 Imath-2_2 Half IlmThread-2_2 Iex-2_2)
-ELSE (WIN32)
-  IF(APPLE)
-    # These libraries are being build shared on Apple. We can find them without
-    # version suffixes but then they do not install properly during "make install".
-    # If we add version suffixes then "make install/make package" do what we want.
-    SET_UP_THIRD_PARTY(OPENEXR
-      INCDIR include/OpenEXR
-      LIBS IlmImf-2_2.22 Imath-2_2.12 Half.12 IlmThread-2_2.12 Iex-2_2.12)
-  ELSE(APPLE)
-    SET_UP_THIRD_PARTY(OPENEXR
-      INCDIR include/OpenEXR
-      LIBS IlmImf Imath Half IlmThread Iex)
-  ENDIF(APPLE)
-ENDIF (WIN32)
 
+if(OPENEXR_VERSION AND OPENEXR_VERSION GREATER_EQUAL(3.3.4))
+    SET_UP_THIRD_PARTY(OPENEXR
+        LIBS Iex-3_3 IlmThread-3_3 Imath-3_1 OpenEXR-3_3) 
+else()
+
+    if(WIN32)
+        SET_UP_THIRD_PARTY(OPENEXR
+            INCDIR include/OpenEXR
+            LIBS IlmImf-2_2 Imath-2_2 Half IlmThread-2_2 Iex-2_2)
+    elseif(APPLE)
+        # These libraries are being build shared on Apple. We can find them without
+        # version suffixes but then they do not install properly during "make install".
+        # If we add version suffixes then "make install/make package" do what we want.
+        SET_UP_THIRD_PARTY(OPENEXR
+            INCDIR include/OpenEXR
+            LIBS IlmImf-2_2.22 Imath-2_2.12 Half.12 IlmThread-2_2.12 Iex-2_2.12)
+    else()
+        SET_UP_THIRD_PARTY(OPENEXR
+            INCDIR include/OpenEXR
+            LIBS IlmImf Imath Half IlmThread Iex)
+    endif()
+endif()
--- src/CMake/PluginVsInstallHelpers.cmake
+++ src/CMake/PluginVsInstallHelpers.cmake
@@ -27,6 +27,9 @@
 #   Kathleen Biagas, Fri Mar 10, 2023 
 #   Replaced VTKh logic with VTKm.
 #
+#   Kathleen Biagas, Thu July 10, 2025
+#   Support OPENEXR libs in 'lib64'.
+#
 #******************************************************************************
 
 
@@ -65,6 +68,9 @@ install(FILES ${VISIT_SOURCE_DIR}/CMake/VisItIncludeVars.cmake
 set(check_libs SILO XDMF OPENEXR)
 foreach(cl ${check_libs})
     string(REPLACE "${VISIT_${cl}_DIR}/lib/" ""
+                    ${cl}_LIB
+                    "${${cl}_LIB}")
+    string(REPLACE "${VISIT_${cl}_DIR}/lib64/" ""
                     ${cl}_LIB
                     "${${cl}_LIB}")
 
