b710715ac670c01448293b10acfe88f833187f48
Mark C. Miller <5720676+markcmiller86@users.noreply.github.com>
2025-10-15 10:26:47 -0700

    Fix compilation issues for unv plugin (#20662)
    * fix compilation issues
    
    * revert accidental change

--- src/databases/unv/avtunvFileFormat.C
+++ src/databases/unv/avtunvFileFormat.C
@@ -30,7 +30,7 @@
 #include <avtunvFileFormat.h>
 
 #include <string>
-#include <stdint.h>
+#include <cstdint>
 
 #include <vtkFloatArray.h>
 #include <vtkRectilinearGrid.h>
@@ -4548,7 +4548,7 @@ avtunvFileFormat::ReadFile()
             if (igtyp == 3)
             {
                 // This is the ARL format:
-                char sousbuf[10];
+                char sousbuf[16];
                 int ic ;
                 int nbcells ;
                 sscanf(buf+4, "%d%d%d", &nbnodes, &nbcells, &nb2dmats) ;
@@ -4721,7 +4721,7 @@ avtunvFileFormat::ReadFile()
             else if (igtyp > 0)
             {
                 // This is the legacy format:
-                char sousbuf[10];
+                char sousbuf[16];
                 int ic ;
                 for (ic=0; ic < 4; ic++)
                 {
