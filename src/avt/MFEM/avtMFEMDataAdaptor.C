59256e2bac855ab51b0023c8489a74302a874f99
Justin Privitera <privitera1@llnl.gov>
2025-11-14 15:43:10 -0800

    remove todos

--- src/avt/MFEM/avtMFEMDataAdaptor.C
+++ src/avt/MFEM/avtMFEMDataAdaptor.C
@@ -406,7 +406,6 @@ avtMFEMDataAdaptor::RefineMeshToVTK(mfem::Mesh *mesh,
         // that disconnects the result for us, we choose to fall back to legacy
         // LOR.
 
-        // TODO we want to revisit this and see if there is a better way
         // The problem is, there is no easy way to tell if a mesh is periodic.
         // We know that all periodic meshes are L2, but not all L2 meshes are
         // periodic. So our best bet is to catch all L2 meshes and fall back
@@ -990,7 +989,6 @@ avtMFEMDataAdaptor::RefineGridFunctionToVTK(mfem::Mesh *mesh,
         // that disconnects the result for us, we choose to fall back to legacy
         // LOR.
 
-        // TODO we want to revisit this and see if there is a better way
         // The problem is, there is no easy way to tell if a mesh is periodic.
         // We know that all periodic meshes are L2, but not all L2 meshes are
         // periodic. So our best bet is to catch all L2 meshes and fall back
