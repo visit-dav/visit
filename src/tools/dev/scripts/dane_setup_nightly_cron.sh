8c1fe586a91eba560cb5021617fe139b6fd8d85e
Cyrus Harrison <cyrush@llnl.gov>
2025-12-01 14:48:53 -0800

    all helper to setup cron scripts

---
+++ src/tools/dev/scripts/dane_setup_nightly_cron.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# copy current scripts to shared exe location in /usr/workspace/visit
+
+cp dane_cron_script_run_test_suite.sh dane_cron_script_change_perms.sh /usr/workspace/visit/test_trunk/nightly_chron/
+
+# Set the permissions so that others may access the test directory.
+chgrp -R visit /usr/workspace/visit/test_trunk/nightly_chron/
+chmod -R g+rwX,o+rX /usr/workspace/visit/test_trunk/nightly_chron/
+
+# install crontab
+crontab dane_crontab
+
