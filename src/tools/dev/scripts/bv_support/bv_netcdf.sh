979b7e9c20535ee19b786e511f5d27b4c860132e
Mark C. Miller <5720676+markcmiller86@users.noreply.github.com>
2025-12-12 18:15:21 -0800

    fix for HDF5-2.0

--- src/tools/dev/scripts/bv_support/bv_netcdf.sh
+++ src/tools/dev/scripts/bv_support/bv_netcdf.sh
@@ -299,9 +299,347 @@ EOF
     return 0;
 }
 
+function apply_netcdf_hdf520_patches
+{
+    info "Patching netcdf for HDF5-2.0 (patch #1)"
+    local retval=0
+    pushd $NETCDF_BUILD_DIR 1>/dev/null 2>&1
+    patch -p0 << \EOF
+diff -c libsrc4/nc4internal.h libsrc4.patched/nc4internal.h
+*** libsrc4/nc4internal.h	2010-01-19 09:45:45.000000000 -0800
+--- libsrc4.patched/nc4internal.h	2025-12-11 19:39:28.947231000 -0800
+***************
+*** 14,29 ****
+  #include <stdarg.h>
+  #include <string.h>
+  #include "netcdf3.h"
+  #include <hdf5.h>
+  #include <netcdf.h>
+  
+- #ifdef USE_PARALLEL
+- #include <mpi.h>
+- #else
+- #define MPI_Info int
+- #define MPI_Comm int
+- #endif /* USE_PARALLEL */
+- 
+  #ifdef USE_HDF4
+  #include <mfhdf.h>
+  #endif
+--- 14,23 ----
+  #include <stdarg.h>
+  #include <string.h>
+  #include "netcdf3.h"
++ #define H5_USE_18_API
+  #include <hdf5.h>
+  #include <netcdf.h>
+  
+  #ifdef USE_HDF4
+  #include <mfhdf.h>
+  #endif
+***************
+*** 376,382 ****
+  NC_FILE_INFO_T *nc4_find_nc_file(int ncid);
+  int nc4_find_dim(NC_GRP_INFO_T *grp, int dimid, NC_DIM_INFO_T **dim, NC_GRP_INFO_T **dim_grp);
+  int nc4_find_dim_len(NC_GRP_INFO_T *grp, int dimid, size_t **len);
+! int nc4_find_type(NC_HDF5_FILE_INFO_T *h5, int typeid, NC_TYPE_INFO_T **type);
+  NC_TYPE_INFO_T *nc4_rec_find_nc_type(NC_GRP_INFO_T *start_grp, hid_t target_nc_typeid);
+  NC_TYPE_INFO_T *nc4_rec_find_hdf_type(NC_GRP_INFO_T *start_grp, hid_t target_hdf_typeid);
+  NC_TYPE_INFO_T *nc4_rec_find_named_type(NC_GRP_INFO_T *start_grp, char *name);
+--- 370,376 ----
+  NC_FILE_INFO_T *nc4_find_nc_file(int ncid);
+  int nc4_find_dim(NC_GRP_INFO_T *grp, int dimid, NC_DIM_INFO_T **dim, NC_GRP_INFO_T **dim_grp);
+  int nc4_find_dim_len(NC_GRP_INFO_T *grp, int dimid, size_t **len);
+! int nc4_find_type(NC_HDF5_FILE_INFO_T *h5, int64_t typeid, NC_TYPE_INFO_T **type);
+  NC_TYPE_INFO_T *nc4_rec_find_nc_type(NC_GRP_INFO_T *start_grp, hid_t target_nc_typeid);
+  NC_TYPE_INFO_T *nc4_rec_find_hdf_type(NC_GRP_INFO_T *start_grp, hid_t target_hdf_typeid);
+  NC_TYPE_INFO_T *nc4_rec_find_named_type(NC_GRP_INFO_T *start_grp, char *name);
+EOF
+
+    if [[ $? != 0 ]] ; then
+      warn "netcdf patch #1 for HDF5-2.0 failed."
+      retval=1
+    fi
+
+    info "Patching netcdf for HDF5-2.0 (patch #2)"
+    patch -p0 << \EOF
+diff -c libsrc4/nc4type.c libsrc4.patched/nc4type.c
+*** libsrc4/nc4type.c	2010-01-11 11:27:12.000000000 -0800
+--- libsrc4.patched/nc4type.c	2025-12-11 19:44:24.385210000 -0800
+***************
+*** 317,323 ****
+  
+  /* Insert a named array into a compound type. */
+  EXTERNL int
+! nc_insert_array_compound(int ncid, int typeid, const char *name, 
+  			 size_t offset, nc_type field_typeid,
+  			 int ndims, const int *dim_sizesp)
+  {
+--- 317,323 ----
+  
+  /* Insert a named array into a compound type. */
+  EXTERNL int
+! nc_insert_array_compound(int ncid, nc_type typeid, const char *name, 
+  			 size_t offset, nc_type field_typeid,
+  			 int ndims, const int *dim_sizesp)
+  {
+EOF
+
+    if [[ $? != 0 ]] ; then
+      warn "netcdf patch #2 for HDF5-2.0 failed."
+      retval=1
+    fi
+
+    info "Patching netcdf for HDF5-2.0 (patch #3)"
+    patch -p0 << \EOF
+diff -c libsrc4/netcdf_base.h libsrc4.patched/netcdf_base.h
+*** libsrc4/netcdf_base.h	2010-01-21 08:00:18.000000000 -0800
+--- libsrc4.patched/netcdf_base.h	2025-12-10 20:52:04.370967000 -0800
+***************
+*** 38,44 ****
+  #include <stddef.h> /* size_t, ptrdiff_t */
+  #include <errno.h>  /* netcdf functions sometimes return system errors */
+  
+! typedef int nc_type;
+  
+  #if defined(__cplusplus)
+  extern "C" {
+--- 38,44 ----
+  #include <stddef.h> /* size_t, ptrdiff_t */
+  #include <errno.h>  /* netcdf functions sometimes return system errors */
+  
+! typedef int64_t nc_type;
+  
+  #if defined(__cplusplus)
+  extern "C" {
+EOF
+
+    if [[ $? != 0 ]] ; then
+      warn "netcdf patch #3 for HDF5-2.0 failed."
+      retval=1
+    fi
+
+    info "Patching netcdf for HDF5-2.0 (patch #4)"
+    patch -p0 << \EOF
+diff -c libsrc4/netcdf.h libsrc4.patched/netcdf.h
+*** libsrc4/netcdf.h	2010-04-12 11:48:02.000000000 -0700
+--- libsrc4.patched/netcdf.h	2025-12-12 18:08:44.691818000 -0800
+***************
+*** 37,51 ****
+  
+  #include <stddef.h> /* size_t, ptrdiff_t */
+  #include <errno.h>  /* netcdf functions sometimes return system errors */
+  /* These defs added by netCDF configure because parallel HDF5 is not present. */
+  #ifndef MPI_INCLUDED
+! #define MPI_Comm int
+! #define MPI_Info int
+! #define MPI_COMM_WORLD 0
+! #define MPI_INFO_NULL 0
+  #endif
+  
+! typedef int nc_type;
+  
+  #if defined(__cplusplus)
+  extern "C" {
+--- 37,52 ----
+  
+  #include <stddef.h> /* size_t, ptrdiff_t */
+  #include <errno.h>  /* netcdf functions sometimes return system errors */
++ #include <stdint.h>
+  /* These defs added by netCDF configure because parallel HDF5 is not present. */
+  #ifndef MPI_INCLUDED
+! #define MPI_Comm_spoof int
+! #define MPI_Info_spoof int
+! #define MPI_COMM_WORLD_SPOOF 0
+! #define MPI_INFO_NULL_SPOOF 0
+  #endif
+  
+! typedef int64_t nc_type;
+  
+  #if defined(__cplusplus)
+  extern "C" {
+***************
+*** 382,392 ****
+  nc_open(const char *path, int mode, int *ncidp);
+  
+  EXTERNL int
+! nc_create_par(const char *path, int cmode, MPI_Comm comm, MPI_Info info,
+  	      int *ncidp);
+  
+  EXTERNL int
+! nc_open_par(const char *path, int mode, MPI_Comm comm, MPI_Info info,
+  	    int *ncidp);
+  
+  /* Use these with nc_var_par_access(). */
+--- 383,393 ----
+  nc_open(const char *path, int mode, int *ncidp);
+  
+  EXTERNL int
+! nc_create_par(const char *path, int cmode, MPI_Comm_spoof comm, MPI_Info_spoof info,
+  	      int *ncidp);
+  
+  EXTERNL int
+! nc_open_par(const char *path, int mode, MPI_Comm_spoof comm, MPI_Info_spoof info,
+  	    int *ncidp);
+  
+  /* Use these with nc_var_par_access(). */
+EOF
+
+    if [[ $? != 0 ]] ; then
+      warn "netcdf patch #4 for HDF5-2.0 failed."
+      retval=1
+    fi
+
+    info "Patching netcdf for HDF5-2.0 (patch #5)"
+    patch -p0 << \EOF
+diff -r -c libsrc4/tst_h_par.c libsrc4.patched/tst_h_par.c
+*** libsrc4/tst_h_par.c	2009-03-06 06:38:23.000000000 -0800
+--- libsrc4.patched/tst_h_par.c	2025-12-12 17:55:31.683605000 -0800
+***************
+*** 38,45 ****
+  #endif /* USE_MPE */
+  
+     MPI_Init(&argc, &argv);
+!    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+!    MPI_Comm_size(MPI_COMM_WORLD, &p);
+  
+  #ifdef USE_MPE
+     MPE_Init_log();
+--- 38,45 ----
+  #endif /* USE_MPE */
+  
+     MPI_Init(&argc, &argv);
+!    MPI_Comm_rank(MPI_COMM_WORLD_SPOOF, &my_rank);
+!    MPI_Comm_size(MPI_COMM_WORLD_SPOOF, &p);
+  
+  #ifdef USE_MPE
+     MPE_Init_log();
+***************
+*** 85,91 ****
+  
+        /* Create file. */
+        if ((fapl_id = H5Pcreate(H5P_FILE_ACCESS)) < 0) ERR;
+!       if (H5Pset_fapl_mpio(fapl_id, MPI_COMM_WORLD, MPI_INFO_NULL) < 0) ERR;
+        if ((fileid = H5Fcreate(FILE_NAME, H5F_ACC_TRUNC, H5P_DEFAULT, 
+        fapl_id)) < 0) ERR;
+  
+--- 85,91 ----
+  
+        /* Create file. */
+        if ((fapl_id = H5Pcreate(H5P_FILE_ACCESS)) < 0) ERR;
+!       if (H5Pset_fapl_mpio(fapl_id, MPI_COMM_WORLD_SPOOF, MPI_INFO_NULL_SPOOF) < 0) ERR;
+        if ((fileid = H5Fcreate(FILE_NAME, H5F_ACC_TRUNC, H5P_DEFAULT, 
+        fapl_id)) < 0) ERR;
+  
+***************
+*** 134,140 ****
+  #endif /* USE_MPE */
+        }
+        write_us = (MPI_Wtime() - ftime) * MILLION;
+!       MPI_Reduce(&write_us, &max_write_us, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+        if (!my_rank)
+        {
+  	 write_rate = (float)(DIM2_LEN * sizeof(int))/(float)max_write_us;      
+--- 134,140 ----
+  #endif /* USE_MPE */
+        }
+        write_us = (MPI_Wtime() - ftime) * MILLION;
+!       MPI_Reduce(&write_us, &max_write_us, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD_SPOOF);
+        if (!my_rank)
+        {
+  	 write_rate = (float)(DIM2_LEN * sizeof(int))/(float)max_write_us;      
+***************
+*** 160,166 ****
+  
+        /* Open the file. */
+        if ((fapl_id = H5Pcreate(H5P_FILE_ACCESS)) < 0) ERR;
+!       if (H5Pset_fapl_mpio(fapl_id, MPI_COMM_WORLD, MPI_INFO_NULL) < 0) ERR;
+  
+  
+        if (H5Pset_libver_bounds(fapl_id, H5F_LIBVER_LATEST, H5F_LIBVER_LATEST) < 0) ERR;
+--- 160,166 ----
+  
+        /* Open the file. */
+        if ((fapl_id = H5Pcreate(H5P_FILE_ACCESS)) < 0) ERR;
+!       if (H5Pset_fapl_mpio(fapl_id, MPI_COMM_WORLD_SPOOF, MPI_INFO_NULL_SPOOF) < 0) ERR;
+  
+  
+        if (H5Pset_libver_bounds(fapl_id, H5F_LIBVER_LATEST, H5F_LIBVER_LATEST) < 0) ERR;
+***************
+*** 205,211 ****
+  /* 	    } */
+        }
+        read_us = (MPI_Wtime() - ftime) * MILLION;
+!       MPI_Reduce(&read_us, &max_read_us, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+        if (!my_rank)
+        {
+  	 read_rate = (float)(DIM2_LEN * sizeof(int))/(float)max_read_us;      
+--- 205,211 ----
+  /* 	    } */
+        }
+        read_us = (MPI_Wtime() - ftime) * MILLION;
+!       MPI_Reduce(&read_us, &max_read_us, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD_SPOOF);
+        if (!my_rank)
+        {
+  	 read_rate = (float)(DIM2_LEN * sizeof(int))/(float)max_read_us;      
+EOF
+
+    if [[ $? != 0 ]] ; then
+      warn "netcdf patch #5 for HDF5-2.0 failed."
+      retval=1
+    fi
+
+    info "Patching netcdf for HDF5-2.0 (patch #6)"
+    patch -p0 << \EOF
+diff -c configure configure.patched
+*** configure	2010-04-12 11:47:47.000000000 -0700
+--- configure.patched	2025-12-12 00:28:23.357195000 -0800
+***************
+*** 25760,25767 ****
+     else
+        { $as_echo "$as_me:${as_lineno-$LINENO}: result: without parallel" >&5
+  $as_echo "without parallel" >&6; }
+-       # Modify netcdf.h to include defs for missing parallel stuff.
+-       sed '/#include <errno.h>/{G;s/$/\/* These defs added by netCDF configure because parallel HDF5 is not present. *\//;G;s/$/#ifndef MPI_INCLUDED/;G;s/$/#define MPI_Comm int/;G;s/$/#define MPI_Info int/;G;s/$/#define MPI_COMM_WORLD 0/;G;s/$/#define MPI_INFO_NULL 0/;G;s/$/#endif/;}' libsrc4/netcdf_base.h > libsrc4/netcdf.h
+     fi
+  
+     { $as_echo "$as_me:${as_lineno-$LINENO}: checking whether parallel I/O features are to be included" >&5
+--- 25760,25765 ----
+***************
+*** 25933,25940 ****
+     fi
+  else
+  # !netcdf-4
+!    # Modify netcdf.h to include defs for missing parallel stuff.
+!    sed '/#include <errno.h>/{G;s/$/\/* These defs added by netCDF configure because parallel HDF5 is not present. *\//;G;s/$/#ifndef MPI_INCLUDED/;G;s/$/#define MPI_Comm int/;G;s/$/#define MPI_Info int/;G;s/$/#define MPI_COMM_WORLD 0/;G;s/$/#define MPI_INFO_NULL 0/;G;s/$/#endif/;}' libsrc4/netcdf_base.h > libsrc4/netcdf.h
+  fi
+  
+  # Restore LDFLAGS to it's old value.
+--- 25931,25937 ----
+     fi
+  else
+  # !netcdf-4
+!     :
+  fi
+  
+  # Restore LDFLAGS to it's old value.
+EOF
+
+    if [[ $? != 0 ]] ; then
+      warn "netcdf patch #6 for HDF5-2.0 failed."
+      retval=1
+    fi
+
+    popd 1>/dev/null 2>&1
+
+    return $retval;
+}
+
 function apply_netcdf_patch
 {
     apply_netcdf_patch_for_exodusii
+
+    apply_netcdf_hdf520_patches
 
     if [[ ${NETCDF_VERSION} == 4.1.1 ]] ; then
         if [[ "$OPSYS" == "Darwin" ]] ; then
@@ -407,11 +745,25 @@ function build_netcdf
     fi
     ZLIBARGS="--with-zlib=$VISITDIR/zlib/$ZLIB_VERSION/$VISITARCH"
 
+    #
+    # Ensure that if #include hdf5.h turns around and does #include mpi.h,
+    # netcdf's compilation will find mpi.h...even though we're not ever
+    # going to use any netcdf parallel features.
+    #
+    probe_hdf5_mpi_dependence
+
+    #
+    # To prevent possible enablement of netcdf parallel features if HDF5
+    # itself is compiled for parallel, we apriori set two controlling
+    # autotools cache variables to "no".
+    #
     C_OPT_FLAGS="-Wno-error=implicit-function-declaration"
     set -x
     env ./configure CXX="$CXX_COMPILER" CC="$C_COMPILER" \
-                CFLAGS="$CFLAGS $C_OPT_FLAGS" CXXFLAGS="$CXXFLAGS $CXX_OPT_FLAGS" \
+                CFLAGS="$CFLAGS $C_OPT_FLAGS $VISIT_HDF5_MPI_INCLUDE_FLAG" CXXFLAGS="$CXXFLAGS $CXX_OPT_FLAGS $VISIT_HDF5_MPI_INCLUDE_FLAG" \
                 FC="" FCFLAGS="" $EXTRA_AC_FLAGS $EXTRA_FLAGS --enable-cxx-4 $H5ARGS $ZLIBARGS \
+                ac_cv_lib_hdf5_H5Pget_fapl_mpiposix=no \
+                ac_cv_lib_hdf5_H5Pget_fapl_mpio=no \
                 --disable-dap --disable-fortran \
                 --prefix="$VISITDIR/netcdf/$NETCDF_VERSION/$VISITARCH"
     set +x
