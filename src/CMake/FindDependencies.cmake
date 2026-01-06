7bacf0d9de9ed49c9fdbd2effcce60663c10a632
biagas <biagas2@llnl.gov>
2026-01-06 14:17:54 -0800

    Update comments, remove unused code.

--- src/CMake/FindDependencies.cmake
+++ src/CMake/FindDependencies.cmake
@@ -1,10 +1,8 @@
 
 ###############################################################################
 # FindDependencies
-# This file defines:
+#
 ###############################################################################
-
-#include(CMakeFindDependencyMacro)
 
 
 # Compute the installation prefix relative to this file.
@@ -23,24 +21,6 @@ if(VISIT_PARALLEL)
     include(${CMAKE_CURRENT_LIST_DIR}/VisItParallel.cmake)
 endif()
 
-# some things needed by VTK that aren't automatically setup yet:
-
-#add_library(ZLIB::ZLIB SHARED IMPORTED)
-#set_target_properties(ZLIB::ZLIB PROPERTIES
-#        INTERFACE_INCLUDE_DIRECTORIES ${_TP_PREFIX}/include/zlib
-#        IMPORTED_LOCATION ${_TP_PREFIX}/lib/libzlib.so)
-
-#add_library(OSMesa::OSMesa SHARED IMPORTED)
-#set_target_properties(OSMesa::OSMesa PROPERTIES
-#    INTERFACE_INCLUDE_DIRECTORIES ${_TP_PREFIX}/include/mesagl
-#    IMPORTED_LOCATION ${_TP_PREFIX}/lib/libOSMesa.so.8)
-
-#if(HAVE_OSPRAY)
-#    add_library(ospray::ospray SHARED IMPORTED)
-#    set_target_properties(ospray::ospray PROPERTIES
-#        INTERFACE_INCLUDE_DIRECTORIES ${_TP_PREFIX}/include/ospray
-#        IMPORTED_LOCATION ${_TP_PREFIX}/lib/libospray.so)
-#endif()
 
 ###############################################################################
 # Import Qt's CMake targets
@@ -48,10 +28,6 @@ endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/SetupQT.cmake)
 
-# satisfy VTK
-if(HAVE_OSMESA)
-    add_library(OSMesa::OSMesa SHARED IMPORTED)
-endif()
 
 ###############################################################################
 # Import Python's CMake targets
