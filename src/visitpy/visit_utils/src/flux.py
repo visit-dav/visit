459297dc24b5dd2f3a958bbfec8d0753f6bcc1af
Cyrus Harrison <cyrush@llnl.gov>
2026-01-22 13:24:32 -0800

    add flux batch helper to visit_utils py module (#20789)
    * add flux batch helper to visit_utils py module
    
    * avoid crossing streams with flux py env

---
+++ src/visitpy/visit_utils/src/flux.py
@@ -0,0 +1,53 @@
+# Copyright (c) Lawrence Livermore National Security, LLC and other VisIt
+# Project developers.  See the top-level LICENSE file for dates and other
+# details.  No copyright assignment is required to contribute to VisIt.
+
+#*****************************************************************************
+"""
+ file: flux.py
+ description:
+    Provides a python command for flux submission.
+"""
+
+import os
+from visit_utils.common import *
+
+class FluxBatchError(Exception):
+    def __init__(self,emsg):
+        self.msg = "<flux batch error:> " + emsg
+
+def flux_batch(cmd,rmin,nnodes=1,mach=None,part=None,bank=None,rdir=None,obase=None,depend=None):
+    if mach is None:
+        mach = hostname(False)
+    # create output file name
+    if obase is None:
+        ctoks = cmd.split()
+        sname = os.path.split(ctoks[0])[1]
+        obase = sname
+    ofile = "out.flux.batch.%s.%s.%s.txt" % (obase,hostname(),timestamp())
+    xcmd  = "flux batch --output %s -N %d -t %s" % (ofile,nnodes, str(rmin))
+    if not rdir is None:
+        xcmd += " -d %s " % (os.path.abspath(rdir))
+    if not depend is None:
+        xcmd += " -after:%s " % depend
+    if not part is None:
+        xcmd += " -q %s" % part
+    if not bank is None:
+        xcmd += " -B %s" % bank
+    xcmd += " %s" % cmd
+    #
+    # Strip env vars that may undermine using flux.
+    #
+    # flux is a python animal, and may fail if not run
+    # within its own python environment.
+    shell_env = os.environ.copy()
+    for k in ["PYTHONHOME","PYTHONPATH"]:
+        if k in shell_env:
+            del shell_env[k]
+    ret,out = sexe(xcmd,ret_output=True,echo=True, env=shell_env)
+    if ret == 0:
+        jid = out.strip()
+        return jid, ofile
+    else:
+        raise FluxBatchError(out)
+
