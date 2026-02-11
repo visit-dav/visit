0ab32a265e2df01e7e9618d341f13fa999ca2b14
Mark C. Miller <5720676+markcmiller86@users.noreply.github.com>
2026-02-11 15:51:30 -0800

    push hacked FindHDF5

--- src/CMake/FindHDF5.cmake
+++ src/CMake/FindHDF5.cmake
@@ -42,8 +42,93 @@ IF(WIN32)
     endif()
   endif()
 ELSE()
-  SET_UP_THIRD_PARTY(HDF5 LIBS hdf5 hdf5_hl)
-  IF(VISIT_PARALLEL)
-      SET_UP_THIRD_PARTY(HDF5_MPI LIBS hdf5 hdf5_hl)
-  ENDIF(VISIT_PARALLEL)
+  find_package(HDF5
+    PATHS ${VISIT_HDF5_DIR}
+    NO_DEFAULT_PATH
+  )
+
+
+# After find_package(HDF5 ...)
+get_cmake_property(_vars VARIABLES)
+
+list(SORT _vars)
+foreach(v IN LISTS _vars)
+  if(v MATCHES "^(HDF5|hdf5)_")
+    # Avoid noisy internal variables unless you want them
+    message(STATUS "${v}='${${v}}'")
+  endif()
+endforeach()
+
+set(HDF5_LIBRARY_DIR ${HDF5_BUILD_DIR}/lib)
+set(HDF5_INCLUDE_DIR ${HDF5_BUILD_DIR}/include)
+set(HDF5_LIB hdf5-shared)
+
+#-- HDF5_ARCHITECTURE=''
+#- HDF5_BUILD_DIR='/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3'
+#- HDF5_BUILD_MODE='Release'
+#- HDF5_CONFIG='/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3/cmake/hdf5-config.cmake'
+#- HDF5_CONSIDERED_CONFIGS='/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3/cmake/hdf5-config.cmake'
+#- HDF5_CONSIDERED_VERSIONS='2.0.0'
+#- HDF5_C_SHARED_LIBRARY='hdf5-shared'
+#- HDF5_DEFAULT_API_VERSION='v200'
+#- HDF5_DIR='/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3/cmake'
+#- HDF5_EXPORT_LIBRARIES='hdf5-shared;hdf5_tools-shared;hdf5_hl-shared'
+#- HDF5_FIND_REQUIRED_shared_C='true'
+#- HDF5_FOUND='1'
+#- HDF5_HL_SHARED_LIBRARY='hdf5_hl-shared'
+#- HDF5_INCLUDE_DIR='/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3/include;'
+#- HDF5_INCLUDE_DIR_HL='/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3/include'
+#- HDF5_INCLUDE_DIR_TOOLS='/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3/include'
+#- HDF5_INSTALL_MOD_FORTRAN=''
+#- HDF5_JAVA_INCLUDE_DIRS='/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3/lib/;/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3/lib/;/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3/lib/;/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3/lib/'
+#- HDF5_LIBDEP='/usr/WS1/visit/visit/thirdparty_shared/3.5.0-python3.13/toss4/szip/2.1/linux-x86_64_gcc-10.3/lib;sz;/usr/WS1/visit/visit/thirdparty_shared/3.5.0-python3.13/toss4/zlib/1.3.1/linux-x86_64_gcc-10.3/lib;z'
+#- HDF5_LIBNAMES_AFFIX_DLL='ON'
+#- HDF5_LIB_TYPE='shared'
+#- HDF5_MPI_C_INCLUDE_PATH='/usr/tce/packages/mvapich2/mvapich2-2.3.7-gcc-10.3.1/include'
+#- HDF5_MPI_C_LIBRARIES='/usr/tce/packages/mvapich2/mvapich2-2.3.7-gcc-10.3.1/lib/libmpi.so'
+#- HDF5_MPI_DIR='/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3'
+#- HDF5_MPI_LIBDEP='/usr/WS1/visit/visit/thirdparty_shared/3.5.0-python3.13/toss4/szip/2.1/linux-x86_64_gcc-10.3/lib;sz;/usr/WS1/visit/visit/thirdparty_shared/3.5.0-python3.13/toss4/zlib/1.3.1/linux-x86_64_gcc-10.3/lib;z'
+#- HDF5_PACKAGE_EXTLIBS='OFF'
+#- HDF5_PACKAGE_NAME='HDF5'
+#- HDF5_PROVIDES_CPP_LIB='OFF'
+#- HDF5_PROVIDES_DEPRECATED_SYMBOLS='ON'
+#- HDF5_PROVIDES_DIRECT_VFD='OFF'
+#- HDF5_PROVIDES_FORTRAN='OFF'
+#- HDF5_PROVIDES_HDFS_VFD='OFF'
+#- HDF5_PROVIDES_HL_LIB='ON'
+#- HDF5_PROVIDES_JAVA='OFF'
+#- HDF5_PROVIDES_JAVA_COMPAT='ON'
+#- HDF5_PROVIDES_JNI='OFF'
+#- HDF5_PROVIDES_LARGE_PARALLEL_IO='ON'
+#- HDF5_PROVIDES_MAP_API='OFF'
+#- HDF5_PROVIDES_MIRROR_VFD='OFF'
+#- HDF5_PROVIDES_NONSTANDARD_FEATURE_FLOAT16='OFF'
+#- HDF5_PROVIDES_PARALLEL='ON'
+#- HDF5_PROVIDES_PARALLEL_FILTERED_WRITES='ON'
+#- HDF5_PROVIDES_PLUGIN_SUPPORT='OFF'
+#- HDF5_PROVIDES_ROS3_VFD='OFF'
+#- HDF5_PROVIDES_SHARED_LIBS='YES'
+#- HDF5_PROVIDES_STATIC_LIBS='NO'
+#- HDF5_PROVIDES_STATIC_TOOLS='OFF'
+#- HDF5_PROVIDES_SUBFILING_VFD='OFF'
+#- HDF5_PROVIDES_SZIP_SUPPORT='OFF'
+#- HDF5_PROVIDES_THREADS='ON'
+#- HDF5_PROVIDES_THREADSAFE='OFF'
+#- HDF5_PROVIDES_TOOLS='ON'
+#- HDF5_PROVIDES_ZLIB_SUPPORT='ON'
+#- HDF5_SHARE_DIR='/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3/cmake'
+#- HDF5_TOOLSET=''
+#- HDF5_TOOLS_DIR='/g/g11/miller86/visit/visit/build-Xdmf/my_install/hdf5/2.0.0/linux-x86_64_gcc-10.3/bin'
+#- HDF5_VALID_COMPONENTS='static;shared;C;CXX;Fortran;HL;CXX_HL;Fortran_HL;Java;Tools'
+#- HDF5_VERSION='2.0.0'
+#- HDF5_VERSION_COUNT='3'
+#- HDF5_VERSION_MAJOR='2.0'
+#- HDF5_VERSION_MINOR='0'
+#- HDF5_VERSION_PATCH='0'
+#- HDF5_VERSION_STRING='2.0.0'
+#- HDF5_VERSION_TWEAK='0'
+#- HDF5_shared_C_FOUND='1'
+#- HDF5_shared_HL_FOUND='1'
+#- hdf5_comp='hdf5_hl'
+
 ENDIF()
