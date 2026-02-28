8a2232482e053c9c2cf43e559d02cc56230a04c9
Mark C. Miller <5720676+markcmiller86@users.noreply.github.com>
2026-02-28 01:01:07 -0800

    fix config-site logic

--- src/tools/dev/scripts/bv_support/bv_mfem.sh
+++ src/tools/dev/scripts/bv_support/bv_mfem.sh
@@ -61,8 +61,6 @@ function bv_mfem_host_profile
             "VISIT_OPTION_DEFAULT(VISIT_MFEM_DIR \${VISITHOME}/mfem/$MFEM_VERSION/\${VISITARCH})" \
             >> $HOSTCONF
 
-        ZLIB_LIBDEP="\${VISITHOME}/zlib/\${ZLIB_VERSION}/\${VISITARCH}/lib z"
-
         CONDUIT_LIBDEP=""
         INCDEP=""
         if [[ "$DO_CONDUIT" == "yes" ]] ; then
@@ -79,7 +77,7 @@ function bv_mfem_host_profile
                     >> $HOSTCONF
         fi
         echo \
-            "VISIT_OPTION_DEFAULT(VISIT_MFEM_LIBDEP $CONDUIT_LIBDEP $ZLIB_LIBDEP TYPE STRING)" \
+            "VISIT_OPTION_DEFAULT(VISIT_MFEM_LIBDEP ZLIB_LIB $CONDUIT_LIBDEP TYPE STRING)" \
                 >> $HOSTCONF
     fi
 }
