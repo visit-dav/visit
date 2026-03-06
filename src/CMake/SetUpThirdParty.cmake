6a242e22ae2eed60fa530c6ba3fc6dc337f8facb
biagas <biagas2@llnl.gov>
2026-03-06 12:10:20 -0800

    Undo fix-for-zlib-fighting to get build working again.

--- src/CMake/SetUpThirdParty.cmake
+++ src/CMake/SetUpThirdParty.cmake
@@ -51,9 +51,6 @@
 #   Kevin Griffin, Mon 24 Feb 2025
 #   Added Include for FindANARI.cmake
 #
-#   Kathleen Biagas, Fri Mar 6, 2026
-#   Moved FindZlib to root CMakeLists.txt, as it must be found before MPI.
-#
 #****************************************************************************/
 
 # ==============================================
@@ -573,6 +570,7 @@ endif()
 
 # These need to come before VTK/Qt because they depend on it.
 
+include(${VISIT_SOURCE_DIR}/CMake/FindZlib.cmake)
 include(${VISIT_SOURCE_DIR}/CMake/FindOSPRay.cmake)
 include(${VISIT_SOURCE_DIR}/CMake/FindANARI.cmake)
 include(${VISIT_SOURCE_DIR}/CMake/FindJPEG.cmake)
