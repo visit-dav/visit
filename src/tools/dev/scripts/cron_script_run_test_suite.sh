9a8c7fd3ef008ad0692e61e34f47d094d5295688
Eric Samuel Brugger <brugger@rzwhippet59.llnl.gov>
2026-03-17 13:43:12 -0700

    Add a comment to manually copy the file to the installation point if updated.

--- src/tools/dev/scripts/cron_script_run_test_suite.sh
+++ src/tools/dev/scripts/cron_script_run_test_suite.sh
@@ -2,6 +2,14 @@
 #
 # Cron driver script to run visit's test suite
 #
+# WARNING WARNING WARNING WARNING WARNING WARNING
+#
+#   If you change this file you will need to manually copy it to the
+#   directory where the crontab runs this script. Currently that is
+#   /usr/WS1/visit/test_trunk/nightly_cron on the RZ. You should also
+#   make sure the permissions are 775.
+#
+# WARNING WARNING WARNING WARNING WARNING WARNING
 
 # Change directories to the working directory.
 cd /usr/WS1/visit/test_trunk/nightly_cron
