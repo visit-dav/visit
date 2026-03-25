158768ad179c8ec865fd70787b6b6e237a39385b
Mark C. Miller <5720676+markcmiller86@users.noreply.github.com>
2026-03-24 22:08:42 -0700

    imprv perf of install_name_tool

--- src/osxfixup/osxfixup.py
+++ src/osxfixup/osxfixup.py
@@ -3,11 +3,12 @@
 # details.  No copyright assignment is required to contribute to VisIt.
 
 
+import fnmatch
 import os
-import sys
+import shlex
 import stat
 import subprocess
-import fnmatch
+import sys
 
 from os.path import join as pjoin
 
@@ -143,84 +144,84 @@ def find_exes(sdir):
                   exes.append(fname)
     return exes
 
-def fixup_items(items,lib_maps,prefix_path):
-    """
-    Fixup libs / exes with the proper paths.
-    """
-    exe_rpaths = ["@executable_path/..", # standard exe
-                  "@executable_path/../../../.." # bundle
-                  ]
+def _dedupe_keep_order(seq):
+    seen = set()
+    out = []
+    for x in seq:
+        if x not in seen:
+            seen.add(x)
+            out.append(x)
+    return out
+
+def fixup_items(items, lib_maps, prefix_path):
+    exe_rpaths = ["@executable_path/..", "@executable_path/../../../.."]
+
     for item in items:
         item_base = os.path.basename(item)
-
         print('\n\nosxfixup: Working on item "{0}"'.format(item))
-       
-        #evaluating symlinks causes add_rpath errors for duplication
-        #not evaluating it skips frameworks..
-        #for now I am okay with duplication error
-        #if os.path.islink(lib):             continue
-        
-        id_cmd = "install_name_tool -id @rpath{0} {1}"
-        if item_base in list(lib_maps.keys()):
-            id_cmd = id_cmd.format(lib_maps[item_base], item)
-        else:
-            id_cmd = id_cmd.format(item.replace(prefix_path,""), item)
-        shexe(id_cmd)
 
-        # Remove rpaths containing user home directory
+        install_args = ["install_name_tool"]
+
+        # --- read existing LC_RPATHs once (also used for invalid + dedupe) ---
+        existing_rpaths = []
         home = os.path.expanduser("~")
-        invalid_paths = []
-        rcode, lc_rpaths = shexe("otool -l {0}".format(item), ret_output=True)
-        if rcode != 0:
-            print("[info: no invalid LC_RPATHS for '%s']" % item)
+        rcode, lc_out = shexe("otool -l {0}".format(shlex.quote(item)), ret_output=True)
+        if rcode == 0:
+            path_lines = [ln for ln in lc_out.split("\n")[1:] if " path" in ln]
+            for ln in path_lines:
+                parts = ln.split()
+                if len(parts) >= 2:
+                    existing_rpaths.append(parts[1])
         else:
-            lc_rpaths = [ path for path in lc_rpaths.split("\n")[1:] if path.find(" path") != -1]
-            for lc_rpath in lc_rpaths:
-                invalid_path = lc_rpath.split()[1]
-                if invalid_path.find(home) != -1:
-                    invalid_paths.append(invalid_path)
+            print("[info: no invalid LC_RPATHS for '%s']" % item)
 
-            del_rpath_cmd = "install_name_tool -delete_rpath {0} {1} 2>&1"
-            for invalid_path in invalid_paths:
-                shexe(del_rpath_cmd.format(invalid_path, item))
+        existing_rpaths = _dedupe_keep_order(existing_rpaths)
 
-        ####################################
-        # Add rpaths relative to the bundle
-        ####################################
-        # if we have an exe exe_rpaths[0]
-        # if we have a bundle exe_rpaths[1]
-        # it doens't hurt to add both for now
-        rpath_base_cmd =  "install_name_tool -add_rpath {0} {1} 2>&1"
-        rpath_cmds = [rpath_base_cmd.format(rp, item) for rp in exe_rpaths]
-        for rp_cmd in rpath_cmds:
-            shexe(rp_cmd)
+        # -id
+        if item_base in lib_maps:
+            new_id = "@rpath{0}".format(lib_maps[item_base])
+        else:
+            new_id = "@rpath{0}".format(item.replace(prefix_path, ""))
+        install_args += ["-id", new_id]
 
-        ###############
-        # re-wire deps
-        ###############
-        rcode, dependencies = shexe("otool -L {0}".format(item), ret_output=True)
+        # -delete_rpath (home-containing)
+        invalid_paths = [p for p in existing_rpaths if home in p]
+        for p in invalid_paths:
+            install_args += ["-delete_rpath", p]
+
+        # -add_rpath (only if missing to prevent duplication errors)
+        for rp in exe_rpaths:
+            if rp not in existing_rpaths:
+                install_args += ["-add_rpath", rp]
+
+        # -change deps
+        rcode, deps_out = shexe("otool -L {0}".format(shlex.quote(item)), ret_output=True)
         if rcode != 0:
             print("[warning: failed to obtain dependencies for '%s']" % item)
         else:
-            dependencies = [ d for d in dependencies.split("\n")[1:] if d.strip() != ""]
-            print("[# of dependencies for {0} = {1}]".format(item,len(dependencies)))
-            for dep in dependencies:
-                index = dep.find(" ")
-                if index >= 0:
-                    dep = dep[0:index].strip()
+            deps = [d for d in deps_out.split("\n")[1:] if d.strip()]
+            print("[# of dependencies for {0} = {1}]".format(item, len(deps)))
+
+            for dep in deps:
+                dep = dep.split(" ", 1)[0].strip()
                 dep_base = os.path.basename(dep)
-                #sometimes dependencies can have extensions, find the appropriate one..
+
                 if dep_base not in lib_maps:
                     tdep = os.path.splitext(dep_base)
-                    while tdep[1] != '':
+                    while tdep[1] != "":
                         if tdep[0] in lib_maps:
                             dep_base = tdep[0]
                             break
                         tdep = os.path.splitext(tdep[0])
+
                 if dep_base in lib_maps:
-                    dep_cmd = "install_name_tool -change {0} @rpath{1} {2}"
-                    dep_cmd = dep_cmd.format(dep,lib_maps[dep_base],item)
-                    shexe(dep_cmd)
+                    install_args += ["-change", dep, "@rpath{0}".format(lib_maps[dep_base])]
+
+        # target must be last
+        install_args.append(item)
+
+        cmd = " ".join(shlex.quote(a) for a in install_args) + " 2>&1"
+        shexe(cmd)
 
 def main():
     if os.uname().machine == "x86_64":
