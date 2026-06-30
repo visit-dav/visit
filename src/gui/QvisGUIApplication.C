66e5c7b9b355f46e678eefb930f4c819f141b45b
biagas <biagas2@llnl.gov>
2026-06-29 17:26:22 -0700

    Fix problem opening session file from command line.
    When corresponding session.gui file does not exist.
    Resolves #2160.

--- src/gui/QvisGUIApplication.C
+++ src/gui/QvisGUIApplication.C
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <algorithm>
 #include <map>
+#include <filesystem> // for determining full-path location of sessionfile
 
 #include <visit-config.h> // To get the version number
 #include <QColor>
@@ -5115,6 +5116,12 @@ QvisGUIApplication::RestoreSessionWithDifferentSources()
 //   David Camp, Tue Aug  4 11:04:14 PDT 2015
 //   Added new ablitiy to load session files from a remote host.
 //
+//   Kathleen Biagas, Mon Jun 29 17:11:10 PDT 2026
+//   Use std::filesystem to determint full path location of session file.
+//   If it doesn't already contain full path, test cwd, then VisIt user dir
+//   and stop processing if it cannot be found.
+//   Only attempt to process .gui file if it exists.
+//
 // ****************************************************************************
 
 void
@@ -5130,40 +5137,66 @@ QvisGUIApplication::RestoreSessionFile(const QString &s,
         restoringSession = true;
         std::string filename(s.toStdString());
 
-        // Make the gui read in its part of the config.
-        std::string guifilename(filename);
-        guifilename += ".gui";
-        DataNode *node;
-        if(host.empty() || host == "localhost")
+        // get full-path to session file, if it doesn't already contain
+        // will first check cwd, then VisIt user dir.
+
+        std::filesystem::path fsp(s.toStdString());
+        if(fsp.has_parent_path())
         {
-            node = ReadConfigFile(guifilename.c_str());
+            // make sure it is absolute before passing it along
+            filename = std::filesystem::absolute(fsp).lexically_normal().string();
         }
         else
         {
-            std::istringstream sessionGUI;
-            std::string sessionGUIStr;
-
-            fileServer->RestoreSessionFile(host, guifilename, sessionGUIStr);
-            sessionGUI.str( sessionGUIStr );
-            node = ReadConfigFile(sessionGUI);
-        }
-
-        // If the file could not be opened then try and prepend the
-        // VisIt directory to it.
-#ifndef _WIN32
-        if(node == 0)
-        {
-            if(guifilename[0] != VISIT_SLASH_CHAR)
+            // no parent path, check cwd
+            if(std::filesystem::exists(fsp))
             {
-                filename = GetUserVisItDirectory() + filename;
-                guifilename = GetUserVisItDirectory() + guifilename;
-                debug1 << "The desired session file " << s.toStdString()
-                       << ".gui could not be opened. VisIt will try and open "
-                       << guifilename.c_str() << endl;
-                node = ReadConfigFile(guifilename.c_str());
+                std::filesystem::path tmp(std::filesystem::current_path());
+                tmp /= fsp;
+                filename = tmp.string();
+            }
+            else
+            {
+                // not in cwd, check VisIt user directory.
+                std::filesystem::path vud(GetUserVisItDirectory());
+                vud /= fsp;
+                if(std::filesystem::exists(vud))
+                {
+                    cerr << "  session file exists in vud" << endl;
+                    filename = vud.string();
+                }
+                else
+                {
+                    QString msg = tr("Could not find session file (%1). Please provide full path if is it not in current working directory or your VisIt user directory.").arg(s.toStdString().c_str());
+                    Error(msg);
+                    restoringSession = false;
+                    return;
+                }
             }
         }
-#endif
+
+        // Make the gui read in its part of the config.
+        std::string guifilename(filename);
+        guifilename += ".gui";
+
+
+        DataNode *node = nullptr;
+        if(std::filesystem::exists(guifilename))
+        {
+            if(host.empty() || host == "localhost")
+            {
+                node = ReadConfigFile(guifilename.c_str());
+            }
+            else
+            {
+                std::istringstream sessionGUI;
+                std::string sessionGUIStr;
+
+                fileServer->RestoreSessionFile(host, guifilename, sessionGUIStr);
+                sessionGUI.str( sessionGUIStr );
+                node = ReadConfigFile(sessionGUI);
+            }
+        }
 
         ProcessSessionNode(node, filename, sources, host);
 
