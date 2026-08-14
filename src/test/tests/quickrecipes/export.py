edd0bf81c96967ef826b00cac27a4709d27f2b0b
Justin Privitera <privitera1@llnl.gov>
2026-08-14 13:59:09 -0700

    add new quickrecipe to the docs for obj export

--- src/test/tests/quickrecipes/export.py
+++ src/test/tests/quickrecipes/export.py
@@ -85,11 +85,51 @@ def export_to_csv():
   TestValueEQ("qr_export_to_csv_cell",os.path.isfile(pjoin("my_csv_export.csv","vertex_data.csv")),True)
   DeleteAllPlots()
 
+def export_to_obj():
+  clean_data("my_obj_export.obj")
+  clean_data("my_obj_export.mtl")
+  clean_data("my_obj_export.png")
+  # exportToOBJ {
+  # Create a Pseudocolor plot and export multiple fields
+  AddPlot("Pseudocolor", "d")
+  # optional: setup operators to modify data before export
+  DrawPlots()
+  eatts = ExportDBAttributes()
+  eatts.db_type = "WavefrontOBJ"
+  eatts.variables = ("d")
+  eatts.filename = "my_obj_export"
+
+  # fetch Pseudocolor plot attributes to hand to OBJ export options
+  # (assumes your current active plot is a pseudocolor plot)
+  pc_atts = GetPlotOptions()
+
+  opts = GetExportOptions("WavefrontOBJ")
+  opts["Output colors"]              = 1 # turn colors on (1) or off (0)
+  opts["Color table"]                = pc_atts.colorTableName # choose a color table by name
+  opts["Number of colors"]           = 256 # choose a color table resolution
+  opts["Invert color table"]         = pc_atts.invertColorTable
+  opts["Use minimum"]                = pc_atts.minFlag
+  opts["Minimum"]                    = pc_atts.min
+  opts["Use color for values < min"] = pc_atts.useBelowMinColor
+  opts["Color for values < min"]     = pc_atts.belowMinColor
+  opts["Use maximum"]                = pc_atts.maxFlag
+  opts["Maximum"]                    = pc_atts.max
+  opts["Use color for values > max"] = pc_atts.useAboveMaxColor
+  opts["Color for values > max"]     = pc_atts.aboveMaxColor
+
+  ExportDatabase(eatts, opts)
+  # exportToOBJ }
+  TestValueEQ("qr_export_to_obj",os.path.isfile("my_obj_export.obj"),True)
+  TestValueEQ("qr_export_to_obj",os.path.isfile("my_obj_export.mtl"),True)
+  TestValueEQ("qr_export_to_obj",os.path.isfile("my_obj_export.png"),True)
+  DeleteAllPlots()
+
 
 OpenDatabase(silo_data_path("rect3d.silo"))
 export_to_vtk()
 export_to_silo()
 export_to_blueprint()
 export_to_csv()
+export_to_obj()
 
 Exit()
